340e6dca13577095b13f116e0fb2ff5e6f01820a
Scott Ullrich <sullrich@pfsense.org>
2004-11-19 17:54:56 +0000

    Change color from brown to new deep red

--- usr/local/www/firewall_nat.php
+++ usr/local/www/firewall_nat.php
@@ -1,22 +1,22 @@
 #!/usr/local/bin/php
-<?php 
+<?php
 /*
 	firewall_nat.php
 	part of m0n0wall (http://m0n0.ch/wall)
-	
+
 	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -49,7 +49,7 @@ if ($_POST) {
 			config_unlock();
 		}
 		$savemsg = get_std_save_message($retval);
-		
+
 		if ($retval == 0) {
 			if (file_exists($d_natconfdirty_path))
 				unlink($d_natconfdirty_path);
@@ -95,10 +95,10 @@ if ($_GET['act'] == "del") {
     <li class="tabinact"><a href="firewall_nat_out.php">Outbound</a></li>
   </ul>
   </td></tr>
-  <tr> 
+  <tr>
     <td class="tabcont">
               <table width="100%" border="0" cellpadding="0" cellspacing="0">
-                <tr> 
+                <tr>
                   <td width="5%" class="listhdrr">If</td>
                   <td width="5%" class="listhdrr">Proto</td>
                   <td width="20%" class="listhdrr">Ext. port range</td>
@@ -108,20 +108,20 @@ if ($_GET['act'] == "del") {
                   <td width="5%" class="list"></td>
 				</tr>
 			  <?php $i = 0; foreach ($a_nat as $natent): ?>
-                <tr valign="top"> 
+                <tr valign="top">
 				  <td class="listlr">
                   <?php
 						if (!$natent['interface'] || ($natent['interface'] == "wan"))
 							echo "WAN";
 						else
-							echo htmlspecialchars($config['interfaces'][$natent['interface']]['descr']);
+							echo "<font color=\"#FFFFFF\">" . htmlspecialchars($config['interfaces'][$natent['interface']]['descr']);
 				  ?>
                   </td>
-                  <td class="listr"> 
+                  <td class="listr">
                     <?=strtoupper($natent['protocol']);?>
                   </td>
                   <td class="listr">
-                    <?php 
+                    <?php
 						list($beginport, $endport) = split("-", $natent['external-port']);
 						if ((!$endport) || ($beginport == $endport)) {
 				  			echo $beginport;
@@ -131,37 +131,37 @@ if ($_GET['act'] == "del") {
 							echo $beginport . " - " . $endport;
 				  ?>
                   </td>
-                  <td class="listr"> 
+                  <td class="listr">
                     <?=$natent['target'];?>
 					<?php if ($natent['external-address'])
 						echo "<br>(ext.: " . $natent['external-address'] . ")";
 					?>
                   </td>
-                  <td class="listr"> 
+                  <td class="listr">
                     <?php if ((!$endport) || ($beginport == $endport)) {
 				  			echo $natent['local-port'];
 							if ($wkports[$natent['local-port']])
 								echo " (" . $wkports[$natent['local-port']] . ")";
 						} else
-							echo $natent['local-port'] . " - " . 
+							echo $natent['local-port'] . " - " .
 								($natent['local-port']+$endport-$beginport);
 				  ?>
                   </td>
-                  <td class="listbg"> 
+                  <td class="listbg"><font color="#FFFFFFF">
                     <?=htmlspecialchars($natent['descr']);?>&nbsp;
                   </td>
                   <td valign="middle" class="list" nowrap> <a href="firewall_nat_edit.php?id=<?=$i;?>"><img src="e.gif" width="17" height="17" border="0"></a>
                      &nbsp;<a href="firewall_nat.php?act=del&id=<?=$i;?>" onclick="return confirm('Do you really want to delete this rule?')"><img src="x.gif" width="17" height="17" border="0"></a></td>
 				</tr>
 			  <?php $i++; endforeach; ?>
-                <tr> 
+                <tr>
                   <td class="list" colspan="6"></td>
                   <td class="list"> <a href="firewall_nat_edit.php"><img src="plus.gif" width="17" height="17" border="0"></a></td>
 				</tr>
               </table>
                     <p><span class="vexpl"><span class="red"><strong>Note:<br>
-                      </strong></span>It is not possible to access NATed services 
-                      using the WAN IP address from within LAN (or an optional 
+                      </strong></span>It is not possible to access NATed services
+                      using the WAN IP address from within LAN (or an optional
                       network).</span></p></td>
   </tr>
 </table>

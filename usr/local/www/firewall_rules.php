6187380b8348ab9ea0aa195f0928a274780d666c
Scott Ullrich <sullrich@pfsense.org>
2004-11-20 00:58:14 +0000

    Update changed files with new copyright, attribute Manuel Kasper

--- usr/local/www/firewall_rules.php
+++ usr/local/www/firewall_rules.php
@@ -2,11 +2,13 @@
 <?php
 /*
 	firewall_rules.php
-	part of m0n0wall (http://m0n0.ch/wall)
-
-	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
+	Copyright (C) 2004 Scott Ullrich
 	All rights reserved.
 
+	originally part of m0n0wall (http://m0n0.ch/wall)
+	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
+	All rights reserved.
+	
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
 

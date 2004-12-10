d03efa9c8b1b6479e92969c684463c802dba741c
Scott Ullrich <sullrich@pfsense.org>
2004-12-10 23:39:57 +0000

    Add &nbsp; trying to add a break between items on elinks

--- usr/local/www/services_dyndns.php
+++ usr/local/www/services_dyndns.php
@@ -1,22 +1,22 @@
 #!/usr/local/bin/php
-<?php 
+<?php
 /*
 	services_dyndns.php
 	part of m0n0wall (http://m0n0.ch/wall)
-	
+
 	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -72,7 +72,7 @@ if ($_POST) {
 		$reqdfieldsn = array_merge($reqdfieldsn, explode(",", "Hostname,TTL,Key name,Key"));
 	}
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
-	
+
 	if (($_POST['host'] && !is_domain($_POST['host']))) {
 		$input_errors[] = "The host name contains invalid characters.";
 	}
@@ -82,7 +82,7 @@ if ($_POST) {
 	if (($_POST['username'] && !is_dyndns_username($_POST['username']))) {
 		$input_errors[] = "The username contains invalid characters.";
 	}
-	
+
 	if (($_POST['dnsupdate_host'] && !is_domain($_POST['dnsupdate_host']))) {
 		$input_errors[] = "The DNS update host name contains invalid characters.";
 	}
@@ -94,14 +94,14 @@ if ($_POST) {
 	}
 
 	if (!$input_errors) {
-		$config['dyndns']['type'] = $_POST['type'];	
+		$config['dyndns']['type'] = $_POST['type'];
 		$config['dyndns']['username'] = $_POST['username'];
 		$config['dyndns']['password'] = $_POST['password'];
 		$config['dyndns']['host'] = $_POST['host'];
 		$config['dyndns']['mx'] = $_POST['mx'];
 		$config['dyndns']['wildcard'] = $_POST['wildcard'] ? true : false;
 		$config['dyndns']['enable'] = $_POST['enable'] ? true : false;
-		
+
 		$config['dnsupdate']['enable'] = $_POST['dnsupdate_enable'] ? true : false;
 		$config['dnsupdate']['host'] = $_POST['dnsupdate_host'];
 		$config['dnsupdate']['ttl'] = $_POST['dnsupdate_ttl'];
@@ -109,9 +109,9 @@ if ($_POST) {
 		$config['dnsupdate']['keytype'] = $_POST['dnsupdate_keytype'];
 		$config['dnsupdate']['keydata'] = $_POST['dnsupdate_keydata'];
 		$config['dnsupdate']['usetcp'] = $_POST['dnsupdate_usetcp'] ? true : false;
-			
+
 		write_config();
-		
+
 		$retval = 0;
 		if (!file_exists($d_sysrebootreqd_path)) {
 			/* nuke the cache file */
@@ -135,7 +135,7 @@ if ($_POST) {
 <!--
 function enable_change(enable_change) {
 	var endis;
-	
+
 	endis = !(document.iform.enable.checked || enable_change);
 	document.iform.host.disabled = endis;
 	document.iform.mx.disabled = endis;
@@ -143,7 +143,7 @@ function enable_change(enable_change) {
 	document.iform.wildcard.disabled = endis;
 	document.iform.username.disabled = endis;
 	document.iform.password.disabled = endis;
-	
+
 	endis = !(document.iform.dnsupdate_enable.checked || enable_change);
 	document.iform.dnsupdate_host.disabled = endis;
 	document.iform.dnsupdate_ttl.disabled = endis;
@@ -165,85 +165,85 @@ function enable_change(enable_change) {
 <?php if ($savemsg) print_info_box($savemsg); ?>
             <form action="services_dyndns.php" method="post" name="iform" id="iform">
               <table width="100%" border="0" cellpadding="6" cellspacing="0">
-                <tr> 
+                <tr>
                   <td colspan="2" valign="top" class="optsect_t">
 				  <table border="0" cellspacing="0" cellpadding="0" width="100%">
 				  <tr><td class="optsect_s"><strong>Dynamic DNS client</strong></td>
 				  <td align="right" class="optsect_s"><input name="enable" type="checkbox" value="yes" <?php if ($pconfig['enable']) echo "checked"; ?> onClick="enable_change(false)"> <strong>Enable</strong></td></tr>
 				  </table></td>
                 </tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncellreq">Service type</td>
                   <td width="78%" class="vtable">
 <select name="type" class="formfld" id="type">
                       <?php $types = explode(",", "DynDNS,DHS,ODS,DyNS,HN.ORG,ZoneEdit,GNUDip,DynDNS (static),DynDNS (custom),easyDNS,EZ-IP,TZO");
 					        $vals = explode(" ", "dyndns dhs ods dyns hn zoneedit gnudip dyndns-static dyndns-custom easydns ezip tzo");
 					  $j = 0; for ($j = 0; $j < count($vals); $j++): ?>
-                      <option value="<?=$vals[$j];?>" <?php if ($vals[$j] == $pconfig['type']) echo "selected";?>> 
+                      <option value="<?=$vals[$j];?>" <?php if ($vals[$j] == $pconfig['type']) echo "selected";?>>
                       <?=htmlspecialchars($types[$j]);?>
                       </option>
                       <?php endfor; ?>
                     </select></td>
 				</tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncellreq">Hostname</td>
-                  <td width="78%" class="vtable"> 
-                    <input name="host" type="text" class="formfld" id="host" size="30" value="<?=htmlspecialchars($pconfig['host']);?>"> 
+                  <td width="78%" class="vtable">
+                    <input name="host" type="text" class="formfld" id="host" size="30" value="<?=htmlspecialchars($pconfig['host']);?>">
                   </td>
 				</tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncell">MX</td>
-                  <td width="78%" class="vtable"> 
-                    <input name="mx" type="text" class="formfld" id="mx" size="30" value="<?=htmlspecialchars($pconfig['mx']);?>"> 
+                  <td width="78%" class="vtable">
+                    <input name="mx" type="text" class="formfld" id="mx" size="30" value="<?=htmlspecialchars($pconfig['mx']);?>">
                     <br>
-                    Set this option only if you need a special MX record. Not 
+                    Set this option only if you need a special MX record. Not
                     all services support this.</td>
 				</tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncellreq">Wildcards</td>
-                  <td width="78%" class="vtable"> 
+                  <td width="78%" class="vtable">
                     <input name="wildcard" type="checkbox" id="wildcard" value="yes" <?php if ($pconfig['wildcard']) echo "checked"; ?>>
                     Enable Wildcard</td>
 				</tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncellreq">Username</td>
-                  <td width="78%" class="vtable"> 
-                    <input name="username" type="text" class="formfld" id="username" size="20" value="<?=htmlspecialchars($pconfig['username']);?>"> 
+                  <td width="78%" class="vtable">
+                    <input name="username" type="text" class="formfld" id="username" size="20" value="<?=htmlspecialchars($pconfig['username']);?>">
                   </td>
                 </tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncellreq">Password</td>
-                  <td width="78%" class="vtable"> 
-                    <input name="password" type="password" class="formfld" id="password" size="20" value="<?=htmlspecialchars($pconfig['password']);?>"> 
+                  <td width="78%" class="vtable">
+                    <input name="password" type="password" class="formfld" id="password" size="20" value="<?=htmlspecialchars($pconfig['password']);?>">
                   </td>
                 </tr>
-                <tr> 
-                  <td colspan="2" class="list" height="12"></td>
+                <tr>
+                  <td colspan="2" class="list" height="12">&nbsp;</td>
                 </tr>
-                <tr> 
+                <tr>
                   <td colspan="2" valign="top" class="optsect_t">
 				  <table border="0" cellspacing="0" cellpadding="0" width="100%">
 				  <tr><td class="optsect_s"><strong>RFC 2163 Dynamic DNS updates</strong></td>
 				  <td align="right" class="optsect_s"><input name="dnsupdate_enable" type="checkbox" value="yes" <?php if ($pconfig['dnsupdate_enable']) echo "checked"; ?> onClick="enable_change(false)"> <strong>Enable</strong></td></tr>
 				  </table></td>
                 </tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncellreq">Hostname</td>
-                  <td width="78%" class="vtable"> 
-                    <input name="dnsupdate_host" type="text" class="formfld" id="dnsupdate_host" size="30" value="<?=htmlspecialchars($pconfig['dnsupdate_host']);?>"> 
+                  <td width="78%" class="vtable">
+                    <input name="dnsupdate_host" type="text" class="formfld" id="dnsupdate_host" size="30" value="<?=htmlspecialchars($pconfig['dnsupdate_host']);?>">
                   </td>
 				</tr>
                 <tr>
                   <td valign="top" class="vncellreq">TTL</td>
                   <td class="vtable">
-                    <input name="dnsupdate_ttl" type="text" class="formfld" id="dnsupdate_ttl" size="6" value="<?=htmlspecialchars($pconfig['dnsupdate_ttl']);?>"> 
+                    <input name="dnsupdate_ttl" type="text" class="formfld" id="dnsupdate_ttl" size="6" value="<?=htmlspecialchars($pconfig['dnsupdate_ttl']);?>">
                   seconds</td>
                 </tr>
                 <tr>
                   <td valign="top" class="vncellreq">Key name</td>
                   <td class="vtable">
                     <input name="dnsupdate_keyname" type="text" class="formfld" id="dnsupdate_keyname" size="30" value="<?=htmlspecialchars($pconfig['dnsupdate_keyname']);?>">
-                    <br> 
+                    <br>
                     This must match the setting on the DNS server.</td>
                 </tr>
                 <tr>
@@ -257,26 +257,26 @@ function enable_change(enable_change) {
                   <td valign="top" class="vncellreq">Key</td>
                   <td class="vtable">
                     <input name="dnsupdate_keydata" type="text" class="formfld" id="dnsupdate_keydata" size="70" value="<?=htmlspecialchars($pconfig['dnsupdate_keydata']);?>">
-                    <br> 
+                    <br>
                     Paste an HMAC-MD5 key here.</td>
                 </tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top" class="vncellreq">Protocol</td>
-                  <td width="78%" class="vtable"> 
+                  <td width="78%" class="vtable">
                     <input name="dnsupdate_usetcp" type="checkbox" id="dnsupdate_usetcp" value="yes" <?php if ($pconfig['dnsupdate_usetcp']) echo "checked"; ?>>
                     <strong>Use TCP instead of UDP</strong></td>
 				</tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top">&nbsp;</td>
-                  <td width="78%"> 
-                    <input name="Submit" type="submit" class="formbtn" value="Save" onClick="enable_change(true)"> 
+                  <td width="78%">
+                    <input name="Submit" type="submit" class="formbtn" value="Save" onClick="enable_change(true)">
                   </td>
                 </tr>
-                <tr> 
+                <tr>
                   <td width="22%" valign="top">&nbsp;</td>
                   <td width="78%"><span class="vexpl"><span class="red"><strong>Note:<br>
-                    </strong></span>You must configure a DNS server in <a href="system.php">System: 
-                    General setup</a> or allow the DNS server list to be overridden 
+                    </strong></span>You must configure a DNS server in <a href="system.php">System:
+                    General setup</a> or allow the DNS server list to be overridden
                     by DHCP/PPP on WAN for dynamic DNS updates to work.</span></td>
                 </tr>
               </table>

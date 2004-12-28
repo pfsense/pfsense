8a3be31a46e7e010b7eabb8bfeb8674765780f57
Scott Ullrich <sullrich@pfsense.org>
2004-12-28 23:23:10 +0000

    Upon post, redirect to halt.php.
    Noticed-by: B.Kharazmi

--- usr/local/www/halt.php
+++ usr/local/www/halt.php
@@ -1,22 +1,22 @@
 #!/usr/local/bin/php
-<?php 
+<?php
 /*
 	reboot.php
 	part of m0n0wall (http://m0n0.ch/wall)
-	
+
 	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -44,7 +44,7 @@ if ($_POST) {
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-<title><?=gentitle("Reboot system");?></title>
+<title><?=gentitle("Halt system");?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link href="gui.css" rel="stylesheet" type="text/css">
 </head>
@@ -53,9 +53,9 @@ if ($_POST) {
 <?php include("fbegin.inc"); ?>
 <p class="pgtitle">Halt system</p>
 <?php if ($rebootmsg): echo print_info_box($rebootmsg); else: ?>
-      <form action="reboot.php" method="post">
+      <form action="halt.php" method="post">
         <p><strong>Are you sure you want to halt the system?</strong></p>
-        <p> 
+        <p>
           <input name="Submit" type="submit" class="formbtn" value=" Yes ">
           <input name="Submit" type="submit" class="formbtn" value=" No ">
         </p>

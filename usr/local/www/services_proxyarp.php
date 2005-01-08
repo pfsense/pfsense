7a1b90bcc6f3ff1e1a26ac4c0ef120394e9757c5
Scott Ullrich <sullrich@pfsense.org>
2005-01-08 22:02:21 +0000

    Change (C)

--- usr/local/www/services_proxyarp.php
+++ usr/local/www/services_proxyarp.php
@@ -2,8 +2,10 @@
 <?php
 /*
 	services_proxyarp.php
-	part of m0n0wall (http://m0n0.ch/wall)
+    part of pfSense
+    Copyright (C) 2004 Scott Ullrich
 
+	originally part of m0n0wall (http://m0n0.ch/wall)
 	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
 	All rights reserved.
 

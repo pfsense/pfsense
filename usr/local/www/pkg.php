8596e0a41709613bc1a3a6b442052125976962a2
Scott Ullrich <sullrich@pfsense.org>
2005-02-09 04:44:28 +0000

    Enclose the php call in <?php ?>

--- usr/local/www/pkg.php
+++ usr/local/www/pkg.php
@@ -105,7 +105,7 @@ include("fbegin.inc");
 ?>
 <p class="pgtitle"><?=$title?></p>
 <form action="firewall_nat_out_load_balancing.php" method="post">
-if($_GET['savemsg'] <> "") $savemsg = $_GET['savemsg'];
+<? if($_GET['savemsg'] <> "") $savemsg = $_GET['savemsg']; ?>
 <?php if ($savemsg) print_info_box($savemsg); ?>
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
   <tr>

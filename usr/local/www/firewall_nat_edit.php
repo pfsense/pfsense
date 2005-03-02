559911e1a44002441ea6e394efebf25cd6d86048
Scott Ullrich <sullrich@pfsense.org>
2005-03-02 23:27:21 +0000

    Add LAN option for redirects aka inbound nat.

--- usr/local/www/firewall_nat_edit.php
+++ usr/local/www/firewall_nat_edit.php
@@ -222,7 +222,7 @@ function ext_rep_change() {
                   <td width="78%" class="vtable">
 					<select name="interface" class="formfld">
 						<?php
-						$interfaces = array('wan' => 'WAN');
+						$interfaces = array('wan' => 'WAN', 'lan' => 'LAN');
 						for ($i = 1; isset($config['interfaces']['opt' . $i]); $i++) {
 							$interfaces['opt' . $i] = $config['interfaces']['opt' . $i]['descr'];
 						}

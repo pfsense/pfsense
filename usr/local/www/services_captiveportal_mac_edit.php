75b927d1cd60c2044fd71fdf7516e1f507dc7d56
Scott Ullrich <sullrich@pfsense.org>
2005-03-14 00:27:49 +0000

    Add back header after Pascals sync

--- usr/local/www/services_captiveportal_mac_edit.php
+++ usr/local/www/services_captiveportal_mac_edit.php
@@ -1,22 +1,22 @@
 #!/usr/local/bin/php
-<?php 
+<?php
 /*
 	services_captiveportal_mac_edit.php
 	part of m0n0wall (http://m0n0.ch/wall)
-	
+
 	Copyright (C) 2004 Dinesh Nair <dinesh@alphaque.com>
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -55,11 +55,11 @@ if ($_POST) {
 	/* input validation */
 	$reqdfields = explode(" ", "mac");
 	$reqdfieldsn = explode(",", "MAC address");
-	
+
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
-	
+
 	$_POST['mac'] = str_replace("-", ":", $_POST['mac']);
-	
+
 	if (($_POST['mac'] && !is_macaddr($_POST['mac']))) {
 		$input_errors[] = "A valid MAC address must be specified. [".$_POST['mac']."]";
 	}
@@ -67,11 +67,11 @@ if ($_POST) {
 	foreach ($a_passthrumacs as $macent) {
 		if (isset($id) && ($a_passthrumacs[$id]) && ($a_passthrumacs[$id] === $macent))
 			continue;
-		
+
 		if ($macent['mac'] == $_POST['mac']){
 			$input_errors[] = "[" . $_POST['mac'] . "] already allowed." ;
 			break;
-		}	
+		}
 	}
 
 	if (!$input_errors) {
@@ -83,37 +83,48 @@ if ($_POST) {
 			$a_passthrumacs[$id] = $mac;
 		else
 			$a_passthrumacs[] = $mac;
-		
+
 		write_config();
 
 		touch($d_passthrumacsdirty_path) ;
-		
+
 		header("Location: services_captiveportal_mac.php");
 		exit;
 	}
 }
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title><?=gentitle("pfSense webGUI");?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="gui.css" rel="stylesheet" type="text/css">
+</head>
+
+<form>
+
+<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
 <?php include("fbegin.inc"); ?>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
             <form action="services_captiveportal_mac_edit.php" method="post" name="iform" id="iform">
               <table width="100%" border="0" cellpadding="6" cellspacing="0">
 				<tr>
                   <td width="22%" valign="top" class="vncellreq">MAC address</td>
-                  <td width="78%" class="vtable"> 
+                  <td width="78%" class="vtable">
                     <?=$mandfldhtml;?><input name="mac" type="text" class="formfld" id="mac" size="17" value="<?=htmlspecialchars($pconfig['mac']);?>">
-                    <br> 
+                    <br>
                     <span class="vexpl">MAC address (6 hex octets separated by colons)</span></td>
                 </tr>
 				<tr>
                   <td width="22%" valign="top" class="vncell">Description</td>
-                  <td width="78%" class="vtable"> 
+                  <td width="78%" class="vtable">
                     <input name="descr" type="text" class="formfld" id="descr" size="40" value="<?=htmlspecialchars($pconfig['descr']);?>">
                     <br> <span class="vexpl">You may enter a description here
                     for your reference (not parsed).</span></td>
                 </tr>
                 <tr>
                   <td width="22%" valign="top">&nbsp;</td>
-                  <td width="78%"> 
+                  <td width="78%">
                     <input name="Submit" type="submit" class="formbtn" value="Save">
                     <?php if (isset($id) && $a_passthrumacs[$id]): ?>
                     <input name="id" type="hidden" value="<?=$id;?>">

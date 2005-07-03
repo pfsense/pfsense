b8de522e4f7feb80d4b896847a11ea0ea78defcd
Scott Ullrich <sullrich@pfsense.org>
2005-07-03 07:35:05 +0000

    Apply changes is now handled in guiconfig.inc

--- usr/local/www/firewall_nat_server.php
+++ usr/local/www/firewall_nat_server.php
@@ -94,7 +94,6 @@ include("head.inc");
 <?php if ($savemsg) print_info_box($savemsg); ?>
 <?php if (file_exists($d_natconfdirty_path)): ?><p>
 <?php print_info_box_np("The NAT configuration has been changed.<br>You must apply the changes in order for them to take effect.");?><br>
-<input name="apply" type="submit" class="formbtn" id="apply" value="Apply changes"></p>
 <?php endif; ?>
 <div id="mainarea">
 <table width="100%" border="0" cellpadding="0" cellspacing="0">  <tr><td>

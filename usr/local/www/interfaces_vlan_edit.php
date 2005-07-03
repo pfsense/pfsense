ccff927c4ce8a5d52713c91e6d2080f06bcd5b35
Bill Marquette <billm@pfsense.org>
2005-07-03 19:53:22 +0000

    Fix pgtitle

--- usr/local/www/interfaces_vlan_edit.php
+++ usr/local/www/interfaces_vlan_edit.php
@@ -99,7 +99,7 @@ include("head.inc");
 
 <body link="#0000CC" vlink="#0000CC" alink="#0000CC">
 <?php include("fbegin.inc"); ?>
-<p class="pgtitle"><?=$pgtitle>?</p>
+<p class="pgtitle"><?=$pgtitle?></p>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
             <form action="interfaces_vlan_edit.php" method="post" name="iform" id="iform">
               <?display_topbar()?>

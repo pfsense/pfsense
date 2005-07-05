fcdfd949d00a578802a6516c310fa8eef71d531a
Scott Ullrich <sullrich@pfsense.org>
2005-07-05 21:03:20 +0000

    Display grey topbar

--- usr/local/www/firewall_nat_edit.php
+++ usr/local/www/firewall_nat_edit.php
@@ -227,7 +227,7 @@ function ext_rep_change() {
 <p class="pgtitle"><?=$pgtitle?></p>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
             <form action="firewall_nat_edit.php" method="post" name="iform" id="iform">
-	      <?display_topbar()?>
+	      <?display_topbar("", "#eeeeee")?>
               <table width="100%" border="0" cellpadding="6" cellspacing="0">
 	  	<tr>
                   <td width="22%" valign="top" class="vncellreq">Interface</td>

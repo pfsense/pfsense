033ede9717bc6916ab8824ff82984b83a005f338
Scott Ullrich <sullrich@pfsense.org>
2005-07-12 02:49:52 +0000

    grey the entire div

--- usr/local/www/diag_ipsec_spd.php
+++ usr/local/www/diag_ipsec_spd.php
@@ -103,7 +103,7 @@ if ($fd) {
 	pclose($fd);
 }
 ?>
-<div id="mainarea">
+<div id="mainarea" style="background:#eeeeee">
             <table class="tabcont" width="100%" border="0" cellpadding="6" cellspacing="0">
 <?php if (count($spd)): ?>
   <tr>

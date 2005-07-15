c0c2f4225c7afd30e10d0b096911f2ca5d4c1e54
Scott Ullrich <sullrich@pfsense.org>
2005-07-15 22:55:04 +0000

    Add Save button back.  It looks somewhat wierd without.

--- usr/local/www/services_dyndns.php
+++ usr/local/www/services_dyndns.php
@@ -219,7 +219,13 @@ function enable_change(enable_change) {
                   </td>
                 </tr>
                 <tr>
+                  <td width="22%" valign="top">&nbsp;</td>
+                  <td width="78%">
+                    <input name="Submit" type="submit" class="formbtn" value="Save" onClick="enable_change(true)">
+                  </td>		
+                <tr>
                   <td colspan="2" class="list" height="12">&nbsp;</td>
+                </tr>		
                 </tr>
                 <tr>
                   <td colspan="2" valign="top" class="optsect_t">

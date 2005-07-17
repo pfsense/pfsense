dd4543f4eaa0f2844348199d7c64c3779444765c
Erik Kristensen <ekristen@pfsense.org>
2005-07-17 20:28:18 +0000

    just making the theme better

--- usr/local/www/themes/pfsense-dropdown/bottom-loader.js
+++ usr/local/www/themes/pfsense-dropdown/bottom-loader.js
@@ -6,6 +6,6 @@ Rounded("div#mainarea","bl br tr","#FFF","#eeeeee","smooth");
 Rounded("div#boxarea","bl br tl tr","#FFF","#eeeeee","smooth");
 Rounded("tr#fend","bl br tl tr","#FFF","#990000","smooth");
 Rounded("div#topbox","all","#FFF","#990000","smooth");
-Rounded("div#footer","bl br tl tr]","#FFF","#990000","smooth");
+Rounded("div#footer","bl br tl tr","#FFF","#990000","smooth");
 
 //-->

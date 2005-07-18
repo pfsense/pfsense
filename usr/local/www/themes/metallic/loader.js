82bb760d428c4946a5a3013902229a64a1d1561e
Erik Kristensen <ekristen@pfsense.org>
2005-07-18 00:00:05 +0000

    new theme (well the beginnings of such)

---
+++ usr/local/www/themes/metallic/loader.js
@@ -0,0 +1,26 @@
+<!-- 
+var browser     = '';
+var version     = '';
+var entrance    = '';
+var cond        = '';
+
+// BROWSER?
+if (browser == '') {
+	if (navigator.appName.indexOf('Microsoft') != -1)
+		browser = 'IE'
+	else if (navigator.appName.indexOf('Netscape') != -1)
+		browser = 'Netscape'
+	else
+		browser = 'IE';
+}
+if (version == '') {
+	version= navigator.appVersion;
+	paren = version.indexOf('(');
+	whole_version = navigator.appVersion.substring(0,paren-1);
+	version = parseInt(whole_version);
+}
+
+
+document.write('<script type="text/javascript" src="/themes/pfsense-dropdown/javascript/niftyjsCode.js"></script>'); 
+
+// -->

6e707d609bf9571e67bbded79abc88da11f0e266
Scott Ullrich <sullrich@pfsense.org>
2005-08-03 16:52:07 +0000

    Remove topbar.   Red is not its color.

--- usr/local/www/services_dhcp_edit.php
+++ usr/local/www/services_dhcp_edit.php
@@ -139,7 +139,6 @@ include("head.inc");
 <p class="pgtitle"><?=$pgtitle?></p>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
             <form action="services_dhcp_edit.php" method="post" name="iform" id="iform">
-              <?display_topbar()?>
               <table width="100%" border="0" cellpadding="6" cellspacing="0">
                 <tr> 
                   <td width="22%" valign="top" class="vncellreq">MAC address</td>

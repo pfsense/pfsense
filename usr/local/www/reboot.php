276ba5212754507be2c8a72a06249c19de34cbc0
Scott Ullrich <sullrich@pfsense.org>
2005-08-06 18:59:44 +0000

    Alert user when shutting down

--- usr/local/www/reboot.php
+++ usr/local/www/reboot.php
@@ -35,6 +35,12 @@ require("guiconfig.inc");
 $pgtitle = "Diagnostics: Reboot System";
 include("head.inc");
 
+if ($_POST) {
+	if ($_POST['Submit'] == " Yes ") {
+		$rebootmsg = "The system is rebooting now. This may take one minute.";
+	}
+}
+
 ?>
 <body link="#0000CC" vlink="#0000CC" alink="#0000CC">
 <?php include("fbegin.inc"); ?>

4198286bcde0c478b8aaa9106c262c6995c33e36
Bill Marquette <billm@pfsense.org>
2005-08-06 21:53:38 +0000

    Load balanced virtual servers can now be entered in the gui

---
+++ usr/local/www/load_balancer_virtual_server.php
@@ -0,0 +1,150 @@
+#!/usr/local/bin/php
+<?php
+/* $Id */
+/*
+	load_balancer_virtual_server.php
+	part of pfSense (http://www.pfsense.com/)
+
+	Copyright (C) 2005 Bill Marquette <bill.marquette@gmail.com>.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+require("guiconfig.inc");
+
+if (!is_array($config['load_balancer']['virtual_server'])) {
+	$config['load_balancer']['virtual_server'] = array();
+}
+$a_vs = &$config['load_balancer']['virtual_server'];
+
+if ($_POST) {
+	$pconfig = $_POST;
+
+	if ($_POST['apply']) {
+		$retval = 0;
+		if (!file_exists($d_sysrebootreqd_path)) {
+			config_lock();
+			$retval |= filter_configure();
+			config_unlock();
+		}
+		$savemsg = get_std_save_message($retval);
+		unlink_if_exists($d_vsconfdirty_path);
+	}
+}
+
+if ($_GET['act'] == "del") {
+	if ($a_vs[$_GET['id']]) {
+		/* make sure no inbound NAT mappings reference this entry */
+		if (is_array($config['nat']['rule'])) {
+			foreach ($config['nat']['rule'] as $rule) {
+				if ($rule['external-address'] == $a_vs[$_GET['id']]['ipaddr']) {
+					$input_errors[] = "This entry cannot be deleted because it is still referenced by at least one NAT mapping.";
+					break;
+				}
+			}
+		}
+
+		if (!$input_errors) {
+			unset($a_vs[$_GET['id']]);
+			write_config();
+			touch($d_vsconfdirty_path);
+			header("Location: firewall_virtual_ip.php");
+			exit;
+		}
+	}
+}
+
+$pgtitle = "Firewall: Virtual IP Addresses";
+include("head.inc");
+
+?>
+<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+<?php include("fbegin.inc"); ?>
+<p class="pgtitle"><?=$pgtitle?></p>
+<form action="firewall_virtual_ip.php" method="post">
+<?php if ($input_errors) print_input_errors($input_errors); ?>
+<?php if ($savemsg) print_info_box($savemsg); ?>
+<?php if (file_exists($d_vsconfdirty_path)): ?><p>
+<?php print_info_box_np("The VIP configuration has been changed.<br>You must apply the changes in order for them to take effect.");?><br>
+<?php endif; ?>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr><td class="tabnavtbl">
+  <?php
+        /* active tabs */
+        $tab_array = array();
+        $tab_array[] = array("Pools", false, "load_balancer_pool.php");
+        $tab_array[] = array("Virtual Servers", true, "load_balancer_virtual_server.php");
+        display_top_tabs($tab_array);
+  ?>
+  </td></tr>
+  <tr>
+    <td>
+	<div id="mainarea">
+              <table class="tabcont" width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td width="30%" class="listhdrr">Virtual server address</td>
+                  <td width="10%" class="listhdrr">Port</td>
+                  <td width="20%" class="listhdrr">Pool</td>
+                  <td width="30%" class="listhdr">Description</td>
+                  <td width="10%" class="list"></td>
+				</tr>
+			  <?php $i = 0; foreach ($a_vs as $vsent): ?>
+                <tr>
+                  <td class="listlr" ondblclick="document.location='load_balancer_virtual_server_edit.php?id=<?=$i;?>';">
+			<?=$vsent['ipaddr'];?>
+                  </td>
+                  <td class="listlr" ondblclick="document.location='load_balancer_virtual_server_edit.php?id=<?=$i;?>';">
+			<?=$vsent['port'];?>
+                  <td class="listlr" align="center" ondblclick="document.location='load_balancer_virtual_server_edit.php?id=<?=$i;?>';">
+			<?=$vsent['pool'];?>
+                  </td>
+                  <td class="listbg" ondblclick="document.location='load_balancer_virtual_server_edit.php?id=<?=$i;?>';">
+                    <font color="#FFFFFF"><?=htmlspecialchars($vsent['desc']);?>&nbsp;
+                  </td>
+                  <td class="list" nowrap>
+                    <table border="0" cellspacing="0" cellpadding="1">
+                      <tr>
+                        <td valign="middle"><a href="load_balancer_virtual_server_edit.php?id=<?=$i;?>"><img src="/themes/<?= $g['theme']; ?>/images/icons/icon_e.gif" width="17" height="17" border="0"></a></td>
+                        <td valign="middle"><a href="firewall_virtual_ip.php?act=del&id=<?=$i;?>" onclick="return confirm('Do you really want to delete this entry?')"><img src="/themes/<?= $g['theme']; ?>/images/icons/icon_x.gif" width="17" height="17" border="0"></a></td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <?php $i++; endforeach; ?>
+                <tr>
+                  <td class="list" colspan="4"></td>
+                  <td class="list">
+                    <table border="0" cellspacing="0" cellpadding="1">
+                      <tr>
+                        <td valign="middle"><a href="load_balancer_virtual_server_edit.php"><img src="/themes/<?= $g['theme']; ?>/images/icons/icon_plus.gif" width="17" height="17" border="0"></a></td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+	   </div>
+	</table>
+            </form>
+<?php include("fend.inc"); ?>
+</body>
+</html>
---
+++ usr/local/www/load_balancer_virtual_server_edit.php
@@ -0,0 +1,144 @@
+#!/usr/local/bin/php
+<?php
+/* $Id */
+/*
+        load_balancer_virtual_server_edit.php
+        part of pfSense (http://www.pfsense.com/)
+
+        Copyright (C) 2005 Bill Marquette <bill.marquette@gmail.com>.
+        All rights reserved.
+
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice,
+           this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright
+           notice, this list of conditions and the following disclaimer in the
+           documentation and/or other materials provided with the distribution.
+
+        THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+        INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+        AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+        AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+        OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+        SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+        INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+        CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+        ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+        POSSIBILITY OF SUCH DAMAGE.
+*/
+
+require("guiconfig.inc");
+if (!is_array($config['load_balancer']['virtual_server'])) {
+        $config['load_balancer']['virtual_server'] = array();
+}
+$a_vs = &$config['load_balancer']['virtual_server'];
+
+if (isset($_POST['id']))
+	$id = $_POST['id'];
+else
+	$id = $_GET['id'];
+
+if (isset($id) && $a_vs[$id]) {
+	$pconfig['ipaddr'] = $a_vs[$id]['ipaddr'];
+	$pconfig['port'] = $a_vs[$id]['port'];
+	$pconfig['pool'] = $a_vs[$id]['pool'];
+	$pconfig['desc'] = $a_vs[$id]['desc'];
+}
+
+if ($_POST) {
+	unset($input_errors);
+	$pconfig = $_POST;
+
+	/* input validation */
+	$reqdfields = explode(" ", "ipaddr");
+	$reqdfieldsn = explode(",", "IP Address");
+
+	do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
+
+	/* Ensure that our pool names are unique */
+	for ($i=0; isset($config['load_balancer']['pool'][$i]); $i++)
+		if (($_POST['name'] == $config['load_balancer']['pool'][$i]['name']) && ($i != $id))
+			$input_errors[] = "This pool name has already been used.  Pool names must be unique.";
+
+	if (!$input_errors) {
+		$vsent = array();
+		
+		$vsent['desc'] = $_POST['desc'];
+		$vsent['pool'] = $_POST['pool'];
+		$vsent['port'] = $_POST['port'];
+		$vsent['ipaddr'] = $_POST['ipaddr'];
+
+		if (isset($id) && $a_vs[$id])
+			$a_vs[$id] = $vsent;
+		else
+			$a_vs[] = $vsent;
+
+		touch($d_poolconfdirty_path);
+
+		write_config();
+
+		header("Location: load_balancer_virtual_server.php");
+		exit;
+	}
+}
+
+$pgtitle = "Load Balancer: Virtual Server: Edit";
+include("head.inc");
+
+?>
+
+<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+<?php include("fbegin.inc"); ?>
+<p class="pgtitle"><?=$pgtitle?></p>
+<?php if ($input_errors) print_input_errors($input_errors); ?>
+            <form action="load_balancer_virtual_server_edit.php" method="post" name="iform" id="iform">
+<script type="text/javascript" language="javascript" src="pool.js">
+</script>
+
+              <table width="100%" border="0" cellpadding="6" cellspacing="0">
+                <tr align="left">
+		  <td width="22%" valign="top" class="vncellreq">Description</td>
+                  <td width="78%" class="vtable" colspan="2">
+                    <input name="desc" type="text" <?if(isset($pconfig['desc'])) echo "value=\"{$pconfig['desc']}\"";?>size="64">
+                  </td>
+		</tr>
+                <tr align="left">
+		  <td width="22%" valign="top" class="vncellreq">IP Address</td>
+                  <td width="78%" class="vtable" colspan="2">
+                    <input name="ipaddr" type="text" <?if(isset($pconfig['ipaddr'])) echo "value=\"{$pconfig['ipaddr']}\"";?> size="16" maxlength="16">
+                  </td>
+		</tr>
+                <tr align="left">
+		  <td width="22%" valign="top" class="vncellreq">Port</td>
+                  <td width="78%" class="vtable" colspan="2">
+                    <input name="port" type="text" <?if(isset($pconfig['port'])) echo "value=\"{$pconfig['port']}\"";?> size="16" maxlength="16">
+                  </td>
+		</tr>
+                <tr align="left">
+		  <td width="22%" valign="top" class="vncellreq">Virtual Server Pool</td>
+                  <td width="78%" class="vtable" colspan="2">
+                    <select id="monitor" name="monitor">
+			<?php
+				for ($i = 0; isset($config['load_balancer']['pool'][$i]); $i++) {
+					echo "<option value=\"{$config['load_balancer']['pool'][$i]['name']}\">{$config['load_balancer']['pool'][$i]['name']}</option>";
+				}
+			?>
+			</select>
+                  </td>
+		</tr>
+                <tr align="left">
+                  <td class="vtable" align="left" valign="bottom">
+			<input name="Submit" type="submit" class="formbtn" value="Submit">
+			<?php if (isset($id) && $a_vs[$id]): ?>
+			<input name="id" type="hidden" value="<?=$id;?>">
+			<?php endif; ?>
+		  </td>
+		</tr>
+              </table>
+</form>
+<?php include("fend.inc"); ?>
+</body>
+</html>

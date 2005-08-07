088748120cdc1bd67ff9ce3949a0178671bf18a4
Erik Kristensen <ekristen@pfsense.org>
2005-08-07 20:04:43 +0000

    Verifies Required Fields Are Filled Out

--- usr/local/www/pool.js
+++ usr/local/www/pool.js
@@ -1,6 +1,23 @@
 function AddServerToPool(form) {
+
+	var theSel = form['servers[]'];
+	for(i=theSel.length-1; i>=0; i--)
+	{
+		if(theSel.options[i].value == form.ipaddr.value) {
+			alert("IP Address Already In List");
+			return true;
+		}
+	}
+
+	if (form.type.selectedIndex == 1) {
+		if (!form.monitorip.value) {
+			alert("Monitor IP Required First!");
+			return true;
+		}
+	}
+
 	var ServerPort=form.ipaddr.value;
-	if(form.type.selectedIndex == 2)
+	if(form.type.selectedIndex == 0)
 		var ServerPort=form.ipaddr.value;
 	else
 		var ServerPort=form.ipaddr.value + "|" + form.monitorip.value;

07bcf7d19261563001e8e7ae2778ce211186d373
Scott Ullrich <sullrich@pfsense.org>
2005-09-15 05:40:51 +0000

    Earth to Esotericisms, check if a client is connected!

--- usr/local/www/sajax/index.sajax.php
+++ usr/local/www/sajax/index.sajax.php
@@ -2,6 +2,10 @@
 <?
 	require("../includes/sajax.class.php");
 	require("../includes/functions.inc.php");
+	
+        if(Connection_Aborted()) {
+                exit;
+        }
 
 	$oSajax = new sajax();
 	$oSajax->sajax_export("mem_usage","cpu_usage","get_uptime","get_pfstate", "get_temp");

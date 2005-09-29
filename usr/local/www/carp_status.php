66766f85d1d40e8f6acab9837db4641a101dc546
Scott Ullrich <sullrich@pfsense.org>
2005-09-29 16:11:35 +0000

    Bring up carp interfaces after changes

--- usr/local/www/carp_status.php
+++ usr/local/www/carp_status.php
@@ -49,6 +49,7 @@ if($_POST['disablecarp'] <> "") {
 		$savemsg = "CARP has been enabled.";
 		mwexec("/sbin/sysctl net.inet.carp.allow=1");
 		interfaces_carp_configure();
+		interfaces_carp_bring_up_final();
 	}
 }
 

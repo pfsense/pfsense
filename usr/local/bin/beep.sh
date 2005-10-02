e4c147db5ad071dd7e90e21c3aa389e46a95e83a
Scott Ullrich <sullrich@pfsense.org>
2005-10-02 21:24:42 +0000

    Full path to beep

--- usr/local/bin/beep.sh
+++ usr/local/bin/beep.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-if [ ! -x "`which beep`" ]; then
-	exit0
-fi
-
 if [ "$1" = "start" ]; then
-	beep -p 500 200
-	beep -p 400 200
-	beep -p 600 200
-	beep -p 800 200
-	beep -p 800 200
+	/usr/local/bin/beep -p 500 200
+	/usr/local/bin/beep -p 400 200
+	/usr/local/bin/beep -p 600 200
+	/usr/local/bin/beep -p 800 200
+	/usr/local/bin/beep -p 800 200
 fi
 if [ "$1" = "stop" ]; then
-	beep -p 600 200
-	beep -p 800 200
-	beep -p 500 200
-	beep -p 400 200
-	beep -p 400 200
+	/usr/local/bin/beep -p 600 200
+	/usr/local/bin/beep -p 800 200
+	/usr/local/bin/beep -p 500 200
+	/usr/local/bin/beep -p 400 200
+	/usr/local/bin/beep -p 400 200
 fi
 

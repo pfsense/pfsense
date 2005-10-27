20d4980cec6c3e30a576e587dcf9214de6a67ca6
Scott Ullrich <sullrich@pfsense.org>
2005-10-27 16:47:35 +0000

    Regenerate slbd configuration after pool changes
    Ticket #646

--- usr/local/www/load_balancer_pool.php
+++ usr/local/www/load_balancer_pool.php
@@ -45,6 +45,7 @@ if ($_POST) {
 		if (!file_exists($d_sysrebootreqd_path)) {
 			config_lock();
 			$retval |= filter_configure();
+			$retval |= slbd_configure();
 			config_unlock();
 		}
 		$savemsg = get_std_save_message($retval);

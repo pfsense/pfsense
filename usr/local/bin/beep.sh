26fde007f13f5114efedad8d411d397bb06c93e8
Scott Ullrich <sullrich@pfsense.org>
2005-12-10 20:50:03 +0000

    Wrap beep script in a check for /dev/speaker

--- usr/local/bin/beep.sh
+++ usr/local/bin/beep.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-if [ "$1" = "start" ]; then
-	/usr/local/bin/beep -p 500 200
-	/usr/local/bin/beep -p 400 200
-	/usr/local/bin/beep -p 600 200
-	/usr/local/bin/beep -p 800 200
-	/usr/local/bin/beep -p 800 200
-fi
-if [ "$1" = "stop" ]; then
-	/usr/local/bin/beep -p 600 200
-	/usr/local/bin/beep -p 800 200
-	/usr/local/bin/beep -p 500 200
-	/usr/local/bin/beep -p 400 200
-	/usr/local/bin/beep -p 400 200
-fi
+if [ -f "/dev/speaker" ]; then
+	
+	if [ "$1" = "start" ]; then
+		/usr/local/bin/beep -p 500 200
+		/usr/local/bin/beep -p 400 200
+		/usr/local/bin/beep -p 600 200
+		/usr/local/bin/beep -p 800 200
+		/usr/local/bin/beep -p 800 200
+	fi
+	if [ "$1" = "stop" ]; then
+		/usr/local/bin/beep -p 600 200
+		/usr/local/bin/beep -p 800 200
+		/usr/local/bin/beep -p 500 200
+		/usr/local/bin/beep -p 400 200
+		/usr/local/bin/beep -p 400 200
+	fi
 
+fi

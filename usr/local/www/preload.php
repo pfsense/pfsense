ae160a484e0ec54ead021a76eb50239521e9a93e
Scott Ullrich <sullrich@pfsense.org>
2006-01-07 00:46:24 +0000

    * Don't pre-cache auth.inc * Remove supporting goo

--- usr/local/www/preload.php
+++ usr/local/www/preload.php
@@ -4,14 +4,9 @@
 $ip = $HTTP_SERVER_VARS['REMOTE_ADDR'];
 if($ip <> "127.0.0.1") 
 	exit;
-else
-	$HTTP_SERVER_VARS['AUTH_USER'] = "root";
 
 /* preload */
 require("config.inc");
 require("functions.inc");
-require("auth.inc");
-
-unset($HTTP_SERVER_VARS['AUTH_USER']);
 
 ?>

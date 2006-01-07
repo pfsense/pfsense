c8b0a897cb6a0808d5b5d4a565808929b5373dad
Scott Ullrich <sullrich@pfsense.org>
2006-01-07 00:32:33 +0000

    Set user to root then back to "" for preload.
    Only preload from 127.0.0.1 requests

--- usr/local/www/preload.php
+++ usr/local/www/preload.php
@@ -4,10 +4,14 @@
 $ip = getenv('REMOTE_ADDR');
 if($ip <> "127.0.0.1") 
 	exit;
+else
+	$HTTP_SERVER_VARS['AUTH_USER'] = "root";
 
 /* preload */
 require("config.inc");
 require("functions.inc");
 require("auth.inc");
+
+unset($HTTP_SERVER_VARS['AUTH_USER']);
 
 ?>

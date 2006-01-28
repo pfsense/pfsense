6758725eebfd01b9fa69edc942ea0293fe8f372d
Scott Ullrich <sullrich@pfsense.org>
2006-01-28 03:12:42 +0000

    Pass $pkg info

--- usr/local/www/pkg_mgr_install.php
+++ usr/local/www/pkg_mgr_install.php
@@ -114,10 +114,9 @@ sync_webgui_passwords();
 
 switch($_GET['mode']) {
 	case "delete":
-            
             $id = get_pkg_id($_GET['pkg']);
             $todel = substr(reverse_strrchr($config['installedpackages']['package'][$id]['depends_on_package'], "."), 0, -1);
-            delete_package($todel);
+            delete_package($todel, $_GET['pkg']);
             delete_package_xml($_GET['pkg']);
             update_status("Package deleted.");
             $static_output .= "\nPackage deleted.";

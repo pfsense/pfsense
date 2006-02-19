e35011e0497c1857d7f7c857b19d97fb5ef1ef01
Scott Ullrich <sullrich@pfsense.org>
2006-02-19 21:50:42 +0000

    Add OpenVPN logs

---
+++ usr/local/www/diag_logs_openvpn.php
@@ -0,0 +1,98 @@
+<?php
+/* $Id$ */
+/*
+	diag_logs.php
+	Copyright (C) 2004 Scott Ullrich
+	All rights reserved.
+
+	originally part of m0n0wall (http://m0n0.ch/wall)
+	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+$pgtitle = array("Diagnostics","System logs","OpenVPN");
+
+require("guiconfig.inc");
+
+$openvpn_logfile = "{$g['varlog_path']}/openvpn.log";
+
+$nentries = $config['syslog']['nentries'];
+if (!$nentries)
+	$nentries = 50;
+
+if ($_POST['clear']) {
+	exec("killall syslogd");
+	exec("/usr/sbin/clog -i -s 262144 {$openvpn_logfile}");
+	system_syslogd_start();	
+}
+
+include("head.inc");
+
+?>
+<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+<?php include("fbegin.inc"); ?>
+
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+ 	<tr>
+		<td>
+<?php
+	$tab_array = array();
+	$tab_array[] = array("System", false, "diag_logs.php");
+	$tab_array[] = array("Firewall", false, "diag_logs_filter.php");
+	$tab_array[] = array("DHCP", false, "diag_logs_dhcp.php");
+	$tab_array[] = array("Portal Auth", false, "diag_logs_auth.php");
+	$tab_array[] = array("IPSEC VPN", false, "diag_logs_ipsec.php");
+	$tab_array[] = array("PPTP VPN", false, "diag_logs_vpn.php");
+	$tab_array[] = array("Load Balance", false, "diag_logs_slbd.php");
+	$tab_array[] = array("OpenVPN", true, "diag_logs_openvpn.php");
+	$tab_array[] = array("Settings", false, "diag_logs_settings.php");
+	display_top_tabs($tab_array);
+?>
+  		</td>
+	</tr>
+	<tr>
+    	<td>
+			<div id="mainarea">
+			<table class="tabcont" width="100%" border="0" cellspacing="0" cellpadding="0">
+		  		<tr>
+					<td colspan="2" class="listtopic">Last <?=$nentries;?> IPSEC log entries</td>
+		  		</tr>
+				<?php dump_clog($ipsec_logfile, $nentries); ?>
+				<tr>
+					<td>
+						<br>
+						<form action="diag_logs_openvpn.php" method="post">
+						<input name="clear" type="submit" class="formbtn" value="Clear log">
+						</form>
+					</td>
+				</tr>
+			</table>
+			</div>
+		</td>
+	</tr>
+</table>
+<?php include("fend.inc"); ?>
+<meta http-equiv="refresh" content="60;url=<?php print $_SERVER['PHP_SELF']; ?>">
+</body>
+</html>

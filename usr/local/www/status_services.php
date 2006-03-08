a862b824469fd3cb9b562ab5eddde69f07ae6bc0
Scott Ullrich <sullrich@pfsense.org>
2006-03-08 18:41:58 +0000

    Refresh to page name, not the GET'd url

--- usr/local/www/status_services.php
+++ usr/local/www/status_services.php
@@ -184,7 +184,7 @@ if($services) {
 </tr></table>
 </div>
 
-<meta http-equiv="refresh" content="120;url=<?php print $_SERVER['PHP_SELF']; ?>">
+<meta http-equiv="refresh" content="120;url=status_services.php">
 
 <?php include("fend.inc"); ?>
 </body>

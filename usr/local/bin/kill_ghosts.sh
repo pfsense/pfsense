6d0e36962bcce42b7c0458b16e3e8a42278db477
Scott Ullrich <sullrich@pfsense.org>
2006-03-25 23:17:13 +0000

    Use awk

--- usr/local/bin/kill_ghosts.sh
+++ usr/local/bin/kill_ghosts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Kill ghost shells
 
-/bin/kill -9 `/bin/ps awux | grep "(sh)" | /usr/bin/grep -v grep | /usr/bin/cut -d" " -f5`
+/bin/kill -9 `/bin/ps awux | grep "(sh)" | /usr/bin/grep -v grep | ps awux | grep "(sh)" | grep -v grep | /usr/bin/awk '{ print $2 }'`
 

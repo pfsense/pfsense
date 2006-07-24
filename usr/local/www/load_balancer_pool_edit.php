039bee92c655021bd1836e4b666991f4bae921f5
Scott Ullrich <sullrich@pfsense.org>
2006-07-24 20:33:32 +0000

    Actually show gateway in question and change a to an.

--- usr/local/www/load_balancer_pool_edit.php
+++ usr/local/www/load_balancer_pool_edit.php
@@ -101,7 +101,7 @@ if ($_POST) {
 		}
 		foreach($ifdescrs as $iface) {
 			if($config[$iface]['gateway'] == $_POST['gateway']) 
-				$input_errors[] = "{$iface['gateway']} is currently being referenced by a interface ip address.";
+				$input_errors[] = "{$config[$iface]['gateway']} is currently being referenced by an interface ip address.";
 		}
 	}
 

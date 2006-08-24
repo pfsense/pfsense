c6fd72725d3af9ed02f7bc086075f149e97ca2f6
Scott Ullrich <sullrich@pfsense.org>
2006-08-24 18:18:59 +0000

    Be even more picky about spacing.

--- usr/local/www/firewall_virtual_ip_edit.php
+++ usr/local/www/firewall_virtual_ip_edit.php
@@ -406,7 +406,7 @@ function typesel_change() {
 				      			<strong>Note:<br></strong>
 				      		</span>&nbsp;&nbsp;
 				      		ProxyARP type IP addresses *DO NOT* work with the FTP Helper and addon packages such as squid.  Use a CARP type address in this case.
-				      		<p>&nbsp;&nbsp;For more information on CARP and the above values, visit the OpenBSD <a href='http://www.openbsd.org/faq/pf/carp.html'>CARP faq</A>.
+				      		<p>&nbsp;&nbsp;&nbsp;For more information on CARP and the above values, visit the OpenBSD <a href='http://www.openbsd.org/faq/pf/carp.html'>CARP faq</A>.
 						</span>
 					  </p>
 				  </td>

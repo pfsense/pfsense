fe4d7d52d27d0b5eab1ba5808bb0ffe6043620e0
Scott Ullrich <sullrich@pfsense.org>
2006-09-12 20:05:36 +0000

    Match on the beginning of the string so that the 110 network is included
    Submitted-by: XAI via IRC

--- etc/rc.update_bogons.sh
+++ etc/rc.update_bogons.sh
@@ -6,6 +6,6 @@
 
 /etc/rc.conf_mount_rw
 /usr/bin/fetch -q -o /tmp/bogons "http://www.cymru.com/Documents/bogon-bn-nonagg.txt"
-egrep -v "192.168.0.0/16|172.16.0.0/12|10.0.0.0/8" /tmp/bogons > /etc/bogons
+egrep -v "^192.168.0.0/16|^172.16.0.0/12|^10.0.0.0/8" /tmp/bogons > /etc/bogons
 /etc/rc.conf_mount_ro
 /sbin/pfctl -t bogons -T replace -f /etc/bogons

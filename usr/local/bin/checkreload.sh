b7f435012837f8c2017119b9823e8566b671313c
Scott Ullrich <sullrich@pfsense.org>
2007-01-27 00:13:46 +0000

    Add check_reload_status checker script to ensure that it is always running.
    Prior attempts to restart from filter.inc lead to socket descriptor inheriting strangeness.

---
+++ usr/local/bin/checkreload.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CHECKRELOADSTATUS=`pgrep check_reload_status | grep -v grep | wc -l`
+DATE=`date +%m-%d-%Y_at_%H%M%S`
+OUTFILE=/root/check_reload_status.log
+
+if [ ${CHECKRELOADSTATUS} -lt 1 ] ; then
+        echo "${DATE}: Re-starting check_reload_status, it died for some reason..." >> ${OUTFILE}
+        /usr/local/sbin/check_reload_status
+fi
+
+if [ ${CHECKRELOADSTATUS} -gt 1 ] ; then
+        echo "${DATE} There appears to be 2 or more check_reload_status processes. Forcing kill and restart of all now..." >> ${OUTFILE}
+        kill -9 `pgrep check_reload_status`
+        /usr/local/sbin/check_reload_status
+fi

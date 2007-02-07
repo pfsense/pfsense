e0597439911f8ad644e0b98dcc9b8ce8a9950ee2
Scott Ullrich <sullrich@pfsense.org>
2007-02-07 00:01:53 +0000

    Add switch to dynamic view link

--- usr/local/www/diag_logs_filter.php
+++ usr/local/www/diag_logs_filter.php
@@ -204,7 +204,7 @@ include("head.inc");
 ?>
 		<tr>
 		  <td colspan="6" class="listtopic">
-			    Last <?=$nentries;?> firewall log entries</td>
+			    Last <?=$nentries;?> firewall log entries &nbsp;&nbsp;&nbsp;(<a href='/diag_logs_filter_dynamic.php'>switch</a> to dynamic view)</td>
 			</tr>
 			<tr>
 			  <td width="10%" class="listhdrr">Act</td>

e415bfab3c1dcf3a8dd50733f797f73fc8601a66
Scott Ullrich <sullrich@pfsense.org>
2007-02-09 20:59:14 +0000

    Include scriptaculous and prototype.js for fancy kill effects

--- usr/local/www/diag_dump_states.php
+++ usr/local/www/diag_dump_states.php
@@ -52,6 +52,8 @@ else {
 
 <body link="#0000CC" vlink="#0000CC" alink="#0000CC" onload="<?=$jsevents["body"]["onload"];?>">
 <script src="/javascript/sorttable.js" type="text/javascript"></script>
+<script src="/javascript/scriptaculous/prototype.js" type="text/javascript"></script>
+<script src="/javascript/scriptaculous/scriptaculous.js" type="text/javascript"></script>
 <?php include("fbegin.inc"); ?>
 <p class="pgtitle"><?=$pgtitle?></p>
 <form action="diag_dump_states.php" method="get" name="iform">

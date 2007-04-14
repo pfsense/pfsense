7f7ad5015c1c285852923a84e8145e8fa7983363
Scott Ullrich <sullrich@pfsense.org>
2007-04-14 22:08:49 +0000

    Add a note about firewall rule schedule logic that will pop up in a new window describing how pass rules work when they are outside of the schedule window, etc.

--- usr/local/www/firewall_rules_edit.php
+++ usr/local/www/firewall_rules_edit.php
@@ -751,7 +751,8 @@ include("head.inc");
 					}
 				}?>
 				</select>
-				<p>Leave as 'none' to leave the rule enabled all the time.</p>
+				<p>Leave as 'none' to leave the rule enabled all the time.</p><br/>
+				<strong>NOTE:  schedule logic can be a bit different.  Click <a target="_new" href='firewall_rules_schedule_logic.php'>here</a> for more information.</strong>
 			</td>
 		</tr>
 		

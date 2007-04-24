f71531ed5765b9f4f0ebf47acfb9a4747eb2c10c
Scott Ullrich <sullrich@pfsense.org>
2007-04-24 01:00:42 +0000

    s/resolved/resolve/

--- usr/local/www/interfaces_assign.php
+++ usr/local/www/interfaces_assign.php
@@ -199,7 +199,7 @@ if(file_exists("/var/run/interface_mismatch_reboot_needed"))
 	if ($_POST) 
 		$savemsg = "The firewall is now rebooting.";
 	else
-		$savemsg = "Interface mismatch deteceted.  Please resolved the mismatch and click Save.  The firewall will reboot afterwards.";
+		$savemsg = "Interface mismatch deteceted.  Please resolve the mismatch and click Save.  The firewall will reboot afterwards.";
 
 ?>
 

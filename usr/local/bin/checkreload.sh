ef84c96a784256865f4c4cf147bc4e9fddf72715
Scott Ullrich <sullrich@pfsense.org>
2007-06-04 22:50:42 +0000

    Catch checkreload.sh back up to check_reload_status changes.

--- usr/local/bin/checkreload.sh
+++ usr/local/bin/checkreload.sh
@@ -6,11 +6,11 @@ OUTFILE=/root/check_reload_status.log
 
 if [ ${CHECKRELOADSTATUS} -lt 1 ] ; then
         echo "${DATE}: Re-starting check_reload_status, it died for some reason..." >> ${OUTFILE}
-        /usr/local/sbin/check_reload_status
+        nohup /usr/bin/nice -n20 /usr/local/sbin/check_reload_status &
 fi
 
 if [ ${CHECKRELOADSTATUS} -gt 1 ] ; then
         echo "${DATE} There appears to be 2 or more check_reload_status processes. Forcing kill and restart of all now..." >> ${OUTFILE}
         kill -9 `pgrep check_reload_status`
-        /usr/local/sbin/check_reload_status
+        nohup /usr/bin/nice -n20 /usr/local/sbin/check_reload_status &
 fi

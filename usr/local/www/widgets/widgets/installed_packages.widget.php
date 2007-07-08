72209e47a4f7798ab6b958f4ee06bc3da95b084a
Scott Dale <sdale@pfsense.org>
2007-07-08 03:52:39 +0000

    initial version for package widget

---
+++ usr/local/www/widgets/widgets/installed_packages.widget.php
@@ -0,0 +1,99 @@
+<?php
+/*
+        $Id$
+        Copyright 2007 Scott Dale
+        Part of pfSense widgets (www.pfsense.com)
+        originally based on m0n0wall (http://m0n0.ch/wall)
+
+        Copyright (C) 2004-2005 T. Lechat <dev@lechat.org>, Manuel Kasper <mk@neon1.net>
+        and Jonathan Watt <jwatt@jwatt.org>.
+        All rights reserved.
+
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice,
+           this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright
+           notice, this list of conditions and the following disclaimer in the
+           documentation and/or other materials provided with the distribution.
+
+        THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+        INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+        AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+        AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+        OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+        SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+        INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+        CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+        ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+        POSSIBILITY OF SUCH DAMAGE.
+*/
+
+if(is_array($config['installedpackages']['package'])) {
+	foreach($config['installedpackages']['package'] as $instpkg) {
+		$tocheck[] = $instpkg['name'];
+	}
+	$currentvers = get_pkg_info($tocheck, array('version', 'xmlver'));
+}
+?>
+
+<table width="100%" border="0" cellpadding="6" cellspacing="0">
+                <tr>
+                  <td width="15%" class="listhdrr">Package Name</td>
+                  <td width="20%" class="listhdrr">Category</td>
+		  <td width="10%" class="listhdrr">Package Version</td>
+                </tr>
+		<?php
+                 if($config['installedpackages']['package'] != "") {
+		    $instpkgs = array();
+		    foreach($config['installedpackages']['package'] as $instpkg) $instpkgs[] = $instpkg['name'];
+		    asort($instpkgs);
+		    foreach ($instpkgs as $index => $pkgname){
+			$pkg = $config['installedpackages']['package'][$index];
+                        if($pkg['name'] <> "") {
+                            ?>
+                            <tr valign="top">
+                                <td class="listlr">
+                                    <?= $pkg['name'] ?>
+                                </td>
+                                <td class="listlr">
+                                    <?= $pkg['category'] ?>
+                                </td>
+                                <td class="listlr">
+                                <?php
+                                        $latest_package = $currentvers[$pkg['name']]['version'];
+					if($latest_package == false) 
+					{
+						// We can't determine this package's version status.
+						echo "Current: Unknown.<br>Installed: " . $pkg['version'];
+					}
+					elseif(strcmp($pkg['version'], $latest_package) > 0) 
+					{
+                        /* we're running a newer version of the package */   
+                        echo "Current: {$latest_package}";
+                        echo "<br>Installed: {$pkg['version']}";					    
+					}
+					elseif(strcmp($pkg['version'], $latest_package) < 0) 
+					{
+						/* our package is out of date */					    
+						echo "Current: {$latest_package}";
+					    echo "<br>Installed: {$pkg['version']}";
+					    
+					}
+					else 
+					{					    
+					    echo $pkg['version'];
+					    
+					}
+                    ?></td>
+                    </tr>
+                            <?php
+                        }
+		    }
+                 } else {
+                    echo "<tr><td colspan=\"5\"><center>There are no packages currently installed.</td></tr>";
+                 }
+		?>
+        </table>

3fb63eb461b3058edcba4d0df0eeb96fd8fcf836
Scott Ullrich <sullrich@pfsense.org>
2007-11-27 22:16:37 +0000

    Throw an error when we cannot download bogons file instead of blindly assuming it succeedded.

--- etc/rc.update_bogons.sh
+++ etc/rc.update_bogons.sh
@@ -12,7 +12,12 @@ sleep $value
 
 /etc/rc.conf_mount_rw
 /usr/bin/fetch -q -o /tmp/bogons "http://www.pfsense.com/mirrors/bogon-bn-nonagg.txt"
+if [ ! -f /tmp/bogons ]; then
+	echo "Could not download http://www.pfsense.com/mirrors/bogon-bn-nonagg.txt" | logger
+	exit
+fi
 egrep -v "^192.168.0.0/16|^172.16.0.0/12|^10.0.0.0/8" /tmp/bogons > /etc/bogons
 /etc/rc.conf_mount_ro
 /sbin/pfctl -t bogons -T replace -f /etc/bogons
+rm /tmp/bogons
 

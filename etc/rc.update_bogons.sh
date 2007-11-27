528ec8dbab5cb14aa8f41c1b84abefcdff59fecb
Scott Ullrich <sullrich@pfsense.org>
2007-11-27 20:28:36 +0000

    * Download bogons entries from pfsense.com * Do not update on every minute on the 1st of the month * Sleep for a random period before updating to avoid killing the server

--- etc/rc.update_bogons.sh
+++ etc/rc.update_bogons.sh
@@ -4,8 +4,18 @@
 # Part of the pfSense project
 # www.pfsense.com
 
+# Grab a random value between 0-999.
+value=$RANDOM
+while [ $value -gt 999 ] ; do
+  value=$RANDOM
+done
+
+# Sleep for that time.
+sleep $value
+
 /etc/rc.conf_mount_rw
-/usr/bin/fetch -q -o /tmp/bogons "http://www.cymru.com/Documents/bogon-bn-nonagg.txt"
+/usr/bin/fetch -q -o /tmp/bogons "http://www.pfsense.com/mirrors/bogon-bn-nonagg.txt"
 egrep -v "^192.168.0.0/16|^172.16.0.0/12|^10.0.0.0/8" /tmp/bogons > /etc/bogons
 /etc/rc.conf_mount_ro
 /sbin/pfctl -t bogons -T replace -f /etc/bogons
+

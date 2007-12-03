3cc8dccb698e6fd78653cf2b493c29f836547ff8
Scott Ullrich <sullrich@pfsense.org>
2007-12-03 02:31:59 +0000

    When showing the firewall rules page without a interface default to the first interface the user has access to.

--- usr/local/www/firewall_rules.php
+++ usr/local/www/firewall_rules.php
@@ -69,14 +69,15 @@ if (isset($config['ipsec']['enable']) || isset($config['ipsec']['mobileclients']
 		$iflist["enc0"] = "IPSEC";
 
 if (!$if || !isset($iflist[$if]))
-	$if = "wan";
+	foreach($iflist as $if => $ifname) 
+		break;
 
 $security_url = "firewall_rules.php?if=". strtolower($if);
 if (!isSystemAdmin($HTTP_SERVER_VARS['AUTH_USER'])) {
 	if(!in_array($security_url, $allowed)) {
 		// User does not have access
 //		echo "displaying error {$security_url}"; print_r($allowed);
-		echo display_error_form("401", "Unauthorized. You do not have access to the page {$pagereq}");
+		echo display_error_form("401", "Unauthorized. You do not have access to the page {$pagereq} for interface {$if}");
 		exit;
 	}
 }

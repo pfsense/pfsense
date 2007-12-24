e88347949d1f2bea3bb60778d1eb06dd703e07e5
Scott Ullrich <sullrich@pfsense.org>
2007-12-24 03:03:26 +0000

    Change the beep duration /4 when the platform is embedded or we detect that the kern.hz has been altered in /boot/loader.conf.

--- usr/local/bin/beep.sh
+++ usr/local/bin/beep.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
-if [ -c "/dev/speaker" ]; then
-	
-	if [ "$1" = "start" ]; then
-		/usr/local/bin/beep -p 500 200
-		/usr/local/bin/beep -p 400 200
-		/usr/local/bin/beep -p 600 200
-		/usr/local/bin/beep -p 800 200
-		/usr/local/bin/beep -p 800 200
-	fi
-	if [ "$1" = "stop" ]; then
-		/usr/local/bin/beep -p 600 200
-		/usr/local/bin/beep -p 800 200
-		/usr/local/bin/beep -p 500 200
-		/usr/local/bin/beep -p 400 200
-		/usr/local/bin/beep -p 400 200
-	fi
+# Standard note length
+NOTELENGTH="200"
 
+# Embedded uses 100HZ
+if [ $PFSENSETYPE = "embedded" ]; then
+	NOTELENGTH="50"
 fi
+
+# Check for different HZ 
+HZ=`cat /boot/loader.conf | grep kern.hz | wc`
+if [ "$HZ" -gt 0 ]; then
+	NOTELENGTH="50"
+fi
+
+if [ -c "/dev/speaker" ]; then	
+		if [ "$1" = "start" ]; then
+			/usr/local/bin/beep -p 500 $NOTELENGTH
+			/usr/local/bin/beep -p 400 $NOTELENGTH
+			/usr/local/bin/beep -p 600 $NOTELENGTH
+			/usr/local/bin/beep -p 800 $NOTELENGTH
+			/usr/local/bin/beep -p 800 $NOTELENGTH
+		fi
+		if [ "$1" = "stop" ]; then
+			/usr/local/bin/beep -p 600 $NOTELENGTH
+			/usr/local/bin/beep -p 800 $NOTELENGTH
+			/usr/local/bin/beep -p 500 $NOTELENGTH
+			/usr/local/bin/beep -p 400 $NOTELENGTH
+			/usr/local/bin/beep -p 400 $NOTELENGTH
+		fi
+fi 

de6b5ecee979c54e9b550945a7f27bed0487be23
Chris Buechler <cmb@pfsense.org>
2008-02-03 23:19:46 +0000

    correct bogons file location

--- etc/rc.update_bogons.sh
+++ etc/rc.update_bogons.sh
@@ -11,9 +11,9 @@ value=`od -A n -d -N2 /dev/random | awk '{print int(($1/65536)*2000)}'`
 sleep $value
 
 /etc/rc.conf_mount_rw
-/usr/bin/fetch -q -o /tmp/bogons "http://files.pfsense.org/mirrors/bogon-bn-nonagg.txt"
+/usr/bin/fetch -q -o /tmp/bogons "http://files.pfsense.org/bogon-bn-nonagg.txt"
 if [ ! -f /tmp/bogons ]; then
-	echo "Could not download http://files.pfsense.org/mirrors/bogon-bn-nonagg.txt" | logger
+	echo "Could not download http://files.pfsense.org/bogon-bn-nonagg.txt" | logger
 	exit
 fi
 egrep -v "^192.168.0.0/16|^172.16.0.0/12|^10.0.0.0/8" /tmp/bogons > /etc/bogons

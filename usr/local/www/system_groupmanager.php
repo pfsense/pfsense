bb6def328910ba6b1b9829ca9997f0f0098e8f01
Scott Ullrich <sullrich@pfsense.org>
2008-02-16 21:51:54 +0000

    s/package/Package/CVS: ----------------------------------------------------------------------

--- usr/local/www/system_groupmanager.php
+++ usr/local/www/system_groupmanager.php
@@ -95,8 +95,8 @@ function getAdminPageList() {
         unset($tmp['diag_new_states.php']);
         unset($tmp['system_usermanager_edit.php']);
         
-        $tmp['pkg.php'] = "{$g['prouct_name']} package manager";
-        $tmp['pkg_edit.php'] = "{$g['product_name']} package manager edit";
+        $tmp['pkg.php'] = "{$g['prouct_name']} Package manager";
+        $tmp['pkg_edit.php'] = "{$g['product_name']} Package manager edit";
         $tmp['wizard.php'] = "{$g['product_name']} wizard subsystem";
         $tmp['graphs.php'] = "Graphing subsystem";
         $tmp['headjs.php'] = "*Required for javascript";

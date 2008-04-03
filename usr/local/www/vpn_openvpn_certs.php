0bcee88054d0878cb4c0ceb442566a76d862790f
Scott Ullrich <sullrich@pfsense.org>
2008-04-03 01:34:06 +0000

    Do not output the page title twice.

--- usr/local/www/vpn_openvpn_certs.php
+++ usr/local/www/vpn_openvpn_certs.php
@@ -65,7 +65,6 @@ include("head.inc");
 
     <body link="#0000CC" vlink="#0000CC" alink="#0000CC">
     <?php include("fbegin.inc"); ?>
-<p class="pgtitle"><?=$pgtitle?></p>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
 
 <form action="vpn_openvpn_certs.php" method="post" name="iform" id="iform">

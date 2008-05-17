2b1d19d64f3f21117f00ed9c49850f854e7c20df
Scott Ullrich <sullrich@pfsense.org>
2008-05-17 20:43:58 +0000

    Ticket #1648 fixes

--- usr/local/www/status_graph.php
+++ usr/local/www/status_graph.php
@@ -66,8 +66,9 @@ include("head.inc");
 <?php
 $ifdescrs = array('wan' => 'WAN', 'lan' => 'LAN');
 
-for ($j = 1; isset($config['interfaces']['opt' . $j]['enable']); $j++) {
-	$ifdescrs['opt' . $j] = $config['interfaces']['opt' . $j]['descr'];
+for($j = 1; isset($config['interfaces']['opt' . $j]); $j++) {
+	if(isset($config['interfaces']['opt' . $j]['enable']))
+		$ifdescrs['opt' . $j] = $config['interfaces']['opt' . $j]['descr'];
 }
 if((isset($config['ipsec']['enable'])) || (isset($config['ipsec']['mobileclients']['enable']))) {
 	$ifdescrs['ipsec'] = "IPSEC";

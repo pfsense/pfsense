942e38a7031bd06f96296525e21348029394fe4f
Scott Ullrich <sullrich@pfsense.org>
2008-07-22 16:18:52 +0000

    Only signal a package reinstall on full installations.

--- tmp/post_upgrade_command.php
+++ tmp/post_upgrade_command.php
@@ -15,4 +15,8 @@
 		unlink("/usr/local/www/fred.png"); 
 	if(file_exists("/usr/local/www/dfuife.cgi"))
 		unlink("/usr/local/www/dfuife.cgi"); 
+
+	if($g['platform'] == "pfSense")
+		touch("/conf/needs_package_sync");
+
 ?>

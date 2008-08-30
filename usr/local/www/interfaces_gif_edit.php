6ad8bd3a4d2d8c7a5e838b510362ae481e25afff
Scott Ullrich <sullrich@pfsense.org>
2008-08-30 19:45:35 +0000

    Dress GIF edit page up with a CSS header like other pages use.

--- usr/local/www/interfaces_gif_edit.php
+++ usr/local/www/interfaces_gif_edit.php
@@ -126,6 +126,9 @@ include("head.inc");
             <form action="interfaces_gif_edit.php" method="post" name="iform" id="iform">
               <table width="100%" border="0" cellpadding="6" cellspacing="0">
 				<tr>
+					<td colspan="2" valign="top" class="listtopic">GIF configuration</td>
+				</tr>
+				<tr>
                   <td width="22%" valign="top" class="vncellreq">Parent interface</td>
                   <td width="78%" class="vtable">
                     <select name="if" class="formselect">

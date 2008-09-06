2cab9166198ad2b15831f0b5e74b8160a1d2286c
Seth Mos <seth.mos@xs4all.nl>
2008-09-06 21:44:47 +0000

    Modify the reset_slbd.sh script a bit. Needs test.

--- usr/local/sbin/reset_slbd.sh
+++ usr/local/sbin/reset_slbd.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+FAILURE=0
+
 for items in `ps auxcwwl | awk '/slbd/{print $3}'|awk -F"." '{print $1}'`
 do
-        if [ "$items" -gt "86" ]; then
-                killall slbd
-				sleep 2
-                killall -9 slbd
-                echo "Resetting slbd due to high cpu usage: ${items}%" | logger
-                /usr/local/sbin/slbd -c/var/etc/slbd.conf -r5000
+        if [ "$items" -ge "20" ]; then
+                FAILURE=`expr $FAILURE + 1`
         fi
 done
+
+if [ "$FAILURE" -ge "1" ]; then
+	killall -9 slbd	
+	sleep 2
+	echo "Resetting slbd due to high cpu usage: ${items}%" | logger
+	/usr/local/sbin/slbd -c/var/etc/slbd.conf -r5000
+	FAILURE=0
+fi

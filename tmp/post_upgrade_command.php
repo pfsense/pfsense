5d8c7db8273ca7bda94245b900e3d75e8638ae11
Scott Ullrich <sullrich@pfsense.org>
2008-09-08 22:08:56 +0000

    Remove obsoleted files after upgrade

--- tmp/post_upgrade_command.php
+++ tmp/post_upgrade_command.php
@@ -19,4 +19,9 @@
 	if($g['platform'] == "pfSense")
 		touch("/conf/needs_package_sync");
 
+	$files_to_process = split("\n", file_get_contents("/etc/pfSense.obsoletedfiles"));
+	foreach($files_to_process as $filename) 
+		if(file_exists($filename)) 
+			exec("rm $filename");
+
 ?>

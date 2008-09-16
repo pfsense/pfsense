4558117267b1c7fbfa9c35ad2b0047a456dcc08b
Ermal Luçi <eri@pfsense.org>
2008-09-16 18:18:36 +0000

    Fix the Traffic Graph for Firefox case, which caches agressively and makes the XMLHttpRequest calls return the same result and the output is just a red line at the end of the graph since the difference between the old and new traffic is always 0.

--- usr/local/www/ifstats.php
+++ usr/local/www/ifstats.php
@@ -65,6 +65,12 @@
 	}
 	$temp = gettimeofday();
 	$timing = (double)$temp["sec"] + (double)$temp["usec"] / 1000000.0;
+	
+	header("Last-Modified: " . gmdate( "D, j M Y H:i:s" ) . " GMT" );
+	header("Expires: " . gmdate( "D, j M Y H:i:s", time() ) . " GMT" );
+	header("Cache-Control: no-store, no-cache, must-revalidate" ); // HTTP/1.1
+	header("Cache-Control: post-check=0, pre-check=0", FALSE );
+	header("Pragma: no-cache"); // HTTP/1.0
 
 	echo "$timing|" . $ifinfo['inbytes'] . "|" . $ifinfo['outbytes'] . "\n";
 

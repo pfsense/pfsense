dec6c2281a336b6a551ca9d796f7a91a29e6d44a
Scott Ullrich <sullrich@pfsense.org>
2008-09-16 18:20:25 +0000

    MFC Fix the Traffic Graph for Firefox case, which caches agressively and makes the XMLHttpRequest calls return the same result and the output is just a red line at the end of the graph since the difference between the old and new traffic is always 0.

--- usr/local/www/graph.php
+++ usr/local/www/graph.php
@@ -29,6 +29,11 @@
 	POSSIBILITY OF SUCH DAMAGE.
 */
 
+header("Last-Modified: " . gmdate( "D, j M Y H:i:s" ) . " GMT" );
+header("Expires: " . gmdate( "D, j M Y H:i:s", time() ) . " GMT" );
+header("Cache-Control: no-store, no-cache, must-revalidate" ); // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", FALSE );
+header("Pragma: no-cache"); // HTTP/1.0
 header("Content-type: image/svg+xml");
 
 /********** HTTP GET Based Conf ***********/

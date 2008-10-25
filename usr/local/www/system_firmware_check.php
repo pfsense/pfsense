10d72e1e7e507ba8e24e6d3e8007ea06b9d1c801
Scott Ullrich <sullrich@pfsense.org>
2008-10-25 07:59:37 +0000

    Use correct css file

--- usr/local/www/system_firmware_check.php
+++ usr/local/www/system_firmware_check.php
@@ -51,7 +51,7 @@ include("head.inc");
 <script src="/javascript/scriptaculous/prototype.js" type="text/javascript"></script>
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link href="gui.css" rel="stylesheet" type="text/css">
+<link href="all.css" rel="stylesheet" type="text/css">
 </head>
 
 <body link="#0000CC" vlink="#0000CC" alink="#0000CC">

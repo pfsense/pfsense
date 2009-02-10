d73497d4bedc8f405a2367baad835070c7f5656f
Scott Ullrich <sullrich@pfsense.org>
2009-02-10 18:06:29 -0500

    Open package info link in new tab / window

--- usr/local/www/pkg_mgr.php
+++ usr/local/www/pkg_mgr.php
@@ -167,7 +167,7 @@ include("fbegin.inc");
 				<?php
 				if($index['pkginfolink']) {
                                         $pkginfolink = $index['pkginfolink'];
-					echo "<a href='$pkginfolink'>Package Info</a>";
+					echo "<a target='_new' href='$pkginfolink'>Package Info</a>";
 				} else {
 					echo "No info, check the <a href='http://forum.pfsense.org/index.php/board,15.0.html'>forum</a>";
 				}

44df08c27b442a9069b90aa49271725e4303ea6f
Scott Ullrich <sullrich@pfsense.org>
2009-02-10 18:04:46 -0500

    Open Package info link in a new tab / window

--- usr/local/www/pkg_mgr_installed.php
+++ usr/local/www/pkg_mgr_installed.php
@@ -130,7 +130,7 @@ include("head.inc");
 							<?php
 							if($currentvers[$pkg['name']]['pkginfolink']) {
 								$pkginfolink = $currentvers[$pkg['name']]['pkginfolink'];
-								echo "<a href='$pkginfolink'>Package Info</a>";
+								echo "<a target='_new' href='$pkginfolink'>Package Info</a>";
 							} else {
 								echo "No info, check the <a href='http://forum.pfsense.org/index.php/board,15.0.html'>forum</a>";
 							}

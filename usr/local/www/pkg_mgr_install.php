f8a437b69cd013ddb3441d21dbcd84b6f161396f
Bill Marquette <bill.marquette@gmail.com>
2009-03-14 18:58:19 -0500

    remove new lines

--- usr/local/www/pkg_mgr_install.php
+++ usr/local/www/pkg_mgr_install.php
@@ -89,12 +89,9 @@ include("head.inc");
 							</table>
 							<br>
 							<!-- status box -->
-							<textarea cols="60" rows="1" name="status" id="status" wrap="hard">
-								Beginning package installation.
-							</textarea>
+							<textarea cols="60" rows="1" name="status" id="status" wrap="hard">Beginning package installation.</textarea>
 							<!-- command output box -->
-							<textarea cols="60" rows="25" name="output" id="output" wrap="hard">
-							</textarea>
+							<textarea cols="60" rows="25" name="output" id="output" wrap="hard"></textarea>
 						</center>
 					</td>
 				</tr>

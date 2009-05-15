82cab169ff5be8e731d09e50951432891b9955b9
Scott Ullrich <sullrich@sullrich-MacBookPro.geekgod.com>
2009-05-14 20:49:01 -0400

    Move firmware update text format to globals.inc

--- usr/local/www/system_firmware.php
+++ usr/local/www/system_firmware.php
@@ -197,7 +197,7 @@ print_info_box($sig_warning);
 		  <td width="22%" valign="baseline" class="vncell">&nbsp;</td>
                   <td width="78%" class="vtable">
             <p>Click &quot;Enable firmware
-              upload&quot; below, then choose the image file (<?=$g['platform'];?>-*.tgz)
+              upload&quot; below, then choose the image file (<?=$g['firmware_update_text'];?>)
 			  to be uploaded.<br>Click &quot;Upgrade firmware&quot;
               to start the upgrade process.</p>
                     <?php if (!file_exists($d_sysrebootreqd_path)): ?>

405f7cf5f5019ea3b4ae75e236908d8299346c0f
Scott Ullrich <sullrich@sullrich-MacBookPro.geekgod.com>
2009-06-05 19:40:15 -0400

    Prevent javascript errors when possible
    Submitted-by: Renato

--- usr/local/www/javascript/index/sajax.js
+++ usr/local/www/javascript/index/sajax.js
@@ -5,66 +5,78 @@ var update_interval = 7000;
 
 function updateMeters()
 {
-	x_get_stats(stats);
-
-	window.setTimeout('updateMeters()', update_interval);
+    try {
+    	x_get_stats(stats);
+    	window.setTimeout('updateMeters()', update_interval);
+    }catch(e){}
 }
 
 function stats(x) {
+    try {
+	    var values = x.split("|");
 
-	var values = x.split("|");
+	    for(var counter=0; counter<x.length; x++) {
+		    if(values[counter] == 'undefined' || values[counter] == null)
+			    return;
+	    }	
 
-	for(var counter=0; counter<x.length; x++) {
-		if(values[counter] == 'undefined' || values[counter] == null)
-			return;
-	}	
-
-	updateCPU(values[0]);
-	updateMemory(values[1]);
-	updateUptime(values[2]);
-	updateState(values[3]);
-	updateTemp(values[4]);
+	    updateCPU(values[0]);
+	    updateMemory(values[1]);
+	    updateUptime(values[2]);
+	    updateState(values[3]);
+	    updateTemp(values[4]);
+    }catch(e){}
 }
 
 function updateMemory(x)
 {
-	document.getElementById("memusagemeter").value = x + '%';
-
-	document.getElementById("memwidtha").style.width = x + 'px';
-	document.getElementById("memwidthb").style.width = (100 - x) + 'px';
+    try {
+    	document.getElementById("memusagemeter").value = x + '%';
+	    document.getElementById("memwidtha").style.width = x + 'px';
+    	document.getElementById("memwidthb").style.width = (100 - x) + 'px';
+    }catch(e){}
 }
 
 function updateCPU(x)
 {
-	document.getElementById("cpumeter").value = x + '%';
-	
-	document.getElementById("cpuwidtha").style.width = x + 'px';
-	document.getElementById("cpuwidthb").style.width = (100 - x) + 'px';
+    try {
+	    document.getElementById("cpumeter").value = x + '%';
+	    document.getElementById("cpuwidtha").style.width = x + 'px';
+	    document.getElementById("cpuwidthb").style.width = (100 - x) + 'px';
+    }catch(e){}
 }
 
 function updateTemp(x)
 {
-	if(document.getElementById("tempmeter") != null) {
-		document.getElementById("tempmeter").value = x + 'C';
-		document.getElementById("tempwidtha").style.width = x + 'px';
-		document.getElementById("tempwidthb").style.width = (100 - x) + 'px';
-	}
+    try {
+	    if(document.getElementById("tempmeter") != null) {
+		    document.getElementById("tempmeter").value = x + 'C';
+		    document.getElementById("tempwidtha").style.width = x + 'px';
+		    document.getElementById("tempwidthb").style.width = (100 - x) + 'px';
+	    }
+    }catch(e){}
 }
 
 function updateUptime(x)
 {
-	document.getElementById("uptime").value = x;
+    try {
+	    document.getElementById("uptime").value = x;
+	}catch(e){}
 }
 
 function updateState(x)
 {
-	document.getElementById("pfstate").value = x;
+    try {
+    	document.getElementById("pfstate").value = x;
+    }catch(e){}
 }
 
 
 /* start ajax helper "thread" if not started */
 if(!ajaxStarted) {
-	window.setTimeout('updateMeters()', update_interval);
-	var ajaxStarted = true;
+    try {
+	    window.setTimeout('updateMeters()', update_interval);
+    	var ajaxStarted = true;
+    }catch(e){}
 }
 

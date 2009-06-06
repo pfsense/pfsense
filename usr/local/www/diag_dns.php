2e87b9d9266f6372c7df00a486831762e7bca34c
jim-p <jim@pingle.org>
2009-06-06 18:23:23 -0400

    Remove the duplicated page header.

--- usr/local/www/diag_dns.php
+++ usr/local/www/diag_dns.php
@@ -75,7 +75,6 @@ if ($_POST) {
 include("head.inc"); ?>
 <body link="#000000" vlink="#000000" alink="#000000">
 <? include("fbegin.inc"); ?>
-<p class="pgtitle"><?=$pgtitle?></p>
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
         <tr>
                 <td>

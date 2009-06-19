39b7cd07b9467b251ac576e2794e20a9751979b9
Scott Ullrich <sullrich@mpb-geekgod.geekgod.com>
2009-06-18 20:27:30 -0400

    Process and remove obsoleted files

--- tmp/post_upgrade_command.php
+++ tmp/post_upgrade_command.php
@@ -16,8 +16,8 @@
 	if(file_exists("/usr/local/www/dfuife.cgi"))
 		unlink("/usr/local/www/dfuife.cgi"); 
 
-	// Needed for major freebsd upgrades (ex: 6.X -> 7.x)
-	//if($g['platform'] == "pfSense")
-	//	touch("/conf/needs_package_sync");
-
+	$files_to_process = split("\n", file_get_contents("/etc/pfSense.obsoletedfiles"));
+	foreach($files_to_process as $filename) 
+		if(file_exists($filename)) 
+			exec("/bin/rm $filename");
 ?>

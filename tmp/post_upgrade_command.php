89f49b8fc583a87fe832ee3349db659891099bdb
Scott Ullrich <sullrich@mpb-geekgod.geekgod.com>
2009-06-18 20:27:10 -0400

    Complete path to rm

--- tmp/post_upgrade_command.php
+++ tmp/post_upgrade_command.php
@@ -22,6 +22,6 @@
 	$files_to_process = split("\n", file_get_contents("/etc/pfSense.obsoletedfiles"));
 	foreach($files_to_process as $filename) 
 		if(file_exists($filename)) 
-			exec("rm $filename");
+			exec("/bin/rm $filename");
 
 ?>

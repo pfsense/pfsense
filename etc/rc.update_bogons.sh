6746f41b04e1fbbbc473febbbc5744087ea68cc6
Chris Buechler <cmb@pfsense.org>
2009-06-24 20:11:44 -0400

    Skip sleep if an argument is specified.

--- etc/rc.update_bogons.sh
+++ etc/rc.update_bogons.sh
@@ -12,7 +12,9 @@ value=`od -A n -d -N2 /dev/random | awk '{ print $1 }'`
 echo "rc.update_bogons.sh is sleeping for $value" | logger
 
 # Sleep for that time.
-sleep $value
+if [ ! $1 ]; then
+    sleep $value
+fi
 
 echo "rc.update_bogons.sh is beginning the update cycle." | logger
 

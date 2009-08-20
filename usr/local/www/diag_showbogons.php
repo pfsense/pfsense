a7fcf2517a98ab8750e15df55fe74152c9fcc7e5
Scott Ullrich <sullrich@pfsense.org>
2009-08-19 23:50:05 -0400

    Add Show Bogons command.

---
+++ usr/local/www/diag_showbogons.php
@@ -0,0 +1,79 @@
+<?php
+/* $Id$ */
+/*
+    diag_showbogons.php
+    Copyright (C) 2009 Scott Ullrich
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
+##|+PRIV
+##|*IDENT=page-diag-showbogons
+##|*NAME=Diagnostics: System Activity
+##|*DESCR=Allows access to the 'Diagnostics: Show Bogons' page
+##|*MATCH=diag_showbogons.php
+##|-PRIV
+
+require("guiconfig.inc");
+
+$bogons = `cat /etc/bogons`;
+
+$pgtitle = "Diagnostics: Show Bogons";
+
+include("head.inc");
+
+include("fbegin.inc"); 
+?>
+<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+<style type="text/css">
+body { font-family: Verdana; font-size: 100%; }
+pre { font-size: 1.15em; }
+</style> 
+<table width="100%" border="0" cellpadding="0" cellspacing="0">  
+  <tr>
+    <td>
+	<table id="backuptable" class="tabcont" align="left" width="100%" border="0" cellpadding="6" cellspacing="0">
+		<tr>
+			<td>
+				<table>
+					<tr>
+						<td>
+<b>Currently loaded bogons table:</b><p/>
+<pre>
+
+<?php echo $bogons; ?>
+</pre>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+	</div>
+    </td>
+  </tr>
+</table>
+</form>
+<?php include("fend.inc"); ?>
+</body>
+</html>

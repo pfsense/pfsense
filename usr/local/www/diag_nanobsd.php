85b23b4b23b1abbf7eadc52b59f6dac5285586d7
Scott Ullrich <sullrich@pfsense.org>
2009-08-23 18:24:55 -0400

    Resurvey slice info after changes.   Resolves #52

--- usr/local/www/diag_nanobsd.php
+++ usr/local/www/diag_nanobsd.php
@@ -50,6 +50,12 @@ $BOOT_DRIVE=trim(`/sbin/glabel list | /usr/bin/grep -B2 ufs/pfsense | /usr/bin/h
 function detect_slice_info() {
 	global $SLICE, $OLDSLICE, $TOFLASH, $COMPLETE_PATH, $COMPLETE_BOOT_PATH;
 	global $GLABEL_SLIZE, $UFS_ID, $OLD_UFS_ID, $BOOTFLASH;
+	global $BOOT_DRIVE, $REAL_BOOT_DEVICE;
+	
+	$BOOT_DEVICE=trim(`/sbin/mount | /usr/bin/grep pfsense | /usr/bin/cut -d'/' -f4 | /usr/bin/cut -d' ' -f1`);
+	$REAL_BOOT_DEVICE=trim(`/sbin/glabel list | /usr/bin/grep -B2 ufs/{$BOOT_DEVICE} | /usr/bin/head -n 1 | /usr/bin/cut -f3 -d' '`);
+	$BOOT_DRIVE=trim(`/sbin/glabel list | /usr/bin/grep -B2 ufs/pfsense | /usr/bin/head -n 1 | /usr/bin/cut -f3 -d' ' | /usr/bin/cut -d's' -f1`);
+
 	// Detect which slice is active and set information.
 	if(strstr($REAL_BOOT_DEVICE, "s1")) {
 		$SLICE="2";

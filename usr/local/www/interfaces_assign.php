5a7d827bfd5a6042ab9802321d7c175b92c37e19
sullrich <sullrich@pfsense.org>
2009-11-26 21:17:51 -0500

    Add ipsec.inc and vpn.inc

--- usr/local/www/interfaces_assign.php
+++ usr/local/www/interfaces_assign.php
@@ -42,9 +42,11 @@
 
 $pgtitle = array("Interfaces", "Assign network ports");
 require("guiconfig.inc");
-require_once("functions.inc");
-require_once("filter.inc");
-require_once("shaper.inc");
+require("functions.inc");
+require("filter.inc");
+require("shaper.inc");
+require("ipsec.inc");
+require("vpn.inc");
 
 /*
 	In this file, "port" refers to the physical port name,

4c7e2f4e1e15cd06a99dc4f400c4c1dccd601062
sullrich <sullrich@pfsense.org>
2009-12-05 19:44:55 -0500

    Add listtopic header

--- usr/local/www/diag_dns.php
+++ usr/local/www/diag_dns.php
@@ -85,6 +85,9 @@ include("head.inc"); ?>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
 	<form action="diag_dns.php" method="post" name="iform" id="iform">
 	  <table width="100%" border="0" cellpadding="6" cellspacing="0">
+		<tr>
+			<td colspan="2" valign="top" class="listtopic">Resolve DNS hostname or IP</td>
+		</tr>
         <tr>
 		  <td width="22%" valign="top" class="vncellreq">Hostname or IP</td>
 		  <td width="78%" class="vtable">

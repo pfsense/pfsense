ffca877988c484499773c393642f17b0203143ad
Chris Buechler <cmb@pfsense.org>
2010-01-01 02:57:15 -0500

    fix text

--- usr/local/www/vpn_openvpn_csc.php
+++ usr/local/www/vpn_openvpn_csc.php
@@ -641,7 +641,7 @@ function netbios_change() {
 								<tr>
 									<td>
 										<textarea rows="6" cols="78" name="custom_options" id="custom_options"><?=$pconfig['custom_options'];?></textarea><br/>
-										Paste any additional options you would like to pass through to the OpenVPN server here seperated by a semicolon<br/>
+										Enter any additional options you would like to add for this client specific override, seperated by a semicolon<br/>
 										EXAMPLE: push "route 10.0.0.0 255.255.255.0";
 									</td>
 								</tr>

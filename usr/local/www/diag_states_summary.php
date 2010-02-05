2a2af6700bf8dc51665ce9d137c614fac8b10cdf
jim-p <jim@pingle.org>
2010-02-05 15:21:32 -0500

    Add license block and auth headers.

--- usr/local/www/diag_states_summary.php
+++ usr/local/www/diag_states_summary.php
@@ -1,4 +1,46 @@
 <?php
+/*
+	diag_states_summary.php
+	Copyright (C) 2010 Jim Pingle
+
+	Portions borrowed from diag_dump_states.php:
+	Copyright (C) 2005-2009 Scott Ullrich
+	Copyright (C) 2005 Colin Smith
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
+	pfSense_BUILDER_BINARIES:	/sbin/pfctl
+	pfSense_MODULE:	filter
+*/
+
+##|+PRIV
+##|*IDENT=page-diagnostics-statessummary
+##|*NAME=Diagnostics: States Summary page
+##|*DESCR=Allow access to the 'Diagnostics: States Summary' page.
+##|*MATCH=diag_states_summary.php*
+##|-PRIV
 
 exec("/sbin/pfctl -s state", $states);
 
@@ -38,13 +80,11 @@ function sort_by_ip($a, $b) {
 	return sprintf("%u", ip2long($a)) < sprintf("%u", ip2long($b)) ? -1 : 1;
 }
 
-$pgtitle = "Diagnostics: State Table Summary";
+$pgtitle = array("Diagnostics", "State Table Summary");
 require_once("guiconfig.inc");
 include("head.inc");
 include("fbegin.inc");
 ?>
-<p class="pgtitle"><?=$pgtitle?></font></p>
-
 <table class="tabcont" width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
 		<td class="listhdrr">IP</td>

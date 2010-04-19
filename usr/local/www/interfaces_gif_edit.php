9460ee11b2c0fde0750902d32898ce233c0c51b7
Chris Buechler <cmb@pfsense.org>
2010-04-19 02:40:56 -0400

    fix text

--- usr/local/www/interfaces_gif_edit.php
+++ usr/local/www/interfaces_gif_edit.php
@@ -75,7 +75,7 @@ if ($_POST) {
 
 	if ((!is_ipaddr($_POST['tunnel-local-addr'])) || (!is_ipaddr($_POST['tunnel-remote-addr'])) ||
 			(!is_ipaddr($_POST['remote-addr']))) {
-		$input_errors[] = "All fildes must have valid ip addresses.";
+		$input_errors[] = "The tunnel local and tunnel remote fields must have valid IP addresses.";
 	}
 
 	foreach ($a_gifs as $gif) {

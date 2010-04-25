a73f50e2bb28d6816f2f42806afa1fe4a7d6b886
Chris Buechler <cmb@pfsense.org>
2010-04-24 22:36:49 -0400

    add note about 1:1s and interface IPs

--- usr/local/www/firewall_nat_1to1.php
+++ usr/local/www/firewall_nat_1to1.php
@@ -158,7 +158,9 @@ include("head.inc");
 		<tr>
 		  <td colspan="4">
 		        <p><span class="vexpl"><span class="red"><strong>Note:<br>
-                      </strong></span>Depending on the way your WAN connection is setup, you may also need a <a href="firewall_virtual_ip.php">Virtual IP</a>.</span></p>
+                      </strong></span>Depending on the way your WAN connection is setup, you may also need a <a href="firewall_virtual_ip.php">Virtual IP</a>.<br/>
+			If you add a 1:1 NAT entry for any of the interface IPs on this system, it will make this system inaccessible on that IP address. i.e. if
+			you use your WAN IP address, any services on this system (IPsec, OpenVPN server, etc.) using the WAN IP address will no longer function.</span></p>
 		  </td>
 		<tr>
               </table>

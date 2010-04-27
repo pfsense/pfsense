267c28abea59e41b86e4956ae6a0a3f92365f3ba
Vinicius Coque <vinicius.coque@bluepex.com>
2010-04-27 10:07:07 -0300

    Adding gettext() to save button

--- usr/local/www/system_advanced_misc.php
+++ usr/local/www/system_advanced_misc.php
@@ -283,7 +283,7 @@ include("head.inc");
 							<tr>
 								<td width="22%" valign="top">&nbsp;</td>
 								<td width="78%">
-									<input name="Submit" type="submit" class="formbtn" value="Save" />
+									<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>" />
 								</td>
 							</tr>
 						</table>
--- usr/local/www/system_advanced_network.php
+++ usr/local/www/system_advanced_network.php
@@ -272,7 +272,7 @@ function enable_change(enable_over) {
 							</tr>							
 							<tr>
 								<td width="22%" valign="top">&nbsp;</td>
-								<td width="78%"><input name="Submit" type="submit" class="formbtn" value="Save" /></td>
+								<td width="78%"><input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>" /></td>
 							</tr>
 						</table>
 					</div>

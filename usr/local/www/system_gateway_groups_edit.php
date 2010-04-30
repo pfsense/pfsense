0307f35126ee87651f213624c4d6c6e302863e52
Ermal <eri@pfsense.org>
2010-04-30 19:47:23 +0000

    Show descriptive name not the internal used one.

--- usr/local/www/system_gateway_groups_edit.php
+++ usr/local/www/system_gateway_groups_edit.php
@@ -179,7 +179,7 @@ include("head.inc");
 				echo "<option value='3' $selected[3] >Tier 3</option>";
 				echo "<option value='4' $selected[4] >Tier 4</option>";
 				echo "<option value='5' $selected[5] >Tier 5</option>";
-				echo "</select> <strong>{$name} - {$gateway['descr']}</strong><br />";
+				echo "</select> <strong>{$gateway['name']} - {$gateway['descr']}</strong><br />";
 		 	}
 		?>
 			<br/><span class="vexpl">

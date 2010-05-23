3d99ee74d0d3df3e3bc72e2bbda413d50c5c19ef
Scott Ullrich <sullrich@pfsense.org>
2010-05-23 00:03:20 -0400

    Customize for both PC-BSD and pfSense

--- PCBSD/pc-sysinstall/backend-query/test-live.sh
+++ PCBSD/pc-sysinstall/backend-query/test-live.sh
@@ -2,13 +2,23 @@
 # Script which checks if we are running from install media, or real system
 #############################################################################
 
-mount | grep "/dev/iso9660/pfSense" >/dev/null 2>/dev/null
+# Test for PC-BSD Style mount
+dmesg | grep "md0: Preloaded image" >/dev/null 2>/dev/null
 if [ "$?" = "0" ]
 then
   echo "INSTALL-MEDIA"
   exit 0
-else
-  echo "REAL-DISK"
-  exit 1
 fi
+
+# Test for pfSense LIVECD
+mount | grep "/dev/iso9660/pfSense" >/dev/null 2>/dev/null
+if [ "$?" = "0" ]
+then
+  echo "pfSense-LiveCD/Installer"
+  exit 0
+fi
+
+echo "REAL-DISK"
+exit 1
+
 

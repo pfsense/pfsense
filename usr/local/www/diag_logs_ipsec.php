229f59f1c2c37ee6389f6d7832e19bdab6569664
Rafael Lucas <rafalucas.unicamp@gmail.com>
2010-06-18 15:27:28 -0300

    Implement gettext() calls

--- usr/local/www/diag_logs_ipsec.php
+++ usr/local/www/diag_logs_ipsec.php
@@ -104,7 +104,7 @@ include("head.inc");
 			<div id="mainarea">
 			<table class="tabcont" width="100%" border="0" cellspacing="0" cellpadding="0">
 		  		<tr>
-					<td colspan="2" class="listtopic"><?=gettext("Last ");?><?=$nentries;?><?=gettext("IPsec log entries");?></td>
+					<td colspan="2" class="listtopic"><?=printf(gettext("Last %s  IPsec log entries"),$nentries);?></td>
 		  		</tr>
 				<?php
 				foreach($ipsec_logarr as $logent){

eb56e9c3247d70b423484248c26604fbb4176453
Vinicius Coque <vinicius.coque@bluepex.com>
2010-06-29 19:57:32 -0300

    Revert "Modified sintaxes on diag_dns.php and diag_dump_states.php"
    This reverts commit e4632c0b7631d04dd0eb520d98b9ac029d47b5fe.
    
    Conflicts:
    
    	usr/local/www/diag_dns.php

--- usr/local/www/diag_dns.php
+++ usr/local/www/diag_dns.php
@@ -155,7 +155,7 @@ include("head.inc"); ?>
 				<p/>
 				<?=gettext("NOTE: The following links are to external services, so their reliability cannot be guaranteed.");?><br/><br/>
 				<a target="_new" href="http://private.dnsstuff.com/tools/whois.ch?ip=<?php echo $ipaddr; ?>"><?=gettext("IP WHOIS @ DNS Stuff");?></a><br />
-				<a target="_new" href="http://private.dnsstuff.com/tools/ipall.ch?ip=<?php echo $ipaddr; ?>"><?=gettext("IP Info @ DNS Stuff");?></a>
+				<a target="_new" href="http://private.dnsstuff.com/tools/ipall.ch?ip=<?php echo $ipaddr; ?>"><?=gettext("IP Info @ DNS Stuff);?></a>
 			</td>
 		</tr>
 		<?php } ?>

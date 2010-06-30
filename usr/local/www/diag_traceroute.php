42f9d20f1eb4c105b9bf20fdc94896f61a7212d1
Vinicius Coque <vinicius.coque@bluepex.com>
2010-06-29 21:18:40 -0300

    Fixing string syntax

--- usr/local/www/diag_traceroute.php
+++ usr/local/www/diag_traceroute.php
@@ -114,7 +114,7 @@ if (!isset($do_traceroute)) {
 				</tr>
 				<tr>
 				<td valign="top" colspan="2">
-				<p><span class="vexpl"><span class="red"><b><?=gettext("Note");?>:</b></span><?=gettext("Traceroute may take a while to complete.".      "You may hit the Stop button on your browser at any time to see the progress of failed traceroutes.");?><p>
+				<p><span class="vexpl"><span class="red"><b><?=gettext("Note");?>:</b></span><?=gettext("Traceroute may take a while to complete. You may hit the Stop button on your browser at any time to see the progress of failed traceroutes.");?><p>
 				<? if ($do_traceroute) {
 					echo "<font face='terminal' size='2'>";
 					echo("<br><strong>Traceroute output:</strong><br>");

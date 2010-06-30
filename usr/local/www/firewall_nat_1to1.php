cb42b4f795f242e9aa18951f651b878536dc4442
Renato Botelho <renato.botelho@bluepex.com>
2010-06-30 20:34:53 -0300

    Review firewall_nat_1to1.php

--- usr/local/www/firewall_nat_1to1.php
+++ usr/local/www/firewall_nat_1to1.php
@@ -84,13 +84,13 @@ include("head.inc");
 <form action="firewall_nat_1to1.php" method="post">
 <?php if ($savemsg) print_info_box($savemsg); ?>
 <?php if (is_subsystem_dirty('natconf')): ?><p>
-<?php print_info_box_np(gettext("The NAT configuration has been changed.<br>You must apply the changes in order for them to take effect."));?><br>
+<?php print_info_box_np(gettext("The NAT configuration has been changed") . ".<br>" . gettext("You must apply the changes in order for them to take effect."));?><br>
 <?php endif; ?>
 <table width="100%" border="0" cellpadding="0" cellspacing="0">  <tr><td>
 <?php
 	$tab_array = array();
 	$tab_array[] = array(gettext("Port Forward"), false, "firewall_nat.php");
-	$tab_array[] = array("1:1", true, "firewall_nat_1to1.php");
+	$tab_array[] = array(gettext("1:1"), true, "firewall_nat_1to1.php");
 	$tab_array[] = array(gettext("Outbound"), false, "firewall_nat_out.php");
 	display_top_tabs($tab_array);
 ?>
@@ -100,7 +100,7 @@ include("head.inc");
 	<div id="mainarea">
               <table class="tabcont" width="100%" border="0" cellpadding="0" cellspacing="0">
                 <tr>
-		  <td width="10%" class="listhdrr">Interface</td>
+		  <td width="10%" class="listhdrr"><?=gettext("Interface"); ?></td>
                   <td width="20%" class="listhdrr"><?=gettext("External IP"); ?></td>
                   <td width="20%" class="listhdrr"><?=gettext("Internal IP"); ?></td>
                   <td width="40%" class="listhdr"><?=gettext("Description"); ?></td>
@@ -138,7 +138,7 @@ include("head.inc");
                     <table border="0" cellspacing="0" cellpadding="1">
                       <tr>
                         <td valign="middle"><a href="firewall_nat_1to1_edit.php?id=<?=$i;?>"><img src="/themes/<?= $g['theme']; ?>/images/icons/icon_e.gif" width="17" height="17" border="0" title="<?=gettext("edit rule"); ?>"></a></td>
-                        <td valign="middle"><a href="firewall_nat_1to1.php?act=del&id=<?=$i;?>" onclick="return confirm(gettext('Do you really want to delete this mapping?'))"><img src="/themes/<?= $g['theme']; ?>/images/icons/icon_x.gif" width="17" height="17" border="0" title="<?=gettext("delete rule"); ?>"></a></td>
+			<td valign="middle"><a href="firewall_nat_1to1.php?act=del&id=<?=$i;?>" onclick="return confirm(<?=gettext('Do you really want to delete this mapping?');?>)"><img src="/themes/<?= $g['theme']; ?>/images/icons/icon_x.gif" width="17" height="17" border="0" title="<?=gettext("delete rule"); ?>"></a></td>
                       </tr>
                     </table>
                   </td>

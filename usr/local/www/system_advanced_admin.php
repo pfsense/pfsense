86b21903616c360707aaf7cc31868eb2a22e238d
jim-p <jimp@pfsense.org>
2010-07-06 17:03:44 -0400

    Add a text box where someone can enter in alternate hostnames for the system to bypass the DNS rebind checks.

--- usr/local/www/system_advanced_admin.php
+++ usr/local/www/system_advanced_admin.php
@@ -56,6 +56,7 @@ $pconfig['disablehttpredirect'] = isset($config['system']['disablehttpredirect']
 $pconfig['disableconsolemenu'] = isset($config['system']['disableconsolemenu']);
 $pconfig['noantilockout'] = isset($config['system']['webgui']['noantilockout']);
 $pconfig['nodnsrebindcheck'] = isset($config['system']['webgui']['nodnsrebindcheck']);
+$pconfig['althostnames'] = $config['system']['webgui']['althostnames'];
 $pconfig['enableserial'] = $config['system']['enableserial'];
 $pconfig['enablesshd'] = $config['system']['enablesshd'];
 $pconfig['sshport'] = $config['system']['ssh']['port'];
@@ -79,6 +80,13 @@ if ($_POST) {
 	if ($_POST['webguiport'])
 		if(!is_port($_POST['webguiport']))
 			$input_errors[] = gettext("You must specify a valid webConfigurator port number");
+
+	if ($_POST['althostnames']) {
+		$althosts = explode(" ", $_POST['althostnames']);
+		foreach ($althosts as $ah)
+			if (!is_hostname($ah))
+				$input_errors[] = gettext("Alternate hostname " . htmlspecialchars($ah) . " is not a valid hostname.");
+	}
 
 	if ($_POST['sshport'])
 		if(!is_port($_POST['sshport']))
@@ -131,6 +139,11 @@ if ($_POST) {
 			$config['system']['webgui']['nodnsrebindcheck'] = true;
 		else
 			unset($config['system']['webgui']['nodnsrebindcheck']);
+
+		if ($_POST['althostnames'])
+			$config['system']['webgui']['althostnames'] = $_POST['althostnames'];
+		else
+			unset($config['system']['webgui']['althostnames']);
 
 		$sshd_enabled = $config['system']['enablesshd'];
 		if($_POST['enablesshd'])
@@ -331,6 +344,17 @@ function prot_change() {
 									"is protected against <a href=\"http://en.wikipedia.org/wiki/DNS_rebinding\">DNS Rebinding attacks</a>. " .
 									"Check this box to disable this protection if you find that it interferes with " .
 									"webConfigurator access in certain corner cases. "); ?>
+								</td>
+							</tr>
+							<tr>
+								<td width="22%" valign="top" class="vncell"><?=gettext("Alternate Hostnames"); ?></td>
+								<td width="78%" class="vtable">
+									<input name="althostnames" type="text" class="formfld unknown" id="althostnames" size="75" value="<?=htmlspecialchars($pconfig['althostnames']);?>"/>
+									<br/>
+									<strong><?=gettext("Alternate Hostnames for DNS Rebinding Checks"); ?></strong>
+									<br/>
+									<?php echo gettext("Here you can specify alternate hostnames by which the router may be queried, to " . 
+									"bypass the DNS Rebinding Attack checks. Separate hostnames with spaces."); ?>
 								</td>
 							</tr>
 							<tr>

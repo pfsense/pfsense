fe4fc20bf06e032daf7993714da85a415f9eb51f
Renato Botelho <renato.botelho@bluepex.com>
2010-07-07 10:32:24 -0300

    Show $g['product_name'] on wizards

--- usr/local/www/wizard.php
+++ usr/local/www/wizard.php
@@ -36,6 +36,7 @@
 ##|-PRIV
 
 
+require("globals.inc");
 require("guiconfig.inc");
 require("functions.inc");
 require("filter.inc");
@@ -46,6 +47,8 @@ function gentitle_pkg($pgname) {
 	global $config;
 	return $config['system']['hostname'] . "." . $config['system']['domain'] . " - " . $pgname;
 }
+
+global $g;
 
 $stepid = htmlspecialchars($_GET['stepid']);
 if (isset($_POST['stepid']))
@@ -75,9 +78,9 @@ if (!is_array($pkg)) {
 	die;
 }
 
-$title          = $pkg['step'][$stepid]['title'];
-$description    = $pkg['step'][$stepid]['description'];
-$totalsteps     = $pkg['totalsteps'];
+$title       = preg_replace("/pfSense/i", $g['product_name'], $pkg['step'][$stepid]['title']);
+$description = preg_replace("/pfSense/i", $g['product_name'], $pkg['step'][$stepid]['description']);
+$totalsteps  = $pkg['totalsteps'];
 
 if ($pkg['includefile'])
         require_once($pkg['includefile']);
@@ -118,9 +121,6 @@ if ($_POST) {
     if($stepid > $totalsteps)
 	$stepid = $totalsteps;
 }
-
-$title          = $pkg['step'][$stepid]['title'];
-$description    = $pkg['step'][$stepid]['description'];
 
 function update_config_field($field, $updatetext, $unset, $arraynum, $field_type) {
 	global $config;

0651f092405d773060d1cefd1050d0d152f0572a
Carlos Eduardo Ramos <carlos.ramos@bluepex.com>
2010-07-08 13:09:13 -0300

    Corrections gettext implementation on diag_routes.php

--- usr/local/www/diag_routes.php
+++ usr/local/www/diag_routes.php
@@ -66,7 +66,7 @@ include('head.inc');
 <tr>
 <td class="vncellreq" width="22%">&nbsp;</td>
 <td class="vtable" width="78%">
-<input type="submit" class="formbtn" name="submit" value="Show" />
+<input type="submit" class="formbtn" name="submit" value="<?=gettext("Show"); ?>" />
 <br />
 <br />
 <span class="vexpl"><span class="red"><strong><?=gettext("Note")?>:</strong></span><?=gettext("By enabling name resolution, the query should take a bit longer. You can stop it at"."any time by clicking the Stop button in your browser");?>.</span>

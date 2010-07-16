902f86b9b91d5ed6ebad6d2e74f94725d42e5f1f
Carlos Eduardo Ramos <carlos.ramos@bluepex.com>
2010-07-16 15:48:39 -0300

    Corrections gettext implementation on load_balancer_pool.php

--- usr/local/www/load_balancer_pool.php
+++ usr/local/www/load_balancer_pool.php
@@ -108,7 +108,7 @@ include("head.inc");
 <?php if ($input_errors) print_input_errors($input_errors); ?>
 <?php if ($savemsg) print_info_box($savemsg); ?>
 <?php if (is_subsystem_dirty('loadbalancer')): ?><p>
-<?php print_info_box_np(gettext("The load balancer configuration has been changed") ".<br>" . gettext("You must apply the changes in order for them to take effect."));?><br>
+<?php print_info_box_np(sprintf(gettext("The load balancer configuration has been changed%sYou must apply the changes in order for them to take effect."), "<br>"));?><br>
 <?php endif; ?>
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
   <tr><td class="tabnavtbl">

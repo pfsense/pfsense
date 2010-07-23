c8927bfba3e893a82b9657b620c0cd4d5d4acaa7
Rafael Lucas <rafalucas.unicamp@gmail.com>
2010-07-23 13:35:39 -0300

    Corrections gettext calls on status_interfaces.php

--- usr/local/www/status_interfaces.php
+++ usr/local/www/status_interfaces.php
@@ -94,10 +94,10 @@ include("head.inc");
 			<?=htmlspecialchars($ifinfo['dhcplink']);?>&nbsp;&nbsp;
 			<?php if ($ifinfo['dhcplink'] == "up"): ?>
 				<a href="status_interfaces.php?action=Release&if=<?php echo $ifdescr; ?>">
-				<input type="button" name="<?php echo $ifdescr; ?>" value="Release" class="formbtns">
+				<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Release");?>" class="formbtns">
 			<?php else: ?>
 				<a href="status_interfaces.php?action=Renew&if=<?php echo $ifdescr; ?>">
-				<input type="button" name="<?php echo $ifdescr; ?>" value="Renew" class="formbtns">
+				<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Renew");?>" class="formbtns">
 			<?php endif; ?>
 			</a>
 		</td>
@@ -109,10 +109,10 @@ include("head.inc");
 			<?=htmlspecialchars($ifinfo['pppoelink']);?>&nbsp;&nbsp;
 			<?php if ($ifinfo['pppoelink'] == "up"): ?>
 				<a href="status_interfaces.php?action=Disconnect&if=<?php echo $ifdescr; ?>">
-				<input type="button" name="<?php echo $ifdescr; ?>" value="Disconnect" class="formbtns">
+				<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Disconnect");?>" class="formbtns">
 			<?php else: ?>
 				<a href="status_interfaces.php?action=Connect&if=<?php echo $ifdescr; ?>">
-				<input type="button" name="<?php echo $ifdescr; ?>" value="Connect" class="formbtns">
+				<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Connect");?>" class="formbtns">
 				<?php endif; ?>
 			</a>
 		</td>
@@ -124,10 +124,10 @@ include("head.inc");
 			<?=htmlspecialchars($ifinfo['pptplink']);?>&nbsp;&nbsp;
 			<?php if ($ifinfo['pptplink'] == "up"): ?>
 				<a href="status_interfaces.php?action=Disconnect&if=<?php echo $ifdescr; ?>">
-				<input type="button" name="<?php echo $ifdescr; ?>" value="Disconnect" class="formbtns">
+				<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Disconnect");?>" class="formbtns">
 			<?php else: ?>
 				<a href="status_interfaces.php?action=Connect&if=<?php echo $ifdescr; ?>">
-				<input type="button" name="<?php echo $ifdescr; ?>" value="Connect" class="formbtns">
+				<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Connect");?>" class="formbtns">
 			<?php endif; ?>
 			</a>
 		</td>
@@ -139,11 +139,11 @@ include("head.inc");
 			<?=htmlspecialchars($ifinfo['pppinfo']);?>
 			<?php if ($ifinfo['ppplink'] == "up"): ?>
 				<a href="status_interfaces.php?action=Disconnect&if=<?php echo $ifdescr; ?>">
-				<input type="button" name="<?php echo $ifdescr; ?>" value="Disconnect" class="formbtns">
+				<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Disconnect");?>" class="formbtns">
 			<?php else: ?>
 				<?php if (!$ifinfo['nodevice']): ?>
 					<a href="status_interfaces.php?action=Connect&if=<?php echo $ifdescr; ?>">
-					<input type="button" name="<?php echo $ifdescr; ?>" value="Connect" class="formbtns">
+					<input type="button" name="<?php echo $ifdescr; ?>" value="<?=gettext("Connect");?>" class="formbtns">
 				<?php endif; ?>
 			<?php endif; ?>
 			</a>
@@ -151,14 +151,14 @@ include("head.inc");
 	</tr>
 	<?php  endif; if ($ifinfo['ppp_uptime'] || $ifinfo['ppp_uptime_accumulated']): ?>
 	<tr>
-		<td width="22%" class="vncellt">Uptime <?php if ($ifinfo['ppp_uptime_accumulated']) echo "(historical)"; ?></td>
+		<td width="22%" class="vncellt"><?=gettext("Uptime ");?><?php if ($ifinfo['ppp_uptime_accumulated']) echo "(historical)"; ?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['ppp_uptime']);?> <?=htmlspecialchars($ifinfo['ppp_uptime_accumulated']);?>
 		</td>
         </tr>
 	<?php  endif; if ($ifinfo['macaddr']): ?>
 	<tr>
-		<td width="22%" class="vncellt">MAC address</td>
+		<td width="22%" class="vncellt"><?=gettext("MAC address");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['macaddr']);?>
 		</td>
@@ -167,7 +167,7 @@ include("head.inc");
 	<?php if ($ifinfo['dhcplink'] != "down" && $ifinfo['pppoelink'] != "down" && $ifinfo['pptplink'] != "down"): ?>
 	<?php if ($ifinfo['ipaddr']): ?>
 	<tr>
-		<td width="22%" class="vncellt">IP address</td>
+		<td width="22%" class="vncellt"><?=gettext("IP address");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['ipaddr']);?>
 			&nbsp; 
@@ -175,14 +175,14 @@ include("head.inc");
 	</tr>
 	<?php endif; ?><?php if ($ifinfo['subnet']): ?>
 	<tr>
-		<td width="22%" class="vncellt">Subnet mask</td>
+		<td width="22%" class="vncellt"><?=gettext("Subnet mask");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['subnet']);?>
 		</td>
 	</tr>
 	<?php endif; ?><?php if ($ifinfo['gateway']): ?>
 	<tr>
-		<td width="22%" class="vncellt">Gateway</td>
+		<td width="22%" class="vncellt"><?=gettext("Gateway");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($config['interfaces'][$ifdescr]['gateway']);?>
 			<?=htmlspecialchars($ifinfo['gateway']);?>
@@ -190,7 +190,7 @@ include("head.inc");
 	</tr>
 	<?php endif; if ($ifdescr == "wan" && file_exists("{$g['varetc_path']}/resolv.conf")): ?>
 	<tr>
-	<td width="22%" class="vncellt">ISP DNS servers</td>
+	<td width="22%" class="vncellt"><?=gettext("ISP DNS servers");?></td>
 	<td width="78%" class="listr">
 		<?php
 			$dns_servers = get_dns_servers();
@@ -202,49 +202,49 @@ include("head.inc");
 	</tr>
 	<?php endif; endif; if ($ifinfo['media']): ?>
 	<tr>
-		<td width="22%" class="vncellt">Media</td>
+		<td width="22%" class="vncellt"><?=gettext("Media");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['media']);?>
 		</td>
 	</tr>
 <?php endif; ?><?php if ($ifinfo['channel']): ?>
 	<tr>
-		<td width="22%" class="vncellt">Channel</td>
+		<td width="22%" class="vncellt"><?=gettext("Channel");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['channel']);?>
 		</td>
 	</tr>
 <?php endif; ?><?php if ($ifinfo['ssid']): ?>
 	<tr>
-		<td width="22%" class="vncellt">SSID</td>
+		<td width="22%" class="vncellt"><?=gettext("SSID");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['ssid']);?>
 		</td>
 	</tr>
 <?php endif; ?><?php if ($ifinfo['bssid']): ?>
 	<tr>
-		<td width="22%" class="vncellt">BSSID</td>
+		<td width="22%" class="vncellt"><?=gettext("BSSID");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['bssid']);?>
 		</td>
 	</tr>
 <?php endif; ?><?php if ($ifinfo['rate']): ?>
 	<tr>
-		<td width="22%" class="vncellt">Rate</td>
+		<td width="22%" class="vncellt"><?=gettext("Rate");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['rate']);?>
 		</td>
 	</tr>
 <?php endif; ?><?php if ($ifinfo['rssi']): ?>
 	<tr>
-		<td width="22%" class="vncellt">RSSI</td>
+		<td width="22%" class="vncellt"><?=gettext("RSSI");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['rssi']);?>
 		</td>
 	</tr>
 <?php endif; ?>
 	<tr>
-		<td width="22%" class="vncellt">In/out packets</td>
+		<td width="22%" class="vncellt"><?=gettext("In/out packets");?></td>
 		<td width="78%" class="listr">
 		<?php
 			echo htmlspecialchars($ifinfo['inpkts'] . "/" . $ifinfo['outpkts'] . " (");
@@ -253,7 +253,7 @@ include("head.inc");
 		</td>
 	</tr>
 	<tr>
-		<td width="22%" class="vncellt">In/out packets (pass)</td>
+		<td width="22%" class="vncellt"><?=gettext("In/out packets (pass)");?></td>
 		<td width="78%" class="listr">
 			<?php
 				echo htmlspecialchars($ifinfo['inpktspass'] . "/" . $ifinfo['outpktspass'] . " (");
@@ -262,7 +262,7 @@ include("head.inc");
 		</td>
 	</tr>
 	<tr>
-		<td width="22%" class="vncellt">In/out packets (block)</td>
+		<td width="22%" class="vncellt"><?=gettext("In/out packets (block)");?></td>
 		<td width="78%" class="listr">
 			<?php
 				echo htmlspecialchars($ifinfo['inpktsblock'] . "/" . $ifinfo['outpktsblock'] . " (");
@@ -272,7 +272,7 @@ include("head.inc");
 	</tr>
 <?php if (isset($ifinfo['inerrs'])): ?>
 	<tr>
-		<td width="22%" class="vncellt">In/out errors</td>
+		<td width="22%" class="vncellt"><?=gettext("In/out errors");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['inerrs'] . "/" . $ifinfo['outerrs']);?>
 		</td>
@@ -280,7 +280,7 @@ include("head.inc");
 <?php endif; ?>
 <?php if (isset($ifinfo['collisions'])): ?>
 	<tr>
-		<td width="22%" class="vncellt">Collisions</td>
+		<td width="22%" class="vncellt"><?=gettext("Collisions");?></td>
 		<td width="78%" class="listr">
 			<?=htmlspecialchars($ifinfo['collisions']);?>
 		</td>
@@ -289,7 +289,7 @@ include("head.inc");
 <?php endif; ?>
 <?php if ($ifinfo['bridge']): ?>
 	<tr>
-		<td width="22%" class="vncellt">Bridge (<?=$ifinfo['bridgeint']?>)</td>
+		<td width="22%" class="vncellt"><?php printf(gettext("Bridge (%s)"),$ifinfo['bridgeint']);?></td>
 		<td width="78%" class="listr">
 			<?=$ifinfo['bridge'];?>
 		</td>
@@ -311,7 +311,7 @@ include("head.inc");
 ?>
 <?php if($interrupt_total): ?>
 	<tr>
-		<td width="22%" class="vncellt">Interrupts/Second</td>
+		<td width="22%" class="vncellt"><?=gettext("Interrupts/Second");?></td>
 		<td width="78%" class="listr">
 			<?php
 				echo $interrupt_total . " total";
@@ -327,10 +327,10 @@ include("head.inc");
 
 <br/>
 
-</strong>Using dial-on-demand will bring the connection up again if any packet
-triggers it. To substantiate this point: disconnecting manually
-will <strong>not</strong> prevent dial-on-demand from making connections
-to the outside! Don't use dial-on-demand if you want to make sure that the line
-is kept disconnected.
+</strong><?php printf(gettext("Using dial-on-demand will bring the connection up again if any packet ".
+"triggers it. To substantiate this point: disconnecting manually ".
+"will %snot%s prevent dial-on-demand from making connections ".
+"to the outside! Don't use dial-on-demand if you want to make sure that the line ".
+"is kept disconnected."),'<strong>','</strong>')?>
 
 <?php include("fend.inc"); ?>

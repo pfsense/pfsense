a4a793c184f8169d9451b053ead9704794cbeb3c
Scott Ullrich <sullrich@pfsense.org>
2010-07-30 21:42:08 -0400

    Handle cases when source or destination is not enabled and you end up with an offset error

--- usr/local/www/firewall_aliases.php
+++ usr/local/www/firewall_aliases.php
@@ -87,6 +87,7 @@ if ($_GET['act'] == "del") {
 		if($is_alias_referenced == false) {
 			if(is_array($config['filter']['rule'])) {
 				foreach($config['filter']['rule'] as $rule) {
+					if($rule['source']) {
 					if($rule['source']['address'] && $rule['source']['address'] == $alias_name) {
 						$is_alias_referenced = true;
 						$referenced_by = $rule['descr'];
@@ -102,6 +103,8 @@ if ($_GET['act'] == "del") {
 						$referenced_by = $rule['descr'];
 						break;
 					}
+					}
+					if($rule['destination'])
 					if($rule['destination']['port'] && $rule['destination']['port'] == $alias_name) {
 						$is_alias_referenced = true;
 						$referenced_by = $rule['descr'];

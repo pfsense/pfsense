bf978a0651b0bc7300ded0e94c0245e0948d607d
Carlos Eduardo Ramos <carlos.ramos@bluepex.com>
2010-08-10 14:15:50 -0300

    Removed duplicated tabs block on status_rrd_graph_settings.php

--- usr/local/www/status_rrd_graph_settings.php
+++ usr/local/www/status_rrd_graph_settings.php
@@ -111,32 +111,6 @@ include("head.inc");
         <tr>
                 <td>
 			<?php
-                                $tab_array = array();
-                                if($curcat == "system") { $tabactive = True; } else { $tabactive = False; }
-                                $tab_array[] = array(gettext("System"), $tabactive, "status_rrd_graph.php?cat=system");
-                                if($curcat == "traffic") { $tabactive = True; } else { $tabactive = False; }
-                                $tab_array[] = array(gettext("Traffic"), $tabactive, "status_rrd_graph.php?cat=traffic");
-                                if($curcat == "packets") { $tabactive = True; } else { $tabactive = False; }
-                                $tab_array[] = array(gettext("Packets"), $tabactive, "status_rrd_graph.php?cat=packets");
-                                if($curcat == "quality") { $tabactive = True; } else { $tabactive = False; }
-                                $tab_array[] = array(gettext("Quality"), $tabactive, "status_rrd_graph.php?cat=quality");
-				if($queues) {
-	                                if($curcat == "queues") { $tabactive = True; } else { $tabactive = False; }
-					$tab_array[] = array(gettext("Queues"), $tabactive, "status_rrd_graph.php?cat=queues");
-					if($curcat == "queuedrops") { $tabactive = True; } else { $tabactive = False; }
-					$tab_array[] = array(gettext("QueueDrops"), $tabactive, "status_rrd_graph.php?cat=queuedrops");
-				}
-				if($wireless) {
-	                                if($curcat == "wireless") { $tabactive = True; } else { $tabactive = False; }
-					$tab_array[] = array(gettext("Wireless"), $tabactive, "status_rrd_graph.php?cat=wireless");
-				}
-				if($cellular) {
-					if($curcat == "cellular") { $tabactive = True; } else { $tabactive = False; }
-					$tab_array[] = array(gettext("Cellular"), $tabactive, "status_rrd_graph.php?cat=cellular");
-				}
-                                if($curcat == "settings") { $tabactive = True; } else { $tabactive = False; }
-                                $tab_array[] = array(gettext("Settings"), $tabactive, "status_rrd_graph_settings.php");
-                                display_top_tabs($tab_array);
 				$tab_array = array();
 				if($curcat == "system") { $tabactive = True; } else { $tabactive = False; }
 				$tab_array[] = array(gettext("System"), $tabactive, "status_rrd_graph.php?cat=system");

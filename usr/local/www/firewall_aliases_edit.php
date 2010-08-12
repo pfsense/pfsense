bcc8d8a33235fb39b915ebe40f4b69b6337639d3
Erik Fonnesbeck <efonnes@gmail.com>
2010-08-11 23:35:47 -0600

    Simplify sprintf

--- usr/local/www/firewall_aliases_edit.php
+++ usr/local/www/firewall_aliases_edit.php
@@ -279,7 +279,7 @@ if ($_POST) {
 			}
 		}
 		if ($wrongaliases <> "")
-			$input_errors[] = sprintf(gettext('The alias(es): %1$s %2$scannot be nested because they are not of the same type.'), $wrongaliases, " \n");
+			$input_errors[] = sprintf(gettext('The alias(es): %s cannot be nested because they are not of the same type.'), $wrongaliases);
 	}
 
 	if (!$input_errors) {

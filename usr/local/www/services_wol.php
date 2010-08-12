f5abb47d9703cb5894c65150ccac8656e06ea678
Erik Fonnesbeck <efonnes@gmail.com>
2010-08-11 23:54:45 -0600

    Fix link in error message for wake on lan.

--- usr/local/www/services_wol.php
+++ usr/local/www/services_wol.php
@@ -61,7 +61,7 @@ if($_GET['wakeall'] <> "") {
 			$savemsg .= sprintf(gettext('Sent magic packet to %1$s (%2$s)%3$s'),$mac, $description, ".<br>");
 		}
 		else {
-			$savemsg .= sprintf(gettext('Please check the %1$ssystem log%2$s, the wol command for %3$s (%4$s) did not complete successfully%5$s'),'<a href=\"/diag_logs.php\">','</a>',$description,$mac,".<br>");
+			$savemsg .= sprintf(gettext('Please check the %1$ssystem log%2$s, the wol command for %3$s (%4$s) did not complete successfully%5$s'),'<a href="/diag_logs.php">','</a>',$description,$mac,".<br>");
 		}
 	}
 }
@@ -96,7 +96,7 @@ if ($_POST || $_GET['mac']) {
 			$savemsg .= sprintf(gettext("Sent magic packet to %s."),$mac);
 		}
 		else {
-			$savemsg .= sprintf(gettext('Please check the %1$ssystem log%2$s, the wol command for %3$s did not complete successfully%4$s'),'<a href=\"/diag_logs.php\">', '</a>', $mac, ".<br>");
+			$savemsg .= sprintf(gettext('Please check the %1$ssystem log%2$s, the wol command for %3$s did not complete successfully%4$s'),'<a href="/diag_logs.php">', '</a>', $mac, ".<br>");
 		}
 	}
 }

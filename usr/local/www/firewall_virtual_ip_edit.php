f4e8db1f8255d892cbe4ab42b4e56be2303ba07b
Ermal <eri@pfsense.org>
2010-09-07 20:37:43 +0000

    Do the check regarding 'none' configured interfaces properly. This fixes breakage on VIP configuration. Reported-by: http://forum.pfsense.org/index.php/topic,28120.0.html

--- usr/local/www/firewall_virtual_ip_edit.php
+++ usr/local/www/firewall_virtual_ip_edit.php
@@ -110,7 +110,7 @@ if ($_POST) {
 
 	$natiflist = get_configured_interface_with_descr();
 	foreach ($natiflist as $natif => $natdescr) {
-		if (empty($config['interfaces'][$natif]['ipaddr']))
+		if ($_POST['interface'] == $natif && empty($config['interfaces'][$natif]['ipaddr']))
 			$input_errors[] = gettext("The interface choosen for the VIP has no ip configured so it cannot be used as a parent for the VIP.");
 		if ($_POST['subnet'] == get_interface_ip($natif))
 			$input_errors[] = sprintf(gettext("The %s IP address may not be used in a virtual entry."),$natdescr);

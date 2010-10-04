e9b0bcf765bd8238d45ec74461c66b13f98709b0
Ermal <eri@pfsense.org>
2010-10-04 13:26:59 +0000

    Ticket #931. Check that directory exist before trying to tar up. Do this for rrd and dhcpleases scripts used during shutdown.

--- etc/rc.backup_rrd.sh
+++ etc/rc.backup_rrd.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Save the rrd databases to the config path.
-/etc/rc.conf_mount_rw
-cd / && tar -czf /cf/conf/rrd.tgz -C / var/db/rrd/*.rrd
-/etc/rc.conf_mount_ro
+if [ -d "/var/db/rrd" ]; then
+	/etc/rc.conf_mount_rw
+	cd / && tar -czf /cf/conf/rrd.tgz -C / var/db/rrd/*.rrd
+	/etc/rc.conf_mount_ro
+fi

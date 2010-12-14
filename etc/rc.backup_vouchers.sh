d12003c99517c25105673c557aebec7e3fa55dcb
jim-p <jimp@pfsense.org>
2010-12-14 17:22:17 -0500

    Revert "Add voucher backup, configurable from Diagnostics > NanoBSD. Fixes #1087" - voucher db backup already existed under a different name. Ticket is still fixed, just by different code that was already there.
    This reverts commit 0d89a2fcac3deea06bdc4a481bbdfae4f18b1ff8.

--- etc/rc.backup_vouchers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Save the voucher databases to the config path.
-if [ -d "/var/db/" ]; then
-	/etc/rc.conf_mount_rw
-	cd / && tar -czf /cf/conf/vouchers.tgz -C / var/db/voucher_*.db
-	/etc/rc.conf_mount_ro
-fi

9f154c1699e0e6486e87f990735aaebde99bb208
Scott Ullrich <sullrich@pfsense.org>
2010-12-17 16:57:30 -0500

    Do not copy non existent /sys

--- usr/local/www/installer.php
+++ usr/local/www/installer.php
@@ -117,7 +117,7 @@ installType=FreeBSD
 packageType=cpdup
 
 # Optional Components
-cpdupPaths=boot,COPYRIGHT,bin,conf,conf.default,dev,etc,home,kernels,libexec,lib,root,sbin,sys,usr,var
+cpdupPaths=boot,COPYRIGHT,bin,conf,conf.default,dev,etc,home,kernels,libexec,lib,root,sbin,usr,var
 
 # runExtCommand=chmod a+rx /usr/local/bin/after_installation_routines.sh ; cd / ; /usr/local/bin/after_installation_routines.sh
 EOF;

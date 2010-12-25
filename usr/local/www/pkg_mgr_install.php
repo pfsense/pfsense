9b3ade76caa359367f71d73de60a88284690d77d
Scott Ullrich <sullrich@pfsense.org>
2010-12-25 13:07:43 -0500

    Increase textarea col size now that there is a lot more pkg install/deinstall information being output

--- usr/local/www/pkg_mgr_install.php
+++ usr/local/www/pkg_mgr_install.php
@@ -98,9 +98,9 @@ include("head.inc");
 							</table>
 							<br>
 							<!-- status box -->
-							<textarea cols="60" rows="1" name="status" id="status" wrap="hard"><?=gettext("Beginning package installation.");?></textarea>
+							<textarea cols="80" rows="1" name="status" id="status" wrap="hard"><?=gettext("Beginning package installation.");?></textarea>
 							<!-- command output box -->
-							<textarea cols="60" rows="25" name="output" id="output" wrap="hard"></textarea>
+							<textarea cols="80" rows="25" name="output" id="output" wrap="hard"></textarea>
 						</center>
 					</td>
 				</tr>

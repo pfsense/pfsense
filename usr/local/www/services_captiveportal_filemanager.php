4e9781359225f503b83b3d1e4dd9bbacbb1a3b0a
Scott Ullrich <sullrich@pfsense.org>
2011-01-25 12:07:43 -0500

    Adding tab for allowed hostnames

--- usr/local/www/services_captiveportal_filemanager.php
+++ usr/local/www/services_captiveportal_filemanager.php
@@ -139,6 +139,7 @@ include("head.inc");
 	$tab_array[] = array(gettext("Captive portal"), false, "services_captiveportal.php");
 	$tab_array[] = array(gettext("Pass-through MAC"), false, "services_captiveportal_mac.php");
 	$tab_array[] = array(gettext("Allowed IP addresses"), false, "services_captiveportal_ip.php");
+	$tab_array[] = array(gettext("Allowed Hostnames"), true, "services_captiveportal_hostname.php");
 	$tab_array[] = array(gettext("Vouchers"), false, "services_captiveportal_vouchers.php");
 	$tab_array[] = array(gettext("File Manager"), true, "services_captiveportal_filemanager.php");
 	display_top_tabs($tab_array);

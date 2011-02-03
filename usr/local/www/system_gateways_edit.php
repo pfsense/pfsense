54ac51b569af3db2a0d4fe29563090265355725e
Seth Mos <seth.mos@dds.nl>
2011-02-03 23:08:58 +0100

    Make the subnet check failure better readable

--- usr/local/www/system_gateways_edit.php
+++ usr/local/www/system_gateways_edit.php
@@ -120,14 +120,16 @@ if ($_POST) {
 		}
 		if (is_ipaddrv4($parent_ip)) {
 			$parent_sn = get_interface_subnet($_POST['interface']);
-			if(!ip_in_subnet($_POST['gateway'], gen_subnet($parent_ip, $parent_sn) . "/" . $parent_sn) && !ip_in_interface_alias_subnet($_POST['interface'], $_POST['gateway'])) {
-				$input_errors[] = sprintf(gettext("The gateway address %s does not lie within the chosen interface's subnet."), $_POST['gateway']);
+			$subnet = gen_subnet($parent_ip, $parent_sn) . "/" . $parent_sn;
+			if(!ip_in_subnet($_POST['gateway'], $subnet) && !ip_in_interface_alias_subnet($_POST['interface'], $_POST['gateway'])) {
+				$input_errors[] = sprintf(gettext("The gateway address %s does not lie within the chosen interface's subnet '{$subnet}'."), $_POST['gateway']);
 			}
 		}
 		if (is_ipaddrv6($parent_ip)) {
 			$parent_sn = get_interface_subnetv6($_POST['interface']);
-			if(!ip_in_subnet($_POST['gateway'], gen_subnetv6($parent_ip, $parent_sn) . "/" . $parent_sn)) {
-				$input_errors[] = sprintf(gettext("The gateway address %s does not lie within the chosen interface's subnet."), $_POST['gateway']);
+			$subnet = gen_subnetv6($parent_ip, $parent_sn) . "/" . $parent_sn;
+			if(!ip_in_subnet($_POST['gateway'], $subnet)) {
+				$input_errors[] = sprintf(gettext("The gateway address %s does not lie within the chosen interface's subnet '{$subnet}'."), $_POST['gateway']);
 			}
 		}
 	}

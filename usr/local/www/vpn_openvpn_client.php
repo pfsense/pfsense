c76fc19f32a34b91ece34740593f86978f4a8911
jim-p <jimp@pfsense.org>
2011-04-07 11:49:40 -0400

    Don't test for a user cert here either, see previous commit.

--- usr/local/www/vpn_openvpn_client.php
+++ usr/local/www/vpn_openvpn_client.php
@@ -654,8 +654,6 @@ if ($savemsg)
 								$caname = "";
 								$inuse = "";
 								$revoked = "";
-								if (is_user_cert($cert['refid']))
-									continue;
 								$ca = lookup_ca($cert['caref']);
 								if ($ca)
 									$caname = " (CA: {$ca['descr']})";

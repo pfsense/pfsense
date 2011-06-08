92ca32ccbe27d00d9fc2a22ceea11b0506433ace
Ermal <eri@pfsense.org>
2011-06-08 18:22:04 +0000

    Actually make this code more readble.

--- usr/local/www/xmlrpc.php
+++ usr/local/www/xmlrpc.php
@@ -198,12 +198,11 @@ function restore_config_section_xmlrpc($raw_params) {
 		$anyproxyarp = false;
 		foreach ($config['virtualip']['vip'] as $vip) {
 			if (isset($oldvips[$vip['vhid']])) {
+				unset($oldvips[$vip['vhid']]);
 				if ($oldvips[$vip['vhid']] == "{$vip['password']}{$vip['advskew']}{$vip['subnet']}{$vip['subnet_bits']}{$vip['advbase']}") {
-					unset($oldvips[$vip['vhid']]);
 					if (does_interface_exist("vip{$vip['vhid']}"))
 						continue; // Skip reconfiguring this vips since nothing has changed.
-				} else
-					unset($oldvips[$vip['vhid']]);
+				}
 			}
 
 			switch ($vip['mode']) {

4f163cb886685867283cd756ded072b82f515674
jim-p <jimp@pfsense.org>
2011-08-06 21:20:58 -0400

    Apparently RFC 1123 overrides RFC 952 and hostnames can start with digits these days.

--- usr/local/www/services_dhcp_edit.php
+++ usr/local/www/services_dhcp_edit.php
@@ -111,9 +111,6 @@ if ($_POST) {
 	$_POST['mac'] = strtolower(str_replace("-", ":", $_POST['mac']));
 	
 	if ($_POST['hostname']) {
-		preg_match("/^[0-9]/", $_POST['hostname'], $matches);
-		if($matches)
-			$input_errors[] = gettext("The hostname cannot start with a numeric character according to RFC952");
 		preg_match("/\-\$/", $_POST['hostname'], $matches);
 		if($matches)
 			$input_errors[] = gettext("The hostname cannot end with a hyphen according to RFC952");		

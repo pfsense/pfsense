2299007e8078329fe4dcc708f672717409ead90c
Warren Baker <warren@decoy.co.za>
2011-08-24 14:01:34 +0200

    These html tags are not required since they are used in head.inc which is included.

--- usr/local/www/system_firmware_check.php
+++ usr/local/www/system_firmware_check.php
@@ -51,8 +51,6 @@ include("head.inc");
 
 ?>
 
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link href="all.css" rel="stylesheet" type="text/css">
 </head>
 
 <body link="#0000CC" vlink="#0000CC" alink="#0000CC">

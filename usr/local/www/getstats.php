108c6dbc5804ba921a413e5e721a0a5d0466f9f1
Scott Ullrich <sullrich@gmail.com>
2011-09-12 15:32:56 -0400

    Add no cache header

--- usr/local/www/getstats.php
+++ usr/local/www/getstats.php
@@ -36,6 +36,12 @@
 ##|*MATCH=getstats.php*
 ##|-PRIV
 
+header("Last-Modified: " . gmdate( "D, j M Y H:i:s" ) . " GMT" );
+header("Expires: " . gmdate( "D, j M Y H:i:s", time() ) . " GMT" );
+header("Cache-Control: no-store, no-cache, must-revalidate" ); // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", FALSE );
+header("Pragma: no-cache"); // HTTP/1.0
+
 require_once("guiconfig.inc");
 include_once("includes/functions.inc.php");
 

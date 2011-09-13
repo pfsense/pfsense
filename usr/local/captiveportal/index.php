90477318aad050eb1f1b5282fac790f2a985fce3
Ermal <eri@pfsense.org>
2011-09-13 19:30:07 +0000

    Fixes #1874. Correctly error out when the user or the pass is not posted during login of CP.

--- usr/local/captiveportal/index.php
+++ usr/local/captiveportal/index.php
@@ -205,15 +205,18 @@ EOD;
 
 } else if ($_POST['accept'] && $config['captiveportal'][$cpzone]['auth_method'] == "local") {
 
+    if ($_POST['auth_user'] && $_POST['auth_pass']) {
 	//check against local user manager
 	$loginok = local_backed($_POST['auth_user'], $_POST['auth_pass']);
-    if ($loginok){
-        captiveportal_logportalauth($_POST['auth_user'],$clientmac,$clientip,"LOGIN");
-        portal_allow($clientip, $clientmac,$_POST['auth_user']);
-    } else {
-        captiveportal_logportalauth($_POST['auth_user'],$clientmac,$clientip,"FAILURE");
+	if ($loginok){
+		captiveportal_logportalauth($_POST['auth_user'],$clientmac,$clientip,"LOGIN");
+		portal_allow($clientip, $clientmac,$_POST['auth_user']);
+	} else {
+		captiveportal_logportalauth($_POST['auth_user'],$clientmac,$clientip,"FAILURE");
+		portal_reply_page($redirurl, "error", $errormsg);
+	}
+    } else
         portal_reply_page($redirurl, "error", $errormsg);
-    }
 } else if ($_POST['accept'] && $clientip && $config['captiveportal'][$cpzone]['auth_method'] == "none") {
     captiveportal_logportalauth("unauthenticated",$clientmac,$clientip,"ACCEPT");
     portal_allow($clientip, $clientmac, "unauthenticated");

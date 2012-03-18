0f54c8065af71119a7080759028e8c6b57359907
Pierre POMES <pierre.pomes@gmail.com>
2012-03-17 21:06:16 -0400

    Do not show tabs for interfaces with 'link local' address (fe80::...)

--- usr/local/www/services_dhcpv6.php
+++ usr/local/www/services_dhcpv6.php
@@ -482,8 +482,8 @@ include("head.inc");
 	$i = 0;
 	foreach ($iflist as $ifent => $ifname) {
 		$oc = $config['interfaces'][$ifent];
-		if ((is_array($config['dhcpdv6'][$ifent]) && !isset($config['dhcpdv6'][$ifent]['enable']) && (!is_ipaddrv6($oc['ipaddrv6']))) ||
-			(!is_array($config['dhcpdv6'][$ifent]) && (!is_ipaddrv6($oc['ipaddrv6']))))
+		if ((is_array($config['dhcpdv6'][$ifent]) && !isset($config['dhcpdv6'][$ifent]['enable']) && !(is_ipaddrv6($oc['ipaddrv6']) && (!preg_match("/fe80::/", $oc['ipaddrv6'])))) ||
+			(!is_array($config['dhcpdv6'][$ifent]) && !(is_ipaddrv6($oc['ipaddrv6']) && (!preg_match("/fe80::/", $oc['ipaddrv6'])))))
 			continue;
 		if ($ifent == $if)
 			$active = true;

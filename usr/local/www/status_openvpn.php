48b490ca58d5472596bf51f91c0ad13c21438f9b
Phil Davis <phil.davis@world.inf.org>
2012-06-27 11:43:22 +0545

    Only display "No OpenVPN instance defined" if there really are none.

--- usr/local/www/status_openvpn.php
+++ usr/local/www/status_openvpn.php
@@ -304,7 +304,7 @@ if ($DisplayNote) {
 	echo "<br/><b>" . gettext("NOTE") . ":</b> " . gettext("You need to bind each OpenVPN client to enable its management daemon: use 'Local port' setting in the OpenVPN client screen");
 }
 
-if ((empty($clients)) && (empty($servers))) {
+if ((empty($clients)) && (empty($servers)) && (empty($sk_servers))) {
 	echo gettext("No OpenVPN instance defined");
 }
 ?>

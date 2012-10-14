c8224e14d303c3392d27ff85724a7ae9a802e1fa
Colin Fleming <cj_fleming@sky.com>
2012-10-14 13:08:45 +0200

    Initially a spelling mistake on line 305.
    Initially just a spelling mistake on line 305, but then some HTML "housekeeping":
    
     <a href="&">   <a href="&amp;">
          nowrap    style="white-space:nowrap"
        selected    selected="selected"
         <input>    <input />
           <img>    <img alt="" />
        <script>    <script type="text/javascript">//<![CDATA[  ...  //]]></script>
    <td><center>    <td align="center">

--- usr/local/www/pkg.php
+++ usr/local/www/pkg.php
@@ -156,7 +156,7 @@ include("head.inc");
 include("fbegin.inc");
 ?>
 <form action="pkg.php" name="pkgform" method="get">
-<input type='hidden' name='xml' value='<?=$_REQUEST['xml']?>'>
+<input type="hidden" name="xml" value="<?=$_REQUEST['xml']?>" />
 <? if($_GET['savemsg'] <> "") $savemsg = htmlspecialchars($_GET['savemsg']); ?>
 <div id="savemsg"></div>
 <?php if ($savemsg) print_info_box($savemsg); ?>
@@ -208,9 +208,10 @@ if ($pkg['tabs'] <> "") {
 ?>
 <script type="text/javascript" src="javascript/domTT/domLib.js"></script>
 <script type="text/javascript" src="javascript/domTT/domTT.js"></script>
-<script type="textjavascript" src="javascript/domTT/behaviour.js"></script>
-<script type="textjavascript" src="javascript/domTT/fadomatic.js"></script>
-<script>
+<script type="text/javascript" src="javascript/domTT/behaviour.js"></script>
+<script type="text/javascript" src="javascript/domTT/fadomatic.js"></script>
+<script type="text/javascript">
+//<![CDATA[
 	function setFilter(filtertext) {
 		jQuery('#pkg_filter').val(filtertext);
 		document.pkgform.submit();
@@ -235,7 +236,7 @@ if ($pkg['tabs'] <> "") {
 			});
 			function save_changes_to_xml(xml) {
 					var ids=jQuery('#mainarea table tbody').sortable('serialize',{key:"ids[]"});
-					var strloading="<img src='/themes/<?= $g['theme']; ?>/images/misc/loader.gif'> " +  "<?=gettext('Saving changes...');?>";
+					var strloading="<img src=\"/themes/<?= $g['theme']; ?>/images/misc/loader.gif\" alt=\"\" /> " +  "<?=gettext('Saving changes...');?>";
 					if(confirm("<?=gettext("Do you really want to save changes?");?>")){
 						jQuery.ajax({
 							type: 'get',
@@ -257,6 +258,7 @@ if ($pkg['tabs'] <> "") {
 	<?php 
 		}
 	?>
+//]]>
 </script>
 <tr><td><div id="mainarea"><table width="100%" border="0" cellpadding="0" cellspacing="0">
 	<tr>
@@ -278,31 +280,31 @@ if ($pkg['tabs'] <> "") {
 							if($display_maximum_rows < 1) 
 								if($field['display_maximum_rows']) 
 									$display_maximum_rows = $field['display_maximum_rows'];
-							echo "<tr><td class='listhdrr' colspan='$colspan'><center>";
+							echo "<tr><td class=\"listhdrr\" colspan=\"$colspan\" align=\"center\">";
 							echo "Filter by: ";
 							$isfirst = true;
 							for($char = 65; $char < 91; $char++) {
 								if(!$isfirst) 
 									echo " | ";
-								echo "<a href=\"#\" onClick=\"setFilter('" . chr($char) . "');\">" . chr($char) . "</a>";
+								echo "<a href=\"#\" onclick=\"setFilter('" . chr($char) . "');\">" . chr($char) . "</a>";
 								$isfirst = false;
 							}
 							echo "</td></tr>";
-							echo "<tr><td class='listhdrr' colspan='$colspan'><center>";
+							echo "<tr><td class=\"listhdrr\" colspan=\"$colspan\" align=\"center\">";
 							if($field['sortablefields']) {
-								echo "Filter field: <select name='pkg_filter_type'>";
+								echo "Filter field: <select name=\"pkg_filter_type\">";
 								foreach($field['sortablefields']['item'] as $si) {
 									if($si['name'] == $_REQUEST['pkg_filter_type']) 
-										$SELECTED = "SELECTED";
+										$SELECTED = "selected=\"selected\"";
 									else 
 										$SELECTED = "";
-									echo "<option value='{$si['name']}' {$SELECTED}>{$si['name']}</option>";
+									echo "<option value=\"{$si['name']}\" {$SELECTED}>{$si['name']}</option>";
 								}
 								echo "</select>";
 							}
 							if($include_filtering_inputbox) 
-								echo "&nbsp;&nbsp;Filter text: <input id='pkg_filter' name='pkg_filter' value='" . $_REQUEST['pkg_filter'] . "'> <input type='submit' value='Filter'>";
-							echo "</td></tr><tr><td><font siez='-3'>&nbsp;</td></tr>";
+								echo "&nbsp;&nbsp;Filter text: <input id=\"pkg_filter\" name=\"pkg_filter' value=\"" . $_REQUEST['pkg_filter'] . "\" /> <input type=\"submit\" value=\"Filter\" />";
+							echo "</td></tr><tr><td><font size=\"-3\">&nbsp;</td></tr>";
 						}
 					}
 				}
@@ -326,17 +328,17 @@ if ($pkg['tabs'] <> "") {
 							$tmppp++;
 						}
 					}
-					echo "<tr><td colspan='" . count($pkg['adddeleteeditpagefields']['columnitem']) . "'>";
-					echo "<table width='100%'>";
+					echo "<tr><td colspan=\"" . count($pkg['adddeleteeditpagefields']['columnitem']) . "\">";
+					echo "<table width=\"100%\">";
 					echo "<tr>";
-					echo "<td align='left'>Displaying page $page of $totalpages</b></td>";
-					echo "<td align='right'>Rows per page: <select onChange='document.pkgform.submit();' name='display_maximum_rows'>";
+					echo "<td align=\"left\">Displaying page $page of $totalpages</b></td>";
+					echo "<td align=\"right\">Rows per page: <select onchange=\"document.pkgform.submit();\" name=\"display_maximum_rows\">";
 					for($x=0; $x<250; $x++) {
 						if($x == $display_maximum_rows)
-							$SELECTED = "SELECTED";
+							$SELECTED = "selected=\"selected\"";
 						else 
 							$SELECTED = "";
-						echo "<option value='$x' $SELECTED>$x</option>\n";
+						echo "<option value=\"$x\" $SELECTED>$x</option>\n";
 						$x=$x+4;
 					}
 					echo "</select></td></tr>";
@@ -443,17 +445,17 @@ if ($pkg['tabs'] <> "") {
 <?php
 					}
 ?>
-				<td valign="middle" class="list" nowrap>
+				<td valign="middle" class="list" style="white-space:nowrap">
 				<table border="0" cellspacing="0" cellpadding="1">
 				<tr>
 				<?php
 				#Show custom description to edit button if defined
 				$edit_msg=($pkg['adddeleteeditpagefields']['edittext']?$pkg['adddeleteeditpagefields']['edittext']:"Edit this item");?>
-				<td valign="middle"><a href="pkg_edit.php?xml=<?=$xml?>&act=edit&id=<?=$i;?>"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_e.gif" width="17" height="17" border="0" <?=domTT_title($edit_msg)?>></a></td>
+				<td valign="middle"><a href="pkg_edit.php?xml=<?=$xml?>&amp;act=edit&amp;id=<?=$i;?>"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_e.gif" width="17" height="17" border="0" <?=domTT_title($edit_msg)?> alt="" /></a></td>
 				<?php
 				#Show custom description to delete button if defined
 				$delete_msg=($pkg['adddeleteeditpagefields']['deletetext']?$pkg['adddeleteeditpagefields']['deletetext']:"Delete this item");?>
-				<td valign="middle"><a href="pkg.php?xml=<?=$xml?>&act=del&id=<?=$i;?>" onclick="return confirm('<?=gettext("Do you really want to delete this item?");?>')"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_x.gif" width="17" height="17" border="0" <?=domTT_title($delete_msg)?>></a></td>
+				<td valign="middle"><a href="pkg.php?xml=<?=$xml?>&amp;act=del&amp;id=<?=$i;?>" onclick="return confirm('<?=gettext("Do you really want to delete this item?");?>')"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_x.gif" width="17" height="17" border="0" <?=domTT_title($delete_msg)?> alt="" /></a></td>
 				</tr>
 				</table>
 				</td>
@@ -465,28 +467,28 @@ if ($pkg['tabs'] <> "") {
 					   $i ==  (count($evaledvar)-1)) {
 						$colcount = count($pkg['adddeleteeditpagefields']['columnitem']);
 						$final_footer = "";
-						$final_footer .= "<tr><td colspan='$colcount'>";
-						$final_footer .=  "<table width='100%'><tr>";
+						$final_footer .= "<tr><td colspan=\"$colcount\">";
+						$final_footer .=  "<table width=\"100%\"><tr>";
 						$final_footer .=  "<td align='left'>";
 						$startingat = $startdisplayingat - $display_maximum_rows;
 						if($startingat > -1) {
-							$final_footer .=  "<a href='pkg.php?xml=" . $_REQUEST['xml'] . "&startdisplayingat={$startingat}&display_maximum_rows={$display_maximum_rows}'>";
+							$final_footer .=  "<a href=\"pkg.php?xml=" . $_REQUEST['xml'] . "&amp;startdisplayingat={$startingat}&amp;display_maximum_rows={$display_maximum_rows}\">";
 						} else {
 							if($startingnat > 1) 
-								$final_footer .=  "<a href='pkg.php?xml=" . $_REQUEST['xml'] . "&startdisplayingat=0&display_maximum_rows={$display_maximum_rows}'>";
+								$final_footer .=  "<a href=\"pkg.php?xml=" . $_REQUEST['xml'] . "&amp;startdisplayingat=0&amp;display_maximum_rows={$display_maximum_rows}\">";
 						}
-						$final_footer .=  "<font size='2'><< Previous page</a>";
+						$final_footer .=  "<font size=\"2\"><< Previous page</a>";
 						if($tmppp + $display_maximum_rows > count($evaledvar)) 
 							$endingrecord = count($evaledvar);
 						else 
 							$endingrecord = $tmppp + $display_maximum_rows;
-						$final_footer .=  "</td><td align='center'>";
+						$final_footer .=  "</td><td align=\"center\">";
 						$tmppp++;
-						$final_footer .=  "<font size='2'>Displaying {$tmppp} - {$endingrecord} / " . count($evaledvar) . " records";
-						$final_footer .=  "</td><td align='right'>&nbsp;";
+						$final_footer .=  "<font size=\"2\">Displaying {$tmppp} - {$endingrecord} / " . count($evaledvar) . " records";
+						$final_footer .=  "</td><td align=\"right\">&nbsp;";
 						if(($i+1) < count($evaledvar))
-							$final_footer .=  "<a href='pkg.php?xml=" . $_REQUEST['xml'] . "&startdisplayingat=" . ($startdisplayingat + $display_maximum_rows) . "&display_maximum_rows={$display_maximum_rows}'>";
-						$final_footer .=  "<font size='2'>Next page >></a>";	
+							$final_footer .=  "<a href=\"pkg.php?xml=" . $_REQUEST['xml'] . "&amp;startdisplayingat=" . ($startdisplayingat + $display_maximum_rows) . "&amp;display_maximum_rows={$display_maximum_rows}\">";
+						$final_footer .=  "<font size=\"2\">Next page >></a>";	
 						$final_footer .=  "</td></tr></table></td></tr>";
 						$i = count($evaledvar);
 						break;
@@ -504,11 +506,11 @@ if ($pkg['tabs'] <> "") {
 								<?php
 								#Show custom description to add button if defined
 								$add_msg=($pkg['adddeleteeditpagefields']['addtext']?$pkg['adddeleteeditpagefields']['addtext']:"Add a new item");?>
-								<td valign="middle"><a href="pkg_edit.php?xml=<?=$xml?>&id=<?=$i?>"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_plus.gif" width="17" height="17" border="0" <?=domTT_title($add_msg)?>></a></td>
+								<td valign="middle"><a href="pkg_edit.php?xml=<?=$xml?>&amp;id=<?=$i?>"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_plus.gif" width="17" height="17" border="0" <?=domTT_title($add_msg)?> alt="" /></a></td>
 								<?php
 								#Show description button and info if defined
 								if($pkg['adddeleteeditpagefields']['description']){?>
-								<td valign="middle"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_info_pkg.gif" width="17" height="17" border="0" <?=domTT_title($pkg['adddeleteeditpagefields']['description'])?>></td>
+								<td valign="middle"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_info_pkg.gif" width="17" height="17" border="0" <?=domTT_title($pkg['adddeleteeditpagefields']['description'])?> alt="" /></td>
 								<?php }?>
 							</tr>
 						</table>
@@ -518,7 +520,7 @@ if ($pkg['tabs'] <> "") {
 				<?php
 				#Show save button only when movable is defined
 				if($pkg['adddeleteeditpagefields']['movable']){?>
-				<td><input class="formbtn" type="button" value="Save" name="Submit" onclick="save_changes_to_xml('<?=$xml?>')"></td>
+				<td><input class="formbtn" type="button" value="Save" name="Submit" onclick="save_changes_to_xml('<?=$xml?>')" /></td>
 				<?php }?>
 		</table>
 	</td>

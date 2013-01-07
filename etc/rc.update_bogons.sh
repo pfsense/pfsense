3cde94cff2077122a3455bacbefaf295211183b3
bcyrill <cyrill@bannwart.info>
2013-01-07 21:42:59 +0100

    Avoid 'Cannot allocate memory' errors

--- etc/rc.update_bogons.sh
+++ etc/rc.update_bogons.sh
@@ -90,29 +90,32 @@ ON_DISK_V6_CKSUM=`md5 /tmp/bogonsv6 | awk '{ print $4 }'`
 if [ "$BOGON_V4_CKSUM" = "$ON_DISK_V4_CKSUM" ] || [ "$BOGON_V6_CKSUM" = "$ON_DISK_V6_CKSUM" ]; then
 	# At least one of the downloaded checksums matches, so mount RW
 	/etc/rc.conf_mount_rw
-fi
+	
+	MAXENTRIES=`pfctl -s memory | awk '/table-entries/ { print $4 }'`
+	
+	if [ "$BOGON_V4_CKSUM" = "$ON_DISK_V4_CKSUM" ]; then
+		egrep -v "^192.168.0.0/16|^172.16.0.0/12|^10.0.0.0/8" /tmp/bogons > /etc/bogons
+		RESULT=`/sbin/pfctl -t bogons -T replace -f /etc/bogons 2>&1`
+		echo "$RESULT" |awk '{ print "Bogons V4 file downloaded: " $0 }' | logger
+		rm /tmp/bogons
+	else
+		echo "Could not download ${v4url} (checksum mismatch)" | logger
+		checksum_error="true"
+	fi
 
-if [ "$BOGON_V4_CKSUM" = "$ON_DISK_V4_CKSUM" ]; then
-	egrep -v "^192.168.0.0/16|^172.16.0.0/12|^10.0.0.0/8" /tmp/bogons > /etc/bogons
-	RESULT=`/sbin/pfctl -t bogons -T replace -f /etc/bogons 2>&1`
-	rm /tmp/bogons
-	echo "$RESULT" |awk '{ print "Bogons V4 file downloaded: " $0 }' | logger
-else
-	echo "Could not download ${v4url} (checksum mismatch)" | logger
-	checksum_error="true"
-fi
-
-if [ "$BOGON_V6_CKSUM" = "$ON_DISK_V6_CKSUM" ]; then
-	egrep -v "^fc00::/7" /tmp/bogonsv6 > /etc/bogonsv6
-	RESULT=`/sbin/pfctl -t bogonsv6 -T replace -f /etc/bogonsv6 2>&1`
-	rm /tmp/bogonsv6
-	echo "$RESULT" |awk '{ print "Bogons V6 file downloaded: " $0 }' | logger
-else
-	echo "Could not download ${v6url} (checksum mismatch)" | logger
-	checksum_error="true"
-fi
-
-if [ "$BOGON_V4_CKSUM" = "$ON_DISK_V4_CKSUM" ] || [ "$BOGON_V6_CKSUM" = "$ON_DISK_V6_CKSUM" ]; then
+	if [ "$BOGON_V6_CKSUM" = "$ON_DISK_V6_CKSUM" ]; then
+		LINES=`wc -l /tmp/bogonsv6 | awk '{ print $1 }'`
+		if [ $MAXENTRIES -gt $((2*LINES)) ]; then
+			egrep -v "^fc00::/7" /tmp/bogonsv6 > /etc/bogonsv6
+			RESULT=`/sbin/pfctl -t bogonsv6 -T replace -f /etc/bogonsv6 2>&1`
+			echo "$RESULT" |awk '{ print "Bogons V6 file downloaded: " $0 }' | logger
+		fi
+		rm /tmp/bogonsv6
+	else
+		echo "Could not download ${v6url} (checksum mismatch)" | logger
+		checksum_error="true"
+	fi
+	
 	# We mounted RW, so switch back to RO
 	/etc/rc.conf_mount_ro
 fi

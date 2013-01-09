610f3ff6965e17cd79cfd51ba6c93c8c9cfd1890
Chris Buechler <cmb@pfsense.org>
2013-01-08 23:06:43 -0600

    use Sunday rather than Monday for weekly bogon update

--- usr/local/www/system_advanced_firewall.php
+++ usr/local/www/system_advanced_firewall.php
@@ -178,7 +178,7 @@ if ($_POST) {
 					install_cron_job("/usr/bin/nice -n20 /etc/rc.update_bogons.sh", true, "1", "3", "*", "*", "*");
 					break;
 				case 'weekly':
-					install_cron_job("/usr/bin/nice -n20 /etc/rc.update_bogons.sh", true, "1", "3", "*", "*", "1");
+					install_cron_job("/usr/bin/nice -n20 /etc/rc.update_bogons.sh", true, "1", "3", "*", "*", "0");
 					break;
 				case 'monthly':
 					// fall through

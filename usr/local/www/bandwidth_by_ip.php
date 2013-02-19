1fea5ad99e477822675b2ba1edf10c34834fe55c
Renato Botelho <garga@FreeBSD.org>
2013-02-19 08:41:57 -0300

    Get the correct interface subnet

--- usr/local/www/bandwidth_by_ip.php
+++ usr/local/www/bandwidth_by_ip.php
@@ -22,24 +22,16 @@ $interface = $_GET['if'];
 $real_interface = convert_friendly_interface_to_real_interface_name($interface);
 $intip = find_interface_ip($real_interface);
 $intip = explode (".", $intip);
-//get interface netmask
+//get interface subnet
 $netmask = find_interface_subnet($real_interface);
+$intsubnet = gen_subnet(find_interface_ip($real_interface), $netmask) . "/$netmask";
 //get the sort method
 $sort = $_GET['sort'];
 if ($sort == "out") 
 	{$sort_method = "-T";}
 else
 	{$sort_method = "-R";}
-//use the same class of the specified interface
-if ($netmask >= 24) {
-	$intsubnet = $intip[0] . "." . $intip[1] . "." . $intip[2] . ".0/24";
-}
-elseif ($netmask >=16){
-	$intsubnet = $intip[0] . "." . $intip[1] . ".0.0/16";
-}
-else {
-	$intsubnet = $intip[0] . ".0.0.0/8";
-}
+
 exec("/usr/local/bin/rate -i {$real_interface} -nlq 1 -Aba 20 {$sort_method} -c {$intsubnet} | tr \"|\" \" \" | awk '{ printf \"%s:%s:%s:%s:%s\\n\", $1,  $2,  $4,  $6,  $8 }'", $listedIPs);
 unset($bandwidthinfo);
 unset($receivebytesarray);

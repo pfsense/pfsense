8de7e1805a44a42f49bb0b9132ef3f6160079ae1
Renato Botelho <garga@FreeBSD.org>
2013-02-21 08:30:11 -0300

    Improve error message since we can have ipv6 set here

--- usr/local/www/firewall_virtual_ip_edit.php
+++ usr/local/www/firewall_virtual_ip_edit.php
@@ -114,7 +114,7 @@ if ($_POST) {
 		if (!is_ipaddr($_POST['subnet']))
 			$input_errors[] = gettext("A valid IP address must be specified.");
 		else if (is_ipaddr_configured($_POST['subnet'], "vip_" . $id))
-			$input_errors[] = gettext("This IPv4 address is being used by another interface or VIP.");
+			$input_errors[] = gettext("This IP address is being used by another interface or VIP.");
 	}
 
 	$natiflist = get_configured_interface_with_descr();

8bca0805439ad67deffb4a1b688fc23dfbd9f156
Phil Davis <phil.davis@world.inf.org>
2013-03-29 14:27:15 +0545

    Remember whcih gateway groups have been edited

--- usr/local/www/system_gateway_groups_edit.php
+++ usr/local/www/system_gateway_groups_edit.php
@@ -135,6 +135,7 @@ if ($_POST) {
 			$a_gateway_groups[] = $gateway_group;
 		
 		mark_subsystem_dirty('staticroutes');
+		mark_subsystem_dirty('gwgroup.' . $gateway_group['name']);
 		
 		write_config();
 		

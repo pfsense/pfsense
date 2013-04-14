5b431a20dead1128687453407a3b0c603154e773
Renato Botelho <garga@FreeBSD.org>
2013-04-14 15:51:44 -0300

    Add a new parameter to return all aliases, including hosts. It fixes #2941

--- usr/local/www/system_routes_edit.php
+++ usr/local/www/system_routes_edit.php
@@ -126,7 +126,7 @@ if ($_POST) {
 	} elseif (is_alias($_POST['network'])) {
 		$osn = $_POST['network'];
 		$fqdn_found = 0;
-		foreach (filter_expand_alias_array($_POST['network']) as $tgt) {
+		foreach (filter_expand_alias_array($_POST['network'], true) as $tgt) {
 			if (!is_ipaddr($tgt) && is_hostname($tgt)) {
 				if ($fqdn_found === 0) {
 					$input_errors[] = sprintf(gettext("The alias (%s) has one or more FQDNs configured and cannot be used to configure a static route."), $_POST['network']);

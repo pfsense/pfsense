7523c8e946de30c63f9b8f57bd5e91cb0617a9f1
jim-p <jimp@pfsense.org>
2013-07-18 14:31:54 -0400

    Use default RRD colors that are a little more visually distinct. Add some better comments/labels to make it easier for people to help customize and tweak the colors. Suggestions welcome.

--- usr/local/www/status_rrd_graph_img.php
+++ usr/local/www/status_rrd_graph_img.php
@@ -203,25 +203,59 @@ $speedlimit = ($upstream + $downstream);
 
 /* Set default colors explicity, the theme can then override them below.
    This prevents missing colors in themes from crashing the graphs. */
-$colortrafficup = array("666666", "CCCCCC", "b36666", "bd9090");
-$colortrafficdown = array("990000", "CC0000", "b36666", "bd9090");
-$colorpacketsup = array("666666", "CCCCCC", "b36666", "bd9090");
-$colorpacketsdown = array("990000", "CC0000", "b36666", "bd9090");
-$colortraffic95 = array("660000", "FF0000");
-$colorstates = array('990000','a83c3c','b36666','bd9090','cccccc','000000');
-$colorprocessor = array('990000','a83c3c','b36666','bd9090','cccccc','000000');
-$colormbuf = array('0080FF','00E344','FF0000','000000');
-$colormemory = array('990000','a83c3c','b36666','bd9090','cccccc','000000');
-$colorqueuesup = array('000000','7B0000','990000','BB0000','CC0000','D90000','EE0000','FF0000','CC0000');
-$colorqueuesdown = array('000000','7B7B7B','999999','BBBBBB','CCCCCC','D9D9D9','EEEEEE','FFFFFF','CCCCCC');
-$colorqueuesdropup = array('000000','7B0000','990000','BB0000','CC0000','D90000','EE0000','FF0000','CC0000');
-$colorqueuesdropdown = array('000000','7B7B7B','999999','BBBBBB','CCCCCC','D9D9D9','EEEEEE','FFFFFF','CCCCCC');
-$colorqualityrtt = array('990000','a83c3c','b36666','bd9090','cccccc','000000');
-$colorqualityloss = "ee0000";
-$colorwireless = array('333333','a83c3c','999999');
-$colorspamdtime = array('DDDDFF', 'AAAAFF', 'DDDDFF', '000066'); 
-$colorspamdconn = array('00AA00BB', 'FFFFFFFF', '00660088', 'FFFFFF88', '006600');
-$colorvpnusers = array('990000');
+/* Traffic Outbound		Out-P-4,  Out-B-4,  Out-P-6,  Out-B-6 */
+$colortrafficup		= array('666666', 'CCCCCC', '2217AA', '625AE7');
+
+/* Traffic Inbound		In-P-4,   In-B-4,    In-P-6,  In-B-6 */
+$colortrafficdown	= array('990000', 'CC0000', 'FFC875', 'FF9900');
+
+/* Packets Outbound		Out-P-4,  Out-B-4,  Out-P-6,  Out-B-6 */
+$colorpacketsup		= array('666666', 'CCCCCC', '2217AA', '625AE7');
+
+/* Packets Inbound		In-P-4,   In-B-4,    In-P-6,  In-B-6 */
+$colorpacketsdown	= array('990000', 'CC0000', 'FFC875', 'FF9900');
+
+/* 95th Percentile Lines	Out,      In */
+$colortraffic95		= array('660000', 'FF0000');
+
+/* State Table			pfrate,  pfstates, pfnat,  srcip,   dstip */
+$colorstates		= array('00AA00','990000','0000FF','000000','DD9B00');
+
+/* Processor Usage		user,    nice,    system,  int,     processes */
+$colorprocessor		= array('00AA00','990000','0000FF','DD9B00','000000');
+
+/* Memory Usage			active,  inact,   free,    cache,   wire */
+$colormemory		= array('00AA00','990000','0000FF','666666','DD9B00');
+
+/* MBUF Usage			current, cache,   total,   max */
+$colormbuf		= array('0080FF','00E344','FF0000','000000');
+
+/* Traffic Shaper Queues	q1,      q2,      q3,      q4,      q5,      q6,      q7,      q8,      q9 */
+$colorqueuesup		= array('000000','7B0000','0080FF','00E344','FF0000','2217AA','FFC875','FF9900','CC0000');
+$colorqueuesdown	= array('000000','7B7B7B','999999','BBBBBB','CCCCCC','D9D9D9','EEEEEE','FFFFFF','CCCCCC');
+
+$colorqueuesdropup	= array('000000','7B0000','0080FF','00E344','FF0000','2217AA','FFC875','FF9900','CC0000');
+$colorqueuesdropdown	= array('000000','7B7B7B','999999','BBBBBB','CCCCCC','D9D9D9','EEEEEE','FFFFFF','CCCCCC');
+
+/* Quality Graph Delay	>420,    180-420, 60-180,  20-60,   <20,     Delay Avg */
+$colorqualityrtt	= array('990000','a83c3c','b36666','bd9090','cccccc','000000');
+/* Quality Graph Loss */
+$colorqualityloss	= 'ee0000';
+
+/* Wireless Graph		SNR,     Rate,    Channel*/
+/* Cellular Graph		RSSI,     */
+$colorwireless		= array('333333','a83c3c','999999');
+
+/* SPAMD Times			min area, avg area, max area, Time line */
+$colorspamdtime		= array('DDDDFF', 'AAAAFF', 'DDDDFF', '000066');
+/* SPAMD Connections		max area,   min area,   min line,   max line,   avg line */
+$colorspamdconn		= array('AA00BB', 'FFFFFF', '660088', 'FFFF88', '006600');
+
+/* OpenVPN Users		Online Users */
+$colorvpnusers		= array('990000');
+
+/* Captive Portal Total Users	Total Users */
+/* Captive Portal Concurrent	Concurrent Users */
 $colorcaptiveportalusers = array('990000');
 
 /* select theme colors if the inclusion file exists */
@@ -910,7 +944,7 @@ elseif((strstr($curdatabase, "-mbuf.rrd")) && (file_exists("$rrddbpath$curdataba
 	/* define graphcmd for mbuf usage stats */
 	$graphcmd = "$rrdtool graph $rrdtmppath$curdatabase-$curgraph.png ";
 	$graphcmd .= "--start $start --end $end --step $step ";
-	$graphcmd .= "--vertical-label \"utilization\" ";
+	$graphcmd .= "--vertical-label \"utilization, percent\" ";
 	$graphcmd .= "--color SHADEA#eeeeee --color SHADEB#eeeeee ";
 	$graphcmd .= "--title \"`hostname` - {$prettydb} - {$hperiod} - {$havg} average\" ";
 	$graphcmd .= "--height 200 --width 620 ";

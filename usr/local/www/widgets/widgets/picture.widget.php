46172e9c991631b93726c01d7c8713b262c2b8fc
Charlie Marshall <charlie0440@gmail.com>
2013-07-25 22:23:06 +0100

    improve picture widget size/proportion and stop border overlapping widget size

--- usr/local/www/widgets/widgets/picture.widget.php
+++ usr/local/www/widgets/widgets/picture.widget.php
@@ -81,7 +81,7 @@ if($_POST) {
 
 <div id="picture-widgets" style="padding: 5px">
 	<a href='/widgets/widgets/picture.widget.php?getpic=true' target='_blank'>
-		<img border="0" width="350" height="350" src="/widgets/widgets/picture.widget.php?getpic=true" alt="picture" />
+		<img border="0" width="100%" height="100%" src="/widgets/widgets/picture.widget.php?getpic=true" alt="picture" />
 	</a>
 </div>											 
 

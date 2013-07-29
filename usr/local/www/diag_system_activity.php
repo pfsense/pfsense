4e8eec4c3c4fb395a90523ca47360550a1400d04
jim-p <jimp@pfsense.org>
2013-07-29 14:31:56 -0400

    Use full path

--- usr/local/www/diag_system_activity.php
+++ usr/local/www/diag_system_activity.php
@@ -46,7 +46,7 @@ $pfSversion = str_replace("\n", "", file_get_contents("/etc/version"));
 $pgtitle = gettext("Diagnostics: System Activity");
 
 if($_REQUEST['getactivity']) {
-	$text = `/usr/bin/top -aHS | cut -c1-105`;
+	$text = `/usr/bin/top -aHS | /usr/bin/cut -c1-105`;
 	echo $text;
 	exit;
 }

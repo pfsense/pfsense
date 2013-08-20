bd6ff3288af51bdc71a17e1a65c66b9bee0d8d26
Renato Botelho <garga@FreeBSD.org>
2013-08-20 14:37:02 -0300

    Add scope to target when it is a link-local, it helps ticket #3150

--- usr/local/www/diag_ping.php
+++ usr/local/www/diag_ping.php
@@ -147,15 +147,21 @@ include("head.inc"); ?>
 		echo "<font face='terminal' size='2'>";
 		echo "<strong>" . gettext("Ping output") . ":</strong><br>";
 		echo('<pre>');
+		$ifscope = '';
 		$command = "/sbin/ping";
 		if ($ipproto == "ipv6") {
 			$command .= "6";
 			$ifaddr = is_ipaddr($sourceip) ? $sourceip : get_interface_ipv6($sourceip);
+			if (is_linklocal($ifaddr))
+				$ifscope = get_ll_scope($ifaddr);
 		} else {
 			$ifaddr = is_ipaddr($sourceip) ? $sourceip : get_interface_ip($sourceip);
 		}
-		if ($ifaddr && (is_ipaddr($host) || is_hostname($host)))
+		if ($ifaddr && (is_ipaddr($host) || is_hostname($host))) {
 			$srcip = "-S" . escapeshellarg($ifaddr);
+			if (is_linklocal($host) && !strstr($host, "%") && !empty($ifscope))
+				$host .= "%{$ifscope}";
+		}
 
 		$cmd = "{$command} {$srcip} -c" . escapeshellarg($count) . " " . escapeshellarg($host);
 		//echo "Ping command: {$cmd}\n";

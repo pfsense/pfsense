65fec80dbbdd7eff492adfad453a22805d96f9a5
Renato Botelho <garga@FreeBSD.org>
2013-09-26 10:45:06 -0300

    require service-utils.inc for find_service_by_openvpn_vpnid()

--- usr/local/www/status_openvpn.php
+++ usr/local/www/status_openvpn.php
@@ -49,6 +49,7 @@ $shortcut_section = "openvpn";
 require("guiconfig.inc");
 require_once("openvpn.inc");
 require_once("shortcuts.inc");
+require_once("service-utils.inc");
 
 /* Handle AJAX */
 if($_GET['action']) {

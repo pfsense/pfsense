949053ef8017e06dcafea8d3a9002983d070d362
Charlie Root <keu_zar@hotmail.com>
2013-10-25 17:15:59 +0000

    code-blue theme

---
+++ usr/local/www/themes/code-blue_fs/bottom-loader.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+
+	NiftyCheck();
+	Rounded("div#niftyMenu","top bottom","#FFFFFF","#000000","smooth");
+	Rounded("div#mainarea","bl br tr","#FFF","#DDDDDD","smooth");
+	Rounded("div#boxarea","bl br tl tr","#FFF","#DDDDDD","smooth");
+	Rounded("tr#fend","bl br tl tr","#FFF","#990000","smooth");
+	Rounded("div#topbox","all","#FFF","#990000","smooth");
+
+//]]>

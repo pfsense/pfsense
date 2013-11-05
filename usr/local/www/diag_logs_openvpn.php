d7ba03704e82523091378ba42365e5efd9af5110
Colin Fleming <cj_fleming@sky.com>
2013-11-05 14:51:59 +0000

    Tidy up "diag_logs_openvpn.php" XHTML
    Add SUMMARY to table statements
    Close BR and INPUT statements

--- usr/local/www/diag_logs_openvpn.php
+++ usr/local/www/diag_logs_openvpn.php
@@ -62,7 +62,7 @@ include("head.inc");
 ?>
 <body link="#0000CC" vlink="#0000CC" alink="#0000CC">
 <?php include("fbegin.inc"); ?>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<table width="100%" border="0" cellpadding="0" cellspacing="0" summary="logs openvpn">
  	<tr>
 		<td>
 <?php
@@ -85,16 +85,16 @@ include("head.inc");
 	<tr>
     	<td>
 			<div id="mainarea">
-			<table class="tabcont" width="100%" border="0" cellspacing="0" cellpadding="0">
+			<table class="tabcont" width="100%" border="0" cellspacing="0" cellpadding="0" summary="main area">
 		  		<tr>
 					<td colspan="2" class="listtopic"><?php printf(gettext("Last %s OpenVPN log entries"),$nentries)?></td>
 		  		</tr>
 				<?php dump_clog($openvpn_logfile, $nentries); ?>
 				<tr>
 					<td>
-						<br>
+						<br/>
 						<form action="diag_logs_openvpn.php" method="post">
-						<input name="clear" type="submit" class="formbtn" value="<?=gettext("Clear log"); ?>">
+						<input name="clear" type="submit" class="formbtn" value="<?=gettext("Clear log"); ?>" />
 						</form>
 					</td>
 				</tr>

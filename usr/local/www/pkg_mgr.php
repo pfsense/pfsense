2984054690a6feffba9608d8f964eff464a9d01e
Renato Botelho <garga@FreeBSD.org>
2013-11-11 10:19:50 -0200

    Fix indent and whitespaces

--- usr/local/www/pkg_mgr.php
+++ usr/local/www/pkg_mgr.php
@@ -1,32 +1,32 @@
 <?php
 /* $Id$ */
 /*
-    pkg_mgr.php
-    Copyright (C) 2004-2012 Scott Ullrich <sullrich@gmail.com>
-    Copyright (C) 2013 Marcello Coutinho
-    
-    All rights reserved.
+	pkg_mgr.php
+	Copyright (C) 2004-2012 Scott Ullrich <sullrich@gmail.com>
+	Copyright (C) 2013 Marcello Coutinho
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
+	All rights reserved.
 
-    1. Redistributions of source code must retain the above copyright notice,
-       this list of conditions and the following disclaimer.
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
 
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
 
-    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    POSSIBILITY OF SUCH DAMAGE.
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
 */
 /*
 	pfSense_BUILDER_BINARIES:	/sbin/ifconfig
@@ -64,25 +64,25 @@ if(is_subsystem_dirty('packagelock')) {
 	echo "</html>";
 	exit;
 }
-function domTT_title($title_msg){
-	if (!empty($title_msg)){
+function domTT_title($title_msg) {
+	if (!empty($title_msg)) {
 		$title_msg=preg_replace("/\s+/"," ",$title_msg);
-        $title_msg=preg_replace("/'/","\'",$title_msg);
+		$title_msg=preg_replace("/'/","\'",$title_msg);
 		echo "onmouseout=\"this.style.color = ''; domTT_mouseout(this, event);\" onmouseover=\"domTT_activate(this, event, 'content', '{$title_msg}', 'trail', true, 'delay', 0, 'fade', 'both', 'fadeMax', 93, 'styleClass', 'niceTitle');\"";
 	}
 }
 //get_pkg_info only if cache file has more then $g[min_pkg_cache_file_time] seconds
 $pkg_cache_file_time=($g['min_pkg_cache_file_time'] ? $g['min_pkg_cache_file_time'] : 120);
 
-if (!file_exists("{$g['tmp_path']}/pkg_info.cache") || (time() - filemtime("{$g['tmp_path']}/pkg_info.cache")) > $pkg_cache_file_time){
+if (!file_exists("{$g['tmp_path']}/pkg_info.cache") || (time() - filemtime("{$g['tmp_path']}/pkg_info.cache")) > $pkg_cache_file_time) {
 	$pkg_info = get_pkg_info('all', array("noembedded", "name", "category", "website", "version", "status", "descr", "maintainer", "required_version", "maximum_version", "pkginfolink", "supportedbybsdperimeter","config_file"));
-	//create cache file after get_pkg_info 
+	//create cache file after get_pkg_info
 	if($pkg_info) {
 		$fout = fopen("{$g['tmp_path']}/pkg_info.cache", "w");
 		fwrite($fout, serialize($pkg_info));
 		fclose($fout);
 		//$pkg_sizes = get_pkg_sizes();
-	}else {
+	} else {
 		$using_cache = true;
 		$xmlrpc_base_url = isset($config['system']['altpkgrepo']['enable']) ? $config['system']['altpkgrepo']['xmlrpcbaseurl'] : $g['xmlrpcbaseurl'];
 		if(file_exists("{$g['tmp_path']}/pkg_info.cache")) {
@@ -91,10 +91,10 @@ if (!file_exists("{$g['tmp_path']}/pkg_info.cache") || (time() - filemtime("{$g[
 		} else {
 			$savemsg = sprintf(gettext('Unable to communicate with %1$s. Please verify DNS and interface configuration, and that %2$s has functional Internet connectivity.'), $xmlrpc_base_url, $g['product_name']);
 		}
-	}	
-}else{
-	$pkg_info = unserialize(@file_get_contents("{$g['tmp_path']}/pkg_info.cache"));
 	}
+} else {
+	$pkg_info = unserialize(@file_get_contents("{$g['tmp_path']}/pkg_info.cache"));
+}
 
 if (! empty($_GET))
 	if (isset($_GET['ver']))
@@ -135,11 +135,11 @@ include("head.inc");
 	if($pkg_info) {
 		$pkg_keys = array_keys($pkg_info);
 		natcasesort($pkg_keys);
-		
+
 		//Check categories
 		$categories=array();
 		if(is_array($pkg_keys)) {
-			foreach($pkg_keys as $key){
+			foreach($pkg_keys as $key) {
 				if (!package_skip_tests($pkg_info[$key],$requested_version))
 					$categories[$pkg_info[$key]['category']]++;
 				}
@@ -150,17 +150,16 @@ include("head.inc");
 		$visible_categories=array();
 		$categories_min_count=($g[$pkg_categories_min_count] ? $g[$pkg_categories_min_count] : 3);
 		$categories_max_display=($g[$pkg_categories_max_display] ? $g[$pkg_categories_max_display] : 6);
-				
+
 		//check selected category or define defaul category to show
-		if (isset($_REQUEST['category'])){
+		if (isset($_REQUEST['category'])) {
 			$menu_category=$_REQUEST['category'];
-			}
-		else{
+		} else {
 			$menu_category=($g[$pkg_default_category] ? $g[$pkg_default_category] : "Services");
-			}
-				
-		foreach ($categories as $category => $c_count){
-			if ($c_count >= $categories_min_count && $cm_count <= $categories_max_display){
+		}
+
+		foreach ($categories as $category => $c_count) {
+			if ($c_count >= $categories_min_count && $cm_count <= $categories_max_display) {
 				$tab_array[] = array(gettext($category) , $menu_category==$category ? true : false, "pkg_mgr.php?category={$category}");
 				$visible_categories[]=$category;
 				$cm_count++;
@@ -169,13 +168,13 @@ include("head.inc");
 		$tab_array[] = array(gettext("Other Categories"), $menu_category=="Other" ? true : false, "pkg_mgr.php?category=Other");
 		if (count($categories) > 1)
 			display_top_tabs($tab_array);
-		}?>
+	}?>
 	</td></tr>
 	<tr><td>
 	<div id="mainarea">
 		<table class="tabcont sortable" width="100%" border="0" cellpadding="6" cellspacing="0" summary="main area">
 		<tr><td width="<? print $menu_category=="Other" ? "10%" : "20%"; ?>" class="listhdrr"><?=gettext("Name"); ?></td>
-		<?php 
+		<?php
 		if ($menu_category== "Other")
 			print '<td width="18%" class="listhdrr">'.gettext("Category").'</td>'."\n";
 		?>
@@ -191,41 +190,41 @@ include("head.inc");
 					$index = &$pkg_info[$key];
 					if(get_pkg_id($index['name']) >= 0 )
 						continue;
-										
+
 					if (package_skip_tests($index,$requested_version))
 						continue;
-										
+
 					/* get history/changelog git dir */
 					$commit_dir=explode("/",$index['config_file']);
 					$changeloglink ="https://github.com/pfsense/pfsense-packages/commits/master/config/".$commit_dir[(count($commit_dir)-2)];
 
 					/* Check package info link */
-					if($index['pkginfolink']){
+					if($index['pkginfolink']) {
 						$pkginfolink = $index['pkginfolink'];
 						$pkginfo=gettext("Package info");
-					}else{
+					} else {
 						$pkginfolink = "http://forum.pfsense.org/index.php/board,15.0.html";
 						$pkginfo=gettext("No package info, check the forum");
-						}
-					if ($index['category'] == $menu_category || ($menu_category == "Other" && !in_array($index['category'],$visible_categories)) ){?>
+					}
+					if ($index['category'] == $menu_category || ($menu_category == "Other" && !in_array($index['category'],$visible_categories)) ) {?>
 						<tr valign="top" class="<?= $index['category'] ?>">
 						<td class="listlr" <?=domTT_title(gettext("Click on package name to access its website."))?>>
 							<a target="_blank" href="<?= $index['website'] ?>"><?= $index['name'] ?></a>
 						</td>
-						<?php 
+						<?php
 						if ($menu_category== "Other")
 							print '<td class="listlr">'.gettext($index['category']).'</td>'."\n";
 
-						if ($g['disablepackagehistory']){
+						if ($g['disablepackagehistory']) {
 							print '<td class="listr">'."\n";
-						}else{
-							print '<td class="listr" '; 
+						} else {
+							print '<td class="listr" ';
 							domTT_title(gettext("Click ").ucfirst($index['name']).gettext(" version to check its change log."));
 							print ">\n";
-							}
+						}
 
 						print "{$index['status']} <br>\n";
-						
+
 						if ($g['disablepackagehistory'])
 							echo"<a>{$index['version']}</a>";
 						else

b6877e065e23856adc9bebcc90db909867afea92
Ermal <eri@pfsense.org>
2013-12-05 10:51:13 +0000

    Handle more carp enahancements for FreeBSD 10. the interface vips will be names $if@$vhid since the interface name is the same as other ips.

--- usr/local/www/firewall_virtual_ip.php
+++ usr/local/www/firewall_virtual_ip.php
@@ -239,9 +239,6 @@ include("head.inc");
 		</tr>
 		<?php
 			$interfaces = get_configured_interface_with_descr(false, true);
-			$carplist = get_configured_carp_interface_list();
-			foreach ($carplist as $cif => $carpip)
-				$interfaces[$cif] = $carpip." (".get_vip_descr($carpip).")";
 			$interfaces['lo0'] = "Localhost";
 		?>
 			  <?php $i = 0; foreach ($a_vip as $vipent): ?>

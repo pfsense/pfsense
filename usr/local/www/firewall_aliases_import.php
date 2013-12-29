d564ed2426621eabf7228dc7304bbaa486aaca39
phildd <ict.advisor@nepal.inf.org>
2013-12-29 08:05:53 -0800

    Validate IP address ranges correctly on Alias Bulk Import
    The code was there to attempt to validate and implement IP address range lines in Alias Bulk Import e.g.
    10.20.0.0-10.21.22.0
    should produce a bunch of smaller ranges with appropriate CIDRs.
    This fixes the code so IP address ranges actually make it through into the resulting Alias.

--- usr/local/www/firewall_aliases_import.php
+++ usr/local/www/firewall_aliases_import.php
@@ -1,7 +1,7 @@
 <?php
 /* $Id$ */
 /*
-	firewall_aliases_edit.php
+	firewall_aliases_import.php
 	Copyright (C) 2005 Scott Ullrich
 	All rights reserved.
 
@@ -89,7 +89,7 @@ if($_POST['aliasimport'] <> "") {
 			if (is_iprange($impip)) {
 				list($startip, $endip) = explode('-', $impip);
 				$rangesubnets = ip_range_to_subnet_array($startip, $endip);
-				$address .= implode(" ", $rangesubnets);
+				$imported = array_merge($imported, $rangesubnets);
 			} else if (!is_ipaddr($impip) && !is_subnet($impip) && !empty($impip)) {
 				$input_errors[] = sprintf(gettext("%s is not an IP address. Please correct the error to continue"), $impip);
 			} elseif (!empty($impip)) {

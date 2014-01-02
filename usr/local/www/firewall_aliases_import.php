790a5bc23d79318f21ef8acd6d4d515997c28c4c
Phil Davis <phil.davis@world.inf.org>
2014-01-01 20:33:37 -0800

    Check for double vertical bar in bulk import descriptions

--- usr/local/www/firewall_aliases_import.php
+++ usr/local/www/firewall_aliases_import.php
@@ -85,21 +85,30 @@ if($_POST['aliasimport'] <> "") {
 		$tocheck = explode("\n", $_POST['aliasimport']);
 		$imported_ips = array();
 		$imported_descs = array();
+		$desc_err_found = false;
 		foreach ($tocheck as $impline) {
 			$implinea = explode(" ",trim($impline),2);
 			$impip = $implinea[0];
 			$impdesc = trim($implinea[1]);
-			if (is_iprange($impip)) {
-				list($startip, $endip) = explode('-', $impip);
-				$rangesubnets = ip_range_to_subnet_array($startip, $endip);
-				$imported_ips = array_merge($imported_ips, $rangesubnets);
-				$rangedescs = array_fill(0, count($rangesubnets), $impdesc);
-				$imported_descs = array_merge($imported_descs, $rangedescs);
-			} else if (!is_ipaddr($impip) && !is_subnet($impip) && !empty($impip)) {
-				$input_errors[] = sprintf(gettext("%s is not an IP address. Please correct the error to continue"), $impip);
-			} elseif (!empty($impip)) {
-				$imported_ips[] = $impip;
-				$imported_descs[] = $impdesc;
+			if (strpos($impdesc, "||") === false) {
+				if (is_iprange($impip)) {
+					list($startip, $endip) = explode('-', $impip);
+					$rangesubnets = ip_range_to_subnet_array($startip, $endip);
+					$imported_ips = array_merge($imported_ips, $rangesubnets);
+					$rangedescs = array_fill(0, count($rangesubnets), $impdesc);
+					$imported_descs = array_merge($imported_descs, $rangedescs);
+				} else if (!is_ipaddr($impip) && !is_subnet($impip) && !empty($impip)) {
+					$input_errors[] = sprintf(gettext("%s is not an IP address. Please correct the error to continue"), $impip);
+				} elseif (!empty($impip)) {
+					$imported_ips[] = $impip;
+					$imported_descs[] = $impdesc;
+				}
+			}
+			else {
+				if (!$desc_err_found) {
+					$input_errors[] = gettext("Descriptions may not contain double vertical bar ||. Please correct the error to continue");
+					$desc_err_found = true;
+				}
 			}
 		}
 	}

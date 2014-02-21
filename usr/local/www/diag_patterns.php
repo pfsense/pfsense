1b90e2d5db089d3b975c3d1cfcb568f106194e31
Renato Botelho <garga@FreeBSD.org>
2014-02-21 09:30:36 -0300

    Fix whitespaces and indent

--- usr/local/www/diag_patterns.php
+++ usr/local/www/diag_patterns.php
@@ -4,30 +4,30 @@
 	Exec+ v1.02-000 - Copyright 2001-2003, All rights reserved
 	Created by André Ribeiro and Hélder Pereira
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
 
-    1. Redistributions of source code must retain the above copyright notice,
-       this list of conditions and the following disclaimer.
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
 
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
 
-    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    POSSIBILITY OF SUCH DAMAGE.
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
 */
 
-/*	
-		pfSense_MODULE:	shaper
+/*
+	pfSense_MODULE:	shaper
 */
 
 ##|+PRIV
@@ -51,8 +51,8 @@ if (($_POST['submit'] == gettext("Upload Pattern file")) && is_uploaded_file($_F
 
 //Check if file has correct extension (.pat)
 function fileExtension($nameFile) {
-	$format = substr($nameFile, -4);	
-	return ($format == ".pat");	
+	$format = substr($nameFile, -4);
+	return ($format == ".pat");
 }
 
 $pgtitle = array(gettext("Diagnostics"), gettext("Add layer7 pattern"));
@@ -64,10 +64,10 @@ include("head.inc");
 <?php if ($ulmsg) echo "<p class=\"red\"><strong>" . $ulmsg . "</strong></p>\n"; ?>
 <div id="mainarea">
 <form action="diag_patterns.php" method="post" enctype="multipart/form-data" name="frmPattern">
-  <table class="tabcont" width="100%" border="0" cellpadding="6" cellspacing="0" summary="upload pattern">
-  	<tr>
-    	<td colspan="4" valign="top" class="listtopic"><?=gettext("Upload layer7 pattern file");?></td>
-  	</tr>
+<table class="tabcont" width="100%" border="0" cellpadding="6" cellspacing="0" summary="upload pattern">
+	<tr>
+		<td colspan="4" valign="top" class="listtopic"><?=gettext("Upload layer7 pattern file");?></td>
+	</tr>
 	<tr>
 		<td align="right"><strong><?=gettext("File to upload:");?></strong></td>
 		<td valign="top" class="label">
@@ -76,14 +76,14 @@ include("head.inc");
 	</tr>
 	<tr>
 		<td valign="top">&nbsp;&nbsp;&nbsp;</td>
-		<td valign="top" class="label">	
+		<td valign="top" class="label">
 			<input name="submit" type="submit" class="button" id="upload" value="<?=gettext("Upload Pattern file");?>" />
 		</td>
 	</tr>
 	<tr>
 		<td colspan="2" valign="top" height="16"></td>
-	</tr>    
-  </table>
+	</tr>
+</table>
 </form>
 </div>
 <?php include("fend.inc"); ?>

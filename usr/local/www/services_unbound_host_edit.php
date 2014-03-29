80120fff137277d0e856fc4d0224d38f1601d8f1
Warren Baker <warren@decoy.co.za>
2014-03-29 08:16:12 +0200

    Email add update

--- usr/local/www/services_unbound_host_edit.php
+++ usr/local/www/services_unbound_host_edit.php
@@ -3,7 +3,7 @@
 /*
 	services_unbound_edit_host.php
 	part of the pfSense project (https://www.pfsense.org)
-	Copyright (C) 2014	Warren Baker (warren@pfsense.org)
+	Copyright (C) 2014 Warren Baker (warren@decoy.co.za)
 	All rights reserved.
 	
 	Copyright (C) 2003-2004 Bob Zoller <bob@kludgebox.com> and Manuel Kasper <mk@neon1.net>.

2a230e80e5848052e4908689e3ae4d68f2ab5736
Nick Daniels <xnick.d@gmail.com>
2014-04-05 19:57:52 +0100

    Update system_advanced_misc.php

--- usr/local/www/system_advanced_misc.php
+++ usr/local/www/system_advanced_misc.php
@@ -173,7 +173,7 @@ if ($_POST) {
 		foreach ($ipsec_loglevels as $lkey => $ldescr) {
 			if (empty($_POST["ipsec_{$lkey}"]))
 				if (isset($config['ipsec']["ipsec_{$lkey}"]))
-					unset($config['ipsec']["ipsec_{$lkey}"]);
+				unset($config['ipsec']["ipsec_{$lkey}"]);
 			else
 				$config['ipsec']["ipsec_{$lkey}"] = $_POST["ipsec_{$lkey}"];
 		}

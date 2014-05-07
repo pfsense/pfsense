f0cf1a649a8c93523f3292e34de2b8e5a8800da1
jim-p <jimp@pfsense.org>
2014-05-07 13:20:57 -0400

    Clarify description to state you must save first before attempting to reset the log file size.

--- usr/local/www/diag_logs_settings.php
+++ usr/local/www/diag_logs_settings.php
@@ -297,7 +297,7 @@ function check_everything() {
 			<input name="logfilesize" id="logfilesize" type="text" class="formfld unknown" size="8" value="<?=htmlspecialchars($pconfig['logfilesize']);?>" /><br />
 			<?=gettext("Logs are held in constant-size circular log files. This field controls how large each log file is, and thus how many entries may exist inside the log By default this is approximately 500KB per log file, and there are nearly 20 such log files.") ?>
 			<br /><br />
-			<?=gettext("NOTE: Log sizes are changed the next time a log file is cleared or deleted. To immediately increase the size of the log files, you must clear all logs using the \"Reset Log Files\" option farther down this page. "); ?>
+			<?=gettext("NOTE: Log sizes are changed the next time a log file is cleared or deleted. To immediately increase the size of the log files, you must first save the options to set the size, then clear all logs using the \"Reset Log Files\" option farther down this page. "); ?>
 			<?=gettext("Be aware that increasing this value increases every log file size, so disk usage will increase significantly."); ?>
 			<?=gettext("Disk space currently used by log files: ") ?><?= exec("/usr/bin/du -sh /var/log | /usr/bin/awk '{print $1;}'"); ?>.
 			<?=gettext("Remaining disk space for log files: ") ?><?= exec("/bin/df -h /var/log | /usr/bin/awk '{print $4;}'"); ?>.

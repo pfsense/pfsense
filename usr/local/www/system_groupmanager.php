pfsense/pfsense

commit b4e9a4da0f438888abfd7f050964776d5bdf463e

XHTML Compliance - System Menu
Advanced - Admin Access Tab
Advanced - Firewall / NAT Tab
Cert Manager - Certificate Revocation Tab
User Manager - Users Tab
User Manager - Groups Tab

usr/local/www/system_groupmanager.php
@@ -299,14 +299,19 @@ function presubmit() {
 											<br />
 												<select size="10" style="width: 75%" name="notmembers[]" class="formselect" id="notmembers" onchange="clear_selected('members')" multiple="multiple">
 												<?php
+													$rowIndex = 0;
 													foreach ($config['system']['user'] as $user):
 														if (is_array($pconfig['members']) && in_array($user['uid'],$pconfig['members']))
 															continue;
+														$rowIndex++;
 												?>
 												<option value="<?=$user['uid'];?>" <?=$selected;?>>
 													<?=htmlspecialchars($user['name']);?>
 												</option>
-												<?php endforeach; ?>
+												<?php endforeach;
+												if ($rowIndex == 0)
+													echo "<option></option>";
+												?>
 											</select>
 											<br />
 										</td>
@@ -325,14 +330,19 @@ function presubmit() {
 											<br />
 											<select size="10" style="width: 75%" name="members[]" class="formselect" id="members" onchange="clear_selected('notmembers')" multiple="multiple">
 												<?php
+													$rowIndex = 0;
 													foreach ($config['system']['user'] as $user):
 														if (!(is_array($pconfig['members']) && in_array($user['uid'],$pconfig['members'])))
 															continue;
+														$rowIndex++;
 												?>
 												<option value="<?=$user['uid'];?>">
 													<?=htmlspecialchars($user['name']);?>
 												</option>
-												<?php endforeach; ?>
+												<?php endforeach;
+												if ($rowIndex == 0)
+													echo "<option></option>";
+												?>
 											</select>
 											<br />
 										</td>

usr/local/www/system_usermanager.php
@@ -523,16 +523,21 @@ function sshkeyClicked(obj) {
 											<br />
 											<select size="10" style="width: 75%" name="notgroups[]" class="formselect" id="notgroups" onchange="clear_selected('groups')" multiple="multiple">
 												<?php
+													$rowIndex = 0;
 													foreach ($config['system']['group'] as $group):
 														if ($group['gid'] == 1998) /* all users group */
 															continue;
 														if (is_array($pconfig['groups']) && in_array($group['name'],$pconfig['groups']))
 															continue;
+														$rowIndex++;
 												?>
 												<option value="<?=$group['name'];?>" <?=$selected;?>>
 													<?=htmlspecialchars($group['name']);?>
 												</option>
-												<?php endforeach; ?>
+												<?php endforeach;
+												if ($rowIndex == 0)
+													echo "<option></option>";
+												?>
 											</select>
 											<br />
 										</td>
@@ -551,18 +556,23 @@ function sshkeyClicked(obj) {
 											<br />
 											<select size="10" style="width: 75%" name="groups[]" class="formselect" id="groups" onchange="clear_selected('nogroups')" multiple="multiple">
 												<?php
+												$rowIndex = 0;
 												if (is_array($pconfig['groups'])) {
 													foreach ($config['system']['group'] as $group):
 														if ($group['gid'] == 1998) /* all users group */
 															continue;
 														if (!in_array($group['name'],$pconfig['groups']))
 															continue;
+														$rowIndex++;
 												?>
 												<option value="<?=$group['name'];?>">
 													<?=htmlspecialchars($group['name']);?>
 												</option>
 												<?php endforeach;
-												} ?>
+												}
+												if ($rowIndex == 0)
+													echo "<option></option>";
+												?>
 											</select>
 											<br />
 										</td>

usr/local/www/system_advanced_admin.php
@@ -360,7 +360,7 @@ function prot_change() {
 												if ($pconfig['ssl-certref'] == $cert['refid'])
 													$selected = "selected=\"selected\"";
 										?>
-										<option value="<?=$cert['refid'];?>"<?=$selected;?>><?=$cert['descr'];?></option>
+										<option value="<?=$cert['refid'];?>" <?=$selected;?>><?=$cert['descr'];?></option>
 										<?php endforeach; ?>
 									</select>
 								</td>

usr/local/www/system_advanced_firewall.php
@@ -324,7 +324,7 @@ function update_description(itemnum) {
 										<option value="conservative"<?php if($config['system']['optimization']=="conservative") echo " selected=\"selected\""; ?>><?=gettext("conservative");?></option>
 									</select>
 									<br />
-									<textarea readonly="readonly" cols="60" rows="2" id="info" name="info"style="padding:5px; border:1px dashed #990000; background-color: #ffffff; color: #000000; font-size: 8pt;"></textarea>
+									<textarea readonly="readonly" cols="60" rows="2" id="info" name="info" style="padding:5px; border:1px dashed #990000; background-color: #ffffff; color: #000000; font-size: 8pt;"></textarea>
 									<script type="text/javascript">
 									//<![CDATA[
 										update_description(document.forms[0].optimization.selectedIndex);

usr/local/www/system_crlmanager.php
@@ -311,7 +311,7 @@ function method_change() {
 									if ($pconfig['method'] == $method)
 										$selected = "selected=\"selected\"";
 								?>
-									<option value="<?=$method;?>"<?=$selected;?>><?=$desc;?></option>
+									<option value="<?=$method;?>" <?=$selected;?>><?=$desc;?></option>
 								<?php endforeach; ?>
 								</select>
 							</td>
@@ -333,7 +333,7 @@ function method_change() {
 									if ($pconfig['caref'] == $ca['refid'])
 										$selected = "selected=\"selected\"";
 								?>
-									<option value="<?=$ca['refid'];?>"<?=$selected;?>><?=$ca['descr'];?></option>
+									<option value="<?=$ca['refid'];?>" <?=$selected;?>><?=$ca['descr'];?></option>
 								<?php endforeach; ?>
 								</select>
 							</td>

commit 4c4c59b94d3054266e34601b48a7d495c8bf9e38

Make sure there are not empty options on dst select to avoid creating empty user or group. This issue was introduced by b4e9a4da

usr/local/www/system_groupmanager.php
@@ -210,6 +210,12 @@ function remove_selected(id) {
 function copy_selected(srcid, dstid) {
 	src_selbox = document.getElementById(srcid);
 	dst_selbox = document.getElementById(dstid);
+	count = dst_selbox.options.length;
+	for (index = count - 1; index >= 0; index--) {
+		if (dst_selbox.options[index].value == '') {
+			dst_selbox.remove(index);
+		}
+	}
 	count = src_selbox.options.length;
 	for (index = 0; index < count; index++) {
 		if (src_selbox.options[index].selected) {

usr/local/www/system_usermanager.php
@@ -393,6 +393,12 @@ function remove_selected(id) {
 function copy_selected(srcid, dstid) {
 	src_selbox = document.getElementById(srcid);
 	dst_selbox = document.getElementById(dstid);
+	count = dst_selbox.options.length;
+	for (index = count - 1; index >= 0; index--) {
+		if (dst_selbox.options[index].value == '') {
+			dst_selbox.remove(index);
+		}
+	}
 	count = src_selbox.options.length;
 	for (index = 0; index < count; index++) {
 		if (src_selbox.options[index].selected) {
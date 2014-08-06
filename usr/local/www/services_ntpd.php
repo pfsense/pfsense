bf2fb3db04c8724dec8eba8fd29dbb507da67655
jim-p <jimp@pfsense.org>
2014-08-06 15:52:36 -0400

    Encode interface/VIP descriptions before displaying them on the NTP daemon settings.

--- usr/local/www/services_ntpd.php
+++ usr/local/www/services_ntpd.php
@@ -112,7 +112,7 @@ include("head.inc");
 		echo "<option value='{$iface}'";
 		if (in_array($iface, $pconfig['interface']))
 			echo "selected";
-		echo ">{$ifacename}</option>\n";
+		echo ">" . htmlspecialchars($ifacename) . "</option>\n";
 	} ?>
 	</select>
 	<br/>

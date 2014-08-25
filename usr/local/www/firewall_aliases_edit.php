eab05c003633e253869ede8fc72a55c8d3a3ccbf
Renato Botelho <garga@FreeBSD.org>
2014-08-25 14:33:04 -0300

    Alias name cannot have more than 31 chars, add maxlength here just as an extra check. Ticket #3827

--- usr/local/www/firewall_aliases_edit.php
+++ usr/local/www/firewall_aliases_edit.php
@@ -643,7 +643,7 @@ if (empty($tab)) {
 		<td valign="top" class="vncellreq"><?=gettext("Name"); ?></td>
 		<td class="vtable">
 			<input name="origname" type="hidden" id="origname" class="formfld unknown" size="40" value="<?=htmlspecialchars($pconfig['name']);?>" />
-			<input name="name" type="text" id="name" class="formfld unknown" size="40" value="<?=htmlspecialchars($pconfig['name']);?>" />
+			<input name="name" type="text" id="name" class="formfld unknown" size="40" maxlength="31" value="<?=htmlspecialchars($pconfig['name']);?>" />
 			<?php if (isset($id) && $a_aliases[$id]): ?>
 				<input name="id" type="hidden" value="<?=htmlspecialchars($id);?>" />
 			<?php endif; ?>

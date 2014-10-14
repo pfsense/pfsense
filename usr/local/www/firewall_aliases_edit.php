dc4a8b9e541b0cffb84978669d1eef3b19094103
Chris Buechler <cmb@pfsense.org>
2014-10-13 23:07:08 -0500

    update input_error description after changes for ticket #3491

--- usr/local/www/firewall_aliases_edit.php
+++ usr/local/www/firewall_aliases_edit.php
@@ -137,7 +137,7 @@ if ($_POST) {
 		$input_errors[] = gettext("Reserved word used for alias name.");
 	} else {
 		if (is_validaliasname($_POST['name']) == false)
-			$input_errors[] = gettext("The alias name must be less than 32 characters long and may only consist of the characters") . " a-z, A-Z, 0-9, _.";
+			$input_errors[] = gettext("The alias name must be less than 32 characters long, may not consist of only numbers, and may only contain the following characters") . " a-z, A-Z, 0-9, _.";
 	}
 	/* check for name conflicts */
 	if (empty($a_aliases[$id])) {

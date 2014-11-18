ae38cb75cc27ba0eab34fea45a31df1343de5519
Renato Botelho <garga@FreeBSD.org>
2014-11-18 11:54:52 -0200

    Pass path parameter through htmlpecialchars()

--- usr/local/www/edit.php
+++ usr/local/www/edit.php
@@ -241,7 +241,7 @@ outputJavaScriptFileInline("javascript/base64.js");
 	<?php if($_GET['action'] == "load"): ?>
 		jQuery(window).load(
 			function() {
-				jQuery("#fbTarget").val("<?=$_GET['path'];?>");
+				jQuery("#fbTarget").val("<?=htmlspecialchars($_GET['path']);?>");
 				loadFile();
 			}
 		);

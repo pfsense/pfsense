c1819b48708ca8d712e1a3b9b8f7b2c4ed4e056b
Ermal LUÇI <eri@pfsense.org>
2014-11-28 17:05:09 +0100

    Process the rrd files one by one to fix https://forum.pfsense.org/index.php?topic=84693.0. Restore will come after

--- etc/rc.backup_rrd.sh
+++ etc/rc.backup_rrd.sh
@@ -6,12 +6,18 @@
 # Save the rrd databases to the config path.
 if [ -d "${RRDDBPATH}" ]; then
 	[ -z "$NO_REMOUNT" ] && /etc/rc.conf_mount_rw
+	[ -f "${CF_CONF_PATH}/rrd.tgz" ] && /bin/rm -f "${CF_CONF_PATH}"/rrd.tgz
+		
 	for rrdfile in "${RRDDBPATH}"/*.rrd ; do
 		xmlfile="${rrdfile%.rrd}.xml"
 		/usr/bin/nice -n20 /usr/local/bin/rrdtool dump "$rrdfile" "$xmlfile"
+		cd / && /usr/bin/tar -rf "${CF_CONF_PATH}"/rrd.tar -C / "${RRDDBPATH#/}"/*.xml
+		/bin/rm -f "${RRDDBPATH}"/*.xml
 	done
-	cd / && tar -czf "${CF_CONF_PATH}"/rrd.tgz -C / "${RRDDBPATH#/}"/*.xml
-	rm "${RRDDBPATH}"/*.xml
+	if [ -f "${CF_CONF_PATH}/rrd.tar" ]; then
+		/usr/bin/gzip "${CF_CONF_PATH}/rrd.tar"
+		/bin/mv "${CF_CONF_PATH}/rrd.tar.gz" "${CF_CONF_PATH}/rrd.tgz"
+	fi
 	[ -z "$NO_REMOUNT" ] && /etc/rc.conf_mount_ro
 fi
 

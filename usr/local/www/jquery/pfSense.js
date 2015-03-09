c39f7276cf398973655341ec505b069a86c8856e
Sander van Leeuwen <sander@kraagjes.nl>
2015-03-09 23:46:02 +0100

    Bind collapsable behaviour to options
    Refs. #21

--- usr/local/www/jquery/pfSense.js
+++ usr/local/www/jquery/pfSense.js
@@ -3,13 +3,48 @@
  */
 
 $(function() {
-	// Enable popovers globally
-	$('[data-toggle="popover"]').popover()
+	$(function() {
+		enableGlobalPopovers();
+		bindCollapseToOptions();
+	});
 
-	while (func = window.events.shift())
-		func();
-});
+	/**
+	 * Enable popovers globally
+	 */
+	var enableGlobalPopovers = function()
+	{
+		$('[data-toggle="popover"]').popover();
 
+		while (func = window.events.shift())
+			func();
+	};
+
+	/**
+	 * Attach collapsable behaviour to select options
+	 */
+	var bindCollapseToOptions = function()
+	{
+		var selects = $('select[data-toggle="collapse"]');
+
+		selects.on('change', function () {
+			var options = $(this).find('option');
+			var selectedValue = $(this).find(':selected').val();
+
+			// Hide related collapsables which are visible (.in)
+			options.each(function () {
+				if ($(this).val() == selectedValue)
+					return;
+
+				$('.toggle-' + $(this).val() + '.in').collapse('hide');
+			});
+
+			$('.toggle-' + selectedValue).collapse('show');
+		});
+
+		// Trigger change to open currently selected item
+		selects.trigger('change');
+	};
+})(jQuery);
 
 // Custom handler for data-toggle=disable
 +function ($) {

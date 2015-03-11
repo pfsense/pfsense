3a0a59c812f5719ff672c52f0f77c699513b713f
Renato Botelho <garga@FreeBSD.org>
2015-03-11 14:03:29 -0300

    Remove BEAST protection option since default cipher is now good and works with hifn cards

--- usr/local/www/system_advanced_admin.php
+++ usr/local/www/system_advanced_admin.php
@@ -58,7 +58,6 @@ $pconfig['disableconsolemenu'] = isset($config['system']['disableconsolemenu']);
 $pconfig['noantilockout'] = isset($config['system']['webgui']['noantilockout']);
 $pconfig['nodnsrebindcheck'] = isset($config['system']['webgui']['nodnsrebindcheck']);
 $pconfig['nohttpreferercheck'] = isset($config['system']['webgui']['nohttpreferercheck']);
-$pconfig['beast_protection'] = isset($config['system']['webgui']['beast_protection']);
 $pconfig['pagenamefirst'] = isset($config['system']['webgui']['pagenamefirst']);
 $pconfig['loginautocomplete'] = isset($config['system']['webgui']['loginautocomplete']);
 $pconfig['althostnames'] = $config['system']['webgui']['althostnames'];
@@ -171,11 +170,6 @@ if ($_POST) {
 		else
 			unset($config['system']['webgui']['nohttpreferercheck']);
 
-		if ($_POST['beast_protection'] == "yes")
-			$config['system']['webgui']['beast_protection'] = true;
-		else
-			unset($config['system']['webgui']['beast_protection']);
-
 		if ($_POST['pagenamefirst'] == "yes")
 			$config['system']['webgui']['pagenamefirst'] = true;
 		else
@@ -255,21 +249,6 @@ if ($_POST) {
 			services_unbound_configure();
 		conf_mount_ro();
 	}
-}
-
-unset($hwcrypto);
-$fd = @fopen("{$g['varlog_path']}/dmesg.boot", "r");
-if ($fd) {
-	while (!feof($fd)) {
-		$dmesgl = fgets($fd);
-		if (preg_match("/^hifn.: (.*?),/", $dmesgl, $matches)) {
-				unset($pconfig['beast_protection']);
-				$disable_beast_option = "disabled";
-				$hwcrypto = $matches[1];
-			break;
-		}
-	}
-	fclose($fd);
 }
 
 $pgtitle = array(gettext("System"),gettext("Advanced: Admin Access"));
@@ -485,22 +464,6 @@ function prot_change() {
 									"is protected against HTTP_REFERER redirection attempts. " .
 									"Check this box to disable this protection if you find that it interferes with " .
 									"webConfigurator access in certain corner cases such as using external scripts to interact with this system. More information on HTTP_REFERER is available from <a target='_blank' href='http://en.wikipedia.org/wiki/HTTP_referrer'>Wikipedia</a>."); ?>
-								</td>
-							</tr>
-							<tr>
-								<td width="22%" valign="top" class="vncell"><?=gettext("BEAST Attack Protection"); ?></td>
-								<td width="78%" class="vtable">
-									<input name="beast_protection" type="checkbox" id="beast_protection" value="yes" <?php if ($pconfig['beast_protection']) echo "checked=\"checked\""; ?> <?= $disable_beast_option ?>/>
-									<strong><?=gettext("Mitigate the BEAST SSL Attack"); ?></strong>
-									<br />
-									<?php echo gettext("When this is checked, the webConfigurator can mitigate BEAST SSL attacks. ") ?>
-									<br />
-									<?php 	if ($disable_beast_option) {
-											echo "<br />" . sprintf(gettext("This option has been automatically disabled because a conflicting cryptographic accelerator card has been detected (%s)."), $hwcrypto) . "<br /><br />";
-										} ?>
-									<?php echo gettext("This option is off by default because Hifn accelerators do NOT work with this option, and the GUI will not function. " .
-									"It is possible that other accelerators have a similar problem that is not yet known/documented. " .
-									"More information on BEAST is available from <a target='_blank' href='https://en.wikipedia.org/wiki/Transport_Layer_Security#BEAST_attack'>Wikipedia</a>."); ?>
 								</td>
 							</tr>
 							<tr>

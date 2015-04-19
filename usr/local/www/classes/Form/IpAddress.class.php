3c128b8c1227f1251aa263d18c06faa19decedaf
Sjon Hortensius <sjon@hortensius.net>
2015-04-19 14:56:26 +0200

    IpAddress - move from max mask=32 to 128, removed method-default @ fw_edit

--- usr/local/www/classes/Form/IpAddress.class.php
+++ usr/local/www/classes/Form/IpAddress.class.php
@@ -42,7 +42,7 @@ class Form_IpAddress extends Form_Input
 			$name,
 			null,
 			$value,
-			array_combine(range(32, 1), range(32, 1))
+			array_combine(range(128, 1), range(128, 1))
 		);
 
 		return $this;

a6ff5ea1298faea65630c370c6c5c2e9b9728595
sbeaver <sbeaver@netgate.com>
2015-04-23 08:27:02 -0400

    Removed unneeded class and fixed tabs/spaces

--- usr/local/www/diag_sockets.php
+++ usr/local/www/diag_sockets.php
@@ -78,49 +78,49 @@ $showAllOption = $showAll ? "" : "?showAll";
 		$name = ($tabindex == 0 ? 'IPv4' : 'IPv6');
 ?>
 
-<div class="table table-responsive">
+		<div class="table table-responsive">
 
-<table class="table table-hover table-striped table-condensed" id="<?=$name?>">
-	<thead>
-		<tr>
-			<th>
-				<?=$name?>
-			</th>
-		</tr>
-	</thead>
-	<tbody>
+			<table class="table table-hover table-striped table-condensed" id="<?=$name?>">
+				<thead>
+					<tr>
+						<th>
+							<?=$name?>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
 
 <?php
-		foreach (explode("\n", $table) as $i => $line) {
-			if ($i == 0)
-				$class = 'info';
-			else
-				$class = '';
+					foreach (explode("\n", $table) as $i => $line) {
+						if ($i == 0)
+							$class = 'info';
+						else
+							$class = '';
 
-			if (trim($line) == "")
-				continue;
+						if (trim($line) == "")
+							continue;
 
-			print("<tr id=\"$name$i\">\n");
-			$j = 0;
-			foreach (explode(' ', $line) as $entry) {
-				if ($entry == '' || $entry == "ADDRESS") continue;
-				if ($i == 0)
-					print("<th class=\"$class\">$entry</th>\n");
-				else
-					print("<td class=\"$class\">$entry</td>\n");
-				if ($i > 0)
-					$class = 'listr';
-				$j++;
-			}
-			print("</tr>\n");
-		}?>
-	</tbody>
-</table>
+						print("<tr id=\"$name$i\">\n");
+						$j = 0;
+						foreach (explode(' ', $line) as $entry) {
+							if ($entry == '' || $entry == "ADDRESS") continue;
+							if ($i == 0)
+								print("<th class=\"$class\">$entry</th>\n");
+							else
+								print("<td class=\"$class\">$entry</td>\n");
 
+							$j++;
+						}
+						print("</tr>\n");
+					}
+?>
+				</tbody>
+			</table>
+		</div>
 <?php
 	}
 ?>
-</div>
+	</div>
 </div>
 
 <a name="about"></a>
@@ -135,15 +135,15 @@ Or in case of showing all sockets the output for: "sockstat -4" and "sockstat -6
 The information listed for each socket is:
 			<br /><br />
 			<dl class="dl-horizontal responsive">
-				<dt>USER</dt>           <dd>The user who owns the socket.</dd>
-				<dt>COMMAND</dt>        <dd>The command which holds the socket.</dd>
-				<dt>PID</dt>            <dd>The process ID of the command which holds the socket.</dd>
-				<dt>FD</dt>             <dd>The file descriptor number of the socket.</dd>
-				<dt>PROTO</dt>          <dd>The transport protocol associated with the socket for Internet sockets, or the type of socket (stream or data-gram) for UNIX sockets.</dd>
-				<dt>ADDRESS</dt>        <dd>(UNIX sockets only) For bound sockets, this is the file-name of the socket.	For other sockets, it is the name, PID and file descriptor number of the peer, or ``(none)'' if the socket is neither bound nor connected.</dd>
-				<dt>LOCAL ADDRESS</dt>  <dd>(Internet sockets only) The address the local end of the socket is bound to (see getsockname(2)).</dd>
+				<dt>USER</dt>			<dd>The user who owns the socket.</dd>
+				<dt>COMMAND</dt>		<dd>The command which holds the socket.</dd>
+				<dt>PID</dt>			<dd>The process ID of the command which holds the socket.</dd>
+				<dt>FD</dt>				<dd>The file descriptor number of the socket.</dd>
+				<dt>PROTO</dt>			<dd>The transport protocol associated with the socket for Internet sockets, or the type of socket (stream or data-gram) for UNIX sockets.</dd>
+				<dt>ADDRESS</dt>		<dd>(UNIX sockets only) For bound sockets, this is the file-name of the socket. For other sockets, it is the name, PID and file descriptor number of the peer, or ``(none)'' if the socket is neither bound nor connected.</dd>
+				<dt>LOCAL ADDRESS</dt>	<dd>(Internet sockets only) The address the local end of the socket is bound to (see getsockname(2)).</dd>
 				<dt>FOREIGN ADDRESS</dt><dd>(Internet sockets only) The address the foreign end of the socket is bound to (see getpeername(2)).</dd>
-            </dl>
+			</dl>
 		</div>
 	</div>
 </div>

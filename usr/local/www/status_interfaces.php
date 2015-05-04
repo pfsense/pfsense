fb6c7bb85a970e5ea372fe754778429eaa7d6e26
sbeaver <sbeaver@netgate.com>
2015-05-04 07:46:49 -0400

    status_interfaces detailed changes per SH
    Useful suggestions. Thank you.
    
    htmlspecialchars() moved to showDef[Btn] functions
    Minor formatting changes

--- usr/local/www/status_interfaces.php
+++ usr/local/www/status_interfaces.php
@@ -68,7 +68,7 @@ $formtemplate = '<form name="%s" action="status_interfaces.php" method="post">' 
 function showDef($show, $term, $def) {
 	if($show) {
 		print('<dt>' . $term . '</dt>');
-		print('<dd>' . $def . '</dd>');
+		print('<dd>' . htmlspecialchars($def) . '</dd>');
 	}
 }
 
@@ -79,7 +79,7 @@ function showDefBtn($show, $term, $def, $ifval, $btnlbl) {
 	if($show) {
 		print('<dt>' . $term . '</dt>');
 		print('<dd>');
-		printf($formtemplate, $term, $ifvalue, $show, $def	. ' ', $btnlbl);
+		printf($formtemplate, $term, $ifvalue, $show, htmlspecialchars($def)	. ' ', $btnlbl);
 		print('</dd>');
 	}
 }
@@ -97,101 +97,101 @@ include("head.inc");
 
 <div class="panel panel-default">
 	<div class="panel-heading"><?=htmlspecialchars($ifname)?><?=gettext(" interface "); ?>(<?=htmlspecialchars($ifdescr)?>, <?=htmlspecialchars($ifinfo['hwif'])?>)</div>
-		<div class="panel-body">
-			<dl class="dl-horizontal">
+	<div class="panel-body">
+		<dl class="dl-horizontal">
 <?php
-			showDef(true, gettext("Status"), $ifinfo['status']);
-			showDefBtn($ifinfo['dhcplink'], 'DHCP', htmlspecialchars($ifinfo['dhcplink']), $ifdescr, $ifinfo['dhcplink'] == "up" ? gettext("Release") : gettext("Renew"));
-			showDefBtn($ifinfo['dhcp6link'], 'DHCP6', htmlspecialchars($ifinfo['dhcp6link']), $ifdescr, $ifinfo['dhcp6link'] == "up" ? gettext("Release") : gettext("Renew"));
-			showDefBtn($ifinfo['pppoelink'], 'PPPoE', htmlspecialchars($ifinfo['pppoelink']), $ifdescr, $ifinfo['pppoelink'] == "up" ? gettext("Disconnect") : gettext("Connect"));
-			showDefBtn($ifinfo['pptplink'], 'PPTP', htmlspecialchars($ifinfo['pptplink']), $ifdescr, $ifinfo['pptplink'] == "up" ? gettext("Disconnect") : gettext("Connect"));
-			showDefBtn($ifinfo['l2tplink'], 'L2TP', htmlspecialchars($ifinfo['l2tplink']), $ifdescr, $ifinfo['l2tplink'] == "up" ? gettext("Disconnect") : gettext("Connect"));
-			showDefBtn($ifinfo['ppplink'], 'L2TP', htmlspecialchars($ifinfo['ppplink']), $ifdescr, $ifinfo['ppplink'] == "up" && !$ifinfo['nodevice'] ? gettext("Disconnect") : gettext("Connect"));
-			showDef($ifinfo['ppp_uptime'] || $ifinfo['ppp_uptime_accumulated'], gettext("Uptime ") . $ifinfo['ppp_uptime_accumulated'] ? '(historical)':'', htmlspecialchars($ifinfo['ppp_uptime']) . htmlspecialchars($ifinfo['ppp_uptime_accumulated']));
-			showDef($ifinfo['cell_rssi'], gettext("Cell Signal (RSSI)"), htmlspecialchars($ifinfo['cell_rssi']));
-			showDef($ifinfo['cell_mode'], gettext("Cell Mode"), htmlspecialchars($ifinfo['cell_mode']));
-			showDef($ifinfo['cell_simstate'], gettext("Cell SIM State"), htmlspecialchars($ifinfo['cell_simstate']));
-			showDef($ifinfo['cell_service'], gettext("Cell Service"), htmlspecialchars($ifinfo['cell_service']));
-			showDef($ifinfo['cell_bwupstream'], gettext("Cell Upstream"), htmlspecialchars($ifinfo['cell_bwupstream']));
-			showDef($ifinfo['cell_bwdownstream'], gettext("Cell Downstream"), htmlspecialchars($ifinfo['cell_bwdownstream']));
-			showDef($ifinfo['cell_upstream'], gettext("Cell Current Up"), htmlspecialchars($ifinfo['cell_upstream']));
-			showDef($ifinfo['cell_downstream'], gettext("Cell Current Down"), htmlspecialchars($ifinfo['cell_downstream']));
+		showDef(true, gettext("Status"), $ifinfo['status']);
+		showDefBtn($ifinfo['dhcplink'], 'DHCP', $ifinfo['dhcplink'], $ifdescr, $ifinfo['dhcplink'] == "up" ? gettext("Release") : gettext("Renew"));
+		showDefBtn($ifinfo['dhcp6link'], 'DHCP6', $ifinfo['dhcp6link'], $ifdescr, $ifinfo['dhcp6link'] == "up" ? gettext("Release") : gettext("Renew"));
+		showDefBtn($ifinfo['pppoelink'], 'PPPoE', $ifinfo['pppoelink'], $ifdescr, $ifinfo['pppoelink'] == "up" ? gettext("Disconnect") : gettext("Connect"));
+		showDefBtn($ifinfo['pptplink'], 'PPTP', $ifinfo['pptplink'], $ifdescr, $ifinfo['pptplink'] == "up" ? gettext("Disconnect") : gettext("Connect"));
+		showDefBtn($ifinfo['l2tplink'], 'L2TP', $ifinfo['l2tplink'], $ifdescr, $ifinfo['l2tplink'] == "up" ? gettext("Disconnect") : gettext("Connect"));
+		showDefBtn($ifinfo['ppplink'], 'L2TP', $ifinfo['ppplink'], $ifdescr, ($ifinfo['ppplink'] == "up" && !$ifinfo['nodevice']) ? gettext("Disconnect") : gettext("Connect"));
+		showDef($ifinfo['ppp_uptime'] || $ifinfo['ppp_uptime_accumulated'], gettext("Uptime ") . $ifinfo['ppp_uptime_accumulated'] ? '(historical)':'', $ifinfo['ppp_uptime'] . $ifinfo['ppp_uptime_accumulated']);
+		showDef($ifinfo['cell_rssi'], gettext("Cell Signal (RSSI)"), $ifinfo['cell_rssi']);
+		showDef($ifinfo['cell_mode'], gettext("Cell Mode"), $ifinfo['cell_mode']);
+		showDef($ifinfo['cell_simstate'], gettext("Cell SIM State"), $ifinfo['cell_simstate']);
+		showDef($ifinfo['cell_service'], gettext("Cell Service"), $ifinfo['cell_service']);
+		showDef($ifinfo['cell_bwupstream'], gettext("Cell Upstream"), $ifinfo['cell_bwupstream']);
+		showDef($ifinfo['cell_bwdownstream'], gettext("Cell Downstream"), $ifinfo['cell_bwdownstream']);
+		showDef($ifinfo['cell_upstream'], gettext("Cell Current Up"), $ifinfo['cell_upstream']);
+		showDef($ifinfo['cell_downstream'], gettext("Cell Current Down"), $ifinfo['cell_downstream']);
 
-			if ($ifinfo['macaddr']) {
-				$mac=$ifinfo['macaddr'];
-				$mac_hi = strtoupper($mac[0] . $mac[1] . $mac[3] . $mac[4] . $mac[6] . $mac[7]);
-				showDef(isset($mac_man[$mac_hi]), gettext('MAC Address'),  ' - ' . htmlspecialchars($mac_man[$mac_hi]));
+		if ($ifinfo['macaddr']) {
+			$mac=$ifinfo['macaddr'];
+			$mac_hi = strtoupper($mac[0] . $mac[1] . $mac[3] . $mac[4] . $mac[6] . $mac[7]);
+			showDef(isset($mac_man[$mac_hi]), gettext('MAC Address'),  ' - ' . $mac_man[$mac_hi]);
 			}
 
-			if ($ifinfo['status'] != "down"):
-				if ($ifinfo['dhcplink'] != "down" && $ifinfo['pppoelink'] != "down" && $ifinfo['pptplink'] != "down"):
-					showDef($ifinfo['ipaddr'], gettext('IPv4 Address'), htmlspecialchars($ifinfo['ipaddr']));
-					showDef($ifinfo['subnet'], gettext('Subnet mask IPv4'), $ifinfo['subnet']);
-					showDef($ifinfo['gateway'], gettext('Gateway IPv4'), $ifinfo['gateway']);
-					showDef($ifinfo['linklocal'], gettext('IPv6 Link Local'), htmlspecialchars($ifinfo['linklocal']));
-					showDef($ifinfo['ipaddrv6'], gettext('IPv6 Address'), htmlspecialchars($ifinfo['ipaddrv6']));
-					showDef($ifinfo['subnetv6'], gettext('Subnet mask IPv6'), htmlspecialchars($ifinfo['subnetv6']));
-					showDef($ifinfo['gatewayv6'], gettext("Gateway IPv6"), htmlspecialchars($config['interfaces'][$ifdescr]['gatewayv6']) . htmlspecialchars($ifinfo['gatewayv6']));
-					if ($ifdescr == "wan" && file_exists("{$g['varetc_path']}/resolv.conf")) {
-						$dns_servers = get_dns_servers();
-						$dnscnt = 0;
-						foreach($dns_servers as $dns) {
-							showDef(true, $dnscnt == 0 ? gettext('ISP DNS servers'):'', $dns);
-							$dnscnt++;
-						}
+		if ($ifinfo['status'] != "down") {
+			if ($ifinfo['dhcplink'] != "down" && $ifinfo['pppoelink'] != "down" && $ifinfo['pptplink'] != "down") {
+				showDef($ifinfo['ipaddr'], gettext('IPv4 Address'), $ifinfo['ipaddr']);
+				showDef($ifinfo['subnet'], gettext('Subnet mask IPv4'), $ifinfo['subnet']);
+				showDef($ifinfo['gateway'], gettext('Gateway IPv4'), $ifinfo['gateway']);
+				showDef($ifinfo['linklocal'], gettext('IPv6 Link Local'), $ifinfo['linklocal']);
+				showDef($ifinfo['ipaddrv6'], gettext('IPv6 Address'), $ifinfo['ipaddrv6']);
+				showDef($ifinfo['subnetv6'], gettext('Subnet mask IPv6'), $ifinfo['subnetv6']);
+				showDef($ifinfo['gatewayv6'], gettext("Gateway IPv6"), $config['interfaces'][$ifdescr]['gatewayv6'] . $ifinfo['gatewayv6']);
+
+				if ($ifdescr == "wan" && file_exists("{$g['varetc_path']}/resolv.conf")) {
+					$dns_servers = get_dns_servers();
+					$dnscnt = 0;
+					foreach($dns_servers as $dns) {
+						showDef(true, $dnscnt == 0 ? gettext('ISP DNS servers'):'', $dns);
+						$dnscnt++;
 					}
-					endif;
-
-				showDef($ifinfo['mtu'], gettext("MTU"), htmlspecialchars($ifinfo['mtu']));
-				showDef($ifinfo['media'], gettext("Media"), htmlspecialchars($ifinfo['media']));
-				showDef($ifinfo['laggproto'], gettext("LAGG Protocol"), htmlspecialchars($ifinfo['laggproto']));
-				showDef($ifinfo['laggport'],gettext("LAGG Ports"),htmlspecialchars($laggport));
-				showDef($ifinfo['channel'],gettext("Channel"),htmlspecialchars($ifinfo['channel']));
-				showDef($ifinfo['ssid'],gettext("SSID"),htmlspecialchars($ifinfo['ssid']));
-				showDef($ifinfo['bssid'],gettext("BSSID"),htmlspecialchars($ifinfo['bssid']));
-				showDef($ifinfo['rate'],gettext("Rate"),htmlspecialchars($ifinfo['rate']));
-				showDef($ifinfo['rssi'],gettext("RSSI"),htmlspecialchars($ifinfo['rssi']));
-				showDef(true,gettext("In/out packets"),htmlspecialchars($ifinfo['inpkts'] . '/' . $ifinfo['outpkts']));
-				showDef(true,gettext("In/out packets (pass)"),htmlspecialchars($ifinfo['inpktspass'] . "/" . $ifinfo['outpktspass']));
-				showDef(true,gettext("In/out packets (block)"),htmlspecialchars($ifinfo['inpktsblock'] . "/" . $ifinfo['outpktsblock']));
-				showDef(isset($ifinfo['inerrs']),gettext("In/out errors"),htmlspecialchars($ifinfo['inerrs'] . "/" . $ifinfo['outerrs']));
-				showDef(isset($ifinfo['collisions']),gettext("Collisions"),htmlspecialchars($ifinfo['collisions']));
-			endif;
-
-			showDef($ifinfo['bridge'], gettext('Bridge (') . $ifinfo['bridgeint'] . ')', $ifinfo['bridge']);
-
-			if(file_exists("/usr/bin/vmstat")) {
-				$real_interface = "";
-				$interrupt_total = "";
-				$interrupt_sec = "";
-				$real_interface = $ifinfo['hwif'];
-				$interrupt_total = `vmstat -i | grep $real_interface | awk '{ print $3 }'`;
-				$interrupt_sec = `vmstat -i | grep $real_interface | awk '{ print $4 }'`;
-
-				if(strstr($interrupt_total, "hci")) {
-					$interrupt_total = `vmstat -i | grep $real_interface | awk '{ print $4 }'`;
-					$interrupt_sec = `vmstat -i | grep $real_interface | awk '{ print $5 }'`;
 				}
-
-				unset($interrupt_total);
-
-				showDef($interrupt_total, gettext('Total interrupts'), $interrupt_total);
-				showDef($interrupt_total, '', $interrupt_sec . " " . $interrupt_total);
 			}
-			break;
-		endforeach;
+
+			showDef($ifinfo['mtu'], gettext("MTU"), $ifinfo['mtu']);
+			showDef($ifinfo['media'], gettext("Media"), $ifinfo['media']);
+			showDef($ifinfo['laggproto'], gettext("LAGG Protocol"), $ifinfo['laggproto']);
+			showDef($ifinfo['laggport'],gettext("LAGG Ports"),$laggport);
+			showDef($ifinfo['channel'],gettext("Channel"),$ifinfo['channel']);
+			showDef($ifinfo['ssid'],gettext("SSID"),$ifinfo['ssid']);
+			showDef($ifinfo['bssid'],gettext("BSSID"),$ifinfo['bssid']);
+			showDef($ifinfo['rate'],gettext("Rate"),$ifinfo['rate']);
+			showDef($ifinfo['rssi'],gettext("RSSI"),$ifinfo['rssi']);
+			showDef(true,gettext("In/out packets"),$ifinfo['inpkts'] . '/' . $ifinfo['outpkts']);
+			showDef(true,gettext("In/out packets (pass)"),$ifinfo['inpktspass'] . "/" . $ifinfo['outpktspass']);
+			showDef(true,gettext("In/out packets (block)"),$ifinfo['inpktsblock'] . "/" . $ifinfo['outpktsblock']);
+			showDef(isset($ifinfo['inerrs']),gettext("In/out errors"),$ifinfo['inerrs'] . "/" . $ifinfo['outerrs']);
+			showDef(isset($ifinfo['collisions']),gettext("Collisions"),$ifinfo['collisions']);
+		} // e-o-if ($ifinfo['status'] != "down")
+
+		showDef($ifinfo['bridge'], gettext('Bridge (') . $ifinfo['bridgeint'] . ')', $ifinfo['bridge']);
+
+		if(file_exists("/usr/bin/vmstat")) {
+			$real_interface = "";
+			$interrupt_total = "";
+			$interrupt_sec = "";
+			$real_interface = $ifinfo['hwif'];
+			$interrupt_total = `vmstat -i | grep $real_interface | awk '{ print $3 }'`;
+			$interrupt_sec = `vmstat -i | grep $real_interface | awk '{ print $4 }'`;
+
+			if(strstr($interrupt_total, "hci")) {
+				$interrupt_total = `vmstat -i | grep $real_interface | awk '{ print $4 }'`;
+				$interrupt_sec = `vmstat -i | grep $real_interface | awk '{ print $5 }'`;
+			}
+
+			unset($interrupt_total);
+
+			showDef($interrupt_total, gettext('Total interrupts'), $interrupt_total);
+			showDef($interrupt_total, '', $interrupt_sec . " " . $interrupt_total);
+		}
+		break;
+	endforeach;
 ?>
-			</dl>
-		</div>
-	</div>
-	<div class="alert alert-warning" role="alert">
-		<?=gettext("Using dial-on-demand will bring the connection up again if any packet ".
-		"triggers it. To substantiate this point: disconnecting manually ".
-		"will <strong>not</strong> prevent dial-on-demand from making connections ".
-		"to the outside! Don't use dial-on-demand if you want to make sure that the line ".
-		"is kept disconnected.");?>
+		</dl>
 	</div>
 </div>
 
-<?php include("foot.inc"); ?>
+<div class="alert alert-warning" role="alert">
+	<?=gettext("Using dial-on-demand will bring the connection up again if any packet ".
+	"triggers it. To substantiate this point: disconnecting manually ".
+	"will <strong>not</strong> prevent dial-on-demand from making connections ".
+	"to the outside! Don't use dial-on-demand if you want to make sure that the line ".
+	"is kept disconnected.");?>
+</div>
 
+<?php include("foot.inc"); ?>

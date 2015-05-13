8d66660a6869d33a3c9a34fa4bc8c5a9fed5a9bf
sbeaver <sbeaver@netgate.com>
2015-05-13 15:52:01 -0400

    services_captiveportal_hostname_edit.php Conversion complete
    Ready for review

--- usr/local/www/services_captiveportal_hostname_edit.php
+++ usr/local/www/services_captiveportal_hostname_edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 	services_captiveportal_hostname_edit.php
 	Copyright (C) 2013-2015 Electric Sheep Fencing, LP
@@ -8,17 +8,17 @@
 	Originally part of m0n0wall (http://m0n0.ch/wall)
 	Copyright (C) 2004 Dinesh Nair <dinesh@alphaque.com>
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -32,7 +32,7 @@
 */
 /*
 	pfSense_BUILDER_BINARIES:	/sbin/ipfw
-	pfSense_MODULE:	captiveportal
+	pfSense_MODULE: captiveportal
 */
 
 ##|+PRIV
@@ -62,24 +62,27 @@ $shortcut_section = "captiveportal";
 
 $cpzone = $_GET['zone'];
 if (isset($_POST['zone']))
-        $cpzone = $_POST['zone'];
+	$cpzone = $_POST['zone'];
 
 if (empty($cpzone) || empty($config['captiveportal'][$cpzone])) {
-        header("Location: services_captiveportal_zones.php");
-        exit;
+	header("Location: services_captiveportal_zones.php");
+	exit;
 }
 
 if (!is_array($config['captiveportal']))
-        $config['captiveportal'] = array();
+	$config['captiveportal'] = array();
+
 $a_cp =& $config['captiveportal'];
 
 if (is_numericint($_GET['id']))
 	$id = $_GET['id'];
+
 if (isset($_POST['id']) && is_numericint($_POST['id']))
 	$id = $_POST['id'];
 
 if (!is_array($a_cp[$cpzone]['allowedhostname']))
 	$a_cp[$cpzone]['allowedhostname'] = array();
+
 $a_allowedhostnames = &$a_cp[$cpzone]['allowedhostname'];
 
 if (isset($id) && $a_allowedhostnames[$id]) {
@@ -93,17 +96,16 @@ if (isset($id) && $a_allowedhostnames[$id]) {
 }
 
 if ($_POST) {
-
 	unset($input_errors);
 	$pconfig = $_POST;
 
 	/* input validation */
 	$reqdfields = explode(" ", "hostname");
 	$reqdfieldsn = array(gettext("Allowed Hostname"));
-	
+
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
-	
-	if (($_POST['hostname'] && !is_hostname($_POST['hostname']))) 
+
+	if (($_POST['hostname'] && !is_hostname($_POST['hostname'])))
 		$input_errors[] = sprintf(gettext("A valid Hostname must be specified. [%s]"), $_POST['hostname']);
 
 	if ($_POST['bw_up'] && !is_numeric($_POST['bw_up']))
@@ -114,11 +116,11 @@ if ($_POST) {
 	foreach ($a_allowedhostnames as $ipent) {
 		if (isset($id) && ($a_allowedhostnames[$id]) && ($a_allowedhostnames[$id] === $ipent))
 			continue;
-		
+
 		if ($ipent['hostname'] == $_POST['hostname']){
 			$input_errors[] = sprintf("[%s] %s.", $_POST['hostname'], gettext("already allowed")) ;
 			break ;
-		}	
+		}
 	}
 
 	if (!$input_errors) {
@@ -137,7 +139,7 @@ if ($_POST) {
 			$a_allowedhostnames[] = $ip;
 
 		allowedhostnames_sort();
-		
+
 		write_config();
 
 		$rules = captiveportal_allowedhostname_configure();
@@ -145,73 +147,90 @@ if ($_POST) {
 		$cpzoneid = $a_cp[$cpzone]['zoneid'];
 		mwexec("/sbin/ipfw -x {$cpzoneid} {$g['tmp_path']}/hostname_rules");
 		unset($rules);
-		
+
 		header("Location: services_captiveportal_hostname.php?zone={$cpzone}");
 		exit;
 	}
 }
 
+function build_dir_list() {
+	$dirs = array(gettext("Both"),gettext("From"),gettext("To"));
+	$dirlist = array();
+
+	foreach ($dirs as $dir) {
+		$dirlist[strtolower($dir)] = $dir;
+	}
+
+	return($dirlist);
+}
+
 include("head.inc");
 
-?>
-<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
-<?php include("fbegin.inc"); ?>
-<?php if ($input_errors) print_input_errors($input_errors); ?>
-		<form action="services_captiveportal_hostname_edit.php" method="post" name="iform" id="iform">
-		<table width="100%" border="0" cellpadding="6" cellspacing="0" summary="captiveportal hostname edit">
-		<tr>
-			<td width="22%" valign="top" class="vncellreq"><?=gettext("Direction"); ?></td>
-			<td width="78%" class="vtable"> 
-			<select name="dir" class="formfld">
-		<?php 
-			$dirs = array(gettext("Both"),gettext("From"),gettext("To")) ;
-			foreach ($dirs as $dir): ?>
-				<option value="<?=strtolower($dir);?>" <?php if (strtolower($dir) == strtolower($pconfig['dir'])) echo "selected=\"selected\"";?> >
-				<?=htmlspecialchars($dir);?>
-				</option>
-		<?php endforeach; ?>
-			</select>
-			<br /> 
-			<span class="vexpl"><?=gettext("Use"); ?> <em><?=gettext("From"); ?></em> <?=gettext("to always allow an Hostname through the captive portal (without authentication)"); ?>. 
-				<?=gettext("Use"); ?> <em><?=gettext("To"); ?></em> <?=gettext("to allow access from all clients (even non-authenticated ones) behind the portal to this Hostname"); ?>.</span></td>
-		</tr>
-		<tr>
-			<td width="22%" valign="top" class="vncellreq"><?=gettext("Hostname"); ?></td>
-			<td width="78%" class="vtable"> 
-				<?=$mandfldhtml;?><input name="hostname" type="text" class="formfld unknown" id="hostname" size="17" value="<?=htmlspecialchars($pconfig['hostname']);?>" />
-			<br /> 
-			<span class="vexpl"><?=gettext("Hostname");?>.</span></td>
-		</tr>
-		<tr>
-			<td width="22%" valign="top" class="vncell"><?=gettext("Description"); ?></td>
-			<td width="78%" class="vtable"> 
-			<input name="descr" type="text" class="formfld unknown" id="descr" size="40" value="<?=htmlspecialchars($pconfig['descr']);?>" />
-			<br /> <span class="vexpl"><?=gettext("You may enter a description here for your reference (not parsed)"); ?>.</span></td>
-			</tr>
-		<tr>
-			<td width="22%" valign="top" class="vncell"><?=gettext("Bandwidth up"); ?></td>
-			<td width="78%" class="vtable">
-			<input name="bw_up" type="text" class="formfld unknown" id="bw_up" size="10" value="<?=htmlspecialchars($pconfig['bw_up']);?>" />
-			<br /> <span class="vexpl"><?=gettext("Enter a upload limit to be enforced on this Hostname in Kbit/s"); ?></span></td>
-		</tr>
-		<tr>
-			<td width="22%" valign="top" class="vncell"><?=gettext("Bandwidth down"); ?></td>
-			<td width="78%" class="vtable">
-			<input name="bw_down" type="text" class="formfld unknown" id="bw_down" size="10" value="<?=htmlspecialchars($pconfig['bw_down']);?>" />
-			<br /> <span class="vexpl"><?=gettext("Enter a download limit to be enforced on this Hostname in Kbit/s"); ?></span></td>
-		</tr>
-		<tr>
-			<td width="22%" valign="top">&nbsp;</td>
-			<td width="78%"> 
-				<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save"); ?>" />
-				<input name="zone" type="hidden" value="<?=htmlspecialchars($cpzone);?>" />
-				<?php if (isset($id) && $a_allowedhostnames[$id]): ?>
-					<input name="id" type="hidden" value="<?=htmlspecialchars($id);?>" />
-				<?php endif; ?>
-			</td>
-		</tr>
-	</table>
-</form>
-<?php include("fend.inc"); ?>
-</body>
-</html>
+if ($input_errors)
+	print_input_errors($input_errors);
+
+require('classes/Form.class.php');
+
+$form = new Form(new Form_Button(
+	'Submit',
+	gettext("Save")
+));
+
+$section = new Form_Section('Captive Portal Hostname settings');
+
+$section->addInput(new Form_Select(
+	'dir',
+	'Direction',
+	strtolower($pconfig['dir']),
+	build_dir_list()
+))->setHelp('Use "From" to always allow an Hostname through the captive portal (without authentication). ' .
+			'Use "To" to allow access from all clients (even non-authenticated ones) behind the portal to this Hostname.');
+
+$section->addInput(new Form_Input(
+	'hostname',
+	'Hostname',
+	'text',
+	$pconfig['hostname']
+));
+
+$section->addInput(new Form_Input(
+	'descr',
+	'Description',
+	'text',
+	$pconfig['descr']
+))->setHelp('You may enter a description here for your reference (not parsed).');
+
+$section->addInput(new Form_Input(
+	'bw_up',
+	'Bandwidth up',
+	'text',
+	$pconfig['bw_up']
+))->setHelp('Enter a upload limit to be enforced on this Hostname in Kbit/s');
+
+$section->addInput(new Form_Input(
+	'bw_down',
+	'Bandwidth down',
+	'text',
+	$pconfig['bw_down']
+))->setHelp('Enter a download limit to be enforced on this Hostname in Kbit/s');
+
+$section->addInput(new Form_Input(
+	'zone',
+	null,
+	'hidden',
+	htmlspecialchars($cpzone)
+));
+
+if (isset($id) && $a_allowedhostnames[$id]) {
+	$section->addInput(new Form_Input(
+		'id',
+		null,
+		'hidden',
+		htmlspecialchars($id)
+	));
+}
+
+$form->add($section);
+print($form);
+
+include("foot.inc");

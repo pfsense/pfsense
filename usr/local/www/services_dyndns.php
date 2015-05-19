f5606ded3e0041f7483cf380bb4da932eab5012e
sbeaver <sbeaver@netgate.com>
2015-05-19 17:16:34 -0400

    services_dyndns.php Conversion complete
    Ready for review
    
    This page contains only a very simple table but with a lot of PHP logic
    and not much HTML. Retaining some print()s seems to make the most sense
    in this case.

--- usr/local/www/services_dyndns.php
+++ usr/local/www/services_dyndns.php
@@ -30,7 +30,7 @@
 */
 /*
 	pfSense_BUILDER_BINARIES:	/usr/bin/host
-	pfSense_MODULE:	dyndns
+	pfSense_MODULE: dyndns
 */
 
 ##|+PRIV
@@ -48,7 +48,6 @@ if (!is_array($config['dyndnses']['dyndns']))
 $a_dyndns = &$config['dyndnses']['dyndns'];
 
 if ($_GET['act'] == "del") {
-
 	$conf = $a_dyndns[$_GET['id']];
 	@unlink("{$g['conf_path']}/dyndns_{$conf['interface']}{$conf['type']}" . escapeshellarg($conf['host']) . "{$conf['id']}.cache");
 	unset($a_dyndns[$_GET['id']]);
@@ -63,144 +62,146 @@ if ($_GET['act'] == "del") {
 $pgtitle = array(gettext("Services"), gettext("Dynamic DNS clients"));
 include("head.inc");
 
-?>
+if ($input_errors)
+	print_input_errors($input_errors);
 
-<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
-<?php include("fbegin.inc"); ?>
-<form action="services_dyndns.php" method="post" name="iform" id="iform">
-<?php if ($input_errors) print_input_errors($input_errors); ?>
-<table width="100%" border="0" cellpadding="0" cellspacing="0" summary="dynamic dns">
-  <tr>
-	<td>
-<?php
-	$tab_array = array();
-	$tab_array[] = array(gettext("DynDns"), true, "services_dyndns.php");
-	$tab_array[] = array(gettext("RFC 2136"), false, "services_rfc2136.php");
-	display_top_tabs($tab_array);
+$tab_array = array();
+$tab_array[] = array(gettext("DynDns"), true, "services_dyndns.php");
+$tab_array[] = array(gettext("RFC 2136"), false, "services_rfc2136.php");
+display_top_tabs($tab_array);
 ?>
-	</td>
-  </tr>
-  <tr>
-	<td>
-	  <div id="mainarea">
-	  <table class="tabcont" width="100%" border="0" cellpadding="0" cellspacing="0" summary="main area">
-		<tr>
-		  <td width="5%"  class="listhdrr"><?=gettext("Interface");?></td>
-		  <td width="15%" class="listhdrr"><?=gettext("Service");?></td>
-		  <td width="20%" class="listhdrr"><?=gettext("Hostname");?></td>
-		  <td width="20%" class="listhdrr"><?=gettext("Cached IP");?></td>
-		  <td width="50%" class="listhdr"><?=gettext("Description");?></td>
-		  <td width="10%" class="list"></td>
-		</tr>
-		<?php $i = 0; foreach ($a_dyndns as $dyndns): ?>
-		<tr ondblclick="document.location='services_dyndns_edit.php?id=<?=$i;?>'">
-		  <td class="listlr">
-		  <?php	$iflist = get_configured_interface_with_descr();
-			foreach ($iflist as $if => $ifdesc) {
-				if ($dyndns['interface'] == $if) {
-					if (!isset($dyndns['enable']))
-						echo "<span class=\"gray\">{$ifdesc}</span>";
-					else
-						echo "{$ifdesc}";
-					break;
-				}
-			}
-			$groupslist = return_gateway_groups_array();
-			foreach ($groupslist as $if => $group) {
-				if ($dyndns['interface'] == $if) {
-					if (!isset($dyndns['enable']))
-						echo "<span class=\"gray\">{$if}</span>";
-					else
-						echo "{$if}";
-					break;
-				}
-			}
-		  ?>
-		  </td>
-		  <td class="listr">
-		  <?php
-			$types = explode(",", DYNDNS_PROVIDER_DESCRIPTIONS);
-			$vals = explode(" ", DYNDNS_PROVIDER_VALUES);
-			for ($j = 0; $j < count($vals); $j++)
-				if ($vals[$j] == $dyndns['type']) {
-					if (!isset($dyndns['enable']))
-						echo "<span class=\"gray\">" . htmlspecialchars($types[$j]) . "</span>";
-					else
-						echo htmlspecialchars($types[$j]);
-					break;
-				}
-		  ?>
-		  </td>
-		  <td class="listr">
-		  <?php
+<form action="services_dyndns.php" method="post" name="iform" id="iform">
+	<div class="table-responsive">
+		<table class="table table-striped table-hover table-condensed">
+			<thead>
+				<tr>
+					<th><?=gettext("Interface")?></th>
+					<th><?=gettext("Service")?></th>
+					<th><?=gettext("Hostname")?></th>
+					<th><?=gettext("Cached IP")?></th>
+					<th><?=gettext("Description")?></th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+<?php
+$i = 0;
+foreach ($a_dyndns as $dyndns):
+?>
+				<tr>
+					<td>
+<?php
+	$iflist = get_configured_interface_with_descr();
+	foreach ($iflist as $if => $ifdesc) {
+		if ($dyndns['interface'] == $if) {
 			if (!isset($dyndns['enable']))
-				echo "<span class=\"gray\">" . htmlspecialchars($dyndns['host']) . "</span>";
+				print('<font color="gray">' . $ifdesc . '</font>');
 			else
-				echo htmlspecialchars($dyndns['host']);
-		  ?>
-		  </td>
-		  <td class="listr">
-		  <?php
-			$filename = "{$g['conf_path']}/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($dyndns['host']) . "{$dyndns['id']}.cache";
-			$filename_v6 = "{$g['conf_path']}/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($dyndns['host']) . "{$dyndns['id']}_v6.cache";
-			if (file_exists($filename)) {
-				$ipaddr = dyndnsCheckIP($dyndns['interface']);
-				$cached_ip_s = explode(":", file_get_contents($filename));
-				$cached_ip = $cached_ip_s[0];
-				if ($ipaddr <> $cached_ip)
-					echo "<font color='red'>";
-				else
-					echo "<font color='green'>";
-				echo htmlspecialchars($cached_ip);
-				echo "</font>";
-			} else if (file_exists($filename_v6)) {
-				$ipv6addr = get_interface_ipv6($dyndns['interface']);
-				$cached_ipv6_s = explode("|", file_get_contents($filename_v6));
-				$cached_ipv6 = $cached_ipv6_s[0];
-				if ($ipv6addr <> $cached_ipv6)
-					echo "<font color='red'>";
-				else
-					echo "<font color='green'>";
-				echo htmlspecialchars($cached_ipv6);
-				echo "</font>";
-			} else {
-				echo "N/A";
-			}
-		  ?>
-		  </td>
-		  <td class="listbg">
-		  <?php
+				print($ifdesc);
+
+			break;
+		}
+	}
+
+	$groupslist = return_gateway_groups_array();
+	foreach ($groupslist as $if => $group) {
+		if ($dyndns['interface'] == $if) {
 			if (!isset($dyndns['enable']))
-				echo "<span class=\"gray\">".htmlspecialchars($dyndns['descr'])."</span>";
+				print('<font color="gray">' . $if . '</font>');
 			else
-				echo htmlspecialchars($dyndns['descr']);
-		  ?>
-		  </td>
-		  <td valign="middle" class="list nowrap">
-			<a href="services_dyndns_edit.php?id=<?=$i;?>"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_e.gif" width="17" height="17" border="0" alt="edit" /></a>
-			&nbsp;<a href="services_dyndns.php?act=del&amp;id=<?=$i;?>" onclick="return confirm('<?=gettext("Do you really want to delete this entry?");?>')"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_x.gif" width="17" height="17" border="0" alt="delete" /></a>
-		  </td>
-		</tr>
-		<?php $i++; endforeach; ?>
-		<tr>
-		  <td class="list" colspan="5"></td>
-		  <td class="list"><a href="services_dyndns_edit.php"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_plus.gif" width="17" height="17" border="0" alt="add" /></a></td>
-		</tr>
-		<tr>
-		  <td colspan="5" class="list"><p class="vexpl"><span class="red"><strong>
-			<?=gettext("Note:");?><br />
-			</strong></span>
-			<?=gettext("IP addresses appearing in green are up to date with Dynamic DNS provider.");?><br />
-			<?=gettext("You can force an update for an IP address on the edit page for that service.");?></p>
-		  </td>
-		  <td class="list">&nbsp;</td>
-		</tr>
+			print($if);
+				break;
+			}
+		}
+?>
+					</td>
+					<td>
+<?php
+	$types = explode(",", DYNDNS_PROVIDER_DESCRIPTIONS);
+	$vals = explode(" ", DYNDNS_PROVIDER_VALUES);
+
+	for ($j = 0; $j < count($vals); $j++) {
+		if ($vals[$j] == $dyndns['type']) {
+			if (!isset($dyndns['enable']))
+				print('<font color="gray">' . htmlspecialchars($types[$j]) . '</font');
+			else
+				print(htmlspecialchars($types[$j]));
+
+			break;
+		}
+	}
+?>
+					</td>
+					<td>
+<?php
+	if (!isset($dyndns['enable']))
+		print('<font color="gray">' . htmlspecialchars($dyndns['host']) . '</font>');
+	else
+		print(htmlspecialchars($dyndns['host']));
+?>
+					</td>
+					<td>
+<?php
+	$filename = "{$g['conf_path']}/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($dyndns['host']) . "{$dyndns['id']}.cache";
+	$filename_v6 = "{$g['conf_path']}/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($dyndns['host']) . "{$dyndns['id']}_v6.cache";
+	if (file_exists($filename)) {
+		$ipaddr = dyndnsCheckIP($dyndns['interface']);
+		$cached_ip_s = explode(":", file_get_contents($filename));
+		$cached_ip = $cached_ip_s[0];
+
+		if ($ipaddr != $cached_ip)
+			print('<font color="red">');
+		else
+			print('<font color="green">');
+
+		print(htmlspecialchars($cached_ip));
+		print('</font>');
+	} else if (file_exists($filename_v6)) {
+		$ipv6addr = get_interface_ipv6($dyndns['interface']);
+		$cached_ipv6_s = explode("|", file_get_contents($filename_v6));
+		$cached_ipv6 = $cached_ipv6_s[0];
+
+		if ($ipv6addr != $cached_ipv6)
+			print('<font color="red">');
+		else
+			print('<font color="green">');
+
+		print(htmlspecialchars($cached_ipv6));
+		print('</font>');
+	} else {
+		print('N/A');
+	}
+?>
+					</td>
+					<td">
+<?php
+	if (!isset($dyndns['enable']))
+		print('<font color="green">' . htmlspecialchars($dyndns['descr']). '</font>');
+	else
+		print(htmlspecialchars($dyndns['descr']));
+?>
+					</td>
+					<td>
+						<a href="services_dyndns_edit.php?id=<?=$i?>" class="btn btn-xs btn-info"><?=gettext('Edit')?></a>
+						<a href="services_dyndns.php?act=del&amp;id=<?=$i?>" class="btn btn-xs btn-danger"><?=gettext("Delete")?></a>
+					</td>
+				</tr>
+<?php
+	$i++;
+	endforeach;
+?>
+			</tbody>
 	  </table>
-	  </div>
-	</td>
-  </tr>
-</table>
+	</div>
 </form>
-<?php include("fend.inc"); ?>
-</body>
-</html>
+
+<nav class="action-buttons">
+	<a href="services_dyndns_edit.php" class="btn btn-sm btn-success"><?=gettext('Add')?></a>
+</nav>
+
+<?php
+
+print_info_box(gettext("IP addresses appearing in green are up to date with Dynamic DNS provider. ") .
+			   gettext("You can force an update for an IP address on the edit page for that service."));
+
+include("foot.inc");

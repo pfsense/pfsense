cce0e08f19bbf610e9128ab2c716acb873470943
sbeaver <sbeaver@netgate.com>
2015-05-19 15:53:07 -0400

    You are so right!
    Thanks

--- usr/local/www/diag_ipsec_leases.php
+++ usr/local/www/diag_ipsec_leases.php
@@ -52,11 +52,11 @@ include("head.inc");
 $mobile = ipsec_dump_mobile();
 
 $tab_array = array();
-$tab_array0 = array(gettext("Overview"), false, "diag_ipsec.php");
-$tab_array1 = array(gettext("Leases"), true, "diag_ipsec_leases.php");
-$tab_array2 = array(gettext("SAD"), false, "diag_ipsec_sad.php");
-$tab_array3 = array(gettext("SPD"), false, "diag_ipsec_spd.php");
-$tab_array4 = array(gettext("Logs"), false, "diag_logs.php?logfile=ipsec");
+$tab_array[] = array(gettext("Overview"), false, "diag_ipsec.php");
+$tab_array[] = array(gettext("Leases"), true, "diag_ipsec_leases.php");
+$tab_array[] = array(gettext("SAD"), false, "diag_ipsec_sad.php");
+$tab_array[] = array(gettext("SPD"), false, "diag_ipsec_spd.php");
+$tab_array[] = array(gettext("Logs"), false, "diag_logs.php?logfile=ipsec");
 display_top_tabs($tab_array);
 
 if (isset($mobile['pool']) && is_array($mobile['pool'])) {

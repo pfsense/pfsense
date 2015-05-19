4b9f0203deb005a6dea6d11d8655c584ae2752e5
sbeaver <sbeaver@netgate.com>
2015-05-19 08:36:27 -0400

    services_dhcpv6_edit.php Conversion complete
    Ready for review

--- usr/local/www/services_dhcpv6_edit.php
+++ usr/local/www/services_dhcpv6_edit.php
@@ -1,24 +1,24 @@
-<?php 
+<?php
 /* $Id$ */
 /*
 	services_dhcpv6_edit.php
 	part of m0n0wall (http://m0n0.ch/wall)
-	
+
 	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
 	Copyright (C) 2011 Seth Mos <seth.mos@dds.nl>.
 	Copyright (C) 2013-2015 Electric Sheep Fencing, LP
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -32,7 +32,7 @@
 */
 /*
 	pfSense_BUILDER_BINARIES:	/usr/sbin/arp
-	pfSense_MODULE:	dhcpserver
+	pfSense_MODULE: dhcpserver
 */
 
 ##|+PRIV
@@ -43,13 +43,13 @@
 ##|-PRIV
 
 function staticmapcmp($a, $b) {
-        return ipcmp($a['ipaddrv6'], $b['ipaddrv6']);
+	return ipcmp($a['ipaddrv6'], $b['ipaddrv6']);
 }
 
 function staticmaps_sort($ifgui) {
-        global $g, $config;
+	global $g, $config;
 
-        usort($config['dhcpdv6'][$ifgui]['staticmap'], "staticmapcmp");
+	usort($config['dhcpdv6'][$ifgui]['staticmap'], "staticmapcmp");
 }
 
 require_once('globals.inc');
@@ -64,9 +64,10 @@ if(!$g['services_dhcp_server_enable']) {
 require("guiconfig.inc");
 
 $if = $_GET['if'];
+
 if ($_POST['if'])
 	$if = $_POST['if'];
-	
+
 if (!$if) {
 	header("Location: services_dhcpv6.php");
 	exit;
@@ -74,8 +75,10 @@ if (!$if) {
 
 if (!is_array($config['dhcpdv6']))
 	$config['dhcpdv6'] = array();
+
 if (!is_array($config['dhcpdv6'][$if]))
 	$config['dhcpdv6'][$if] = array();
+
 if (!is_array($config['dhcpdv6'][$if]['staticmap']))
 	$config['dhcpdv6'][$if]['staticmap'] = array();
 
@@ -87,22 +90,23 @@ $ifcfgdescr = convert_friendly_interface_to_friendly_descr($if);
 
 if (is_numericint($_GET['id']))
 	$id = $_GET['id'];
+
 if (isset($_POST['id']) && is_numericint($_POST['id']))
 	$id = $_POST['id'];
 
 if (isset($id) && $a_maps[$id]) {
-        $pconfig['duid'] = $a_maps[$id]['duid'];
+	$pconfig['duid'] = $a_maps[$id]['duid'];
 	$pconfig['hostname'] = $a_maps[$id]['hostname'];
-        $pconfig['ipaddrv6'] = $a_maps[$id]['ipaddrv6'];
+	$pconfig['ipaddrv6'] = $a_maps[$id]['ipaddrv6'];
 	$pconfig['filename'] = $a_maps[$id]['filename'];
-		$pconfig['rootpath'] = $a_maps[$id]['rootpath'];
-        $pconfig['descr'] = $a_maps[$id]['descr'];
+	$pconfig['rootpath'] = $a_maps[$id]['rootpath'];
+	$pconfig['descr'] = $a_maps[$id]['descr'];
 } else {
-        $pconfig['duid'] = $_GET['duid'];
+	$pconfig['duid'] = $_GET['duid'];
 	$pconfig['hostname'] = $_GET['hostname'];
 	$pconfig['filename'] = $_GET['filename'];
-		$pconfig['rootpath'] = $a_maps[$id]['rootpath'];
-        $pconfig['descr'] = $_GET['descr'];
+	$pconfig['rootpath'] = $a_maps[$id]['rootpath'];
+	$pconfig['descr'] = $_GET['descr'];
 }
 
 if ($_POST) {
@@ -113,13 +117,13 @@ if ($_POST) {
 	/* input validation */
 	$reqdfields = explode(" ", "duid");
 	$reqdfieldsn = array(gettext("DUID"));
-	
+
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
 
 	if ($_POST['hostname']) {
 		preg_match("/\-\$/", $_POST['hostname'], $matches);
 		if($matches)
-			$input_errors[] = gettext("The hostname cannot end with a hyphen according to RFC952");		
+			$input_errors[] = gettext("The hostname cannot end with a hyphen according to RFC952");
 		if (!is_hostname($_POST['hostname'])) {
 			$input_errors[] = gettext("The hostname can only contain the characters A-Z, 0-9 and '-'.");
 		} else {
@@ -131,10 +135,11 @@ if ($_POST) {
 	if (($_POST['ipaddrv6'] && !is_ipaddrv6($_POST['ipaddrv6']))) {
 		$input_errors[] = gettext("A valid IPv6 address must be specified.");
 	}
+
 	if (empty($_POST['duid'])) {
 		$input_errors[] = gettext("A valid DUID must be specified.");
 	}
-	
+
 	/* check for overlaps */
 	foreach ($a_maps as $mapent) {
 		if (isset($id) && ($a_maps[$id]) && ($a_maps[$id] === $mapent))
@@ -145,7 +150,7 @@ if ($_POST) {
 			break;
 		}
 	}
-		
+
 	/* make sure it's not within the dynamic subnet */
 	if ($_POST['ipaddrv6']) {
 		/* oh boy, we need to be able to somehow do this at some point. skip */
@@ -164,8 +169,9 @@ if ($_POST) {
 			$a_maps[$id] = $mapent;
 		else
 			$a_maps[] = $mapent;
+
 		staticmaps_sort($if);
-		
+
 		write_config();
 
 		if(isset($config['dhcpdv6'][$if]['enable'])) {
@@ -174,7 +180,6 @@ if ($_POST) {
 				mark_subsystem_dirty('hosts');
 			if (isset($config['unbound']['enable']) && isset($config['unbound']['regdhcpstatic']))
 				mark_subsystem_dirty('unbound');
-
 		}
 
 		header("Location: services_dhcpv6.php?if={$if}");
@@ -187,75 +192,86 @@ $shortcut_section = "dhcp6";
 
 include("head.inc");
 
-?>
+if ($input_errors)
+	print_input_errors($input_errors);
 
-<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
-<?php include("fbegin.inc"); ?>
-<?php if ($input_errors) print_input_errors($input_errors); ?>
-            <form action="services_dhcpv6_edit.php" method="post" name="iform" id="iform">
-              <table width="100%" border="0" cellpadding="6" cellspacing="0" summary="static mapping">
-				<tr>
-					<td colspan="2" valign="top" class="listtopic"><?=gettext("Static DHCPv6 Mapping");?></td>
-				</tr>	
-                <tr> 
-                  <td width="22%" valign="top" class="vncellreq"><?=gettext("DUID");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="duid" type="text" class="formfld unknown" id="duid" size="40" value="<?=htmlspecialchars($pconfig['duid']);?>" />
-                    <br />
-                    <span class="vexpl"><?=gettext("Enter a DUID in the following format: ");?><br />
-"DUID-LLT - ETH -- TIME --- ---- address ----" <br />
-"xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"</span></td>
-                </tr>
-                <tr>
-                  <td width="22%" valign="top" class="vncell"><?=gettext("IPv6 address");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="ipaddrv6" type="text" class="formfld unknown" id="ipaddrv6" size="28" value="<?=htmlspecialchars($pconfig['ipaddrv6']);?>" />
-                    <br />
-			<?=gettext("If an IPv6 address is entered, the address must be outside of the pool.");?>
-			<br />
-			<?=gettext("If no IPv6 address is given, one will be dynamically allocated from the pool.");?>
-			</td>
-                </tr>
-                <tr> 
-                  <td width="22%" valign="top" class="vncell"><?=gettext("Hostname");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="hostname" type="text" class="formfld unknown" id="hostname" size="28" value="<?=htmlspecialchars($pconfig['hostname']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("Name of the host, without domain part.");?></span></td>
-                </tr>				
-                <?php if($netboot_enabled) { ?>
-		<tr>
-		  <td width="22%" valign="top" class="vncell">Netboot filename</td>
-		  <td width="78%" class="vtable">
-		    <input name="filename" type="text" class="formfld unknown" id="filename" size="28" value="<?=htmlspecialchars($pconfig['filename']);?>" />
-		    <br /> <span class="vexpl">Name of the file that should be loaded when this host boots off of the network, overrides setting on main page.</span></td>
-		</tr>
-		<tr>
-		  <td width="22%" valign="top" class="vncell">Root Path</td>
-		  <td width="78%" class="vtable">
-			<input name="rootpath" type="text" class="formfld unknown" id="rootpath" size="90" value="<?=htmlspecialchars($pconfig['rootpath']);?>" />
-		    <br /> <span class="vexpl"><?=gettext("Enter the"); ?> <b><?=gettext("root-path"); ?></b>-<?=gettext("string");?>, overrides setting on main page.</span></td>
-		</tr>
-		<?php } ?>
-                <tr> 
-                  <td width="22%" valign="top" class="vncell"><?=gettext("Description");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="descr" type="text" class="formfld unknown" id="descr" size="40" value="<?=htmlspecialchars($pconfig['descr']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("You may enter a description here ".
-                    "for your reference (not parsed).");?></span></td>
-                </tr>
-                <tr> 
-                  <td width="22%" valign="top">&nbsp;</td>
-                  <td width="78%"> 
-                    <input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>" />
-                    <input type="button" class="formbtn" value="<?=gettext("Cancel");?>" onclick="window.location.href='<?=$referer;?>'" />
-                    <?php if (isset($id) && $a_maps[$id]): ?>
-                    <input name="id" type="hidden" value="<?=htmlspecialchars($id);?>" />
-                    <?php endif; ?>
-                    <input name="if" type="hidden" value="<?=htmlspecialchars($if);?>" />
-                  </td>
-                </tr>
-              </table>
-</form>
-<?php include("fend.inc"); ?>
-</body>
-</html>
+require('classes/Form.class.php');
+
+$form = new Form();
+
+$section = new Form_Section('Static DHCPv6 Mapping');
+
+$section->addInput(new Form_Input(
+	'duid',
+	'DUID',
+	'text',
+	$pconfig['duid'],
+	['placeholder' => 'DUID-LLT - ETH -- TIME --- ---- address ---- xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx']
+))->setHelp(gettext('Enter a DUID in the following format: ') . '<br />' .
+			'DUID-LLT - ETH -- TIME --- ---- address ---- ' .
+			'xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx');
+
+$section->addInput(new Form_Input(
+	'ipaddrv6',
+	'Server 3',
+	'text',
+	$pconfig['ipaddrv6']
+))->setHelp('If an IPv6 address is entered, the address must be outside of the pool.' . '<br />' .
+			'If no IPv6 address is given, one will be dynamically allocated from the pool.');
+
+$section->addInput(new Form_Input(
+	'hostname',
+	'Hostname',
+	'text',
+	$pconfig['hostname']
+))->setHelp('Name of the host, without domain part.');
+
+$section->addInput(new Form_Input(
+	'descr',
+	'Description',
+	'text',
+	$pconfig['descr']
+))->setHelp('You may enter a description here for your reference (not parsed).');
+
+if($netboot_enabled) {
+	$section->addInput(new Form_Input(
+		'filename',
+		'Netboot filename',
+		'text',
+		$pconfig['remoteserver3']
+	))->setHelp('Name of the file that should be loaded when this host boots off of the network, overrides setting on main page.');
+
+	$section->addInput(new Form_Input(
+		'rootpath',
+		'Root path',
+		'text',
+		$pconfig['remoteserver3']
+	))->setHelp('Enter the root-path string. This overrides setting on main page.');
+}
+
+if (isset($id) && $a_maps[$id]) {
+	$section->addInput(new Form_Input(
+		'id',
+		null,
+		'hidden',
+		$id
+	));
+}
+
+$section->addInput(new Form_Input(
+	'if',
+	null,
+	'hidden',
+	$if
+));
+
+$form->addGlobal(new Form_Button(
+	'cancel',
+	'Cancel',
+	$referer
+));
+
+$form->add($section);
+print($form);
+
+include("foot.inc");

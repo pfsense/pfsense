966ed61177768899b7cda1dc995a0bf7df1072f5
Phil Davis <phil.davis@inf.org>
2015-05-24 11:49:58 +0545

    Code style services dnsmasq

--- usr/local/www/services_dnsmasq_edit.php
+++ usr/local/www/services_dnsmasq_edit.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 /* $Id$ */
 /*
 	services_dnsmasq_edit.php
 	part of m0n0wall (http://m0n0.ch/wall)
-	
+
 	Copyright (C) 2003-2004 Bob Zoller <bob@kludgebox.com> and Manuel Kasper <mk@neon1.net>.
 	Copyright (C) 2013-2015 Electric Sheep Fencing, LP
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -45,27 +45,31 @@ function hostcmp($a, $b) {
 }
 
 function hosts_sort() {
-        global $g, $config;
+	global $g, $config;
 
-        if (!is_array($config['dnsmasq']['hosts']))
-                return;
+	if (!is_array($config['dnsmasq']['hosts'])) {
+		return;
+	}
 
-        usort($config['dnsmasq']['hosts'], "hostcmp");
+	usort($config['dnsmasq']['hosts'], "hostcmp");
 }
 
 require("guiconfig.inc");
 
 $referer = (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/services_dnsmasq.php');
 
-if (!is_array($config['dnsmasq']['hosts'])) 
+if (!is_array($config['dnsmasq']['hosts'])) {
 	$config['dnsmasq']['hosts'] = array();
+}
 
 $a_hosts = &$config['dnsmasq']['hosts'];
 
-if (is_numericint($_GET['id']))
+if (is_numericint($_GET['id'])) {
 	$id = $_GET['id'];
-if (isset($_POST['id']) && is_numericint($_POST['id']))
+}
+if (isset($_POST['id']) && is_numericint($_POST['id'])) {
 	$id = $_POST['id'];
+}
 
 if (isset($id) && $a_hosts[$id]) {
 	$pconfig['host'] = $a_hosts[$id]['host'];
@@ -82,12 +86,12 @@ if ($_POST) {
 
 	/* input validation */
 	$reqdfields = explode(" ", "domain ip");
-	$reqdfieldsn = array(gettext("Domain"),gettext("IP address"));
-	
+	$reqdfieldsn = array(gettext("Domain"), gettext("IP address"));
+
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
-	
+
 	if ($_POST['host']) {
-		if (!is_hostname($_POST['host'])) { 
+		if (!is_hostname($_POST['host'])) {
 			$input_errors[] = gettext("The hostname can only contain the characters A-Z, 0-9 and '-'. It may not start or end with '-'.");
 		} else {
 			if (!is_unqualified_hostname($_POST['host'])) {
@@ -96,11 +100,13 @@ if ($_POST) {
 		}
 	}
 
-	if (($_POST['domain'] && !is_domain($_POST['domain']))) 
+	if (($_POST['domain'] && !is_domain($_POST['domain']))) {
 		$input_errors[] = gettext("A valid domain must be specified.");
-		
-	if (($_POST['ip'] && !is_ipaddr($_POST['ip']))) 
+	}
+
+	if (($_POST['ip'] && !is_ipaddr($_POST['ip']))) {
 		$input_errors[] = gettext("A valid IP address must be specified.");
+	}
 
 	/* collect aliases */
 	$aliases = array();
@@ -146,11 +152,14 @@ if ($_POST) {
 
 	/* check for overlaps */
 	foreach ($a_hosts as $hostent) {
-		if (isset($id) && ($a_hosts[$id]) && ($a_hosts[$id] === $hostent))
+		if (isset($id) && ($a_hosts[$id]) && ($a_hosts[$id] === $hostent)) {
 			continue;
+		}
 
-		if (($hostent['host'] == $_POST['host']) && ($hostent['domain'] == $_POST['domain'])
-			&& ((is_ipaddrv4($hostent['ip']) && is_ipaddrv4($_POST['ip'])) || (is_ipaddrv6($hostent['ip']) && is_ipaddrv6($_POST['ip'])))) {
+		if (($hostent['host'] == $_POST['host']) &&
+		    ($hostent['domain'] == $_POST['domain']) &&
+		    ((is_ipaddrv4($hostent['ip']) && is_ipaddrv4($_POST['ip'])) ||
+		     (is_ipaddrv6($hostent['ip']) && is_ipaddrv6($_POST['ip'])))) {
 			$input_errors[] = gettext("This host/domain already exists.");
 			break;
 		}
@@ -164,22 +173,23 @@ if ($_POST) {
 		$hostent['descr'] = $_POST['descr'];
 		$hostent['aliases']['item'] = $aliases;
 
-		if (isset($id) && $a_hosts[$id])
+		if (isset($id) && $a_hosts[$id]) {
 			$a_hosts[$id] = $hostent;
-		else
+		} else {
 			$a_hosts[] = $hostent;
+		}
 		hosts_sort();
-		
+
 		mark_subsystem_dirty('hosts');
-		
+
 		write_config();
-		
+
 		header("Location: services_dnsmasq.php");
 		exit;
 	}
 }
 
-$pgtitle = array(gettext("Services"),gettext("DNS forwarder"),gettext("Edit host"));
+$pgtitle = array(gettext("Services"), gettext("DNS forwarder"), gettext("Edit host"));
 $shortcut_section = "forwarder";
 include("head.inc");
 
@@ -206,110 +216,124 @@ include("head.inc");
 </script>
 
 <?php if ($input_errors) print_input_errors($input_errors); ?>
-        <form action="services_dnsmasq_edit.php" method="post" name="iform" id="iform">
-        <table width="100%" border="0" cellpadding="6" cellspacing="0" summary="dns edit">
-				<tr>
-					<td colspan="2" valign="top" class="listtopic"><?=gettext("Edit DNS Forwarder entry");?></td>
-				</tr>	
-                <tr>
-                  <td width="22%" valign="top" class="vncell"><?=gettext("Host");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="host" type="text" class="formfld unknown" id="host" size="40" value="<?=htmlspecialchars($pconfig['host']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("Name of the host, without".
-                   " domain part"); ?><br />
-                   <?=gettext("e.g."); ?> <em><?=gettext("myhost"); ?></em></span></td>
-                </tr>
-				<tr>
-                  <td width="22%" valign="top" class="vncellreq"><?=gettext("Domain");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="domain" type="text" class="formfld unknown" id="domain" size="40" value="<?=htmlspecialchars($pconfig['domain']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("Domain of the host"); ?><br />
-                   <?=gettext("e.g."); ?> <em><?=gettext("example.com"); ?></em></span></td>
-                </tr>
-				<tr>
-                  <td width="22%" valign="top" class="vncellreq"><?=gettext("IP address");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="ip" type="text" class="formfld unknown" id="ip" size="40" value="<?=htmlspecialchars($pconfig['ip']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("IP address of the host"); ?><br />
-                   <?=gettext("e.g."); ?> <em>192.168.100.100</em> <?=gettext("or"); ?> <em>fd00:abcd::1</em></span></td>
-                </tr>
-				<tr>
-                  <td width="22%" valign="top" class="vncell"><?=gettext("Description");?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="descr" type="text" class="formfld unknown" id="descr" size="40" value="<?=htmlspecialchars($pconfig['descr']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("You may enter a description here".
-                   " for your reference (not parsed).");?></span></td>
-                </tr>
-				<tr>
-                  <td width="22%" valign="top" class="vncell"><div id="addressnetworkport"><?=gettext("Aliases"); ?></div></td>
-                  <td width="78%" class="vtable">
-                    <table id="maintable" summary="aliases">
-                      <tbody>
-                        <tr>
-                          <td colspan="4">
-                            <div style="padding:5px; margin-top: 16px; margin-bottom: 16px; border:1px dashed #000066; background-color: #ffffff; color: #000000; font-size: 8pt;" id="itemhelp">
-                              <?=gettext("Enter additional names for this host."); ?>
-                            </div>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td><div id="onecolumn"><?=gettext("Host");?></div></td>
-                          <td><div id="twocolumn"><?=gettext("Domain");?></div></td>
-                          <td><div id="threecolumn"><?=gettext("Description");?></div></td>
-                        </tr>
-                        <?php
-                          $counter = 0;
-                          if($pconfig['aliases']['item']):
-                            foreach($pconfig['aliases']['item'] as $item):
-                              $host = $item['host'];
-                              $domain = $item['domain'];
-                              $description = $item['description'];
-                        ?>
-                        <tr>
-                          <td>
-                            <input autocomplete="off" name="aliashost<?php echo $counter; ?>" type="text" class="formfld unknown" id="aliashost<?php echo $counter; ?>" size="20" value="<?=htmlspecialchars($host);?>" />
-                          </td>
-                          <td>
-                            <input autocomplete="off" name="aliasdomain<?php echo $counter; ?>" type="text" class="formfld unknown" id="aliasdomain<?php echo $counter; ?>" size="20" value="<?=htmlspecialchars($domain);?>" />
-                          </td>
-                          <td>
-                            <input name="aliasdescription<?php echo $counter; ?>" type="text" class="formfld unknown" id="aliasdescription<?php echo $counter; ?>" size="20" value="<?=htmlspecialchars($description);?>" />
-                          </td>
-                          <td>
-                            <a onclick="removeRow(this); return false;" href="#"><img border="0" src="/themes/<?echo $g['theme'];?>/images/icons/icon_x.gif" alt="" title="<?=gettext("remove this entry"); ?>" /></a>
-                          </td>
-                        </tr>
-                        <?php
-                              $counter++;
-                            endforeach;
-                          endif;
-                        ?>
-                      </tbody>
-                    </table>
-                    <a onclick="javascript:addRowTo('maintable', 'formfldalias'); return false;" href="#">
-                      <img border="0" src="/themes/<?= $g['theme']; ?>/images/icons/icon_plus.gif" alt="" title="<?=gettext("add another entry");?>" />
-                    </a>
-                    <script type="text/javascript">
-                    //<![CDATA[
-                      field_counter_js = 3;
-                      rows = 1;
-                      totalrows = <?php echo $counter; ?>;
-                      loaded = <?php echo $counter; ?>;
-                    //]]>
-                    </script>
-                  </td>
-                </tr>
-                <tr>
-                  <td width="22%" valign="top">&nbsp;</td>
-                  <td width="78%"> 
-                    <input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>" />
-                    <input type="button" class="formbtn" value="<?=gettext("Cancel");?>" onclick="window.location.href='<?=$referer;?>'" />
-                    <?php if (isset($id) && $a_hosts[$id]): ?>
-                    <input name="id" type="hidden" value="<?=htmlspecialchars($id);?>" />
-                    <?php endif; ?>
-                  </td>
-                </tr>
-        </table>
+<form action="services_dnsmasq_edit.php" method="post" name="iform" id="iform">
+	<table width="100%" border="0" cellpadding="6" cellspacing="0" summary="dns edit">
+		<tr>
+			<td colspan="2" valign="top" class="listtopic"><?=gettext("Edit DNS Forwarder entry");?></td>
+		</tr>
+		<tr>
+			<td width="22%" valign="top" class="vncell"><?=gettext("Host");?></td>
+			<td width="78%" class="vtable">
+				<input name="host" type="text" class="formfld unknown" id="host" size="40" value="<?=htmlspecialchars($pconfig['host']);?>" />
+				<br /> 
+				<span class="vexpl">
+					<?=gettext("Name of the host, without domain part"); ?><br />
+					<?=gettext("e.g."); ?> <em><?=gettext("myhost"); ?></em>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td width="22%" valign="top" class="vncellreq"><?=gettext("Domain");?></td>
+			<td width="78%" class="vtable">
+				<input name="domain" type="text" class="formfld unknown" id="domain" size="40" value="<?=htmlspecialchars($pconfig['domain']);?>" />
+				<br />
+				<span class="vexpl">
+					<?=gettext("Domain of the host"); ?><br />
+					<?=gettext("e.g."); ?> <em><?=gettext("example.com"); ?></em>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td width="22%" valign="top" class="vncellreq"><?=gettext("IP address");?></td>
+			<td width="78%" class="vtable">
+				<input name="ip" type="text" class="formfld unknown" id="ip" size="40" value="<?=htmlspecialchars($pconfig['ip']);?>" />
+				<br />
+				<span class="vexpl">
+					<?=gettext("IP address of the host"); ?><br />
+					<?=gettext("e.g."); ?> <em>192.168.100.100</em> <?=gettext("or"); ?> <em>fd00:abcd::1</em>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td width="22%" valign="top" class="vncell"><?=gettext("Description");?></td>
+			<td width="78%" class="vtable">
+				<input name="descr" type="text" class="formfld unknown" id="descr" size="40" value="<?=htmlspecialchars($pconfig['descr']);?>" />
+				<br />
+				<span class="vexpl">
+					<?=gettext("You may enter a description here for your reference (not parsed).");?>
+				</span>
+			</td>
+		</tr>
+		<tr>
+			<td width="22%" valign="top" class="vncell"><div id="addressnetworkport"><?=gettext("Aliases"); ?></div></td>
+			<td width="78%" class="vtable">
+				<table id="maintable" summary="aliases">
+					<tbody>
+					<tr>
+						<td colspan="4">
+							<div style="padding:5px; margin-top: 16px; margin-bottom: 16px; border:1px dashed #000066; background-color: #ffffff; color: #000000; font-size: 8pt;" id="itemhelp">
+								<?=gettext("Enter additional names for this host."); ?>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td><div id="onecolumn"><?=gettext("Host");?></div></td>
+						<td><div id="twocolumn"><?=gettext("Domain");?></div></td>
+						<td><div id="threecolumn"><?=gettext("Description");?></div></td>
+					</tr>
+<?php
+	$counter = 0;
+	if ($pconfig['aliases']['item']):
+		foreach ($pconfig['aliases']['item'] as $item):
+			$host = $item['host'];
+			$domain = $item['domain'];
+			$description = $item['description'];
+?>
+					<tr>
+						<td>
+							<input autocomplete="off" name="aliashost<?php echo $counter; ?>" type="text" class="formfld unknown" id="aliashost<?php echo $counter; ?>" size="20" value="<?=htmlspecialchars($host);?>" />
+						</td>
+						<td>
+							<input autocomplete="off" name="aliasdomain<?php echo $counter; ?>" type="text" class="formfld unknown" id="aliasdomain<?php echo $counter; ?>" size="20" value="<?=htmlspecialchars($domain);?>" />
+						</td>
+						<td>
+							<input name="aliasdescription<?php echo $counter; ?>" type="text" class="formfld unknown" id="aliasdescription<?php echo $counter; ?>" size="20" value="<?=htmlspecialchars($description);?>" />
+						</td>
+						<td>
+							<a onclick="removeRow(this); return false;" href="#"><img border="0" src="/themes/<?echo $g['theme'];?>/images/icons/icon_x.gif" alt="" title="<?=gettext("remove this entry"); ?>" /></a>
+						</td>
+					</tr>
+<?php
+			$counter++;
+		endforeach;
+	endif;
+?>
+					</tbody>
+				</table>
+				<a onclick="javascript:addRowTo('maintable', 'formfldalias'); return false;" href="#">
+					<img border="0" src="/themes/<?= $g['theme']; ?>/images/icons/icon_plus.gif" alt="" title="<?=gettext("add another entry");?>" />
+				</a>
+				<script type="text/javascript">
+				//<![CDATA[
+					field_counter_js = 3;
+					rows = 1;
+					totalrows = <?php echo $counter; ?>;
+					loaded = <?php echo $counter; ?>;
+				//]]>
+				</script>
+			</td>
+		</tr>
+		<tr>
+			<td width="22%" valign="top">&nbsp;</td>
+			<td width="78%">
+				<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>" />
+				<input type="button" class="formbtn" value="<?=gettext("Cancel");?>" onclick="window.location.href='<?=$referer;?>'" />
+				<?php if (isset($id) && $a_hosts[$id]): ?>
+				<input name="id" type="hidden" value="<?=htmlspecialchars($id);?>" />
+				<?php endif; ?>
+			</td>
+		</tr>
+	</table>
 </form>
 <?php include("fend.inc"); ?>
 </body>

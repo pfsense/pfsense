e92ee59894bfc96764512dd3fa9916dafd9d873b
Phil Davis <phil.davis@inf.org>
2015-05-28 23:15:45 +0545

    Code style services unbound

--- usr/local/www/services_unbound_advanced.php
+++ usr/local/www/services_unbound_advanced.php
@@ -42,14 +42,14 @@
 require_once("guiconfig.inc");
 require_once("unbound.inc");
 
-if(!is_array($config['unbound'])) {
+if (!is_array($config['unbound'])) {
 	$config['unbound'] = array();
 }
 
 if (isset($config['unbound']['hideidentity'])) {
 	$pconfig['hideidentity'] = true;
 }
-if (isset($config['unbound']['hideversion'])){
+if (isset($config['unbound']['hideversion'])) {
 	$pconfig['hideversion'] = true;
 }
 if (isset($config['unbound']['prefetch'])) {
@@ -133,7 +133,7 @@ if ($_POST) {
 		if (isset($_POST['dnssecstripped']) && !isset($config['unbound']['dnssec'])) {
 			$input_errors[] = "Harden DNSSEC Data option can only be enabled if DNSSEC support is enabled.";
 		}
-		
+
 		if (!$input_errors) {
 
 			if (isset($_POST['hideidentity'])) {
@@ -184,21 +184,21 @@ if ($_POST) {
 				unset($config['unbound']['use_caps']);
 			}
 			write_config("DNS Resolver configured.");
-	
+
 			mark_subsystem_dirty('unbound');
 		}
 	}
 }
 
 $closehead = false;
-$pgtitle = array(gettext("Services"),gettext("DNS Resolver"),gettext("Advanced"));
+$pgtitle = array(gettext("Services"), gettext("DNS Resolver"), gettext("Advanced"));
 $shortcut_section = "resolver";
 include_once("head.inc");
 
 ?>
 
 </head>
-	
+
 <body>
 <?php include("fbegin.inc"); ?>
 <form action="services_unbound_advanced.php" method="post" name="iform" id="iform">
@@ -266,7 +266,7 @@ include_once("head.inc");
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Message Cache Size");?></td>
 								<td width="78%" class="vtable">
-										<select id="msgcachesize" name="msgcachesize">
+									<select id="msgcachesize" name="msgcachesize">
 <?php
 										foreach (array("4", "10", "20", "50", "100", "250", "512") as $size) :
 ?>
@@ -276,14 +276,14 @@ include_once("head.inc");
 <?php
 										endforeach;
 ?>
-										</select><br />
-										<?=gettext("Size of the message cache. The message cache stores DNS rcodes and validation statuses. The RRSet cache will automatically be set to twice this amount. The RRSet cache contains the actual RR data. The default is 4 megabytes.");?>
+									</select><br />
+									<?=gettext("Size of the message cache. The message cache stores DNS rcodes and validation statuses. The RRSet cache will automatically be set to twice this amount. The RRSet cache contains the actual RR data. The default is 4 megabytes.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Outgoing TCP Buffers");?></td>
 								<td width="78%" class="vtable">
-										<select id="outgoing_num_tcp" name="outgoing_num_tcp">
+									<select id="outgoing_num_tcp" name="outgoing_num_tcp">
 <?php
 										for ($num_tcp = 0; $num_tcp <= 50; $num_tcp += 10):
 ?>
@@ -293,14 +293,14 @@ include_once("head.inc");
 <?php
 										endfor;
 ?>
-										</select><br />
-										<?=gettext("The number of outgoing TCP buffers to allocate per thread. The default value is 10. If 0 is selected then no TCP queries, to authoritative servers, are done.");?>
+									</select><br />
+									<?=gettext("The number of outgoing TCP buffers to allocate per thread. The default value is 10. If 0 is selected then no TCP queries, to authoritative servers, are done.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Incoming TCP Buffers");?></td>
 								<td width="78%" class="vtable">
-										<select id="incoming_num_tcp" name="incoming_num_tcp">
+									<select id="incoming_num_tcp" name="incoming_num_tcp">
 <?php
 										for ($num_tcp = 0; $num_tcp <= 50; $num_tcp += 10):
 ?>
@@ -310,14 +310,14 @@ include_once("head.inc");
 <?php
 										endfor;
 ?>
-										</select><br />
-										<?=gettext("The number of incoming TCP buffers to allocate per thread. The default value is 10. If 0 is selected then no TCP queries, from clients, are accepted.");?>
+									</select><br />
+									<?=gettext("The number of incoming TCP buffers to allocate per thread. The default value is 10. If 0 is selected then no TCP queries, from clients, are accepted.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("EDNS Buffer Size");?></td>
 								<td width="78%" class="vtable">
-										<select id="edns_buffer_size" name="edns_buffer_size">
+									<select id="edns_buffer_size" name="edns_buffer_size">
 <?php
 										foreach (array("512", "1480", "4096") as $size) :
 ?>
@@ -327,14 +327,14 @@ include_once("head.inc");
 <?php
 										endforeach;
 ?>
-										</select><br />
-										<?=gettext("Number of bytes size to advertise as the EDNS reassembly buffer size. This is the value that is used in UDP datagrams sent to peers. RFC recommendation is 4096 (which is the default). If you have fragmentation reassemble problems, usually seen as timeouts, then a value of 1480 should help. The 512 value bypasses most MTU path problems, but it can generate an excessive amount of TCP fallback.");?>
+									</select><br />
+									<?=gettext("Number of bytes size to advertise as the EDNS reassembly buffer size. This is the value that is used in UDP datagrams sent to peers. RFC recommendation is 4096 (which is the default). If you have fragmentation reassemble problems, usually seen as timeouts, then a value of 1480 should help. The 512 value bypasses most MTU path problems, but it can generate an excessive amount of TCP fallback.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Number of queries per thread");?></td>
 								<td width="78%" class="vtable">
-										<select id="num_queries_per_thread" name="num_queries_per_thread">
+									<select id="num_queries_per_thread" name="num_queries_per_thread">
 <?php
 										foreach (array("512", "1024", "2048") as $queries) :
 ?>
@@ -344,14 +344,14 @@ include_once("head.inc");
 <?php
 										endforeach;
 ?>
-										</select><br />
-										<?=gettext("The number of queries that every thread will service simultaneously. If more queries arrive that need to be serviced, and no queries can be jostled, then these queries are dropped.");?>
+									</select><br />
+									<?=gettext("The number of queries that every thread will service simultaneously. If more queries arrive that need to be serviced, and no queries can be jostled, then these queries are dropped.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Jostle Timeout");?></td>
 								<td width="78%" class="vtable">
-										<select id="jostle_timeout" name="jostle_timeout">
+									<select id="jostle_timeout" name="jostle_timeout">
 <?php
 										foreach (array("100", "200", "500", "1000") as $timeout) :
 ?>
@@ -361,68 +361,68 @@ include_once("head.inc");
 <?php
 										endforeach;
 ?>
-										</select><br />
-										<?=gettext("This timeout is used for when the server is very busy. This protects against denial of service by slow queries or high query rates. The default value is 200 milliseconds.");?>
+									</select><br />
+									<?=gettext("This timeout is used for when the server is very busy. This protects against denial of service by slow queries or high query rates. The default value is 200 milliseconds.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Maximum TTL for RRsets and messages");?></td>
 								<td width="78%" class="vtable">
-										<input type="text" id="cache_max_ttl" name="cache_max_ttl" size="5" value="<?php if(isset($pconfig['cache_max_ttl'])) echo $pconfig['cache_max_ttl']; ?>" /><br />
-										<?=gettext("Configure a maximum Time to live for RRsets and messages in the cache. The default is 86400 seconds (1 day). When the internal TTL expires the cache item is expired. This can be configured to force the resolver to query for data more often and not trust (very large) TTL values.");?>
+									<input type="text" id="cache_max_ttl" name="cache_max_ttl" size="5" value="<?php if (isset($pconfig['cache_max_ttl'])) echo $pconfig['cache_max_ttl']; ?>" /><br />
+									<?=gettext("Configure a maximum Time to live for RRsets and messages in the cache. The default is 86400 seconds (1 day). When the internal TTL expires the cache item is expired. This can be configured to force the resolver to query for data more often and not trust (very large) TTL values.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Minimum TTL for RRsets and messages");?></td>
 								<td width="78%" class="vtable">
-										<input type="text" id="cache_min_ttl" name="cache_min_ttl" size="5" value="<?php if(isset($pconfig['cache_min_ttl'])) echo $pconfig['cache_min_ttl']; ?>" /><br />
-										<?=gettext("Configure a minimum Time to live for RRsets and messages in the cache. The default is 0 seconds. If the minimum value kicks in, the data is cached for longer than the domain owner intended, and thus less queries are made to look up the data. The 0 value ensures the data in the cache is as the domain owner intended. High values can lead to trouble as the data in the cache might not match up with the actual data anymore.");?>
+									<input type="text" id="cache_min_ttl" name="cache_min_ttl" size="5" value="<?php if (isset($pconfig['cache_min_ttl'])) echo $pconfig['cache_min_ttl']; ?>" /><br />
+									<?=gettext("Configure a minimum Time to live for RRsets and messages in the cache. The default is 0 seconds. If the minimum value kicks in, the data is cached for longer than the domain owner intended, and thus less queries are made to look up the data. The 0 value ensures the data in the cache is as the domain owner intended. High values can lead to trouble as the data in the cache might not match up with the actual data anymore.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("TTL for Host cache entries");?></td>
 								<td width="78%" class="vtable">
-										<select id="infra_host_ttl" name="infra_host_ttl">
-											<option value="60"  <?php if ($pconfig['infra_host_ttl'] == "60")  echo "selected=\"selected\""; ?>>1 minute</option>
-											<option value="120" <?php if ($pconfig['infra_host_ttl'] == "120") echo "selected=\"selected\""; ?>>2 minutes</option>
-											<option value="300" <?php if ($pconfig['infra_host_ttl'] == "300") echo "selected=\"selected\""; ?>>5 minutes</option>
-											<option value="600" <?php if ($pconfig['infra_host_ttl'] == "600") echo "selected=\"selected\""; ?>>10 minutes</option>
-											<option value="900" <?php if ($pconfig['infra_host_ttl'] == "900") echo "selected=\"selected\""; ?>>15 minutes</option>
-										</select><br />
-										<?=gettext("Time to live for entries in the host cache. The host cache contains roundtrip timing, lameness and EDNS support information. The default is 15 minutes.");?>
+									<select id="infra_host_ttl" name="infra_host_ttl">
+										<option value="60"  <?php if ($pconfig['infra_host_ttl'] == "60")  echo "selected=\"selected\""; ?>>1 minute</option>
+										<option value="120" <?php if ($pconfig['infra_host_ttl'] == "120") echo "selected=\"selected\""; ?>>2 minutes</option>
+										<option value="300" <?php if ($pconfig['infra_host_ttl'] == "300") echo "selected=\"selected\""; ?>>5 minutes</option>
+										<option value="600" <?php if ($pconfig['infra_host_ttl'] == "600") echo "selected=\"selected\""; ?>>10 minutes</option>
+										<option value="900" <?php if ($pconfig['infra_host_ttl'] == "900") echo "selected=\"selected\""; ?>>15 minutes</option>
+									</select><br />
+									<?=gettext("Time to live for entries in the host cache. The host cache contains roundtrip timing, lameness and EDNS support information. The default is 15 minutes.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Number of Hosts to cache");?></td>
 								<td width="78%" class="vtable">
-										<select id="infra_cache_numhosts" name="infra_cache_numhosts">
-											<option value="1000"  <?php if ($pconfig['infra_cache_numhosts'] == "1000")  echo "selected=\"selected\""; ?>>1000</option>
-											<option value="5000"  <?php if ($pconfig['infra_cache_numhosts'] == "5000")  echo "selected=\"selected\""; ?>>5000</option>
-											<option value="10000" <?php if ($pconfig['infra_cache_numhosts'] == "10000") echo "selected=\"selected\""; ?>>10 000</option>
-											<option value="20000" <?php if ($pconfig['infra_cache_numhosts'] == "20000") echo "selected=\"selected\""; ?>>20 000</option>
-											<option value="50000" <?php if ($pconfig['infra_cache_numhosts'] == "50000") echo "selected=\"selected\""; ?>>50 000</option>
-										</select><br />
-										<?=gettext("Number of hosts for which information is cached. The default is 10,000.");?>
+									<select id="infra_cache_numhosts" name="infra_cache_numhosts">
+										<option value="1000"  <?php if ($pconfig['infra_cache_numhosts'] == "1000")  echo "selected=\"selected\""; ?>>1000</option>
+										<option value="5000"  <?php if ($pconfig['infra_cache_numhosts'] == "5000")  echo "selected=\"selected\""; ?>>5000</option>
+										<option value="10000" <?php if ($pconfig['infra_cache_numhosts'] == "10000") echo "selected=\"selected\""; ?>>10 000</option>
+										<option value="20000" <?php if ($pconfig['infra_cache_numhosts'] == "20000") echo "selected=\"selected\""; ?>>20 000</option>
+										<option value="50000" <?php if ($pconfig['infra_cache_numhosts'] == "50000") echo "selected=\"selected\""; ?>>50 000</option>
+									</select><br />
+									<?=gettext("Number of hosts for which information is cached. The default is 10,000.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Unwanted Reply Threshold");?></td>
 								<td width="78%" class="vtable">
-										<select id="unwanted_reply_threshold" name="unwanted_reply_threshold">
-											<option value="disabled" <?php if ($pconfig['unwanted_reply_threshold'] == "disabled") echo "selected=\"selected\""; ?>>disabled</option>
-											<option value="5000000"  <?php if ($pconfig['unwanted_reply_threshold'] == "5000000")  echo "selected=\"selected\""; ?>>5 million</option>
-											<option value="10000000" <?php if ($pconfig['unwanted_reply_threshold'] == "10000000") echo "selected=\"selected\""; ?>>10 million</option>
-											<option value="20000000" <?php if ($pconfig['unwanted_reply_threshold'] == "20000000") echo "selected=\"selected\""; ?>>20 million</option>
-											<option value="40000000" <?php if ($pconfig['unwanted_reply_threshold'] == "40000000") echo "selected=\"selected\""; ?>>40 million</option>
-											<option value="50000000" <?php if ($pconfig['unwanted_reply_threshold'] == "50000000") echo "selected=\"selected\""; ?>>50 million</option>
-										</select><br />
-										<?=gettext("If enabled, a total number of unwanted replies is kept track of in every thread. When it reaches the threshold, a defensive action is taken and a warning is printed to the log file. This defensive action is to clear the RRSet and message caches, hopefully flushing away any poison. The default is disabled, but if enabled a value of 10 million is suggested.");?>
+									<select id="unwanted_reply_threshold" name="unwanted_reply_threshold">
+										<option value="disabled" <?php if ($pconfig['unwanted_reply_threshold'] == "disabled") echo "selected=\"selected\""; ?>>disabled</option>
+										<option value="5000000"  <?php if ($pconfig['unwanted_reply_threshold'] == "5000000")  echo "selected=\"selected\""; ?>>5 million</option>
+										<option value="10000000" <?php if ($pconfig['unwanted_reply_threshold'] == "10000000") echo "selected=\"selected\""; ?>>10 million</option>
+										<option value="20000000" <?php if ($pconfig['unwanted_reply_threshold'] == "20000000") echo "selected=\"selected\""; ?>>20 million</option>
+										<option value="40000000" <?php if ($pconfig['unwanted_reply_threshold'] == "40000000") echo "selected=\"selected\""; ?>>40 million</option>
+										<option value="50000000" <?php if ($pconfig['unwanted_reply_threshold'] == "50000000") echo "selected=\"selected\""; ?>>50 million</option>
+									</select><br />
+									<?=gettext("If enabled, a total number of unwanted replies is kept track of in every thread. When it reaches the threshold, a defensive action is taken and a warning is printed to the log file. This defensive action is to clear the RRSet and message caches, hopefully flushing away any poison. The default is disabled, but if enabled a value of 10 million is suggested.");?>
 								</td>
 							</tr>
 							<tr>
 								<td width="22%" valign="top" class="vncell"><?=gettext("Log level verbosity");?></td>
 								<td width="78%" class="vtable">
-										<select id="log_verbosity" name="log_verbosity">
+									<select id="log_verbosity" name="log_verbosity">
 <?php
 										for ($level = 0; $level <= 5; $level++):
 ?>
@@ -432,8 +432,8 @@ include_once("head.inc");
 <?php
 										endfor;
 ?>
-										</select><br />
-										<?=gettext("Select the log verbosity.");?>
+									</select><br />
+									<?=gettext("Select the log verbosity.");?>
 								</td>
 							</tr>
 							<tr>

6e3bd9d25605798f9642750fc67eb9209e4a7e7a
sbeaver <sbeaver@netgate.com>
2015-06-02 08:46:37 -0400

    servies_unbound_acls.php Conversion complete
    Ready for review

--- usr/local/www/services_unbound_acls.php
+++ usr/local/www/services_unbound_acls.php
@@ -42,6 +42,7 @@ if (!is_array($config['unbound']['acls'])) {
 $a_acls = &$config['unbound']['acls'];
 
 $id = $_GET['id'];
+
 if (isset($_POST['aclid'])) {
 	$id = $_POST['aclid'];
 }
@@ -52,6 +53,7 @@ if (!empty($id) && !is_numeric($id)) {
 }
 
 $act = $_GET['act'];
+
 if (isset($_POST['act'])) {
 	$act = $_POST['act'];
 }
@@ -78,16 +80,30 @@ if ($act == "edit") {
 	}
 }
 
+// Add a row to the networks table
+if($_GET && $_GET['addrow'])
+    array_push($networkacl, array('acl_network' => '', 'mask' => '32', 'description' => ''));
+             
 if ($_POST) {
 	unset($input_errors);
 	$pconfig = $_POST;
-
+    $deleting = false;
+    
+    // Delete a row from the networks table
+    for($idx = 0; $idx<50; $idx++) {
+        if($pconfig['dlt' . $idx] == 'Delete') {
+            unset($networkacl[$idx]);
+            $deleting = true;
+            break;
+        }
+    }
+  
 	if ($_POST['apply']) {
 		$retval = services_unbound_configure();
 		$savemsg = get_std_save_message($retval);
 		if ($retval == 0)
 			clear_subsystem_dirty('unbound');
-	} else {
+	} else if(!$deleting) {
 
 		// input validation - only allow 50 entries in a single ACL
 		for($x=0; $x<50; $x++) {
@@ -148,253 +164,190 @@ if ($_POST) {
 	}
 }
 
+//DEBUG
+
 $closehead = false;
 $pgtitle = "Services: DNS Resolver: Access Lists";
 $shortcut_section = "resolver";
 include("head.inc");
 
+if ($input_errors)
+    print_input_errors($input_errors);
+    
+if ($savemsg)
+    print_info_box($savemsg, 'success');
+    
+if (is_subsystem_dirty('unbound'))
+    print_info_box_np(gettext("The configuration of the DNS Resolver, has been changed") . ".<br />" . gettext("You must apply the changes in order for them to take effect."));
+
+$tab_array = array();
+$tab_array[] = array(gettext("General Settings"), false, "/services_unbound.php");
+$tab_array[] = array(gettext("Advanced settings"), false, "services_unbound_advanced.php");
+$tab_array[] = array(gettext("Access Lists"), true, "/services_unbound_acls.php");
+display_top_tabs($tab_array, true);
+
+require('classes/Form.class.php');
+
+if($act=="new" || $act=="edit") {
+	
+    $form = new Form();
+    
+    $section = new Form_Section('New Access List');
+						
+    $section->addInput(new Form_Input(
+    	'aclid',
+    	null,
+    	'hidden',
+    	$id
+    ));
+    
+    $section->addInput(new Form_Input(
+    	'act',
+    	null,
+    	'hidden',
+    	$act
+    ));
+        						
+    $section->addInput(new Form_Input(
+    	'aclname',
+    	'Access LIst name',
+    	'text',
+    	$pconfig['aclname']
+    ))->setHelp('Provide an Access List name.');
+    
+    $section->addInput(new Form_Select(
+	'action',
+	'Action',
+	strtolower($pconfig['aclaction']),
+	array('allow' => 'Allow','deny' => 'Deny','refuse' => 'Refuse','allow snoop' => 'Allow Snoop')
+    ))->setHelp('Choose what to do with DNS requests that match the criteria specified below.' . '<br />' .
+                'Deny: Stops queries from hosts within the netblock defined below.' . '<br />' . 
+                'Refuse: Stops queries from hosts within the netblock defined below, but sends a DNS rcode REFUSED error message back to the client.' . '<br />' .
+                'Allow: Allow queries from hosts within the netblock defined below.' . '<br />' .
+                'Allow Snoop: Allow recursive and nonrecursive access from hosts within the netblock defined below. Used for cache snooping and ideally should only be configured for your administrative host.');
+
+    $counter = 0;
+    $numrows = count($networkacl) - 1;
+    
+    foreach($networkacl as $item) {
+    	$network = $item['acl_network'];
+    	$cidr = $item['mask'];
+    	$description = $item['description'];
+    	
+    	$group = new Form_Group($counter == 0 ? 'Networks':null);
+    	
+    	$group->add(new Form_IpAddress(
+    	    'acl_network' . $counter,
+    	    null,
+    	    $network
+    	))->setHelp(($counter == $numrows) ? 'Network':null);
+    	
+    	$group->add(new Form_Select(
+	        'mask' . $counter,
+	        null,
+	        $cidr,
+	        array_combine(range(32, 1, -1), range(32, 1, -1))
+        ))->setWidth(2)->setHelp(($counter == $numrows) ? 'Mask':null);
+        
+        $group->add(new Form_Input(
+    	    'description' . $counter,
+    	    null,
+    	    'text',
+    	    $description
+    	))->setWidth(3)->setHelp(($counter == $numrows) ? 'Description':null);
+    	
+    	$btndlt = new Form_Button(
+    	    'dlt' . $counter,
+    	    'Delete'
+	    );
+	   
+	    $btndlt->removeClass('btn-primary')->addClass('btn-sm btn-danger');
+	   
+    	$group->add($btndlt);
+
+    	$section->add($group);
+    	
+    	$counter++;
+    }
+
+	$btnadd = new Form_Button(
+	    'btnadd',
+	    'Add row',
+	    'services_unbound_acls.php?act=' . $act . '&addrow=yes'
+	);
+	
+	$btnadd->removeClass(btn-primary)->addClass('btn-sm btn-success');
+	
+	$section->addInput(new Form_StaticText(
+	    'Add row',
+	    $btnadd
+	))->setHelp('Remember to save after each Add or Delete');
+	
+    $section->addInput(new Form_Input(
+    	'descr',
+    	'Description',
+    	'text',
+    	$pconfig['descr']
+    ))->setHelp('You may enter a description here for your reference.');
+    
+    $form->addGlobal(new Form_Button(
+		'cancel',
+		'Cancel',
+		$referer
+	));
+	
+    $form->add($section);
+    print($form);
+}
+else // NOT 'edit' or 'add'
+{
 ?>
+<div class="panel panel-default">
+    <div class="panel-heading"><?=gettext('Access Lists to control access to the DNS Resolver')?></div>
+    <div class="panel-body">
+        <div class="table-responsive">
+            <table class="table table-striped table-hover table-condensed">
+                <thead>
+                    <tr>
+        				<th><?=gettext("Access List Name"); ?></th>
+        				<th><?=gettext("Action"); ?></th>
+        				<th><?=gettext("Description"); ?></th>
+        				<th>&nbsp;</th>
+                    </tr>                
+                </thead>
+                <tbody>
+<?php                
+    $i = 0;
+    foreach($a_acls as $acl):
+?>
+				    <tr ondblclick="document.location='services_unbound_acls.php?act=edit&amp;id=<?=$i?>'">
+						<td>
+						    <?=htmlspecialchars($acl['aclname'])?>
+						</td>
+						<td>
+							<?=htmlspecialchars($acl['aclaction'])?>
+						</td>
+						<td>
+							<?=htmlspecialchars($acl['description'])?>
+						</td>
+						<td>
+							<a href="services_unbound_acls.php?act=edit&amp;id=<?=$i?>" class="btn btn-xs btn-info" >Edit</a>
+							<a href="services_unbound_acls.php?act=del&amp;id=<?=$i?>" class="btn btn-xs btn-danger">Delete</a>
+						</td>
+					</tr>
+<?php
+        $i++;
+	endforeach;
+?>						                
+                </tbody>
+            </table>
+        </div>
+        <nav class="action-buttons">
+            <a href="services_unbound_acls.php?act=new" class="btn btn-sm btn-success">Add</a>
+        </nav>        
+    </div>									
+</div>
+<?php	
+}
 
-<script type="text/javascript" src="/javascript/jquery.ipv4v6ify.js"></script>
-<script type="text/javascript" src="/javascript/row_helper.js"></script>
-
-<script type="text/javascript">
-//<![CDATA[
-	rowname[0] = "acl_network";
-	rowtype[0] = "textbox,ipv4v6";
-	rowsize[0] = "30";
-
-	rowname[1] = "mask";
-	rowtype[1] = "select,ipv4v6";
-	rowsize[1] = "1";
-
-	rowname[2] = "description";
-	rowtype[2] = "textbox";
-	rowsize[2] = "40";
-//]]>
-</script>
-</head>
-
-<body>
-
-<?php include("fbegin.inc"); ?>
-<form action="services_unbound_acls.php" method="post" name="iform" id="iform">
-<?php if ($input_errors) print_input_errors($input_errors); ?>
-<?php if ($savemsg) print_info_box($savemsg); ?>
-<?php if (is_subsystem_dirty('unbound')): ?><br/>
-<?php print_info_box_np(gettext("The configuration of the DNS Resolver, has been changed") . ".<br />" . gettext("You must apply the changes in order for them to take effect."));?><br />
-<?php endif; ?>
-
-<table width="100%" border="0" cellpadding="0" cellspacing="0" summary="services unbound acls">
-	<tbody>
-		<tr>
-			<td class="tabnavtbl">
-				<?php
-					$tab_array = array();
-					$tab_array[] = array(gettext("General Settings"), false, "/services_unbound.php");
-					$tab_array[] = array(gettext("Advanced settings"), false, "services_unbound_advanced.php");
-					$tab_array[] = array(gettext("Access Lists"), true, "/services_unbound_acls.php");
-					display_top_tabs($tab_array, true);
-				?>
-			</td>
-		</tr>
-		<tr>
-			<td id="mainarea">
-				<div class="tabcont">
-					<?php if($act=="new" || $act=="edit"): ?>
-						<input name="aclid" type="hidden" value="<?=$id;?>" />
-						<input name="act" type="hidden" value="<?=$act;?>" />
-
-					<table width="100%" border="0" cellpadding="6" cellspacing="0" summary="main area">
-						<tr>
-							<td colspan="2" valign="top" class="listtopic"><?=ucwords(sprintf(gettext("%s Access List"),$act));?></td>
-						</tr>
-						<tr>
-							<td width="22%" valign="top" class="vncellreq"><?=gettext("Access List name");?></td>
-							<td width="78%" class="vtable">
-								<input name="aclname" type="text" class="formfld" id="aclname" size="30" maxlength="30" value="<?=htmlspecialchars($pconfig['aclname']);?>" />
-								<br />
-								<span class="vexpl"><?=gettext("Provide an Access List name.");?></span>
-							</td>
-						</tr>
-						<tr>
-							<td width="22%" valign="top" class="vncellreq"><?=gettext("Action");?></td>
-							<td width="78%" class="vtable">
-								<select name="aclaction" class="formselect">
-									<?php $types = explode(",", "Allow,Deny,Refuse,Allow Snoop"); foreach ($types as $type): ?>
-									<option value="<?=strtolower($type);?>" <?php if (strtolower($type) == strtolower($pconfig['aclaction'])) echo "selected=\"selected\""; ?>>
-									<?=htmlspecialchars($type);?>
-									</option>
-									<?php endforeach; ?>
-								</select>
-								<br />
-								<span class="vexpl">
-									<?=gettext("Choose what to do with DNS requests that match the criteria specified below.");?> <br />
-									<?=gettext("<b>Deny:</b> This action stops queries from hosts within the netblock defined below.");?> <br />
-									<?=gettext("<b>Refuse:</b> This action also stops queries from hosts within the netblock defined below, but sends a DNS rcode REFUSED error message back to the client.");?> <br />
-									<?=gettext("<b>Allow:</b> This action allows queries from hosts within the netblock defined below.");?> <br />
-									<?=gettext("<b>Allow Snoop:</b> This action allows recursive and nonrecursive access from hosts within the netblock defined below. Used for cache snooping and ideally should only be configured for your administrative host.");?> <br />
-								</span>
-							</td>
-						</tr>
-						<tr>
-						<td width="22%" valign="top" class="vncellreq"><?=gettext("Networks");?></td>
-						<td width="78%" class="vtable">
-							<table id="maintable" summary="networks">
-								<tbody>
-									<tr>
-										<td><div id="onecolumn"><?=gettext("Network");?></div></td>
-										<td><div id="twocolumn"><?=gettext("CIDR");?></div></td>
-										<td><div id="threecolumn"><?=gettext("Description");?></div></td>
-									</tr>
-									<?php $counter = 0; ?>
-									<?php
-										if($networkacl)
-											foreach($networkacl as $item):
-									?>
-											<?php
-												$network = $item['acl_network'];
-												$cidr = $item['mask'];
-												$description = $item['description'];
-											?>
-									<tr>
-										<td>
-											<input name="acl_network<?=$counter;?>" type="text" class="formfld unknown ipv4v6" id="acl_network<?=$counter;?>" size="30" value="<?=htmlspecialchars($network);?>" />
-										</td>
-										<td>
-											<select name="mask<?=$counter;?>" class="formselect ipv4v6" id="mask<?=$counter;?>">
-											<?php
-												for ($i = 128; $i > 0; $i--) {
-													echo "<option value=\"$i\" ";
-													if ($i == $cidr) echo "selected=\"selected\"";
-													echo ">" . $i . "</option>";
-												}
-											?>
-											</select>
-										</td>
-										<td>
-											<input name="description<?=$counter;?>" type="text" class="formfld unknown" id="description<?=$counter;?>" size="40" value="<?=htmlspecialchars($description);?>" />
-										</td>
-										<td>
-											<a onclick="removeRow(this); return false;" href="#"><img border="0" src="/themes/<?=$g['theme'];?>/images/icons/icon_x.gif" alt="delete" /></a>
-										</td>
-									</tr>
-									<?php $counter++; ?>
-									<?php endforeach; ?>
-								</tbody>
-							</table>
-							<a onclick="javascript:addRowTo('maintable', 'formfldalias'); return false;" href="#">
-								<img border="0" src="/themes/<?= $g['theme']; ?>/images/icons/icon_plus.gif" alt="" title="<?=gettext("add another entry");?>" />
-							</a>
-							<script type="text/javascript">
-							//<![CDATA[
-								field_counter_js = 3;
-								rows = 1;
-								totalrows = <?php echo $counter; ?>;
-								loaded = <?php echo $counter; ?>;
-							//]]>
-							</script>
-
-							</td>
-						</tr>
-
-						<tr>
-							<td width="22%" valign="top" class="vncell"><?=gettext("Description");?></td>
-							<td width="78%" class="vtable">
-								<input name="description" type="text" class="formfld unknown" id="description" size="52" maxlength="52" value="<?=htmlspecialchars($pconfig['description']);?>" />
-								<br />
-								<span class="vexpl"><?=gettext("You may enter a description here for your reference.");?></span>
-							</td>
-						</tr>
-						<tr>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td width="22%" valign="top">&nbsp;</td>
-							<td width="78%">
-								&nbsp;<br />&nbsp;
-								<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save"); ?>" />
-								<input type="button" class="formbtn" value="<?=gettext("Cancel");?>" onclick="window.location.href='<?=$referer;?>'" />
-							</td>
-						</tr>
-					</table>
-
-				<?php else: ?>
-
-				<table class="sortable" width="100%" border="0" cellpadding="0" cellspacing="0" summary="results">
-					<thead>
-						<tr>
-							<td width="25%" class="listhdrr"><?=gettext("Access List Name"); ?></td>
-							<td width="25%" class="listhdrr"><?=gettext("Action"); ?></td>
-							<td width="45%" class="listhdr"><?=gettext("Description"); ?></td>
-							<td width="5%" class="list">&nbsp;</td>
-						</tr>
-					</thead>
-					<tfoot>
-						<tr>
-							<td class="list" colspan="3">&nbsp;</td>
-							<td class="list">
-								<table border="0" cellspacing="0" cellpadding="1" summary="icons">
-									<tr>
-										<td width="17">&nbsp;</td>
-										<td valign="middle"><a href="services_unbound_acls.php?act=new">
-											<img src="./themes/<?=$g['theme'];?>/images/icons/icon_plus.gif" title="<?=gettext("Add new Access List"); ?>" border="0" alt="add" />
-										</a></td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="4">
-								<p>
-									<?=gettext("Access Lists to control access to the DNS Resolver can be defined here.");?>
-								</p>
-							</td>
-						</tr>
-					</tfoot>
-					<tbody>
-					<?php
-						$i = 0;
-						foreach($a_acls as $acl):
-					?>
-						<tr ondblclick="document.location='services_unbound_acls.php?act=edit&amp;id=<?=$i;?>'">
-							<td class="listlr">
-								<?=htmlspecialchars($acl['aclname']);?>
-							</td>
-							<td class="listr">
-								<?=htmlspecialchars($acl['aclaction']);?>
-							</td>
-							<td class="listbg">
-								<?=htmlspecialchars($acl['description']);?>
-							</td>
-							<td valign="middle" class="list nowrap">
-								<table border="0" cellspacing="0" cellpadding="1" summary="icons">
-									<tr>
-										<td valign="middle"><a href="services_unbound_acls.php?act=edit&amp;id=<?=$i;?>">
-											<img src="./themes/<?=$g['theme'];?>/images/icons/icon_e.gif" title="<?=gettext("edit access list"); ?>" width="17" height="17" border="0" alt="edit" />
-										</a></td>
-										<td valign="middle"><a href="services_unbound_acls.php?act=del&amp;id=<?=$i;?>" onclick="return confirm('<?=gettext("Do you really want to delete this access list?"); ?>')">
-											<img src="/themes/<?=$g['theme'];?>/images/icons/icon_x.gif" title="<?=gettext("delete access list"); ?>" width="17" height="17" border="0" alt="delete" />
-										</a></td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-					<?php
-						$i++;
-						endforeach;
-					?>
-					<tr style="display:none"><td></td></tr>
-					</tbody>
-				</table>
-			<?php endif; ?>
-			</div>
-			</td>
-		</tr>
-	</tbody>
-</table>
-</form>
-
-<?php include("fend.inc"); ?>
-</body>
-</html>
+include("foot.inc");

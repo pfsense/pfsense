c39d178c8c1ba176dcc36d1565d5a04719cc1358
Sjon Hortensius <sjon@hortensius.net>
2015-06-13 12:48:06 +0200

    Remove help-text from hidden input #225

--- usr/local/www/services_dnsmasq_edit.php
+++ usr/local/www/services_dnsmasq_edit.php
@@ -246,7 +246,7 @@ if (isset($id) && $a_hosts[$id]) {
 		null,
 		'hidden',
 		$pconfig['id']
-	))->setHelp('You may enter a description here for your reference (not parsed).');
+	));
 }
 
 $form->add($section);

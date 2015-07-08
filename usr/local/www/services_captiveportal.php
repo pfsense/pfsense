a2a5983a12857cccf3307644385a35a9035b99a2
sbeaver <sbeaver@netgate.com>
2015-07-08 13:33:38 -0400

    Restore section commented out for testing

--- usr/local/www/services_captiveportal.php
+++ usr/local/www/services_captiveportal.php
@@ -57,10 +57,10 @@ $cpzone = $_GET['zone'];
 if (isset($_POST['zone']))
 	$cpzone = $_POST['zone'];
 
-//if (empty($cpzone) || empty($config['captiveportal'][$cpzone])) {
-//	header("Location: services_captiveportal_zones.php");
-//	exit;
-//}
+if (empty($cpzone) || empty($config['captiveportal'][$cpzone])) {
+	header("Location: services_captiveportal_zones.php");
+	exit;
+}
 
 if (!is_array($config['captiveportal']))
 	$config['captiveportal'] = array();

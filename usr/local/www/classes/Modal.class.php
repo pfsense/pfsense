561cc25db7cddb242a3c020c7009750faf95f61a
Sjon Hortensius <sjon@hortensius.net>
2015-07-18 15:37:59 +0200

    Modal - refactored using new attributes
    refs #30

--- usr/local/www/classes/Modal.class.php
+++ usr/local/www/classes/Modal.class.php
@@ -1,106 +1,57 @@
 <?php
 
 require_once('classes/Form.class.php');
-foreach (glob('classes/Form/*.class.php') as $file)
-	require_once($file);
 
-class Modal extends Form
+class Modal extends Form_Section
 {
-	protected $_id = '';
-	protected $_title = '';
-	protected $_groups = array();
-	protected $_footer = array();
-	protected $_html = '';
-	protected $_modal_class_size = '';
+	protected $_attributes = array(
+		'id' => null,
+		'class' => array(
+			'modal' => true,
+			'fade' => true,
+		),
+		'role' => 'dialog',
+		'aria-labelledby' => null,
+		'aria-hidden' => 'true',
+	);
+	protected $_global = array();
+	protected $_isLarge;
 
-	public function __construct($title, $id, $size = '')
+	public function __construct($title, $id, $isLarge = false, $submit = null)
 	{
-		switch ($size) {
-			case 'small':
-				$this->_modal_class_size = 'modal-sm';
-				break;
-			case 'large':
-				$this->_modal_class_size = 'modal-lg';
-				break;
-			case '':
-				break;
-			default:
-				throw new Exception('Incorrect size, pass either large or small');
-		}
-		$this->_id = $id;
 		$this->_title = $title;
+		$this->_attributes['id'] = $this->_attributes['aria-labelledby'] = $id;
+		$this->_isLarge = $isLarge;
 
-		$this->addClass('modal', 'fade');
+		if (gettype($submit) == 'string')
+			$submit = (new Form_Button(
+				'save',
+				$submit
+			))->setAttribute('data-dismiss', 'modal');
 
-		return $this;
-	}
-
-	public function add(Form_Group $group)
-	{
-		array_push($this->_groups, $group);
-		$group->_setParent($this);
-
-		return $group;
-	}
-
-	public function addInput(Form_Input $input)
-	{
-		$group = new Form_Group($input->getTitle());
-		$group->add($input);
-
-		$this->add($group);
-
-		return $input;
-	}
-
-	public function addHtml($html)
-	{
-		$this->_html .= $html;
-
-		return $this;
-	}
-
-	public function addFooter(Form_Input $input)
-	{
-		array_push($this->_footer, $input);
-
-		return $input;
+		if (false !== $submit)
+			$this->addGlobal($submit);
 	}
 
 	public function __toString()
 	{
-		if (empty($this->_footer)) {
-			$this->addFooter(new Form_Button(
-				'submit',
-				'Submit'
-			));
-
-			$this->addFooter(new Form_Button(
-				'close',
-				'Close',
-				''
-			))->setAttribute('data-dismiss', 'modal');
-		}
-
-		$title = gettext($title);
-
-		$html = implode('', $this->_groups);
-		$html .= $this->_html;
-
+		$element = Form_Element::__toString();
+		$title = htmlspecialchars(gettext($this->_title));
+		$body = implode('', $this->_groups);
 		$footer = implode('', $this->_global);
-		$footer .= implode('', $this->_footer);
+		$modalClass = $this->_isLarge ? 'modal-lg' : 'modal-sm';
 
 		return <<<EOT
-	<div {$this->getHtmlClass()} id="{$this->_id}" role="dialog" aria-labelledby="{$this->_id}" aria-hidden="true">
-		<div class="modal-dialog {$this->_modal_class_size}">
+	{$element}
+		<div class="modal-dialog {$modalClass}">
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
-					<h3 class="modal-title">{$this->_title}</h3>
+					<h3 class="modal-title">{$title}</h3>
 				</div>
-				<form class="form-horizontal" action="{$this->_action}" method="post">
+				<form class="form-horizontal" action="" method="post">
 					<div class="modal-body">
 						{$html}
 					</div>

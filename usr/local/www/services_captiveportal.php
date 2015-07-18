896607c3a512fb095f74c7b3846a7c20aecae822
doktornotor <notordoktor@gmail.com>
2015-07-18 20:34:43 +0200

    Bug #4551 - consistent usage of Forwarder/Resolver across the WebGUI
    Clarify that this applies to DNS Resolver as well.

--- usr/local/www/services_captiveportal.php
+++ usr/local/www/services_captiveportal.php
@@ -1234,7 +1234,7 @@ function enable_change(enable_change) {
 									<?=gettext("Note:"); ?><br />
 								</strong>
 							</span>
-							<?=gettext("Changing any settings on this page will disconnect all clients! Don't forget to enable the DHCP server on your captive portal interface! Make sure that the default/maximum DHCP lease time is higher than the timeout entered on this page. Also, the DNS forwarder needs to be enabled for DNS lookups by unauthenticated clients to work."); ?>
+							<?=gettext("Changing any settings on this page will disconnect all clients! Don't forget to enable the DHCP server on your captive portal interface! Make sure that the default/maximum DHCP lease time is higher than the timeout entered on this page. Also, the DNS Forwarder or Resolver needs to be enabled for DNS lookups by unauthenticated clients to work."); ?>
 						</span>
 					</td>
 				</tr>

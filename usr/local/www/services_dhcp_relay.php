93d6380d8d62e422d98ce1b739eea2beb9369616
Stephen Beaver <sbeaver@netgate.com>
2015-08-20 17:20:42 -0400

    Bug 4973 fixed
    Programmer has mixed array and lists when saving/reading to/from config

--- usr/local/www/services_dhcp_relay.php
+++ usr/local/www/services_dhcp_relay.php
@@ -1,33 +1,59 @@
 <?php
 /*
 	services_dhcp_relay.php
-
-	Copyright (C) 2003-2004 Justin Ellison <justin@techadvise.com>.
-	Copyright (C) 2010 	Ermal Luçi
-	Copyright (C) 2013-2015 Electric Sheep Fencing, LP
-	All rights reserved.
-
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
-
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
-
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
 */
+/* ====================================================================
+ *  Copyright (c)  2004-2015  Electric Sheep Fencing, LLC. All rights reserved. 
+ *  Copyright (c)  22003-2004 Justin Ellison <justin@techadvise.com>
+ *  Copyright (c)  22010 	Ermal Luçi
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, 
+ *  are permitted provided that the following conditions are met: 
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution. 
+ *
+ *  3. All advertising materials mentioning features or use of this software 
+ *      must display the following acknowledgment:
+ *      "This product includes software developed by the pfSense Project
+ *       for use in the pfSense software distribution. (http://www.pfsense.org/). 
+ *
+ *  4. The names "pfSense" and "pfSense Project" must not be used to
+ *       endorse or promote products derived from this software without
+ *       prior written permission. For written permission, please contact
+ *       coreteam@pfsense.org.
+ *
+ *  5. Products derived from this software may not be called "pfSense"
+ *      nor may "pfSense" appear in their names without prior written
+ *      permission of the Electric Sheep Fencing, LLC.
+ *
+ *  6. Redistributions of any form whatsoever must retain the following
+ *      acknowledgment:
+ *
+ *  "This product includes software developed by the pfSense Project
+ *  for use in the pfSense software distribution (http://www.pfsense.org/).
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE pfSense PROJECT ``AS IS'' AND ANY
+ *  EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE pfSense PROJECT OR
+ *  ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *  OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  ====================================================================
+ *
+ */
 /*
 	pfSense_MODULE:	dhcprelay
 */
@@ -42,13 +68,6 @@
 require("guiconfig.inc");
 require('classes/Form.class.php');
 
-function filterDestinationServers(array $destinationServers)
-{
-	return array_unique(
-		array_filter($destinationServers)
-	);
-}
-
 $pconfig['enable'] = isset($config['dhcrelay']['enable']);
 
 if (empty($config['dhcrelay']['interface'])) {
@@ -56,10 +75,6 @@ if (empty($config['dhcrelay']['interface'])) {
 } else {
 	$pconfig['interface'] = explode(",", $config['dhcrelay']['interface']);
 }
-
-$pconfig['server'] = filterDestinationServers(
-	explode(',', $config['dhcrelay']['server'])
-);
 
 $pconfig['agentoption'] = isset($config['dhcrelay']['agentoption']);
 
@@ -90,12 +105,8 @@ if (is_array($config['dhcpd'])) {
 }
 
 if ($_POST) {
-
 	unset($input_errors);
-
-	if ($_POST['server'])
-		$_POST['server'] = filterDestinationServers($_POST['server']);
-
+	
 	$pconfig = $_POST;
 
 	/* input validation */
@@ -105,21 +116,29 @@ if ($_POST) {
 
 		do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
 
+		$svrlist = '';
+		
 		if ($_POST['server']) {
-			$checksrv = explode(",", $_POST['server']);
-			foreach ($checksrv as $srv) {
-				if (!is_ipaddr($srv)) {
+			foreach($_POST['server'] as $checksrv => $srv) {
+				if (!is_ipaddr($srv[0]))
 					$input_errors[] = gettext("A valid Destination Server IP address must be specified.");
-				}
+					
+				if(!empty($svrlist))
+					$svrlist .= ',';
+										
+				$svrlist .= $srv[0];
 			}
 		}
 	}
 
+	// Now $svrlist is a comma separated list of servers ready to save to the config system
+	$pconfig['server'] = $svrlist;
+	
 	if (!$input_errors) {
 		$config['dhcrelay']['enable'] = $_POST['enable'] ? true : false;
 		$config['dhcrelay']['interface'] = implode(",", $_POST['interface']);
 		$config['dhcrelay']['agentoption'] = $_POST['agentoption'] ? true : false;
-		$config['dhcrelay']['server'] = implode(',', $_POST['server']);
+		$config['dhcrelay']['server'] = $pconfig['server'];
 
 		write_config();
 
@@ -144,7 +163,7 @@ if ($dhcpd_enabled) {
 if ($input_errors)
 	print_input_errors($input_errors);
 
-if ($savemsg)
+if ($savemsg, 'success')
 	print_info_box($savemsg);
 
 $form = new Form;
@@ -196,9 +215,11 @@ function createDestinationServerInputGroup($value = null)
 
 if (!isset($pconfig['server']) || count($pconfig['server']) < 1)
 	$section->add(createDestinationServerInputGroup());
-else
-	foreach ($pconfig['server'] as $idx => $server)
-		$section->add(createDestinationServerInputGroup($server));
+else {
+	foreach (explode(',', $pconfig['server']) as $server) {
+			$section->add(createDestinationServerInputGroup($server));
+	}
+}
 
 $form->add($section);
 print $form;

19ce41751c52edd75f6bc3866a6953b9ee05bc0d
tiago barrionuevo <tiago.bar@openmailbox.org>
2015-08-28 17:56:20 -0300

    This fix the issues #3900 and #5051 to make No-ip round robin DNS works.

--- usr/local/www/services_dyndns_edit.php
+++ usr/local/www/services_dyndns_edit.php
@@ -115,9 +115,13 @@ if ($_POST) {
 		/* Namecheap can have a @. in hostname */
 		if ($pconfig['type'] == "namecheap" && substr($_POST['host'], 0, 2) == '@.')
 			$host_to_check = substr($_POST['host'], 2);
-		else
+		else {
+			$host_to_check = $_POST['host'];
+
 			/* No-ip (and maybe others) can have a @ in hostname */
-			$host_to_check = str_replace('@', '', $_POST['host']);
+			$last = strrpos($host_to_check, '@');
+			$host_to_check = substr_replace($host_to_check, '.', $last, 1);
+		}
 
 		if ($pconfig['type'] != "custom" && $pconfig['type'] != "custom-v6")
 			if (!is_domain($host_to_check))

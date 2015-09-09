635f649d039c5a39ed25cb9c745b0e5870996526
Stephen Beaver <sbeaver@netgate.com>
2015-09-08 21:59:13 -0400

    Fix typo

--- src/usr/local/www/interfaces.php
+++ src/usr/local/www/interfaces.php
@@ -2619,7 +2619,7 @@ $section->addInput(new Form_Select(
 	['' => 'Disabled', 'custom' => 'Custom', 'preset' => 'Pre-set']
 ))->setHelp('Select a reset timing type');
 
-$group = new Form_Group('Custom resest');
+$group = new Form_Group('Custom reset');
 $group->addClass('pppoecustom');
 
 $group->add(new Form_Input(

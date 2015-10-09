d3dbd302ad2c4d7143f26af2c407b3a1afbcbeee
Stephen Beaver <sbeaver@netgate.com>
2015-10-09 19:36:25 -0400

    Fixed #5292

--- src/usr/local/www/widgets/widgets/log.widget.php
+++ src/usr/local/www/widgets/widgets/log.widget.php
@@ -136,7 +136,8 @@ else
 		$rule = find_rule_by_number($filterent['rulenum'], $filterent['tracker'], $filterent['act']);
 ?>
 		<tr>
-			<td><a role="button" data-toggle="popover" data-trigger="hover"
+			<td><a <a href="#" onclick="javascript:getURL('diag_logs_filter.php?getrulenum=<?php echo "{$filterent['rulenum']},{$filterent['tracker']},{$filterent['act']}"; ?>', outputrule);" 
+			role="button" data-toggle="popover" data-trigger="hover"
 				data-title="Rule that triggered this action"
 				data-content="<?=htmlspecialchars($rule)?>"> <i
 					class="icon icon-<?=$iconfn?>"></i>
@@ -232,3 +233,47 @@ events.push(function(){
 			</div>
 		</div>
 	</form>
+
+<script>
+if (typeof getURL == 'undefined') {
+	getURL = function(url, callback) {
+		if (!url)
+			throw 'No URL for getURL';
+		try {
+			if (typeof callback.operationComplete == 'function')
+				callback = callback.operationComplete;
+		} catch (e) {}
+			if (typeof callback != 'function')
+				throw 'No callback function for getURL';
+		var http_request = null;
+		if (typeof XMLHttpRequest != 'undefined') {
+		    http_request = new XMLHttpRequest();
+		}
+		else if (typeof ActiveXObject != 'undefined') {
+			try {
+				http_request = new ActiveXObject('Msxml2.XMLHTTP');
+			} catch (e) {
+				try {
+					http_request = new ActiveXObject('Microsoft.XMLHTTP');
+				} catch (e) {}
+			}
+		}
+		if (!http_request)
+			throw 'Both getURL and XMLHttpRequest are undefined';
+		http_request.onreadystatechange = function() {
+			if (http_request.readyState == 4) {
+				callback( { success : true,
+				  content : http_request.responseText,
+				  contentType : http_request.getResponseHeader("Content-Type") } );
+			}
+		};
+		http_request.open('GET', url, true);
+		http_request.send(null);
+	};
+}
+
+function outputrule(req) {
+	alert(req.content);
+}
+
+</script>

1ab880ef232ee94d08700f3ca3717ad69fcdb247
Chris Buechler <cmb@pfsense.org>
2015-10-14 01:37:12 -0500

    Auto-add firewall rules for DHCP Relay, same as is done for DHCP Server. Add filter reload to DHCP Relay config so rules are immediately added/removed. Ticket #4558

--- src/usr/local/www/services_dhcp_relay.php
+++ src/usr/local/www/services_dhcp_relay.php
@@ -66,6 +66,7 @@
 ##|-PRIV
 
 require("guiconfig.inc");
+require_once("filter.inc");
 require_once('classes/Form.class.php');
 
 $pconfig['enable'] = isset($config['dhcrelay']['enable']);
@@ -147,7 +148,7 @@ if ($_POST) {
 		$retval = 0;
 		$retval = services_dhcrelay_configure();
 		$savemsg = get_std_save_message($retval);
-
+		filter_configure();
 	}
 }
 

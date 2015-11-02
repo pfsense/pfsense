b76a8ff4f56156d51034042f77dbafacfe81c585
heper <heper@users.noreply.github.com>
2015-11-02 17:17:23 +0100

    convert interfaces_gif to fa

--- src/usr/local/www/interfaces_gif.php
+++ src/usr/local/www/interfaces_gif.php
@@ -149,8 +149,8 @@ display_top_tabs($tab_array);
 					<?=htmlspecialchars($gif['descr'])?>
 				</td>
 				<td>
-					<a href="interfaces_gif_edit.php?id=<?=$i?>" class="btn btn-default btn-xs"><?=gettext("Edit")?></a>
-					<a href="interfaces_gif.php?act=del&amp;id=<?=$i?>" class="btn btn-danger btn-xs"><?=gettext("Delete")?></a>
+					<a class="fa fa-pencil"	title="<?=gettext('Edit GIF interface')?>"	href="interfaces_gif_edit.php?id=<?=$i?>"></a>
+					<a class="fa fa-trash"	title="<?=gettext('Delete GIF interface')?>"	href="interfaces_gif.php?act=del&amp;id=<?=$i?>" onclick="return confirm('<?=gettext("Are you sure you want to delete this GIF interface?")?>')"></a>
 				</td>
 			</tr>
 <?php endforeach; ?>

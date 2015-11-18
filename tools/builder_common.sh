9d8dacc238e1c3feeb557b8fca0f87269cd8b8ed
Renato Botelho <renato@netgate.com>
2015-11-18 10:52:24 -0200

    Create pkg repo directory before copy

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -1076,6 +1076,8 @@ clone_to_staging_area() {
 		-X ${_exclude_files} \
 		.
 
+	mkdir -p ${STAGE_CHROOT_DIR}${PKG_REPO_PATH} >/dev/null 2>&1
+
 	# Create repo and repo-devel packages
 	cp -f ${STAGE_CHROOT_DIR}${PRODUCT_SHARE_DIR}/${PRODUCT_NAME}-repo.conf \
 		${STAGE_CHROOT_DIR}${PKG_REPO_PATH}

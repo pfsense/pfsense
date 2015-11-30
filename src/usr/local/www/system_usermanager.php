945204b1590dbd3d8eb774b961b3cfa77d1e9407
Stephen Beaver <sbeaver@netgate.com>
2015-11-30 15:21:03 -0500

    Fixed #5389

--- src/usr/local/www/system_usermanager.php
+++ src/usr/local/www/system_usermanager.php
@@ -167,15 +167,14 @@ if ($_POST['act'] == "delcert") {
 	$_POST['act'] = "edit";
 	$savemsg = gettext("Certificate") . " {$certdeleted} " . gettext("association removed.") . "<br />";
 }
-if ($_POST['act'] == "delprivid") {
 
-		if ($a_user[$id] && !empty($_POST['privid'])) {
-			unset($a_user[$id]['priv'][$_POST['privid']]);
-			local_user_set($a_user[$id]);
-			write_config();
-			$_POST['act'] = "edit";
-			$savemsg = gettext("Privilege removed.") . "<br />";
-		}
+if ($_POST['act'] == "delprivid") {
+	$privdeleted = $priv_list[$a_user[$id]['priv'][$_POST['privid']]]['name'];
+	unset($a_user[$id]['priv'][$_POST['privid']]);
+	local_user_set($a_user[$id]);
+	write_config();
+	$_POST['act'] = "edit";
+	$savemsg = gettext("Privilege ") . $privdeleted . gettext(" removed") . "<br />";
 }
 
 if ($_POST['save']) {
@@ -388,13 +387,27 @@ function build_priv_table() {
 	$privhtml .=		'</thead>';
 	$privhtml .=		'<tbody>';
 
-	foreach (get_user_privdesc($a_user[$id]) as $i => $priv) {
+	$i = 0;
+
+	foreach (get_user_privdesc($a_user[$id]) as $priv) {
+		$group = false;
+		if ($priv['group']) {
+			$group = $priv['group'];
+		}
+
 		$privhtml .=		'<tr>';
 		$privhtml .=			'<td>' . htmlspecialchars($priv['group']) . '</td>';
 		$privhtml .=			'<td>' . htmlspecialchars($priv['name']) . '</td>';
 		$privhtml .=			'<td>' . htmlspecialchars($priv['descr']) . '</td>';
-		$privhtml .=			'<td><a class="fa fa-trash no-confirm" title="'.gettext('Delete Privilege').'" id="delprivid' .$i. '"></a></td>';
+		$privhtml .=			'<td>';
+		if (!$group)
+			$privhtml .=			'<a class="fa fa-trash no-confirm icon-pointer" title="'.gettext('Delete Privilege').'" id="delprivid' .$i. '"></a></td>';
+
+		$privhtml .=			'</td>';
 		$privhtml .=		'</tr>';
+
+		if(!$group)
+			$i++;
 	}
 
 	$privhtml .=		'</tbody>';
@@ -434,7 +447,7 @@ function build_cert_table() {
 			$certhtml .=		'<td>' . htmlspecialchars($cert['descr']) . $revokedstr . '</td>';
 			$certhtml .=		'<td>' . htmlspecialchars($ca['descr']) . '</td>';
 			$certhtml .=		'<td>';
-			$certhtml .=			'<a id="delcert' . $i .'" class="btn btn-xs btn-warning" title="';
+			$certhtml .=			'<a id="delcert' . $i .'" class="fa fa-trash no-confirm icon-pointer" title="';
 			$certhtml .=			gettext('Remove this certificate association? (Certificate will not be deleted)') . '">Delete</a>';
 			$certhtml .=		'</td>';
 			$certhtml .=	'</tr>';
@@ -900,6 +913,7 @@ events.push(function(){
 			$('form').submit();
 		}
 	});
+
 	$('[id^=delprivid]').click(function(event) {
 		if(confirm(event.target.title)) {
 			$('#privid').val(event.target.id.match(/\d+$/)[0]);

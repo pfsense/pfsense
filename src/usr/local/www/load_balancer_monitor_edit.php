12745e51a69c8bd5850c94f6063732f49512f53d
Phil Davis <phil.davis@inf.org>
2015-12-15 17:04:23 +0545

    Code style l

--- src/usr/local/www/load_balancer_monitor_edit.php
+++ src/usr/local/www/load_balancer_monitor_edit.php
@@ -242,19 +242,20 @@ $types = array("icmp" => gettext("ICMP"), "tcp" => gettext("TCP"), "http" => get
 
 <script type="text/javascript">
 //<![CDATA[
-events.push(function(){
+events.push(function() {
 
 	// Hides all elements of the specified class. This will usually be a section
 	function hideClass(s_class, hide) {
-		if(hide)
+		if (hide) {
 			$('.' + s_class).hide();
-		else
+		} else {
 			$('.' + s_class).show();
+		}
 	}
 
 	// Hide all sections except 't'
-	function updateType(t){
-		switch(t) {
+	function updateType(t) {
+		switch (t) {
 	<?php
 		/* OK, so this is sick using php to generate javascript, but it needed to be done */
 		foreach ($types as $key => $val) {
@@ -288,8 +289,9 @@ events.push(function(){
 </script>
 
 <?php
-if ($input_errors)
+if ($input_errors) {
 	print_input_errors($input_errors);
+}
 
 $form = new Form(new Form_Button(
 	'Submit',

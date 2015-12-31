e23e99d9103be7a6797067bc48bcffa17235a221
stilez <stilez@users.noreply.github.com>
2015-12-31 05:00:27 +0000

    IPv4 manipulation - use standard function (more)
    Note - line 428, is the logic right? 
    
    If the input range _contains_ a pool range, then the start and end won't be in the pool range but surely it should still give an error?
    Shouldn't the logic here, be: 
    
    if (TEST_START > POOL_END || TEST_END < POOL_START) 
    { is ok} 
    else 
    {bad}
    
    Also line 447-8 shouldn't it be <= and >= not < and >, to test for overlap?
    Changed this, please check this one and change back if incorrect

--- src/usr/local/www/services_dhcp.php
+++ src/usr/local/www/services_dhcp.php
@@ -400,16 +400,15 @@ if (isset($_POST['submit'])) {
 
 		if (!$input_errors) {
 			/* make sure the range lies within the current subnet */
-			$subnet_start = ip2ulong(gen_subnetv4($ifcfgip, $ifcfgsn));
-			$subnet_end = ip2ulong(gen_subnetv4_max($ifcfgip, $ifcfgsn));
+			$subnet_start = gen_subnetv4($ifcfgip, $ifcfgsn);
+			$subnet_end = gen_subnetv4_max($ifcfgip, $ifcfgsn);
 
-			if ((ip2ulong($_POST['range_from']) < $subnet_start) || (ip2ulong($_POST['range_from']) > $subnet_end) ||
-			    (ip2ulong($_POST['range_to']) < $subnet_start) || (ip2ulong($_POST['range_to']) > $subnet_end)) {
-				$input_errors[] = gettext("The specified range lies outside of the current subnet.");
+			if (ip_greater_than($_POST['range_from'], $_POST['range_to'])) {
+				$input_errors[] = gettext("The range is invalid (first element higher than second element).");
 			}
 
-			if (ip2ulong($_POST['range_from']) > ip2ulong($_POST['range_to'])) {
-				$input_errors[] = gettext("The range is invalid (first element higher than second element).");
+			if (!is_inrange_v4($_POST['range_from'], $subnet_start, $subnet_end)) {
+				$input_errors[] = gettext("The specified range lies outside of the current subnet.");
 			}
 
 			if (is_numeric($pool) || ($act == "newpool")) {
@@ -438,15 +437,12 @@ if (isset($_POST['submit'])) {
 				$input_errors[] = sprintf(gettext("You must disable the DHCP relay on the %s interface before enabling the DHCP server."), $iflist[$if]);
 			}
 
-			$dynsubnet_start = ip2ulong($_POST['range_from']);
-			$dynsubnet_end = ip2ulong($_POST['range_to']);
 			if (is_array($a_maps)) {
 				foreach ($a_maps as $map) {
 					if (empty($map['ipaddr'])) {
 						continue;
 					}
-					if ((ip2ulong($map['ipaddr']) > $dynsubnet_start) &&
-					    (ip2ulong($map['ipaddr']) < $dynsubnet_end)) {
+					if (is_inrange_v4($map['ipaddr'], $_POST['range_from'], $_POST['range_to'])) {
 						$input_errors[] = sprintf(gettext("The DHCP range cannot overlap any static DHCP mappings."));
 						break;
 					}

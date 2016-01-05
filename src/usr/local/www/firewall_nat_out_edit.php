2768a9d9fc72cf9d89a42e7254d9dacea96605ef
NOYB <Al_Stu@Frontier.com>
2016-01-05 07:52:01 -0800

    Remove errant bracket from character class.

--- src/usr/local/www/firewall_nat_out_edit.php
+++ src/usr/local/www/firewall_nat_out_edit.php
@@ -541,7 +541,7 @@ $group->add(new Form_IpAddress(
 	'destination',
 	null,
 	$pconfig['destination'] == "any" ? "":$pconfig['destination']
-))->addMask('destination_subnet', $pconfig['destination_subnet'])->setHelp('Destination network for the outbound NAT mapping.')->setPattern('[[a-zA-Z0-9\_\.\:]+');
+))->addMask('destination_subnet', $pconfig['destination_subnet'])->setHelp('Destination network for the outbound NAT mapping.')->setPattern('[a-zA-Z0-9\_\.\:]+');
 
 $group->add(new Form_Input(
 	'dstport',

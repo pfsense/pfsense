3290234610ada9f82163bbfb75d2cb39a6f1c3b4
Marcello Coutinho <marcellocoutinho@gmail.com>
2016-01-27 13:04:22 -0200

    firewall_rules.php - improve bd_nice_number function

--- src/usr/local/www/firewall_rules.php
+++ src/usr/local/www/firewall_rules.php
@@ -170,25 +170,14 @@ function get_rule_ht($tracker, $sum_ht=array(), $buit_in_rule=false) {
 	));
 }
 
-function bd_nice_number($n) {
-	// first strip any formatting;
-	$n = (0+str_replace(",","",$n));
-
-	// is this a number?
-	if(!is_numeric($n)) return false;
-
-	// now filter it;
-	if ( $n > 1000000000000 ) {
-		return round(($n / 1000000000000),1).'t';
-	} else if ( $n > 1000000000 ) {
-		return round(($n / 1000000000),1).'g';
-	} else if ( $n > 1000000 ) {
-		return round(($n / 1000000),1).'m';
-	} else if( $n > 1000 ) {
-		return round(($n / 1000),1).'k';
+function bd_nice_number ($a) {
+	$unim = array("B","K","M","G","T","P");
+	$c = 0;
+	while ($a>=1024) {
+		$c++;
+		$a = $a/1024;
 	}
-
-	return number_format($n);
+	return number_format($a,($c ? 2 : 0),",",".").$unim[$c];
 }
 
 function delete_nat_association($id) {

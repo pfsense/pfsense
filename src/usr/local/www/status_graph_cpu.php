96bfacf0475fdc2dd615d43a5a5459c2dd992054
Phil Davis <phil.davis@inf.org>
2016-01-27 21:25:03 +0545

    Remove need for escaped single quote

--- src/usr/local/www/status_graph_cpu.php
+++ src/usr/local/www/status_graph_cpu.php
@@ -77,7 +77,7 @@ include("head.inc");
 
 	<p class="text-center">
 		<strong><?=gettext("Note"); ?>:</strong>
-		<?=sprintf(gettext('If you can\'t see the graph, you may have to install the %1$sAdobe SVG viewer%2$s'), '<a href="http://www.adobe.com/svg/viewer/install/" target="_blank">', '</a>')?>
+		<?=sprintf(gettext('If you cannot see the graph, you may have to install the %1$sAdobe SVG viewer%2$s'), '<a href="http://www.adobe.com/svg/viewer/install/" target="_blank">', '</a>')?>
 	</p>
 </div>
 

a677c3b0e9508b5b98bbf88a780b394c451d3774
Stephen Beaver <sbeaver@netgate.com>
2016-02-08 20:25:03 -0500

    Fix help text index when the privs are filtered.

--- src/usr/local/www/system_usermanager_addprivs.php
+++ src/usr/local/www/system_usermanager_addprivs.php
@@ -275,7 +275,10 @@ events.push(function() {
 
 	// When the 'sysprivs" selector is clicked, we display a description
 	$('.multiselect').click(function() {
-		$('#pdesc').html('<span class="text-info">' + descs[$(this).children('option:selected').index()] + '</span>');
+		var targetoption = $(this).children('option:selected').val();
+		var idx =  $('.shadowselect option[value="' + targetoption + '"]').index();
+
+		$('#pdesc').html('<span class="text-info">' + descs[idx] + '</span>');
 
 		// and update the shadow list from the real list
 		$(".multiselect option").each(function() {

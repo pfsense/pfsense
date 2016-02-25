e517f06a932c9ae7ee2514ec599bb017513deeb8
NOYB <Al_Stu@Frontier.com>
2016-02-25 00:21:33 -0800

    Collapsible Panels JavaScript Error - Fixes 5926
    Not the most elegant solution but should be functional.
    Would prefer each widget to have an id attribute with something unique to match that identifies it as a widget panel.  Or test if 'updateWidgets' is defined as a function since it should only be so on the index page (dashboard).  But that seems sort of a hackish means.
    
    Changing the widgets available panel href to "#widgets-available_panel-body" (note the added 's' to widget) may be the next best thing so that only the one if match test would be needed.
    
    Is a call to updateWidgets even need here?  It's not readily obvious to me what it's accomplishing here when collapsing/expanding a widget panel.  My guess is that the intention is to save the panel collapsible state.  But I'm not experiencing that.

--- src/usr/local/www/jquery/pfSenseHelpers.js
+++ src/usr/local/www/jquery/pfSenseHelpers.js
@@ -451,7 +451,8 @@ $('.container .panel-heading a[data-toggle="collapse"]').each(function (idx, el)
 		$(el).children('i').toggleClass('fa-minus-circle', true);
 		$(el).children('i').toggleClass('fa-plus-circle', false);
 
-		if($(el).closest('a').attr('id') != 'widgets-available') {
+		if(($(el).closest('a').attr('id') != 'widgets-available') && 
+		   ($(el).closest('a').attr('href').match(/widget-.*?_panel-body/i))) {
 			updateWidgets();
 		}
 	});
@@ -460,7 +461,8 @@ $('.container .panel-heading a[data-toggle="collapse"]').each(function (idx, el)
 		$(el).children('i').toggleClass('fa-minus-circle', false);
 		$(el).children('i').toggleClass('fa-plus-circle', true);
 
-		if($(el).closest('a').attr('id') != 'widgets-available') {
+		if(($(el).closest('a').attr('id') != 'widgets-available') && 
+		   ($(el).closest('a').attr('href').match(/widget-.*?_panel-body/i))) {
 			updateWidgets();
 		}
 	});

b715db5d7396ddd5eb46f48d7d30cd9177777e02
Chris Buechler <cmb@pfsense.org>
2016-02-27 06:32:25 -0600

    Update license page. Ticket #5903

--- src/usr/local/www/license.php
+++ src/usr/local/www/license.php
@@ -104,26 +104,15 @@ include("head.inc");
 				<?=gettext("Copyright"); ?> &copy; <?=gettext("1999-2016 The PHP Group. All rights reserved.")?>
 			</li>
 			<li class="list-group-item">
-				<strong>nginx</strong> (<a href="http://www.nginx.org" target="_blank">http://www.nginx.org)</a><br />
-				<?=gettext("Copyright"); ?> &copy;<?=gettext("2011-2016 Nginx, Inc.")?>
-				<?=gettext("All rights reserved.")?>
-			</li>
-			<li class="list-group-item">
-				<strong>ISC DHCP</strong> <?=gettext("server")?> (<a href="http://www.isc.org/products/DHCP/" target="_blank">http://www.isc.org/products/DHCP</a>)<br />
-				<?=gettext("Copyright"); ?> &copy; <?=gettext("2004-2013 Internet Software Consortium, Inc.")?><br />
-				<?=gettext("Copyright"); ?> &copy; <?=gettext("1995-2003 Internet Software Consortium")?>
-			</li>
-			<li class="list-group-item">
 				<strong>PF</strong> originally from OpenBSD (<a href="http://www.openbsd.org/faq/pf" target="_blank">http://www.openbsd.org</a>)
-			</li>
-			<li class="list-group-item">
-				<strong>MPD</strong> - Multi-link PPP daemon for FreeBSD (<a href="http://mpd.sourceforge.net/" target="_blank">http://mpd.sourceforge.net/</a>)<br />
-				<?=gettext("Copyright"); ?> &copy; 2003-2004, Archie L. Cobbs, Michael Bretterklieber, Alexander Motin<br />
-				<?=gettext("All rights reserved.")?>
 			</li>
 			<li class="list-group-item">
 				<strong>bind-tools</strong> (<a href="https://www.isc.org/downloads/bind/" target="_blank">https://www.isc.org/downloads/bind/</a>)<br />
 				<?=gettext("Copyright"); ?> &copy; 2004-2013 Internet Systems Consortium, Inc. ("ISC")
+			</li>
+			<li class="list-group-item">
+				<strong>Bootstrap</strong> HTML, CSS and JS framework (<a href="https://getbootstrap.com/" target="_blank">https://getbootstrap.com/</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; 2015 Twitter
 			</li>
 			<li class="list-group-item">
 				<strong>ca_root_nss</strong> Root certificates from certificate authorities included in the (<a href="https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS" target="_blank">Mozilla
@@ -147,6 +136,15 @@ NSS library</a>)<br />
 				<?=gettext("Copyright"); ?> &copy; 1998-2016 Daniel Stenberg.
 			</li>
 			<li class="list-group-item">
+				<strong>D3.js</strong> (<a href="https://d3js.org/" target="_blank">https://d3js.org/</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; 2015-2016 Mike Bostock.
+			</li>
+			<li class="list-group-item">
+				<strong>d3pie</strong> (<a href="http://d3pie.org/" target="_blank">http://d3pie.org/</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; 2014-2016 Benjamin Keen.
+			</li>
+			
+			<li class="list-group-item">
 				<strong>Dnsmasq</strong> - a DNS forwarder for NAT firewalls (<a href="http://www.thekelleys.org.uk" target="_blank">http://www.thekelleys.org.uk</a>)<br />
 				<?=gettext("Copyright"); ?> &copy; 2000-2016 Simon Kelley.
 			</li>
@@ -159,13 +157,32 @@ NSS library</a>)<br />
 				<?=gettext("Copyright"); ?> &copy; 2003-2016 Sun Microsystems, Inc.
 			</li>
 			<li class="list-group-item">
-				<strong>strongSwan</strong> (<a href="https://www.strongswan.org/" target="_blank">https://www.strongswan.org</a>)<br />
-				<?=gettext("Copyright"); ?> &copy; <?=gettext("2005-2016 University of Applied Sciences Rapperswil")?>
+				<strong>ISC DHCP</strong> <?=gettext("server")?> (<a href="http://www.isc.org/products/DHCP/" target="_blank">http://www.isc.org/products/DHCP</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; <?=gettext("2004-2013 Internet Software Consortium, Inc.")?><br />
+				<?=gettext("Copyright"); ?> &copy; <?=gettext("1995-2003 Internet Software Consortium")?>
+			</li>
+			<li class="list-group-item">
+				<strong>jQuery</strong> (<a href="https://jquery.com/" target="_blank">https://jquery.com/</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; 2005-2016 jQuery Foundation and other contributors. 
+			</li>
+			<li class="list-group-item">
+				<strong>MPD</strong> - Multi-link PPP daemon for FreeBSD (<a href="http://mpd.sourceforge.net/" target="_blank">http://mpd.sourceforge.net/</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; 2003-2004, Archie L. Cobbs, Michael Bretterklieber, Alexander Motin<br />
+				<?=gettext("All rights reserved.")?>
+			</li>
+			<li class="list-group-item">
+				<strong>nginx</strong> (<a href="http://www.nginx.org" target="_blank">http://www.nginx.org)</a><br />
+				<?=gettext("Copyright"); ?> &copy;<?=gettext("2011-2016 Nginx, Inc.")?>
+				<?=gettext("All rights reserved.")?>
 			</li>
 			<li class="list-group-item">
 				<strong>php-radius</strong> (<a href="http://www.mavetju.org/programming/php.php" target="_blank">http://www.mavetju.org/programming/php.php</a>)<br />
 				<?=gettext("Copyright 2000, 2001, 2002 by Edwin Groothuis. All rights reserved.")?><br />
 				<?=gettext("This product includes software developed by Edwin Groothuis.")?>
+			</li>
+			<li class="list-group-item">
+				<strong>strongSwan</strong> (<a href="https://www.strongswan.org/" target="_blank">https://www.strongswan.org</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; <?=gettext("2005-2016 University of Applied Sciences Rapperswil")?>
 			</li>
 			<li class="list-group-item">
 				<strong>wol</strong> (<a href="http://ahh.sourceforge.net/wol" target="_blank">http://ahh.sourceforge.net/wol</a>)<br />
@@ -207,6 +224,10 @@ NSS library</a>)<br />
 			<li class="list-group-item">
 				<strong>smartmontools</strong> (<a href="https://www.smartmontools.org/" target="_blank">https://www.smartmontools.org/</a>)<br />
 				<?=gettext("Copyright"); ?> &copy; 2002-2016 Bruce Allen, Christian Franke
+			</li>
+			<li class="list-group-item">
+				<strong>sortable</strong> Sortable JavaScript and CSS library (<a href="http://github.hubspot.com/sortable/" target="_blank">http://github.hubspot.com/sortable/</a>)<br />
+				<?=gettext("Copyright"); ?> &copy; 2013 Adam Schwartz
 			</li>
 			<li class="list-group-item">
 				<strong>sqlite3</strong> (<a href="https://www.sqlite.org/" target="_blank">https://www.sqlite.org/</a>)<br />

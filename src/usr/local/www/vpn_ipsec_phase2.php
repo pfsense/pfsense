40b65f4e7d00c977cacd15e02f67426464e1c608
Stephen Beaver <sbeaver@netgate.com>
2016-03-08 14:46:26 -0500

    Changed all selectoraction to"change" instead of "click"

--- src/usr/local/www/vpn_ipsec_phase2.php
+++ src/usr/local/www/vpn_ipsec_phase2.php
@@ -944,12 +944,12 @@ events.push(function() {
 	// ---------- Monitor elements for change and call the appropriate display functions ----------
 
 	 // Protocol
-	$('#proto').click(function () {
+	$('#proto').change(function () {
 		change_protocol();
 	});
 
 	 // Localid
-	$('#localid_type').click(function () {
+	$('#localid_type').change(function () {
 		typesel_change_local(<?=htmlspecialchars($pconfig['localid_netbits'])?>);
 	});
 
@@ -959,12 +959,12 @@ events.push(function() {
 	});
 
 	 // NATLocalid
-	$('#natlocalid_type').click(function () {
+	$('#natlocalid_type').change(function () {
 		typesel_change_natlocal(<?=htmlspecialchars($pconfig['natlocalid_netbits'])?>);
 	});
 
 	 // Mode
-	$('#mode').click(function () {
+	$('#mode').change(function () {
 		change_mode();
 	});
 

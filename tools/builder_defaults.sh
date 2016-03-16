d4db26c24f3c9936c865b5151d0f9c19a904cfba
Renato Botelho <renato@netgate.com>
2016-03-15 22:17:52 -0300

    Adjust some ovf parameters to netgate

--- tools/builder_defaults.sh
+++ tools/builder_defaults.sh
@@ -364,8 +364,8 @@ else
 fi
 
 if [ "${PRODUCT_NAME}" = "pfSense" ]; then
-	export VENDOR_NAME=${VENDOR_NAME:-"Electric Sheep Fencing, LLC"}
-	export OVF_INFO=${OVF_INFO:-"pfSense is a free, open source customized distribution of FreeBSD tailored for use as a firewall and router. In addition to being a powerful, flexible firewalling and routing platform, it includes a long list of related features and a package system allowing further expandability without adding bloat and potential security vulnerabilities to the base distribution. pfSense is a popular project with more than 1 million downloads since its inception, and proven in countless installations ranging from small home networks protecting a PC and an Xbox to large corporations, universities and other organizations protecting thousands of network devices."}
+	export VENDOR_NAME=${VENDOR_NAME:-"Netgate"}
+	export OVF_INFO=${OVF_INFO:-"The Netgate pfSense Certified firewall and VPN appliance runs pfSense, a customized distribution of FreeBSD tailored for use as a firewall and router. In addition to being a powerful, flexible firewalling and routing platform, it includes a long list of related features and a package system allowing further expandability without adding bloat and potential security vulnerabilities to the base distribution."}
 else
 	export VENDOR_NAME=${VENDOR_NAME:-"nonSense"}
 	export OVF_INFO=${OVF_INFO:-"none"}

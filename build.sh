9176de83c1e3c382374a96e33be0e417dfa2254b
Renato Botelho <renato@netgate.com>
2016-03-22 13:09:06 -0300

    Use pfSense-ec2_setup

--- build.sh
+++ build.sh
@@ -445,7 +445,7 @@ for _IMGTOBUILD in $_IMAGESTOBUILD; do
 			;;
 		ec2)
 			old_custom_package_list="${custom_package_list}"
-			export custom_package_list="${custom_package_list} ${PRODUCT_NAME}-openvpn_connect_profile"
+			export custom_package_list="${custom_package_list} ${PRODUCT_NAME}-ec2_setup"
 			install_pkg_install_ports
 			create_virt_images ${_IMGTOBUILD} "default-config-${_IMGTOBUILD}"
 			export custom_package_list="${old_custom_package_list}"

7fdca5ff3c43fab31587eecc039d67675f78bda6
NOYB <Al_Stu@Frontier.com>
2016-03-29 00:15:38 -0700

    System / Routing / Gateways - Remove Personalizations
    Remove "you" personalizations.

--- src/usr/local/www/system_gateways.php
+++ src/usr/local/www/system_gateways.php
@@ -255,7 +255,7 @@ if ($savemsg) {
 }
 
 if (is_subsystem_dirty('staticroutes')) {
-	print_apply_box(gettext("The gateway configuration has been changed.") . "<br />" . gettext("You must apply the changes in order for them to take effect."));
+	print_apply_box(gettext("The gateway configuration has been changed.") . "<br />" . gettext("The changes must be applied for them to take effect."));
 }
 
 $tab_array = array();

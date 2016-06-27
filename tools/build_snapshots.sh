f310decfbb6beea0955357700469a0c2244468b7
Renato Botelho <renato@netgate.com>
2016-06-27 19:17:44 -0300

    Only build memstick images for now

--- tools/build_snapshots.sh
+++ tools/build_snapshots.sh
@@ -191,7 +191,8 @@ while [ /bin/true ]; do
 		done
 
 		(${BUILDER_ROOT}/build.sh ${NO_UPLOAD} --flash-size '2g 4g' \
-		    --snapshots ${NO_IMAGES} 2>&1) | while read -r LINE; do
+		    --snapshots ${NO_IMAGES} "memstick memstickadi memstickserial" 2>&1) \
+		    | while read -r LINE; do
 			snapshot_update_status "${LINE}"
 		done
 	fi

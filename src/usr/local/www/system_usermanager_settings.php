cba6dcbbb72e96feda0e7ac64c2405d21670f2a3
BBcan177 <bbcan177@gmail.com>
2016-07-01 17:21:45 -0400

    Update system_usermanager_settings.php
    Use of undefined constant min - assumed 'min'

--- src/usr/local/www/system_usermanager_settings.php
+++ src/usr/local/www/system_usermanager_settings.php
@@ -216,7 +216,7 @@ $section->addInput(new Form_Input(
 	'Session timeout',
 	'number',
 	$pconfig['session_timeout'],
-	[min => 0]
+	['min' => 0]
 ))->setHelp('Time in minutes to expire idle management sessions. The default is 4 '.
 	'hours (240 minutes). Enter 0 to never expire sessions. NOTE: This is a security '.
 	'risk!');

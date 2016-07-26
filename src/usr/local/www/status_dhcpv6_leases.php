4e5477eace9aa5f00c53ff62ca3e35e38177bb09
Phil Davis <phil.davis@inf.org>
2016-07-26 14:28:24 +0930

    Display local DHCP lease times in 24-hour clock
    It seems odd to me that when the times are displayed in UTC they have
    24-hour clock, but when displayed in local time they are formatted with
    12-our clock and AM/PM.
    24-hour format takes less screen space, and I would have thought that
    network admins would prefer working consistently in 24-hour clock
    format.
    
    (cherry picked from commit ede534659ca54d2268ce51ec2f9e15f77051a60c)

--- src/usr/local/www/status_dhcpv6_leases.php
+++ src/usr/local/www/status_dhcpv6_leases.php
@@ -132,7 +132,7 @@ function adjust_gmt($dt) {
 	if ($dhcpv6leaseinlocaltime == "yes") {
 		$ts = strtotime($dt . " GMT");
 		if ($ts !== false) {
-			return strftime("%Y/%m/%d %I:%M:%S%p", $ts);
+			return strftime("%Y/%m/%d %H:%M:%S", $ts);
 		}
 	}
 	/* If we did not need to convert to local time or the conversion failed, just return the input. */

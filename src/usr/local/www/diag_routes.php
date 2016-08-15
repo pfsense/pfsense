f7f0f1c52d97fdfc0087068a128edaa454b503d5
Stephen Beaver <sbeaver@netgate.com>
2016-08-15 09:38:15 -0400

    Fixed #6706
    (cherry picked from commit b86938e7e19a09a5b1f3f54825193b57f7c73131)

--- src/usr/local/www/diag_routes.php
+++ src/usr/local/www/diag_routes.php
@@ -129,7 +129,7 @@ $section->addInput(new Form_Input(
 	'Filter',
 	'text',
 	$host
-))->setHelp('Use a regular expression to filter IP address or hostnames.');
+))->setHelp('Use a regular expression to filter the tables.');
 
 $form->add($section);
 

4d4782fcd7213b5f279cb372853f5fedb07f6178
Stephen Beaver <sbeaver@netgate.com>
2016-08-15 14:47:05 -0400

    Fixed #6716

--- src/usr/local/www/services_unbound_acls.php
+++ src/usr/local/www/services_unbound_acls.php
@@ -169,7 +169,7 @@ $actionHelp =
 					sprintf(gettext('%sDeny:%s Stops queries from hosts within the netblock defined below.%s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
 					sprintf(gettext('%sRefuse:%s Stops queries from hosts within the netblock defined below, but sends a DNS rcode REFUSED error message back to the client.%s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
 					sprintf(gettext('%sAllow:%s Allow queries from hosts within the netblock defined below.%s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
-					sprintf(gettext('%sAllow Snoop:%s Allow recursive and nonrecursive access from hosts within the netblock defined below. Used for cache snooping and ideally should only be configured for the administrative host.'), '<span class="text-success"><strong>', '</strong></span>'); 
+					sprintf(gettext('%sAllow Snoop:%s Allow recursive and nonrecursive access from hosts within the netblock defined below. Used for cache snooping and ideally should only be configured for the administrative host.'), '<span class="text-success"><strong>', '</strong></span>');
 
 $pgtitle = array(gettext("Services"), gettext("DNS Resolver"), gettext("Access Lists"));
 
@@ -336,15 +336,20 @@ if ($act == "new" || $act == "edit") {
 	</a>
 </nav>
 
-<script type="text/javascript">
-//<![CDATA[
-events.push(function() {
-	// Suppress "Delete row" button if there are fewer than two rows
-	checkLastRow();
-});
-//]]>
-</script>
 <?php
 }
 
+?>
+<script type="text/javascript">
+//<![CDATA[
+events.push(function() {
+
+	// Suppress "Delete row" button if there are fewer than two rows
+	checkLastRow();
+
+});
+//]]>
+</script>
+
+<?php
 include("foot.inc");

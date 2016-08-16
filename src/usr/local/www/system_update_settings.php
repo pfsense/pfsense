pfsense/pfsense

commit 9d3e8723171c727cf43338bd8e95ab2bb7e6a66c

Code style and comments
No functional change - just making style consistent

src/usr/local/www/system_update_settings.php
@@ -154,6 +154,7 @@ if (file_exists("/usr/local/bin/git") && $g['platform'] == $g['product_name']) {
 		'synconupgrade',
 		'Auto sync on update',
 		'Enable repository/branch sync before reboot',
+		'Enable repository/branch sync before reboot',
 		isset($gitcfg['synconupgrade'])
 		))->setHelp('After updating, sync with the following repository/branch before reboot.');
 
@@ -195,7 +196,7 @@ if (file_exists("/usr/local/bin/git") && $g['platform'] == $g['product_name']) {
 					'<br />Note: Sync will not be performed if a branch is not specified.', [$lastbranch]);
 
 	$form->add($section);
-} // e-o-if(file_exista()
+} // e-o-if (file_exists())
 
 print($form);
 

src/etc/inc/filter_log.inc
@@ -223,7 +223,7 @@ function parse_vpn_login_log_line($line) {
 	if (!( (trim($flent['time']) == "") && (trim($flent['action']) == "") && (trim($flent['user']) == "") && (trim($flent['ip_address']) == "") )) {
 		return $flent;
 	} else {
-		if($g['debug']) {
+		if ($g['debug']) {
 			log_error(sprintf(gettext("There was a error parsing log entry: %s. Please report to mailing list or forum."), $line));
 		}
 		return "";
@@ -245,7 +245,7 @@ function parse_vpn_service_log_line($line) {
 	if (!( (trim($flent['time']) == "") && (trim($flent['type']) == "") && (trim($flent['message']) == "") )) {
 		return $flent;
 	} else {
-		if($g['debug']) {
+		if ($g['debug']) {
 			log_error(sprintf(gettext("There was a error parsing log entry: %s. Please report to mailing list or forum."), $line));
 		}
 		return "";

src/etc/inc/pfsense-utils.inc
@@ -707,7 +707,7 @@ function get_filename_from_url($url) {
 function get_dir($dir) {
 	$dir_array = array();
 	$d = dir($dir);
-	if(!is_object($d)) {
+	if (!is_object($d)) {
 		return array();
 	}
 	while (false !== ($entry = $d->read())) {

src/etc/inc/upgrade_config.inc
@@ -4437,7 +4437,7 @@ function upgrade_142_to_143() {
 	if (is_array($config['filter']['separator'])) {
 		foreach ($config['filter']['separator'] as $interface => $separators) {
 
-			if(is_array($separators)) {
+			if (is_array($separators)) {
 				foreach ($separators as $sepn => $separator) {
 
 					$seprow = substr($separator['row']['0'], 2);

src/etc/inc/util.inc
@@ -872,7 +872,7 @@ function check_subnetsv4_overlap($subnet1, $bits1, $subnet2, $bits2) {
 	$subnetv4_start1 = gen_subnetv4($subnet1, $largest_sn);
 	$subnetv4_start2 = gen_subnetv4($subnet2, $largest_sn);
 	
-	if($subnetv4_start1 == '' || $subnetv4_start2 == '') {
+	if ($subnetv4_start1 == '' || $subnetv4_start2 == '') {
 		// One or both args is not a valid IPv4 subnet
 		//FIXME: needs to return "bad data" not true/false if bad. For now return false, best we can do until fixed
 		return false;
@@ -887,7 +887,7 @@ function check_subnetsv6_overlap($subnet1, $bits1, $subnet2, $bits2) {
 	$subnetv6_start1 = gen_subnetv6($subnet1, $largest_sn);
 	$subnetv6_start2 = gen_subnetv6($subnet2, $largest_sn);
 	
-	if($subnetv6_start1 == '' || $subnetv6_start2 == '') {
+	if ($subnetv6_start1 == '' || $subnetv6_start2 == '') {
 		// One or both args is not a valid IPv6 subnet
 		//FIXME: needs to return "bad data" not true/false if bad. For now return false, best we can do until fixed
 		return false;

src/usr/local/www/classes/Form/Button.class.php
@@ -39,11 +39,11 @@ class Form_Button extends Form_Input
 			$this->_tagName = 'a';
 			$this->addClass('btn-default');
 			unset($this->_attributes['type']);
-			if(isset($icon)) {
+			if (isset($icon)) {
 				$this->_attributes['icon'] = $icon;
 			}
 		}
-		else if(isset($icon))
+		else if (isset($icon))
 		{
 			$this->_tagSelfClosing = false;
 			$this->_tagName = 'button';

src/usr/local/www/classes/Form/Group.class.php
@@ -64,7 +64,7 @@ class Form_Group extends Form_Element
 
 	public function enableDuplication($max = null, $horiz = false)
 	{
-		if($horiz)
+		if ($horiz)
 			$this->addClass('user-duplication-horiz');	// added buttons are 2 cols wide with no offset
 		else
 			$this->addClass('user-duplication');		// added buttons 10 cols wide with 2 col offset

src/usr/local/www/classes/Form/Input.class.php
@@ -209,7 +209,7 @@ class Form_Input extends Form_Element
 	// These methods required by pkg_edit and the wizards that map xml element definitions to Form elements
 	public function setOnclick($text)
 	{
-		if($text)
+		if ($text)
 			$this->_attributes['onclick'] = $text;
 
 		return $this;
@@ -217,7 +217,7 @@ class Form_Input extends Form_Element
 
 	public function setOnchange($text)
 	{
-		if($text)
+		if ($text)
 			$this->_attributes['onchange'] = $text;
 
 		return $this;

commit d5b993fc310147d68bc3c3654a512b0ac981ecd2

Fix messup caused by 9d3e872

src/usr/local/www/system_update_settings.php
@@ -154,7 +154,6 @@ if (file_exists("/usr/local/bin/git") && $g['platform'] == $g['product_name']) {
 		'synconupgrade',
 		'Auto sync on update',
 		'Enable repository/branch sync before reboot',
-		'Enable repository/branch sync before reboot',
 		isset($gitcfg['synconupgrade'])
 		))->setHelp('After updating, sync with the following repository/branch before reboot.');
 
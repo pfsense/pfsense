9f33ec64330d0b50896772abd73156abbd762305
Jared Dillard <jdillard@netgate.com>
2016-08-17 14:46:22 -0500

    change traffic graphs to d3

--- src/usr/local/www/ifstats.php
+++ src/usr/local/www/ifstats.php
@@ -1,22 +1,56 @@
 <?php
 /*
- * ifstats.php
+	ifstats.php
+*/
+/* ====================================================================
+ *  Copyright (c)  2004-2015  Electric Sheep Fencing, LLC. All rights reserved.
  *
- * part of pfSense (https://www.pfsense.org)
- * Copyright (c) 2004-2016 Electric Sheep Fencing, LLC
- * All rights reserved.
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  3. All advertising materials mentioning features or use of this software
+ *      must display the following acknowledgment:
+ *      "This product includes software developed by the pfSense Project
+ *       for use in the pfSense software distribution. (http://www.pfsense.org/).
+ *
+ *  4. The names "pfSense" and "pfSense Project" must not be used to
+ *       endorse or promote products derived from this software without
+ *       prior written permission. For written permission, please contact
+ *       coreteam@pfsense.org.
+ *
+ *  5. Products derived from this software may not be called "pfSense"
+ *      nor may "pfSense" appear in their names without prior written
+ *      permission of the Electric Sheep Fencing, LLC.
+ *
+ *  6. Redistributions of any form whatsoever must retain the following
+ *      acknowledgment:
+ *
+ *  "This product includes software developed by the pfSense Project
+ *  for use in the pfSense software distribution (http://www.pfsense.org/).
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE pfSense PROJECT ``AS IS'' AND ANY
+ *  EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE pfSense PROJECT OR
+ *  ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *  OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  ====================================================================
+ *
  */
 
 ##|+PRIV
@@ -26,12 +60,65 @@
 ##|*MATCH=ifstats.php*
 ##|-PRIV
 
-	require_once('guiconfig.inc');
-	require_once("interfaces.inc");
+$nocsrf = true;
+
+require_once('guiconfig.inc');
+require_once("interfaces.inc");
+
+
+//overload the use of this page until the conversion of both traffic graphs have been completed
+if($_POST['if']) {
+
+	$ifs = $_POST['if'];
+
+	$ifarray = explode("|", $ifs);
+
+	$temp = gettimeofday();
+	$timing = (double)$temp["sec"] + (double)$temp["usec"] / 1000000.0;
+	$obj = [];
+	$count = 0;
+
+	foreach ($ifarray as $if) {
+
+		$realif = get_real_interface($if);
+
+		if (!$realif) {
+			$realif = $if; // Need for IPsec case interface.
+		}
+
+		$ifinfo = pfSense_get_interface_stats($realif);
+
+		$obj[$if] = [];
+
+		$obj[$if][0]['key'] = $if . "in";
+		$obj[$if][0]['values'] = array($timing, $ifinfo['inbytes']);
+
+		$obj[$if][1]['key'] = $if . "out";
+		$obj[$if][1]['values'] = array($timing, $ifinfo['outbytes']);
+/*
+		$obj[$count]['key'] = $if . "in";
+		$obj[$count]['name'] = $if . " (in)";
+		$obj[$count]['values'] = array($timing, $ifinfo['inbytes']);
+
+		$count++;
+
+		$obj[$count]['key'] = $if . "out";
+		$obj[$count]['name'] = $if . " (out)";
+		$obj[$count]['values'] = array($timing, $ifinfo['outbytes']);
+
+		$count++;
+*/
+	}
+
+	header('Content-Type: application/json');
+	echo json_encode($obj,JSON_PRETTY_PRINT|JSON_PARTIAL_OUTPUT_ON_ERROR|JSON_NUMERIC_CHECK);
+
+} else {
 
 	$if = $_GET['if'];
 
 	$realif = get_real_interface($if);
+
 	if (!$realif) {
 		$realif = $if; // Need for IPsec case interface.
 	}
@@ -47,5 +134,7 @@
 	header("Pragma: no-cache"); // HTTP/1.0
 
 	echo "$timing|" . $ifinfo['inbytes'] . "|" . $ifinfo['outbytes'] . "\n";
+
+}
 
 ?>

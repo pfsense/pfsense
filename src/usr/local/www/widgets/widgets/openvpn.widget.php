pfsense/pfsense

commit c93d289bb926111a4b8ecd71fa59771185b0c7b6

Added control to set dashboard widget refresh period Start each widget refresh system after a short random delay to prevent all widgets hitting the server at the exactsame time Fix issues wherein two widgets were resetting the refresh timer from the AJAX call, not the call-back function, thereby risking stepping on themselves
(cherry picked from commit 512f2c10af716243046bdd08140856bb8e45222b)

src/usr/local/www/widgets/widgets/openvpn.widget.php
@@ -71,9 +71,208 @@ if ($_GET['action']) {
 	}
 }
 
+<<<<<<< HEAD
 $servers = openvpn_get_active_servers();
 $sk_servers = openvpn_get_active_servers("p2p");
 $clients = openvpn_get_active_clients();
+=======
+// Compose the table contents and pass it back to the ajax caller
+if ($_REQUEST && $_REQUEST['ajax']) {
+	printPanel();
+	exit;
+}
+
+// Output the widget panel from this function so that it can be called from the AJAX handler as well as
+// when first rendering the page
+function printPanel() {
+
+	$servers = openvpn_get_active_servers();
+	$sk_servers = openvpn_get_active_servers("p2p");
+	$clients = openvpn_get_active_clients();
+
+	$opstring = "";
+
+	foreach ($servers as $server):
+
+	$opstring .= "<div class=\"widget panel panel-default\">";
+	$opstring .=	"<div class=\"panel-heading\"><h2 class=\"panel-title\">" . htmlspecialchars($server['name']) . "</h2></div>";
+	$opstring .=	"<div class=\"table-responsive\">";
+	$opstring .=		"<table class=\"table table-striped table-hover table-condensed sortable-theme-bootstrap\" data-sortable>";
+	$opstring .=			"<thead>";
+	$opstring .=				"<tr>";
+	$opstring .=					"<th>" . gettext('Name/Time') . "</th>";
+	$opstring .=					"<th>" . gettext('Real/Virtual IP') . "</th>";
+	$opstring .=					"<th></th>";
+	$opstring .=				"</tr>";
+	$opstring .=			"</thead>";
+	$opstring .=			"<tbody>";
+
+				$rowIndex = 0;
+				foreach ($server['conns'] as $conn):
+					$evenRowClass = $rowIndex % 2 ? " listMReven" : " listMRodd";
+					$rowIndex++;
+
+	$opstring .=				"<tr name=\"" . "r:" . $server['mgmt'] . ":" . $conn['remote_host'] . "\" class=\"" . $evenRowClass . "\">";
+	$opstring .=					"<td>";
+	$opstring .=						$conn['common_name'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+	$opstring .=						$conn['remote_host'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+	$opstring .=						"<i class=\"fa fa-times-circle\" ";
+	$opstring .=							"onclick=\"killClient('" . $server['mgmt'] . "', '" . $conn['remote_host'] . "');\" ";
+	$opstring .=							"style=\"cursor:pointer;\" ";
+	$opstring .=							"name=\"" . "i:" . $server['mgmt'] . ":" . $conn['remote_host'] . "\" ";
+	$opstring .=							"title=\"" . sprintf(gettext('Kill client connection from %s'), $conn['remote_host']) . "\">";
+	$opstring .=						"</i>";
+	$opstring .=					"</td>";
+	$opstring .=				"</tr>";
+	$opstring .=				"<tr name=\"" . "r:" . $server['mgmt'] . ":" . $conn['remote_host'] . "\" class=\"" . $evenRowClass . "\">";
+	$opstring .=					"<td>";
+	$opstring .=						$conn['connect_time'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+	$opstring .=						$conn['virtual_addr'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td></td>";
+	$opstring .=				"</tr>";
+
+				endforeach;
+
+	$opstring .=			"</tbody>";
+	$opstring .=		"</table>";
+	$opstring .=	"</div>";
+	$opstring .= "</div>";
+
+	endforeach;
+
+	print($opstring);
+
+	if (!empty($sk_servers)):
+
+	$opstring = "";
+	$opstring .= "<div class=\"widget panel panel-default\">";
+	$opstring .=	"<div class=\"panel-heading\"><h2 class=\"panel-title\">" . gettext("Peer to Peer Server Instance Statistics") . "</h2></div>";
+	$opstring .=	"<div class=\"table-responsive\">";
+	$opstring .=		"<table class=\"table table-striped table-hover table-condensed sortable-theme-bootstrap\" data-sortable>";
+	$opstring .=			"<thead>";
+	$opstring .=				"<tr>";
+	$opstring .=					"<th>" . gettext('Nome/Time') . "</th>";
+	$opstring .=					"<th>" . gettext('Remote/Virtual IP') . "</th>";
+	$opstring .=					"<th></th>";
+	$opstring .=				"</tr>";
+	$opstring .=			"</thead>";
+	$opstring .=			"<tbody>";
+
+				foreach ($sk_servers as $sk_server):
+
+	$opstring .=				"<tr name=\"r:" . $sk_server['port'] . ":" . $sk_server['remote_host'] . "\">";
+	$opstring .=					"<td>";
+	$opstring .=						$sk_server['name'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+	$opstring .=						$sk_server['remote_host'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+
+					if ($sk_server['status'] == "up") {
+						/* tunnel is up */
+	$opstring .=						"<i class=\"fa fa-arrow-up text-success\"></i>";
+					} else {
+						/* tunnel is down */
+	$opstring .=						"<i class=\"fa fa-arrow-down text-danger\"></i>";
+					}
+
+	$opstring .=					"</td>";
+	$opstring .=				"</tr>";
+	$opstring .=				"<tr name=\"r:" . $sk_server['port'] . ":" . $sk_server['remote_host'] . "\">";
+	$opstring .=					"<td>";
+	$opstring .=						$sk_server['connect_time'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+	$opstring .=						$sk_server['virtual_addr'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td></td>";
+	$opstring .=				"</tr>";
+
+				endforeach;
+
+	$opstring .=			"</tbody>";
+	$opstring .=		"</table>";
+	$opstring .=	"</div>";
+	$opstring .= "</div>";
+
+	print($opstring);
+
+	endif;
+
+	if (!empty($clients)):
+		$opstring = "";
+
+	$opstring .= "<div class=\"widget panel panel-default\">";
+	$opstring .=	"<div class=\"panel-heading\"><h2 class=\"panel-title\">" . gettext("Client Instance Statistics") . "</h2></div>";
+	$opstring .=	"<div class=\"table-responsive\">";
+	$opstring .=		"<table class=\"table table-striped table-hover table-condensed sortable-theme-bootstrap\" data-sortable>";
+	$opstring .=			"<thead>";
+	$opstring .=				"<tr>";
+	$opstring .=					"<th>" . gettext('Name/Time') . "</th>";
+	$opstring .=					"<th>" . gettext('Remote/Virtual IP') . "</th>";
+	$opstring .=					"<th></th>";
+	$opstring .=				"</tr>";
+	$opstring .=			"</thead>";
+	$opstring .=			"<tbody>";
+
+				foreach ($clients as $client):
+
+	$opstring .=				"<tr name=\"r:" . client['port'] . ":" . $client['remote_host'] . "\">";
+	$opstring .=					"<td>";
+	$opstring .=						$client['name'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+	$opstring .=						$client['remote_host'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+
+					if ($client['status'] == "up") {
+						/* tunnel is up */
+	$opstring .=						"<i class=\"fa fa-arrow-up text-success\"></i>";
+					} else {
+						/* tunnel is down */
+	$opstring .=						"<i class=\"fa fa-arrow-down text-danger\"></i>";
+					}
+
+	$opstring .=					"</td>";
+	$opstring .=				"</tr>";
+	$opstring .=				"<tr name=\"r:" . $client['port'] . ":" . $client['remote_host'] . "\">";
+	$opstring .=					"<td>";
+	$opstring .=						$client['connect_time'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td>";
+	$opstring .=						$client['virtual_addr'];
+	$opstring .=					"</td>";
+	$opstring .=					"<td></td>";
+	$opstring .=				"</tr>";
+
+				endforeach;
+
+	$opstring .=			"</tbody>";
+	$opstring .=		"</table>";
+	$opstring .=	"</div>";
+	$opstring .= "</div>";
+
+	print($opstring);
+
+	endif;
+
+	if ((empty($clients)) && (empty($servers)) && (empty($sk_servers))) {
+		print(gettext("No OpenVPN instances defined"));
+	}
+}
+
+$widgetperiod = isset($config['widgets']['period']) ? $config['widgets']['period'] * 1000 : 10000;
+
+>>>>>>> 512f2c1... Added control to set dashboard widget refresh period
 ?>
 
 <script type="text/javascript">
@@ -210,6 +409,7 @@ if (!empty($sk_servers)):
 	</div>
 </div>
 
+<<<<<<< HEAD
 <?php
 endif;
 
@@ -265,6 +465,21 @@ if (!empty($clients)):
 		</table>
 	</div>
 </div>
+=======
+			// and do it again
+			setTimeout(get_update, "<?=$widgetperiod?>");
+		});
+	}
+
+	events.push(function(){
+		// Start polling for updates some small random number of seconds from now (so that all the widgets don't
+		// hit the server at exactly the same time)
+		setTimeout(get_update, Math.floor((Math.random() * 10000) + 1000));
+	});
+//]]>
+</script>
+<div id="mainpanel" class="content">
+>>>>>>> 512f2c1... Added control to set dashboard widget refresh period
 
 <?php
 endif;

src/conf.default/config.xml
@@ -263,6 +263,7 @@
 	</load_balancer>
 	<widgets>
 		<sequence>system_information:col1:show,interfaces:col2:show</sequence>
+		<period>10</period>
 	</widgets>
 	<openvpn/>
 	<dnshaper/>

src/usr/local/www/system.php
@@ -98,6 +98,7 @@ $pconfig['systemlogsmanagelogpanel'] = isset($config['system']['webgui']['system
 $pconfig['statusmonitoringsettingspanel'] = isset($config['system']['webgui']['statusmonitoringsettingspanel']);
 $pconfig['webguihostnamemenu'] = $config['system']['webgui']['webguihostnamemenu'];
 $pconfig['dnslocalhost'] = isset($config['system']['dnslocalhost']);
+$pconfig['dashboardperiod'] = isset($config['widgets']['period']) ? $config['widgets']['period']:"10";
 
 if (!$pconfig['timezone']) {
 	if (isset($g['default_timezone']) && !empty($g['default_timezone'])) {
@@ -144,6 +145,10 @@ if ($_POST) {
 	$reqdfieldsn = array(gettext("Hostname"), gettext("Domain"));
 
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
+
+	if ($_POST['dashboardperiod']) {
+		$config['widgets']['period'] = $_POST['dashboardperiod'];
+	}
 
 	if ($_POST['webguicss']) {
 		$config['system']['webgui']['webguicss'] = $_POST['webguicss'];
@@ -388,6 +393,7 @@ $section->addInput(new Form_Input(
 	$pconfig['hostname'],
 	['placeholder' => 'pfSense']
 ))->setHelp('Name of the firewall host, without domain part');
+
 $section->addInput(new Form_Input(
 	'domain',
 	'Domain',
@@ -397,6 +403,7 @@ $section->addInput(new Form_Input(
 ))->setHelp('Do not use \'local\' as a domain name. It will cause local '.
 	'hosts running mDNS (avahi, bonjour, etc.) to be unable to resolve '.
 	'local hosts not running mDNS.');
+
 $form->add($section);
 
 $section = new Form_Section('DNS Server Settings');
@@ -473,12 +480,14 @@ $section->addInput(new Form_Checkbox(
 $form->add($section);
 
 $section = new Form_Section('Localization');
+
 $section->addInput(new Form_Select(
 	'timezone',
 	'Timezone',
 	$pconfig['timezone'],
 	array_combine($timezonelist, $timezonelist)
 ))->setHelp('Select the timezone or location within the timezone to be used by this system.');
+
 $section->addInput(new Form_Input(
 	'timeservers',
 	'Timeservers',
@@ -486,6 +495,7 @@ $section->addInput(new Form_Input(
 	$pconfig['timeservers']
 ))->setHelp('Use a space to separate multiple hosts (only one required). '.
 	'Remember to set up at least one DNS server if a host name is entered here!');
+
 $section->addInput(new Form_Select(
 	'language',
 	'Language',
@@ -508,6 +518,16 @@ gen_associatedpanels_fields(
 	$pconfig['systemlogsmanagelogpanel'],
 	$pconfig['statusmonitoringsettingspanel']);
 gen_webguileftcolumnhyper_field($section, $pconfig['webguileftcolumnhyper']);
+
+$section->addInput(new Form_Input(
+	'dashboardperiod',
+	'Dashboard update period',
+	'number',
+	$pconfig['dashboardperiod'],
+	['min' => '10', 'max' => '600']
+))->setHelp('Time in seconds between dashboard widget updates. Small values cause ' .
+			'more frequent updates but increase the load on the web server. ' .
+			'Minimum is 5 seconds, maximum 600 seconds');
 
 $form->add($section);
 

src/usr/local/www/widgets/widgets/dyn_dns_status.widget.php
@@ -198,8 +198,7 @@ if ($_REQUEST['getdyndnsstatus']) {
 				data: pars,
 				complete: dyndnscallback
 			});
-		// Refresh the status every 5 minutes
-		setTimeout('dyndns_getstatus()', 5*60*1000);
+
 	}
 	function dyndnscallback(transport) {
 		// The server returns a string of statuses separated by vertical bars
@@ -208,6 +207,9 @@ if ($_REQUEST['getdyndnsstatus']) {
 			var divlabel = '#dyndnsstatus' + count;
 			$(divlabel).prop('innerHTML',responseStrings[count]);
 		}
+
+		// Refresh the status every 5 minutes
+		setTimeout('dyndns_getstatus()', 5*60*1000);
 	}
 	// Do the first status check 2 seconds after the dashboard opens
 	setTimeout('dyndns_getstatus()', 2000);

src/usr/local/www/widgets/widgets/gateways.widget.php
@@ -80,6 +80,8 @@ if ($_POST) {
 	header("Location: /");
 	exit(0);
 }
+
+$widgetperiod = isset($config['widgets']['period']) ? $config['widgets']['period'] * 1000 : 10000;
 ?>
 
 <div class="table-responsive">
@@ -165,12 +167,14 @@ if ($_POST) {
 		ajaxRequest.done(function (response, textStatus, jqXHR) {
 			$('#gwtblbody').html(response);
 			// and do it again
-			setTimeout(get_gw_stats, 5000);
+			setTimeout(get_gw_stats, "<?=$widgetperiod?>");
 		});
 	}
 
 	events.push(function(){
-		get_gw_stats();
+		// Start polling for updates some small random number of seconds from now (so that all the widgets don't
+		// hit the server at exactly the same time)
+		setTimeout(get_gw_stats, Math.floor((Math.random() * 10000) + 1000));
 	});
 //]]>
 </script>

src/usr/local/www/widgets/widgets/installed_packages.widget.php
@@ -195,7 +195,8 @@ if ($_REQUEST && $_REQUEST['ajax']) {
 			$('#pkgtbl').html(response);
 
 			// and do it again
-			setTimeout(get_pkg_stats, 5000);
+			// NOT! There is no need to refresh this widget
+			// setTimeout(get_pkg_stats, 5000);
 		});
 	}
 

src/usr/local/www/widgets/widgets/interface_statistics.widget.php
@@ -116,6 +116,8 @@ if ($_REQUEST && $_REQUEST['ajax']) {
 	exit;
 }
 
+$widgetperiod = isset($config['widgets']['period']) ? $config['widgets']['period'] * 1000 : 10000;
+
 ?>
 <table id="iftbl" class="table table-striped table-hover">
 	<tr><td><?=gettext("Retrieving interface data")?></td></tr>
@@ -138,12 +140,14 @@ if ($_REQUEST && $_REQUEST['ajax']) {
 			$('#iftbl').html(response);
 
 			// and do it again
-			setTimeout(get_if_stats, 5000);
+			setTimeout(get_if_stats, "<?=$widgetperiod?>");
 		});
 	}
 
 	events.push(function(){
-		get_if_stats();
+		// Start polling for updates some small random number of seconds from now (so that all the widgets don't
+		// hit the server at exactly the same time)
+		setTimeout(get_if_stats, Math.floor((Math.random() * 10000) + 1000));
 	});
 //]]>
 </script>

src/usr/local/www/widgets/widgets/ipsec.widget.php
@@ -212,6 +212,7 @@ if (isset($config['ipsec']['phase1'])) {
 }
 
 $mobile = ipsec_dump_mobile();
+$widgetperiod = isset($config['widgets']['period']) ? $config['widgets']['period'] * 1000 : 10000;
 
 if (isset($config['ipsec']['phase2'])): ?>
 <div id="ipsec-Overview" style="display:block;"  class="table-responsive">
@@ -337,12 +338,14 @@ function get_ipsec_stats() {
 		$('tbody', '#ipsec-' + curtab).html(response);
 
 		// and do it again
-		setTimeout(get_ipsec_stats, 6000);
+		setTimeout(get_ipsec_stats, "<?=$widgetperiod?>");
 	});
 }
 
 events.push(function(){
-	get_ipsec_stats();
+	// Start polling for updates some small random number of seconds from now (so that all the widgets don't
+	// hit the server at exactly the same time)
+	setTimeout(get_ipsec_stats, Math.floor((Math.random() * 10000) + 1000));
 });
 //]]>
 </script>

commit 13e04a2540ef388288c97ed4fde9136b7b55b53c

Fix problems caused by c93d289
This commit syncs openvpn.widget.php with version from RELENG_2_3 branch

src/usr/local/www/widgets/widgets/openvpn.widget.php
@@ -71,11 +71,6 @@ if ($_GET['action']) {
 	}
 }
 
-<<<<<<< HEAD
-$servers = openvpn_get_active_servers();
-$sk_servers = openvpn_get_active_servers("p2p");
-$clients = openvpn_get_active_clients();
-=======
 // Compose the table contents and pass it back to the ajax caller
 if ($_REQUEST && $_REQUEST['ajax']) {
 	printPanel();
@@ -272,7 +267,6 @@ function printPanel() {
 
 $widgetperiod = isset($config['widgets']['period']) ? $config['widgets']['period'] * 1000 : 10000;
 
->>>>>>> 512f2c1... Added control to set dashboard widget refresh period
 ?>
 
 <script type="text/javascript">
@@ -297,181 +291,21 @@ $widgetperiod = isset($config['widgets']['period']) ? $config['widgets']['period
 			function(index,row) { $(row).fadeOut(1000); }
 		);
 	}
-//]]>
-</script>
-<div class="content">
-<?php foreach ($servers as $server): ?>
 
-<div class="widget panel panel-default">
-	<div class="panel-heading"><h2 class="panel-title"><?=htmlspecialchars($server['name']);?></h2></div>
-	<div class="table-responsive">
-		<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable>
-			<thead>
-				<tr>
-					<th><?=gettext('Name/Time')?></th>
-					<th><?=gettext('Real/Virtual IP')?></th>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-<?php
-			$rowIndex = 0;
-			foreach ($server['conns'] as $conn):
-				$evenRowClass = $rowIndex % 2 ? " listMReven" : " listMRodd";
-				$rowIndex++;
-?>
-				<tr name="<?php echo "r:{$server['mgmt']}:{$conn['remote_host']}"; ?>" class="<?=$evenRowClass?>">
-					<td>
-						<?=$conn['common_name'];?>
-					</td>
-					<td>
-						<?=$conn['remote_host'];?>
-					</td>
-					<td>
-						<i class="fa fa-times-circle"
-							onclick="killClient('<?=$server['mgmt']; ?>', '<?=$conn['remote_host']; ?>');"
-							style="cursor:pointer;"
-							name="<?php echo "i:{$server['mgmt']}:{$conn['remote_host']}"; ?>"
-							title=<?=sprintf(gettext('Kill client connection from %s'), $conn['remote_host']);?>>
-						</i>
-					</td>
-				</tr>
-				<tr name="<?php echo "r:{$server['mgmt']}:{$conn['remote_host']}"; ?>" class="<?=$evenRowClass?>">
-					<td>
-						<?=$conn['connect_time'];?>
-					</td>
-					<td>
-						<?=$conn['virtual_addr'];?>
-					</td>
-					<td></td>
-				</tr>
-<?php
-			endforeach;
-?>
-			</tbody>
-		</table>
-	</div>
-</div>
-
-<<<<<<< HEAD
-<?php
-endforeach;
-=======
 	// Refresh the panel
 	function get_update() {
 		var ajaxRequest;
->>>>>>> 8da4847... Text typos in openvpn.widget.php
 
-if (!empty($sk_servers)):
-?>
-<div class="widget panel panel-default">
-	<div class="panel-heading"><h2 class="panel-title"><?=gettext("Peer to Peer Server Instance Statistics");?></h2></div>
-	<div class="table-responsive">
-		<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable>
-			<thead>
-				<tr>
-					<th><?=gettext('Name/Time')?></th>
-					<th><?=gettext('Remote/Virtual IP')?></th>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-<?php
-			foreach ($sk_servers as $sk_server):
-?>
-				<tr name='<?php echo "r:{$sk_server['port']}:{$sk_server['remote_host']}"; ?>'>
-					<td>
-						<?=$sk_server['name'];?>
-					</td>
-					<td>
-						<?=$sk_server['remote_host'];?>
-					</td>
-					<td>
-<?php
-				if ($sk_server['status'] == "up") {
-					/* tunnel is up */
-					echo '<i class="fa fa-arrow-up text-success"></i>';
-				} else {
-					/* tunnel is down */
-					echo '<i class="fa fa-arrow-down text-danger"></i>';
-				}
-?>
-					</td>
-				</tr>
-				<tr name="<?php echo "r:{$sk_server['port']}:{$sk_server['remote_host']}"; ?>">
-					<td>
-						<?=$sk_server['connect_time'];?>
-					</td>
-					<td>
-						<?=$sk_server['virtual_addr'];?>
-					</td>
-					<td></td>
-				</tr>
-<?php
-			endforeach;
-?>
-			</tbody>
-		</table>
-	</div>
-</div>
+		ajaxRequest = $.ajax({
+				url: "/widgets/widgets/openvpn.widget.php",
+				type: "post",
+				data: { ajax: "ajax"}
+			});
 
-<<<<<<< HEAD
-<?php
-endif;
+		// Deal with the results of the above ajax call
+		ajaxRequest.done(function (response, textStatus, jqXHR) {
+			$('#mainpanel').html(response);
 
-if (!empty($clients)):
-?>
-<div class="widget panel panel-default">
-	<div class="panel-heading"><h2 class="panel-title"><?=gettext("Client Instance Statistics");?></h2></div>
-	<div class="table-responsive">
-		<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable>
-			<thead>
-				<tr>
-					<th><?=gettext('Name/Time')?></th>
-					<th><?=gettext('Remote/Virtual IP')?></th>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-<?php
-			foreach ($clients as $client):
-?>
-				<tr name="<?php echo "r:{$client['port']}:{$client['remote_host']}"; ?>">
-					<td>
-						<?=$client['name'];?>
-					</td>
-					<td>
-					<?=$client['remote_host'];?>
-					</td>
-					<td>
-<?php
-				if ($client['status'] == "up") {
-					/* tunnel is up */
-					echo '<i class="fa fa-arrow-up text-success"></i>';
-				} else {
-					/* tunnel is down */
-					echo '<i class="fa fa-arrow-down text-danger"></i>';
-				}
-?>
-					</td>
-				</tr>
-				<tr name="<?php echo "r:{$client['port']}:{$client['remote_host']}"; ?>">
-					<td>
-						<?=$client['connect_time'];?>
-					</td>
-					<td>
-						<?=$client['virtual_addr'];?>
-					</td>
-					<td></td>
-				</tr>
-<?php
-			endforeach;
-?>
-			</tbody>
-		</table>
-	</div>
-</div>
-=======
 			// and do it again
 			setTimeout(get_update, "<?=$widgetperiod?>");
 		});
@@ -485,14 +319,9 @@ if (!empty($clients)):
 //]]>
 </script>
 <div id="mainpanel" class="content">
->>>>>>> 512f2c1... Added control to set dashboard widget refresh period
 
 <?php
-endif;
-
-if ((empty($clients)) && (empty($servers)) && (empty($sk_servers))) {
-	echo gettext("No OpenVPN instances defined");
-}
+	printPanel();
 ?>
 </div>
 
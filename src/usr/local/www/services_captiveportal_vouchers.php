e3b0eeb23a2ae570037f91606b160f8faac42890
PiBa-NL <pba_2k3@yahoo.com>
2016-09-13 19:42:04 +0200

    XMLRPC, xmlrpc_client remove extended class and define setConnectionData(.) function instead.

--- src/usr/local/www/services_captiveportal_vouchers.php
+++ src/usr/local/www/services_captiveportal_vouchers.php
@@ -300,7 +300,8 @@ if ($_POST) {
 
 EOF;
 				require_once("xmlrpc_client.inc");
-				$rpc_client = new pfsense_xmlrpc_client_extended(
+				$rpc_client = new pfsense_xmlrpc_client();
+				$rpc_client->setConnectionData(
 						$newvoucher['vouchersyncdbip'], $newvoucher['vouchersyncport'], 
 						$newvoucher['vouchersyncusername'], $newvoucher['vouchersyncpass']);
 				$rpc_client->set_noticefile("CaptivePortalVoucherSync");

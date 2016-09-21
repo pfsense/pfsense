b65bfb624655a0a19ee70d5d4afde22f95a7c7ff
Colin Fleming <cj_fleming@sky.com>
2016-09-21 20:39:17 +0100

    Spelling mistake "system_gateways.php"
    Fix spelling mistake in "system_gateways.php"
    
    (cherry picked from commit 47180823dc0560801085a227abf512e265363b16)

--- src/usr/local/www/system_gateways.php
+++ src/usr/local/www/system_gateways.php
@@ -267,7 +267,7 @@ display_top_tabs($tab_array);
 	<div class="panel-heading"><h2 class="panel-title"><?=gettext('Gateways')?></h2></div>
 	<div class="panel-body">
 		<div class="table-responsive">
-			<table class="table table-striped tabel-hover table-condensed table-rowdblclickedit">
+			<table class="table table-striped table-hover table-condensed table-rowdblclickedit">
 				<thead>
 					<tr>
 						<th></th>

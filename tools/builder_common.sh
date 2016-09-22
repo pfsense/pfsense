c09203c2ba6bf10332fe432b72e7e57c2f20cf18
Renato Botelho <renato@netgate.com>
2016-09-22 18:37:10 -0300

    Use _umount()

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -291,13 +291,13 @@ prestage_on_ram_setup() {
 prestage_on_ram_cleanup() {
 	if [ -c /dev/md10001 ]; then
 		if [ -d ${STAGE_CHROOT_DIR} ]; then
-			umount ${STAGE_CHROOT_DIR}
+			_umount ${STAGE_CHROOT_DIR}
 		fi
 		mdconfig -d -u 10001
 	fi
 	if [ -c /dev/md10002 ]; then
 		if [ -d ${FINAL_CHROOT_DIR} ]; then
-			umount ${FINAL_CHROOT_DIR}
+			_umount ${FINAL_CHROOT_DIR}
 		fi
 		mdconfig -d -u 10002
 	fi

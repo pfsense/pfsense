de8d3c070bcae3af0fb0322ad048aadb71652043
Lorenz Schori <lo@znerol.ch>
2016-10-11 17:19:45 +0200

    Specify chroot and directory when testing config, use variables for common paths

--- src/usr/local/sbin/openvpn.learn-address.sh
+++ src/usr/local/sbin/openvpn.learn-address.sh
@@ -7,13 +7,15 @@ OP="$2"
 IP="$3"
 CN="$4"
 
+DIR="/var/unbound"
+CONF="$DIR/openvpn.client.$IP.conf"
+PIDFILE="/var/run/unbound.pid"
+
 case "$OP" in
 
 	add|update)
-		CONF="/var/unbound/openvpn.client.$IP.conf"
-
 		# Remove all configs which mention the FQDN
-		grep -l -null -F "local-data: \"$CN.$DOMAIN A $IP\"" /var/unbound/openvpn.client.*.conf | xargs -0 rm
+		grep -l -null -F "local-data: \"$CN.$DOMAIN A $IP\"" $DIR/openvpn.client.*.conf | xargs -0 rm
 		rm -f "$CONF"
 
 		TMPCONF=$(mktemp "$CONF.XXXXXX")
@@ -26,22 +28,24 @@ case "$OP" in
 			echo "local-data: \"$CN A $IP\""
 		) > "$TMPCONF"
 
-
 		# Check syntax, install configuration and restart unbound.
 		(
 			echo "server:" &&
+			echo "chroot: $DIR" &&
+			echo "directory: $DIR" &&
 			echo "include: $TMPCONF"
 		) > "$TMPSRV"
 
+		chmod 644 "$TMPCONF" "$TMPSRV"
 		/usr/local/sbin/unbound-checkconf "$TMPSRV" && mv "$TMPCONF" "$CONF"
 		rm -f "$TMPCONF" "$TMPSRV"
 
-		/bin/pkill -HUP -F /var/run/unbound.pid
+		/bin/pkill -HUP -F "$PIDFILE"
 	;;
 
 	delete)
 		# CN is not set on delete.
-		rm -f /var/unbound/openvpn.client.$IP.conf && /bin/pkill -HUP -F /var/run/unbound.pid
+		rm -f "$CONF" && /bin/pkill -HUP -F "$PIDFILE"
 	;;
 
 esac

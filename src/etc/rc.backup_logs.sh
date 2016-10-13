7cb1e19c3897f51493f1836a366e85473b1eb5e0
NOYB <Al_Stu@Frontier.com>
2016-10-12 22:21:29 -0700

    RAM Disk Management
    1) Treat the RAM disk more like a permanent storage device with content managed/restored by the system and made available at boot up, before needed by any services.
     a) Handle saving and restoring RAM disk content at reboot/shutdown/boot centrally in more of a system manged fashion.
     b) If it's in the RAM disk store it gets restored early in the pfSense startup so it's available for whatever needs to use it.
     c) Services utilizing RAM disk don't need to be aware that their content is on a RAM disk, and handling content restore individually.
    
    2) Has the benefit of eliminating some issues with the previous code as well. Such as...
     a) Restoring RRD multiple times during boot, potentially at least 3 times, by rc.newwanip, rc.newwanipv6, and rc.boot. Some even overlapping.
     b) Not removing the backups if/when not being utilized. Such as on a full install with the RAM disk option not enabled.
     c) Eliminate some duplicate code.
    
    3) Eliminate rrd dump/restore to/from xlm file.
    Backup and restore the entire /var/db/rrd/ to/from the RAM Disk Store.
    
    4) Add logic to backup alias tables if the backup is older than the table.
    
    5) Include the logs in the RAM disk store so they will persist across reboots.
    
    6) Looking forward.
     a) The more centrally system managed approach and restoring the RAM disk content as early as possible will make it easier to include other content that may be useful/desirable or need to persist in the RAM disk across reboots. Some packages data for instance. Simply tar -czf what needs to be restored to /cf/conf/RAM_Disk_Store/*.tgz. See existing /etc/rc.backup*.sh for examples.

--- src/etc/rc.backup_logs.sh
+++ src/etc/rc.backup_logs.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-: ${DBPATH:=/var/db/rrd}
+: ${DBPATH:=/var/log}
 : ${CF_CONF_PATH:=/cf/conf}
 
 : ${RAM_Disk_Store:=${CF_CONF_PATH}/RAM_Disk_Store}
 
-# Save the rrd databases to the RAM disk store.
+# Save the logs database to the RAM disk store.
 if [ -d "${DBPATH}" ]; then
-	echo -n "Saving RRD to RAM disk store...";
+	echo -n "Saving Logs to RAM disk store...";
 
 	[ -z "$NO_REMOUNT" ] && /etc/rc.conf_mount_rw
-	[ -f "${RAM_Disk_Store}/rrd.tgz" ] && /bin/rm -f "${RAM_Disk_Store}/rrd.tgz"
+	[ -f "${RAM_Disk_Store}/logs.tgz" ] && /bin/rm -f "${RAM_Disk_Store}/logs.tgz"
 
 	if [ ! -d "${RAM_Disk_Store}" ]; then
 		mkdir -p "${RAM_Disk_Store}"
 	fi
 
-	cd / && /usr/bin/tar -czf "${RAM_Disk_Store}/rrd.tgz" -C / "${DBPATH#/}/"
+	cd / && /usr/bin/tar -czf "${RAM_Disk_Store}/logs.tgz" -C / "${DBPATH#/}/"
 
 	[ -z "$NO_REMOUNT" ] && /etc/rc.conf_mount_ro
 	echo "done.";

3947f294a6d998999f472bf3d6588c257c3001b7
Renato Botelho <renato@netgate.com>
2016-11-23 12:36:44 -0200

    Add a note that wireless clones must be created before they can be assigned. This should fix #6770

--- src/usr/local/www/interfaces_assign.php
+++ src/usr/local/www/interfaces_assign.php
@@ -578,6 +578,7 @@ display_top_tabs($tab_array);
 
 <?php
 print_info_box(gettext("Interfaces that are configured as members of a lagg(4) interface will not be shown."), 'info', false);
+print_info_box(gettext("Wireless clones must be created at Wireless tab before they can be assigned."), 'info', false);
 ?>
 
 <?php include("foot.inc")?>

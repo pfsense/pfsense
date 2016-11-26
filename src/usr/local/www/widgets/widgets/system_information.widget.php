0d1760eccdcacf700f3849659128ef4b29afa3f8
Luiz Otavio O Souza <luiz@netgate.com>
2016-11-25 20:41:43 -0600

    Show TI SoC model on system information widget.

--- src/usr/local/www/widgets/widgets/system_information.widget.php
+++ src/usr/local/www/widgets/widgets/system_information.widget.php
@@ -128,6 +128,12 @@ $filesystems = get_mounted_filesystems();
 		<tr>
 			<th><?=gettext("CPU Type");?></th>
 			<td><?=htmlspecialchars(get_single_sysctl("hw.model"))?>
+<?
+	if ($platform['name'] == "uFW") {
+		echo "<br>\n";
+		echo htmlspecialchars(get_single_sysctl("hw.ti_model"));
+	}
+?>
 				<div id="cpufreq"><?= get_cpufreq(); ?></div>
 		<?php
 			$cpucount = get_cpu_count();

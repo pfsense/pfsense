53f59af71fac7d554bc81efba872dfa5e76e64f1
Steve Beaver <sbeaver@netgate.com>
2016-11-29 13:17:26 -0500

    Added select action, delete, edit and add buttons/icons

--- src/usr/local/www/switch_vlans.php
+++ src/usr/local/www/switch_vlans.php
@@ -38,67 +38,95 @@ $tab_array[] = array(gettext("System"), false, "switch_system.php");
 $tab_array[] = array(gettext("Ports"), false, "switch_ports.php");
 $tab_array[] = array(gettext("VLANs"), true, "switch_vlans.php");
 display_top_tabs($tab_array);
-?>
-<div class="panel panel-default">
-<form action="interfaces_assign.php" method="post">
-	<div class="table-responsive">
-	<table class="table table-striped table-hover">
-	<thead>
-		<tr>
-			<th>&nbsp;</th>
-			<th><?=gettext("Switch")?></th>
-			<th>&nbsp;</th>
-		</tr>
-	</thead>
-	<tbody>
-<?php
 
-/* Available switch devices on platform. */
-$swdevices = array();
-/* Read-only VLANs (necessary to basic system operation). */
-$vlans_system = array();
 
-$platform = system_identify_specific_platform();
-if ($platform['name'] == "uFW") {
-	/* Only one switch on uFW. */
-	$swdevices[] = "/dev/etherswitch0";
-	$vlans_system = switch_get_system_vlans();
-}
+// Build an array with which to populate the switch device selector
+function get_switches($devicelist) {
 
-$swdevice = NULL;
-foreach ($swdevices as $swdev) {
-	/* Just in case... */
-	pfSense_etherswitch_close();
+	$switches = array();
 
-	if (pfSense_etherswitch_open($swdev) == false) {
-		continue;
-	}
-
-	$swinfo = pfSense_etherswitch_getinfo();
-	if ($swinfo == NULL) {
+	foreach ($devicelist as $swdev) {
+		/* Just in case... */
 		pfSense_etherswitch_close();
-		continue;
-	}
-	if ($swdevice == NULL)
-		$swdevice = $swdev;
-?>
-		<tr>
-			<td>&nbsp;</td>
-			<td><select name="swdevice" id="swdevice" class="form-control">
-				<option value="<?= $swdev ?>"><?= $swinfo['name'] ?>
-			    </select>
-			</td>
-			<td>&nbsp;</td>
-		</tr>
-<?
-	pfSense_etherswitch_close();
-}
-?>
-	</tbody>
-	</table>
-	</div>
-</form>
 
+		if (pfSense_etherswitch_open($swdev) == false) {
+			continue;
+		}
+
+		$swinfo = pfSense_etherswitch_getinfo();
+
+		if ($swinfo == NULL) {
+			pfSense_etherswitch_close();
+			continue;
+		}
+		if ($swdevice == NULL)
+			$swdevice = $swdev;
+
+		$switches[$swdev] = $swinfo['name'];
+
+		pfSense_etherswitch_close();
+	}
+
+	return($switches);
+}
+
+// Delete a VLAN
+if ($_GET['act'] == "del") {
+	$vid = $_GET['vid'];
+	$device = $_GET['swdevice'];
+
+	print("Deleting VID: " . $vid . " from device: " . $device . "<br />");
+
+	// ToDo: Add some code to delete the VLAN
+}
+
+// List the available switches
+// ToDo: Check this is the correct way to get teh switch information
+$swdevices = array();
+$swdevices = glob("/dev/etherswitch*");
+$vlans_system = switch_get_system_vlans();
+
+// If there is more than one switch, draw a selector to allow the user to choose which one to look at
+if (count($swdevices) > 1) {
+	$form = new Form(false);
+
+	$section = new Form_Section('Dynamic DNS Client');
+
+	$section->addInput(new Form_Select(
+		'swdevice',
+		'Switch',
+		$_POST['swdevice'],
+		get_switches($swdevices)
+	));
+
+	$form->add($section);
+
+	print($form);
+
+} else {
+	// If running on a Netgate micro-firewall, display that in the panel title
+	if (system_identify_specific_platform()['name'] == "uFW") {
+		$ufwname = "uFW ";
+	}
+}
+
+if ($_GET['swdevice']) {
+	$_POST['swdevice'] = $_GET['swdevice'];
+}
+
+// If the selector was changed, the selected value becomes the default
+if($_POST['swdevice']) {
+	$swdevice = $_POST['swdevice'];
+} else {
+	$swdevice = $swdevices[0];
+}
+
+
+pfSense_etherswitch_close();
+
+?>
+
+<div class="panel panel-default">
 	<div class="panel-heading"><h2 class="panel-title"><?=gettext('Switch VLANs')?></h2></div>
 	<div class="panel-body">
 		<div class="table-responsive">
@@ -109,28 +137,30 @@ foreach ($swdevices as $swdev) {
 						<th><?=gettext("VLAN ID"); ?></th>
 						<th><?=gettext("Members"); ?></th>
 						<th><?=gettext("Description"); ?></th>
+						<th><?=gettext("Action"); ?></th>
 					</tr>
 				</thead>
 				<tbody>
 <?php
 
-	/* Just in case... */
+/* Just in case... */
+pfSense_etherswitch_close();
+
+if (pfSense_etherswitch_open($swdevice) == false) {
+	continue;
+}
+
+$swinfo = pfSense_etherswitch_getinfo();
+if ($swinfo == NULL) {
 	pfSense_etherswitch_close();
+	continue;
+}
 
-	if (pfSense_etherswitch_open($swdevice) == false) {
+for ($i = 0; $i < $swinfo['nvlangroups']; $i++) {
+	$vgroup = pfSense_etherswitch_getvlangroup($i);
+	if ($vgroup == NULL) {
 		continue;
 	}
-
-	$swinfo = pfSense_etherswitch_getinfo();
-	if ($swinfo == NULL) {
-		pfSense_etherswitch_close();
-		continue;
-	}
-	for ($i = 0; $i < $swinfo['nvlangroups']; $i++) {
-		$vgroup = pfSense_etherswitch_getvlangroup($i);
-		if ($vgroup == NULL) {
-			continue;
-		}
 ?>
 					<tr>
 						<td>
@@ -141,24 +171,39 @@ foreach ($swdevices as $swdev) {
 						</td>
 						<td>
 <?
-		$comma = false;
-		foreach ($vgroup['members'] as $member => $val) {
-			if ($comma)
-				echo ",";
-			echo "$member";
-			$comma = true;
+	$comma = false;
+
+	foreach ($vgroup['members'] as $member => $val) {
+		if ($comma) {
+			echo ",";
 		}
+
+		echo "$member";
+		$comma = true;
+	}
 ?>
 						</td>
 						<td>
 <?
-		foreach ($vlans_system as $svlan) {
-			if ($svlan['vid'] != $vgroup['vid'])
-				continue;
-			echo "Default System VLAN";
-			break;
+	foreach ($vlans_system as $svlan) {
+		if ($svlan['vid'] != $vgroup['vid']) {
+			$sys = true;
+			continue;
 		}
+
+		echo "Default System VLAN";
+		$sys = false;
+
+		break;
+	}
 ?>
+						</td>
+						<td>
+<?php
+		if (!$sys) { ?>
+							<a class="fa fa-pencil" title="<?=gettext("Edit"); ?>" href="switch_vlans_edit.php?vid=<?=htmlspecialchars($vgroup['vid'])?>&amp;act=edit&amp;swdevice=<?=$swdevice?>"></a>
+							<a class="fa fa-trash"	title="<?=gettext("Delete")?>" href="?act=del&amp;vid=<?=htmlspecialchars($vgroup['vid'])?>&amp;swdevice=<?=$swdevice?>"></a>
+<?php 	} ?>
 						</td>
 					</tr>
 <?
@@ -172,5 +217,25 @@ foreach ($swdevices as $swdev) {
 		</div>
 	</div>
 </div>
+
+<nav class="action-buttons">
+	<a href="switch_vlans_edit.php?swdevice=<?=$swdevice?>&amp;act=new" role="button" class="btn btn-success btn-sm">
+		<i class="fa fa-plus icon-embed-btn"></i>
+		<?=gettext("Add");?>
+	</a>
+</nav>
+
+<script type="text/javascript">
+//<![CDATA[
+events.push(function() {
+
+	// Automatically submit the form when the selector is changed
+	$('#swdevice').on('change', function () {
+		$('form').submit();
+	});
+});
+//]]>
+</script>
+
 <?php
 include("foot.inc");
---
+++ src/usr/local/www/switch_vlans_edit.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * switch_vlans.php
+ *
+ * part of pfSense (https://www.pfsense.org)
+ * Copyright (c) 2004-2016 Rubicon Communications, LLC (Netgate)
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+##|+PRIV
+##|*IDENT=page-switch-vlans-edit
+##|*NAME=Switch: VLANs Edit
+##|*DESCR=Allow access to the 'Switch: VLANs Edit' page.
+##|*MATCH=switch_vlans_edit.php*
+##|-PRIV
+
+require_once("guiconfig.inc");
+require_once("switch.inc");
+
+$pgtitle = array(gettext("Interfaces"), gettext("Switch"), gettext("VLANs"), gettext("Edits"));
+$shortcut_section = "vlans";
+include("head.inc");
+
+print("<h3>Under construction</h3>");
+
+<?php
+include("foot.inc");

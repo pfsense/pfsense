76895c7f1d77c8e17913a3ddf87636fc4d50058d
Phil Davis <phil.davis@inf.org>
2016-12-31 17:10:23 +0545

    firewall_rules_edit stop Floating field displaying
    Seems to fix https://redmine.pfsense.org/issues/7057
    But I have not looked underneath the hood - just copied the way other hidden fields are done in that code.

--- src/usr/local/www/firewall_rules_edit.php
+++ src/usr/local/www/firewall_rules_edit.php
@@ -1239,7 +1239,7 @@ if ($if == "FloatingRules" || isset($pconfig['floating'])) {
 		)
 	));
 
-	$section->addInput(new Form_Input(
+	$form->addGlobal(new Form_Input(
 		'floating',
 		'Floating',
 		'hidden',

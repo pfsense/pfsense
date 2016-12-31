e399d9193a27dc4f6883d8ba7a4d85083ccba877
stilez <stilez@users.noreply.github.com>
2016-12-31 13:16:01 +0000

    Enhance security note for privelege assignment

--- src/usr/local/www/system_groupmanager_addprivs.php
+++ src/usr/local/www/system_groupmanager_addprivs.php
@@ -153,7 +153,9 @@ $section->addInput(new Form_Select(
 	build_priv_list(),
 	true
 ))->addClass('multiselect')
-  ->setHelp('Hold down CTRL (PC)/COMMAND (Mac) key to select multiple items.');
+  ->setHelp('Hold down CTRL (PC)/COMMAND (Mac) key to select multiple items.<br /><br />' .
+	   'Security note: Some privileges in this list expose root or other highly privileged access if access is granted.<br />' .
+	    'In particular, <i>WebCfg-All pages</i> exposes root access via the command prompt and edit file pages.');
 
 $section->addInput(new Form_Select(
 	'shadow',
@@ -162,7 +164,9 @@ $section->addInput(new Form_Select(
 	build_priv_list(),
 	true
 ))->addClass('shadowselect')
-  ->setHelp('Hold down CTRL (PC)/COMMAND (Mac) key to select multiple items.');
+  ->setHelp('Hold down CTRL (PC)/COMMAND (Mac) key to select multiple items.<br /><br />' .
+	   'Security note: Some privileges in this list expose root or other highly privileged access if access is granted.<br />' .
+	    'In particular, <i>WebCfg-All pages</i> exposes root access via the command prompt and edit file pages.');
 
 $section->addInput(new Form_Input(
 	'filtertxt',

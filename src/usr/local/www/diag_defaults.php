bf5ac0b9b3dc025353cd96a8e863758e836f3013
Phil Davis <phil.davis@inf.org>
2017-01-01 16:51:24 +0545

    Add security notes for privilege assignment pages
    Suggested solution for Redmine 2247

--- src/usr/local/www/diag_defaults.php
+++ src/usr/local/www/diag_defaults.php
@@ -27,6 +27,7 @@
 ##|*IDENT=page-diagnostics-factorydefaults
 ##|*NAME=Diagnostics: Factory defaults
 ##|*DESCR=Allow access to the 'Diagnostics: Factory defaults' page.
+##|*WARN=standard-warning-root
 ##|*MATCH=diag_defaults.php*
 ##|-PRIV
 

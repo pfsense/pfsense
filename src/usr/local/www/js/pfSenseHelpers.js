fab3c245cfb52964cebdab2ea47dddb21731352a
Steve Beaver <sbeaver@netgate.com>
2017-01-02 11:34:38 -0500

    Fixed #6972 I was only able to reproduce tis issue using Chrome, but it should now be resolved

--- src/usr/local/www/js/pfSenseHelpers.js
+++ src/usr/local/www/js/pfSenseHelpers.js
@@ -375,9 +375,9 @@ $('[id^=addrow]').click(function() {
 $('[id^=delete]').click(function(event) {
 	if ($('.repeatable').length > 1) {
 		if ((typeof retainhelp) == "undefined")
-			moveHelpText(event.target.id);
+			moveHelpText($(this).attr("id"));
 
-		delete_row(event.target.id);
+		delete_row($(this).attr("id"));
 	} else {
 		alert('The last row may not be deleted.');
 	}

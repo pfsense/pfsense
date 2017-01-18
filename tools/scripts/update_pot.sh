42f2c3e9884c773233b34198c97897edd4bdecd6
Renato Botelho <renato@netgate.com>
2017-01-18 14:09:15 -0200

    Fixup update_pot.sh adding --from-code to xgettext calls

--- tools/scripts/update_pot.sh
+++ tools/scripts/update_pot.sh
@@ -55,6 +55,7 @@ POT=$ROOT/src/usr/local/share/locale/en/LC_MESSAGES/pfSense.pot
 		-f $FILES \
 		-o $POT \
 		-L php \
+		--from-code UTF-8 \
 		-ksetHelp \
 		-kForm_Section \
 		-kForm_Group \
@@ -77,6 +78,7 @@ POT=$ROOT/src/usr/local/share/locale/en/LC_MESSAGES/pfSense.pot
 		-o $POT \
 		-L php \
 		-j \
+		--from-code UTF-8 \
 		-kForm_Checkbox:3 \
 		-kForm_StaticText:2 \
 		-kForm_MultiCheckbox:3 \

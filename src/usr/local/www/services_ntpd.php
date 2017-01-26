19c25a5377dbdb6a1ea35c479038571b669e1df3
jskyboo <jack@ron.hog.mooo.info>
2017-01-26 08:42:03 -0800

    Fix indentation
    (cherry picked from commit c18d0d12b3e683f07b4bac933240332cd4d95829)

--- src/usr/local/www/services_ntpd.php
+++ src/usr/local/www/services_ntpd.php
@@ -252,9 +252,9 @@ $timeservers = explode(' ', $config['system']['timeservers']);
 $maxrows = max(count($timeservers), 1);
 for ($counter=0; $counter < $maxrows; $counter++) {
 	$group = new Form_Group($counter == 0 ? 'Time Servers':'');
-    $group->addClass('repeatable');
-    $group->setAttribute('max_repeats', NUMTIMESERVERS);
-    $group->setAttribute('max_repeats_alert', sprintf(gettext('%d is the maximum number of configured servers.'), NUMTIMESERVERS));
+	$group->addClass('repeatable');
+	$group->setAttribute('max_repeats', NUMTIMESERVERS);
+	$group->setAttribute('max_repeats_alert', sprintf(gettext('%d is the maximum number of configured servers.'), NUMTIMESERVERS));
 
 	$group->add(new Form_Input(
 		'server' . $counter,

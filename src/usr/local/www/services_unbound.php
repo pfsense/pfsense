pfsense/pfsense

commit 13541a81e1173fc02af9af8ab7fe46df2a51007d

GET/POST conversions

src/usr/local/www/services_unbound.php
@@ -86,106 +86,108 @@ if (empty($a_unboundcfg['system_domain_local_zone_type'])) {
 	$pconfig['system_domain_local_zone_type'] = $a_unboundcfg['system_domain_local_zone_type'];
 }
 
-if ($_POST) {
-	if ($_POST['apply']) {
-		$retval = 0;
-		$retval |= services_unbound_configure();
-		if ($retval == 0) {
-			clear_subsystem_dirty('unbound');
-		}
-		/* Update resolv.conf in case the interface bindings exclude localhost. */
-		system_resolvconf_generate();
-		/* Start or restart dhcpleases when it's necessary */
-		system_dhcpleases_configure();
-	} else {
-		$pconfig = $_POST;
-		unset($input_errors);
 
-		if (isset($pconfig['enable']) && isset($config['dnsmasq']['enable'])) {
-			if ($pconfig['port'] == $config['dnsmasq']['port']) {
-				$input_errors[] = gettext("The DNS Forwarder is enabled using this port. Choose a non-conflicting port, or disable the DNS Forwarder.");
-			}
-		}
+if ($_POST['apply']) {
+	$retval = 0;
+	$retval |= services_unbound_configure();
+	if ($retval == 0) {
+		clear_subsystem_dirty('unbound');
+	}
+	/* Update resolv.conf in case the interface bindings exclude localhost. */
+	system_resolvconf_generate();
+	/* Start or restart dhcpleases when it's necessary */
+	system_dhcpleases_configure();
+}
 
-		// forwarding mode requires having valid DNS servers
-		if (isset($pconfig['forwarding'])) {
-			$founddns = false;
-			if (isset($config['system']['dnsallowoverride'])) {
-				$dns_servers = get_dns_servers();
-				if (is_array($dns_servers)) {
-					foreach ($dns_servers as $dns_server) {
-						if (!ip_in_subnet($dns_server, "127.0.0.0/8")) {
-							$founddns = true;
-						}
-					}
-				}
-			}
-			if (is_array($config['system']['dnsserver'])) {
-				foreach ($config['system']['dnsserver'] as $dnsserver) {
-					if (is_ipaddr($dnsserver)) {
+if ($_POST['save']) {
+	$pconfig = $_POST;
+	unset($input_errors);
+
+	if (isset($pconfig['enable']) && isset($config['dnsmasq']['enable'])) {
+		if ($pconfig['port'] == $config['dnsmasq']['port']) {
+			$input_errors[] = gettext("The DNS Forwarder is enabled using this port. Choose a non-conflicting port, or disable the DNS Forwarder.");
+		}
+	}
+
+	// forwarding mode requires having valid DNS servers
+	if (isset($pconfig['forwarding'])) {
+		$founddns = false;
+		if (isset($config['system']['dnsallowoverride'])) {
+			$dns_servers = get_dns_servers();
+			if (is_array($dns_servers)) {
+				foreach ($dns_servers as $dns_server) {
+					if (!ip_in_subnet($dns_server, "127.0.0.0/8")) {
 						$founddns = true;
 					}
 				}
 			}
-			if ($founddns == false) {
-				$input_errors[] = gettext("At least one DNS server must be specified under System &gt; General Setup to enable Forwarding mode.");
+		}
+		if (is_array($config['system']['dnsserver'])) {
+			foreach ($config['system']['dnsserver'] as $dnsserver) {
+				if (is_ipaddr($dnsserver)) {
+					$founddns = true;
+				}
 			}
 		}
-
-		if (empty($pconfig['active_interface'])) {
-			$input_errors[] = gettext("One or more Network Interfaces must be selected for binding.");
-		} else if (!isset($config['system']['dnslocalhost']) && (!in_array("lo0", $pconfig['active_interface']) && !in_array("all", $pconfig['active_interface']))) {
-			$input_errors[] = gettext("This system is configured to use the DNS Resolver as its DNS server, so Localhost or All must be selected in Network Interfaces.");
+		if ($founddns == false) {
+			$input_errors[] = gettext("At least one DNS server must be specified under System &gt; General Setup to enable Forwarding mode.");
 		}
-
-		if (empty($pconfig['outgoing_interface'])) {
-			$input_errors[] = gettext("One or more Outgoing Network Interfaces must be selected.");
-		}
-
-		if ($pconfig['port'] && !is_port($pconfig['port'])) {
-			$input_errors[] = gettext("A valid port number must be specified.");
-		}
-
-		if (is_array($pconfig['active_interface']) && !empty($pconfig['active_interface'])) {
-			$display_active_interface = $pconfig['active_interface'];
-			$pconfig['active_interface'] = implode(",", $pconfig['active_interface']);
-		}
-
-		$display_custom_options = $pconfig['custom_options'];
-		$pconfig['custom_options'] = base64_encode(str_replace("\r\n", "\n", $pconfig['custom_options']));
-
-		if (is_array($pconfig['outgoing_interface']) && !empty($pconfig['outgoing_interface'])) {
-			$display_outgoing_interface = $pconfig['outgoing_interface'];
-			$pconfig['outgoing_interface'] = implode(",", $pconfig['outgoing_interface']);
-		}
-
-		$test_output = array();
-		if (test_unbound_config($pconfig, $test_output)) {
-			$input_errors[] = gettext("The generated config file cannot be parsed by unbound. Please correct the following errors:");
-			$input_errors = array_merge($input_errors, $test_output);
-		}
-
-		if (!$input_errors) {
-			$a_unboundcfg['enable'] = isset($pconfig['enable']);
-			$a_unboundcfg['port'] = $pconfig['port'];
-			$a_unboundcfg['dnssec'] = isset($pconfig['dnssec']);
-			$a_unboundcfg['forwarding'] = isset($pconfig['forwarding']);
-			$a_unboundcfg['regdhcp'] = isset($pconfig['regdhcp']);
-			$a_unboundcfg['regdhcpstatic'] = isset($pconfig['regdhcpstatic']);
-			$a_unboundcfg['active_interface'] = $pconfig['active_interface'];
-			$a_unboundcfg['outgoing_interface'] = $pconfig['outgoing_interface'];
-			$a_unboundcfg['system_domain_local_zone_type'] = $pconfig['system_domain_local_zone_type'];
-			$a_unboundcfg['custom_options'] = $pconfig['custom_options'];
-
-			write_config(gettext("DNS Resolver configured."));
-			mark_subsystem_dirty('unbound');
-		}
-
-		$pconfig['active_interface'] = $display_active_interface;
-		$pconfig['outgoing_interface'] = $display_outgoing_interface;
-		$pconfig['custom_options'] = $display_custom_options;
 	}
+
+	if (empty($pconfig['active_interface'])) {
+		$input_errors[] = gettext("One or more Network Interfaces must be selected for binding.");
+	} else if (!isset($config['system']['dnslocalhost']) && (!in_array("lo0", $pconfig['active_interface']) && !in_array("all", $pconfig['active_interface']))) {
+		$input_errors[] = gettext("This system is configured to use the DNS Resolver as its DNS server, so Localhost or All must be selected in Network Interfaces.");
+	}
+
+	if (empty($pconfig['outgoing_interface'])) {
+		$input_errors[] = gettext("One or more Outgoing Network Interfaces must be selected.");
+	}
+
+	if ($pconfig['port'] && !is_port($pconfig['port'])) {
+		$input_errors[] = gettext("A valid port number must be specified.");
+	}
+
+	if (is_array($pconfig['active_interface']) && !empty($pconfig['active_interface'])) {
+		$display_active_interface = $pconfig['active_interface'];
+		$pconfig['active_interface'] = implode(",", $pconfig['active_interface']);
+	}
+
+	$display_custom_options = $pconfig['custom_options'];
+	$pconfig['custom_options'] = base64_encode(str_replace("\r\n", "\n", $pconfig['custom_options']));
+
+	if (is_array($pconfig['outgoing_interface']) && !empty($pconfig['outgoing_interface'])) {
+		$display_outgoing_interface = $pconfig['outgoing_interface'];
+		$pconfig['outgoing_interface'] = implode(",", $pconfig['outgoing_interface']);
+	}
+
+	$test_output = array();
+	if (test_unbound_config($pconfig, $test_output)) {
+		$input_errors[] = gettext("The generated config file cannot be parsed by unbound. Please correct the following errors:");
+		$input_errors = array_merge($input_errors, $test_output);
+	}
+
+	if (!$input_errors) {
+		$a_unboundcfg['enable'] = isset($pconfig['enable']);
+		$a_unboundcfg['port'] = $pconfig['port'];
+		$a_unboundcfg['dnssec'] = isset($pconfig['dnssec']);
+		$a_unboundcfg['forwarding'] = isset($pconfig['forwarding']);
+		$a_unboundcfg['regdhcp'] = isset($pconfig['regdhcp']);
+		$a_unboundcfg['regdhcpstatic'] = isset($pconfig['regdhcpstatic']);
+		$a_unboundcfg['active_interface'] = $pconfig['active_interface'];
+		$a_unboundcfg['outgoing_interface'] = $pconfig['outgoing_interface'];
+		$a_unboundcfg['system_domain_local_zone_type'] = $pconfig['system_domain_local_zone_type'];
+		$a_unboundcfg['custom_options'] = $pconfig['custom_options'];
+
+		write_config(gettext("DNS Resolver configured."));
+		mark_subsystem_dirty('unbound');
+	}
+
+	$pconfig['active_interface'] = $display_active_interface;
+	$pconfig['outgoing_interface'] = $display_outgoing_interface;
+	$pconfig['custom_options'] = $display_custom_options;
 }
+
 
 if ($pconfig['custom_options']) {
 	$customoptions = true;
@@ -193,18 +195,18 @@ if ($pconfig['custom_options']) {
 	$customoptions = false;
 }
 
-if ($_GET['act'] == "del") {
-	if ($_GET['type'] == 'host') {
-		if ($a_hosts[$_GET['id']]) {
-			unset($a_hosts[$_GET['id']]);
+if ($_POST['act'] == "del") {
+	if ($_POST['type'] == 'host') {
+		if ($a_hosts[$_POST['id']]) {
+			unset($a_hosts[$_POST['id']]);
 			write_config();
 			mark_subsystem_dirty('unbound');
 			header("Location: services_unbound.php");
 			exit;
 		}
-	} elseif ($_GET['type'] == 'doverride') {
-		if ($a_domainOverrides[$_GET['id']]) {
-			unset($a_domainOverrides[$_GET['id']]);
+	} elseif ($_POST['type'] == 'doverride') {
+		if ($a_domainOverrides[$_POST['id']]) {
+			unset($a_domainOverrides[$_POST['id']]);
 			write_config();
 			mark_subsystem_dirty('unbound');
 			header("Location: services_unbound.php");
@@ -460,7 +462,7 @@ foreach ($a_hosts as $hostent):
 					</td>
 					<td>
 						<a class="fa fa-pencil"	title="<?=gettext('Edit host override')?>" href="services_unbound_host_edit.php?id=<?=$i?>"></a>
-						<a class="fa fa-trash"	title="<?=gettext('Delete host override')?>" href="services_unbound.php?type=host&amp;act=del&amp;id=<?=$i?>"></a>
+						<a class="fa fa-trash"	title="<?=gettext('Delete host override')?>" href="services_unbound.php?type=host&amp;act=del&amp;id=<?=$i?>" usepost></a>
 					</td>
 				</tr>
 
@@ -534,7 +536,7 @@ foreach ($a_domainOverrides as $doment):
 					</td>
 					<td>
 						<a class="fa fa-pencil"	title="<?=gettext('Edit domain override')?>" href="services_unbound_domainoverride_edit.php?id=<?=$i?>"></a>
-						<a class="fa fa-trash"	title="<?=gettext('Delete domain override')?>" href="services_unbound.php?act=del&amp;type=doverride&amp;id=<?=$i?>"></a>
+						<a class="fa fa-trash"	title="<?=gettext('Delete domain override')?>" href="services_unbound.php?act=del&amp;type=doverride&amp;id=<?=$i?>" usepost></a>
 					</td>
 				</tr>
 <?php

src/usr/local/www/services_unbound_host_edit.php
@@ -53,13 +53,7 @@ if (!is_array($config['unbound']['hosts'])) {
 }
 
 $a_hosts = &$config['unbound']['hosts'];
-
-if (is_numericint($_GET['id'])) {
-	$id = $_GET['id'];
-}
-if (isset($_POST['id']) && is_numericint($_POST['id'])) {
-	$id = $_POST['id'];
-}
+$id = $_REQUEST['id'];
 
 if (isset($id) && $a_hosts[$id]) {
 	$pconfig['host'] = $a_hosts[$id]['host'];
@@ -69,7 +63,7 @@ if (isset($id) && $a_hosts[$id]) {
 	$pconfig['aliases'] = $a_hosts[$id]['aliases'];
 }
 
-if ($_POST) {
+if ($_POST['save']) {
 	unset($input_errors);
 	$pconfig = $_POST;
 
@@ -177,24 +171,6 @@ if ($_POST) {
 		header("Location: services_unbound.php");
 		exit;
 	}
-}
-
-// Delete a row in the options table
-if ($_GET['act'] == "delopt") {
-	$idx = $_GET['id'];
-
-	if ($pconfig['aliases'] && is_array($pconfig['aliases']['item'][$idx])) {
-	   unset($pconfig['aliases']['item'][$idx]);
-	}
-}
-
-// Add an option row
-if ($_GET['act'] == "addopt") {
-	if (!is_array($pconfig['aliases']['item'])) {
-		$pconfig['aliases']['item'] = array();
-	}
-
-	array_push($pconfig['aliases']['item'], array('host' => null, 'domain' => null, 'description' => null));
 }
 
 $pgtitle = array(gettext("Services"), gettext("DNS Resolver"), gettext("General Settings"), gettext("Edit Host Override"));

src/usr/local/www/services_dnsmasq.php
@@ -110,87 +110,89 @@ for ($idx=0; $idx<count($a_domainOverrides); $idx++) {
 
 domains_sort();
 
-if ($_POST) {
-	if ($_POST['apply']) {
-		$retval = 0;
-		$retval |= services_dnsmasq_configure();
 
-		// Reload filter (we might need to sync to CARP hosts)
-		filter_configure();
-		/* Update resolv.conf in case the interface bindings exclude localhost. */
-		system_resolvconf_generate();
-		/* Start or restart dhcpleases when it's necessary */
-		system_dhcpleases_configure();
+if ($_POST['apply']) {
+	$retval = 0;
+	$retval |= services_dnsmasq_configure();
 
-		if ($retval == 0) {
-			clear_subsystem_dirty('hosts');
-		}
-	} else {
-		$pconfig = $_POST;
-		unset($input_errors);
+	// Reload filter (we might need to sync to CARP hosts)
+	filter_configure();
+	/* Update resolv.conf in case the interface bindings exclude localhost. */
+	system_resolvconf_generate();
+	/* Start or restart dhcpleases when it's necessary */
+	system_dhcpleases_configure();
 
-		$config['dnsmasq']['enable'] = ($_POST['enable']) ? true : false;
-		$config['dnsmasq']['regdhcp'] = ($_POST['regdhcp']) ? true : false;
-		$config['dnsmasq']['regdhcpstatic'] = ($_POST['regdhcpstatic']) ? true : false;
-		$config['dnsmasq']['dhcpfirst'] = ($_POST['dhcpfirst']) ? true : false;
-		$config['dnsmasq']['strict_order'] = ($_POST['strict_order']) ? true : false;
-		$config['dnsmasq']['domain_needed'] = ($_POST['domain_needed']) ? true : false;
-		$config['dnsmasq']['no_private_reverse'] = ($_POST['no_private_reverse']) ? true : false;
-		$config['dnsmasq']['custom_options'] = str_replace("\r\n", "\n", $_POST['custom_options']);
-		$config['dnsmasq']['strictbind'] = ($_POST['strictbind']) ? true : false;
-
-		if (isset($_POST['enable']) && isset($config['unbound']['enable'])) {
-			if ($_POST['port'] == $config['unbound']['port']) {
-				$input_errors[] = gettext("The DNS Resolver is enabled using this port. Choose a non-conflicting port, or disable DNS Resolver.");
-			}
-		}
-
-		if ($_POST['port']) {
-			if (is_port($_POST['port'])) {
-				$config['dnsmasq']['port'] = $_POST['port'];
-			} else {
-				$input_errors[] = gettext("A valid port number must be specified.");
-			}
-		} else if (isset($config['dnsmasq']['port'])) {
-			unset($config['dnsmasq']['port']);
-		}
-
-		if (is_array($_POST['interface'])) {
-			$config['dnsmasq']['interface'] = implode(",", $_POST['interface']);
-		} elseif (isset($config['dnsmasq']['interface'])) {
-			unset($config['dnsmasq']['interface']);
-		}
-
-		if ($config['dnsmasq']['custom_options']) {
-			$args = '';
-			foreach (preg_split('/\s+/', $config['dnsmasq']['custom_options']) as $c) {
-				$args .= escapeshellarg("--{$c}") . " ";
-			}
-			exec("/usr/local/sbin/dnsmasq --test $args", $output, $rc);
-			if ($rc != 0) {
-				$input_errors[] = gettext("Invalid custom options");
-			}
-		}
-
-		if (!$input_errors) {
-			write_config();
-			mark_subsystem_dirty('hosts');
-		}
+	if ($retval == 0) {
+		clear_subsystem_dirty('hosts');
 	}
 }
 
-if ($_GET['act'] == "del") {
-	if ($_GET['type'] == 'host') {
-		if ($a_hosts[$_GET['id']]) {
-			unset($a_hosts[$_GET['id']]);
+if ($_POST['save']) {
+	$pconfig = $_POST;
+	unset($input_errors);
+
+	$config['dnsmasq']['enable'] = ($_POST['enable']) ? true : false;
+	$config['dnsmasq']['regdhcp'] = ($_POST['regdhcp']) ? true : false;
+	$config['dnsmasq']['regdhcpstatic'] = ($_POST['regdhcpstatic']) ? true : false;
+	$config['dnsmasq']['dhcpfirst'] = ($_POST['dhcpfirst']) ? true : false;
+	$config['dnsmasq']['strict_order'] = ($_POST['strict_order']) ? true : false;
+	$config['dnsmasq']['domain_needed'] = ($_POST['domain_needed']) ? true : false;
+	$config['dnsmasq']['no_private_reverse'] = ($_POST['no_private_reverse']) ? true : false;
+	$config['dnsmasq']['custom_options'] = str_replace("\r\n", "\n", $_POST['custom_options']);
+	$config['dnsmasq']['strictbind'] = ($_POST['strictbind']) ? true : false;
+
+	if (isset($_POST['enable']) && isset($config['unbound']['enable'])) {
+		if ($_POST['port'] == $config['unbound']['port']) {
+			$input_errors[] = gettext("The DNS Resolver is enabled using this port. Choose a non-conflicting port, or disable DNS Resolver.");
+		}
+	}
+
+	if ($_POST['port']) {
+		if (is_port($_POST['port'])) {
+			$config['dnsmasq']['port'] = $_POST['port'];
+		} else {
+			$input_errors[] = gettext("A valid port number must be specified.");
+		}
+	} else if (isset($config['dnsmasq']['port'])) {
+		unset($config['dnsmasq']['port']);
+	}
+
+	if (is_array($_POST['interface'])) {
+		$config['dnsmasq']['interface'] = implode(",", $_POST['interface']);
+	} elseif (isset($config['dnsmasq']['interface'])) {
+		unset($config['dnsmasq']['interface']);
+	}
+
+	if ($config['dnsmasq']['custom_options']) {
+		$args = '';
+		foreach (preg_split('/\s+/', $config['dnsmasq']['custom_options']) as $c) {
+			$args .= escapeshellarg("--{$c}") . " ";
+		}
+		exec("/usr/local/sbin/dnsmasq --test $args", $output, $rc);
+		if ($rc != 0) {
+			$input_errors[] = gettext("Invalid custom options");
+		}
+	}
+
+	if (!$input_errors) {
+		write_config();
+		mark_subsystem_dirty('hosts');
+	}
+}
+
+
+if ($_POST['act'] == "del") {
+	if ($_POST['type'] == 'host') {
+		if ($a_hosts[$_POST['id']]) {
+			unset($a_hosts[$_POST['id']]);
 			write_config();
 			mark_subsystem_dirty('hosts');
 			header("Location: services_dnsmasq.php");
 			exit;
 		}
-	} elseif ($_GET['type'] == 'doverride') {
-		if ($a_domainOverrides[$_GET['id']]) {
-			unset($a_domainOverrides[$_GET['id']]);
+	} elseif ($_POST['type'] == 'doverride') {
+		if ($a_domainOverrides[$_POST['id']]) {
+			unset($a_domainOverrides[$_POST['id']]);
 			write_config();
 			mark_subsystem_dirty('hosts');
 			header("Location: services_dnsmasq.php");
@@ -386,7 +388,7 @@ foreach ($a_hosts as $i => $hostent):
 					</td>
 					<td>
 						<a class="fa fa-pencil"	title="<?=gettext('Edit host override')?>" 	href="services_dnsmasq_edit.php?id=<?=$hostent['idx']?>"></a>
-						<a class="fa fa-trash"	title="<?=gettext('Delete host override')?>"	href="services_dnsmasq.php?type=host&amp;act=del&amp;id=<?=$hostent['idx']?>"></a>
+						<a class="fa fa-trash"	title="<?=gettext('Delete host override')?>"	href="services_dnsmasq.php?type=host&amp;act=del&amp;id=<?=$hostent['idx']?>" usepost></a>
 					</td>
 				</tr>
 
@@ -458,7 +460,7 @@ foreach ($a_domainOverrides as $i => $doment):
 					</td>
 					<td>
 						<a class="fa fa-pencil"	title="<?=gettext('Edit domain override')?>" href="services_dnsmasq_domainoverride_edit.php?id=<?=$doment['idx']?>"></a>
-						<a class="fa fa-trash"	title="<?=gettext('Delete domain override')?>" href="services_dnsmasq.php?act=del&amp;type=doverride&amp;id=<?=$doment['idx']?>"></a>
+						<a class="fa fa-trash"	title="<?=gettext('Delete domain override')?>" href="services_dnsmasq.php?act=del&amp;type=doverride&amp;id=<?=$doment['idx']?>" usepost></a>
 					</td>
 				</tr>
 <?php

src/usr/local/www/services_dnsmasq_domainoverride_edit.php
@@ -39,11 +39,8 @@ if (!is_array($config['dnsmasq']['domainoverrides'])) {
 
 $a_domainOverrides = &$config['dnsmasq']['domainoverrides'];
 
-if (is_numericint($_GET['id'])) {
-	$id = $_GET['id'];
-}
-if (isset($_POST['id']) && is_numericint($_POST['id'])) {
-	$id = $_POST['id'];
+if (is_numericint($_REQUEST['id'])) {
+	$id = $_REQUEST['id'];
 }
 
 if (isset($id) && $a_domainOverrides[$id]) {
@@ -58,7 +55,7 @@ if (isset($id) && $a_domainOverrides[$id]) {
 	$pconfig['descr'] = $a_domainOverrides[$id]['descr'];
 }
 
-if ($_POST) {
+if ($_POST['save']) {
 		unset($input_errors);
 		$pconfig = $_POST;
 

src/usr/local/www/services_dnsmasq_edit.php
@@ -39,12 +39,10 @@ if (!is_array($config['dnsmasq']['hosts'])) {
 
 $a_hosts = &$config['dnsmasq']['hosts'];
 
-if (is_numericint($_GET['id'])) {
-	$id = $_GET['id'];
+if (is_numericint($_REQUEST['id'])) {
+	$id = $_REQUEST['id'];
 }
-if (isset($_POST['id']) && is_numericint($_POST['id'])) {
-	$id = $_POST['id'];
-}
+
 
 if (isset($id) && $a_hosts[$id]) {
 	$pconfig['host'] = $a_hosts[$id]['host'];
@@ -54,7 +52,7 @@ if (isset($id) && $a_hosts[$id]) {
 	$pconfig['aliases'] = $a_hosts[$id]['aliases'];
 }
 
-if ($_POST) {
+if ($_POST['save']) {
 	unset($input_errors);
 	$pconfig = $_POST;
 
@@ -165,8 +163,8 @@ if ($_POST) {
 }
 
 // Delete a row in the options table
-if ($_GET['act'] == "delopt") {
-	$idx = $_GET['id'];
+if ($_POST['act'] == "delopt") {
+	$idx = $_POST['id'];
 
 	if ($pconfig['aliases'] && is_array($pconfig['aliases']['item'][$idx])) {
 	   unset($pconfig['aliases']['item'][$idx]);
@@ -174,7 +172,7 @@ if ($_GET['act'] == "delopt") {
 }
 
 // Add an option row
-if ($_GET['act'] == "addopt") {
+if ($_REQUEST['act'] == "addopt") {
     if (!is_array($pconfig['aliases']['item'])) {
         $pconfig['aliases']['item'] = array();
 	}

src/usr/local/www/services_dyndns.php
@@ -35,27 +35,27 @@ if (!is_array($config['dyndnses']['dyndns'])) {
 $a_dyndns = &$config['dyndnses']['dyndns'];
 global $dyndns_split_domain_types;
 
-if ($_GET['act'] == "del") {
-	$conf = $a_dyndns[$_GET['id']];
+if ($_POST['act'] == "del") {
+	$conf = $a_dyndns[$_POST['id']];
 	if (in_array($conf['type'], $dyndns_split_domain_types)) {
 		$hostname = $conf['host'] . "." . $conf['domainname'];
 	} else {
 		$hostname = $conf['host'];
 	}
 	@unlink("{$g['conf_path']}/dyndns_{$conf['interface']}{$conf['type']}" . escapeshellarg($hostname) . "{$conf['id']}.cache");
-	unset($a_dyndns[$_GET['id']]);
+	unset($a_dyndns[$_POST['id']]);
 
 	write_config();
 	services_dyndns_configure();
 
 	header("Location: services_dyndns.php");
 	exit;
-} else if ($_GET['act'] == "toggle") {
-	if ($a_dyndns[$_GET['id']]) {
-		if (isset($a_dyndns[$_GET['id']]['enable'])) {
-			unset($a_dyndns[$_GET['id']]['enable']);
+} else if ($_POST['act'] == "toggle") {
+	if ($a_dyndns[$_POST['id']]) {
+		if (isset($a_dyndns[$_POST['id']]['enable'])) {
+			unset($a_dyndns[$_POST['id']]['enable']);
 		} else {
-			$a_dyndns[$_GET['id']]['enable'] = true;
+			$a_dyndns[$_POST['id']]['enable'] = true;
 		}
 		write_config();
 		services_dyndns_configure();
@@ -64,6 +64,7 @@ if ($_GET['act'] == "del") {
 		exit;
 	}
 }
+
 $pgtitle = array(gettext("Services"), gettext("Dynamic DNS"), gettext("Dynamic DNS Clients"));
 $pglinks = array("", "@self", "@self");
 include("head.inc");
@@ -188,13 +189,13 @@ foreach ($a_dyndns as $dyndns):
 								<a class="fa fa-pencil" title="<?=gettext('Edit service')?>" href="services_dyndns_edit.php?id=<?=$i?>"></a>
 <?php if (isset($dyndns['enable'])) {
 ?>
-								<a class="fa fa-ban" title="<?=gettext('Disable service')?>" href="?act=toggle&amp;id=<?=$i?>"></a>
+								<a class="fa fa-ban" title="<?=gettext('Disable service')?>" href="?act=toggle&amp;id=<?=$i?>" usepost></a>
 <?php } else {
 ?>
-								<a class="fa fa-check-square-o" title="<?=gettext('Enable service')?>" href="?act=toggle&amp;id=<?=$i?>"></a>
+								<a class="fa fa-check-square-o" title="<?=gettext('Enable service')?>" href="?act=toggle&amp;id=<?=$i?>" usepost></a>
 <?php }
 ?>
-								<a class="fa fa-trash" title="<?=gettext('Delete service')?>"	href="services_dyndns.php?act=del&amp;id=<?=$i?>"></a>
+								<a class="fa fa-trash" title="<?=gettext('Delete service')?>"	href="services_dyndns.php?act=del&amp;id=<?=$i?>" usepost></a>
 							</td>
 						</tr>
 <?php

commit 5964d006a20e2c8f93495ce6c23c9c890d4ffa44

Adds some useful config options to DNS Resolver that mitigate a (bug?) with Unifi gear failing to reach the controller due to unqualified DNS lookups to 'unifi.' - adds ability to specify on a host-by-host basis whether to create both qualified and unqualified DNS entries - adds option to auto-track 'domain' according to System > General - can be nice time saver / safety net when you have a lot of overrides within the default domain - Unbound overview page displays these as checkmarks/additional columns, domain will be shown in green if it is set to 'track' - fixes a bug (seemingly caused by commit 13541a81e1173fc02af9af8ab7fe46df2a51007d) that caused an 'host/domain already exists' error to print when trying to edit an existing Host Override - minimal testing done - "it works for me!"
references:
https://www.reddit.com/r/PFSENSE/comments/5vdjel/cant_ping_hostname_just_fqdn_since_233_upgrade/
https://doc.pfsense.org/index.php/2.3.3_New_Features_and_Changes#DNS_.2F_Resolver_.2F_Forwarder

src/etc/inc/system.inc
@@ -345,6 +345,8 @@ function system_hosts_local_entries() {
 
 /* Read host override entries from dnsmasq or unbound */
 function system_hosts_override_entries($dnscfg) {
+	global $config;
+	$syscfg = $config['system'];
 	$hosts = array();
 
 	if (!is_array($dnscfg) ||
@@ -358,11 +360,16 @@ function system_hosts_override_entries($dnscfg) {
 		if ($host['host'] || $host['host'] == "0") {
 			$fqdn .= "{$host['host']}.";
 		}
-		$fqdn .= $host['domain'];
-
+		if ($host['track_system_domain']) {
+			$fqdn .= $syscfg['domain'];
+		} else {
+			$fqdn .= $host['domain'];
+		}
 		$hosts[] = array(
 		    'ipaddr' => $host['ip'],
-		    'fqdn' => $fqdn
+		    'fqdn' => $fqdn,
+		    'name' => $host['host'],
+		    'add_unqualified' => $host['add_unqualified']
 		);
 
 		if (!is_array($host['aliases']) ||

src/etc/inc/unbound.inc
@@ -564,6 +564,9 @@ function unbound_add_host_entries($cfgsubdir = "") {
 			$added_ptr[$host['ipaddr']] = true;
 		}
 		$unbound_entries .= "local-data: \"{$host['fqdn']} {$type} {$host['ipaddr']}\"\n";
+		if ($host['add_unqualified']) {
+			$unbound_entries .= "local-data: \"{$host['name']} {$type} {$host['ipaddr']}\"\n";
+		}
 	}
 
 	// Write out entries

src/usr/local/www/services_unbound.php
@@ -442,6 +442,8 @@ events.push(function() {
 				<tr>
 					<th><?=gettext("Host")?></th>
 					<th><?=gettext("Domain")?></th>
+					<th><?=gettext("Track")?></th>
+					<th><?=gettext("Unqual")?></th>
 					<th><?=gettext("IP")?></th>
 					<th><?=gettext("Description")?></th>
 					<th><?=gettext("Actions")?></th>
@@ -457,7 +459,19 @@ foreach ($a_hosts as $hostent):
 						<?=$hostent['host']?>
 					</td>
 					<td>
-						<?=$hostent['domain']?>
+					<?php
+						if ($hostent['track_system_domain']) {
+							print '<span class="text-success">' . $hostent['domain'] . '</span>';
+						} else {
+							print $hostent['domain'];
+						}
+					?>
+					</td>
+					<td>
+						<?=$hostent['track_system_domain'] ? '<i class="fa fa-check" title="Domain is being dynamically set based on the setting in System &gt; General"></i>' : '&nbsp;'?>
+					</td>
+					<td>
+						<?=$hostent['add_unqualified'] ? '<i class="fa fa-check" title="Both qualified and unqualified entries have been created for this host"></i>' : '&nbsp;'?>
 					</td>
 					<td>
 						<?=$hostent['ip']?>
@@ -481,6 +495,12 @@ foreach ($a_hosts as $hostent):
 					</td>
 					<td>
 						<?=$alias['domain']?>
+					</td>
+					<td>
+						&nbsp;
+					</td>
+					<td>
+						&nbsp;
 					</td>
 					<td>
 						<?=gettext("Alias for ");?><?=$hostent['host'] ? $hostent['host'] . '.' . $hostent['domain'] : $hostent['domain']?>

src/usr/local/www/services_unbound_host_edit.php
@@ -47,6 +47,7 @@ function hosts_sort() {
 }
 
 require_once("guiconfig.inc");
+require_once("system.inc");
 
 if (!is_array($config['unbound']['hosts'])) {
 	$config['unbound']['hosts'] = array();
@@ -58,6 +59,8 @@ $id = $_REQUEST['id'];
 if (isset($id) && $a_hosts[$id]) {
 	$pconfig['host'] = $a_hosts[$id]['host'];
 	$pconfig['domain'] = $a_hosts[$id]['domain'];
+	$pconfig['track_system_domain'] = $a_hosts[$id]['track_system_domain'];
+	$pconfig['add_unqualified'] = $a_hosts[$id]['add_unqualified'];
 	$pconfig['ip'] = $a_hosts[$id]['ip'];
 	$pconfig['descr'] = $a_hosts[$id]['descr'];
 	$pconfig['aliases'] = $a_hosts[$id]['aliases'];
@@ -142,8 +145,8 @@ if ($_POST['save']) {
 		}
 
 		if (($hostent['host'] == $_POST['host']) &&
-		    ($hostent['domain'] == $_POST['domain']) &&
-		    ((is_ipaddrv4($hostent['ip']) && is_ipaddrv4($_POST['ip'])) || (is_ipaddrv6($hostent['ip']) && is_ipaddrv6($_POST['ip'])))) {
+			($hostent['domain'] == $_POST['domain']) &&
+			((is_ipaddrv4($hostent['ip']) && is_ipaddrv4($_POST['ip'])) || (is_ipaddrv6($hostent['ip']) && is_ipaddrv6($_POST['ip'])))) {
 			$input_errors[] = gettext("This host/domain already exists.");
 			break;
 		}
@@ -153,6 +156,8 @@ if ($_POST['save']) {
 		$hostent = array();
 		$hostent['host'] = $_POST['host'];
 		$hostent['domain'] = $_POST['domain'];
+		$hostent['track_system_domain'] = $_POST['track_system_domain'];
+		$hostent['add_unqualified'] = $_POST['add_unqualified'];
 		$hostent['ip'] = $_POST['ip'];
 		$hostent['descr'] = $_POST['descr'];
 		$hostent['aliases']['item'] = $aliases;
@@ -202,6 +207,24 @@ $section->addInput(new Form_Input(
 ))->setHelp('Domain of the host%1$s' .
 			'e.g.: "example.com"', '<br />');
 
+$group = new Form_Group('Options');
+
+$group->add(new Form_Checkbox(
+	'track_system_domain',
+	'track_system_domain',
+	'Automatically track system domain',
+	$pconfig['track_system_domain']
+))->setHelp('The domain will be dynamically updated based on the setting in %1$sSystem &gt; General Setup%2$s','<a href="system.php">','</a>');
+
+$group->add(new Form_Checkbox(
+	'add_unqualified',
+	null,
+	'Also add unqualified (short) hostname',
+	$pconfig['add_unqualified']
+))->setHelp('Creates an entry for e.g. "foo" in addition to "foo.example.com"');
+
+$section->add($group);
+
 $section->addInput(new Form_IpAddress(
 	'ip',
 	'*IP Address',
@@ -221,7 +244,7 @@ if (isset($id) && $a_hosts[$id]) {
 		'id',
 		null,
 		'hidden',
-		$pconfig['id']
+		$id
 	));
 }
 
@@ -283,5 +306,25 @@ $form->addGlobal(new Form_Button(
 
 $form->add($section);
 print($form);
+?>
 
-include("foot.inc");
+<script type="text/javascript">
+//<![CDATA[
+events.push(function() {
+	function setDomainFromSystem() {
+		var s_track = $('#track_system_domain').prop('checked');
+		if (s_track) {
+			$('#domain').val('<?=$config['system']['domain'];?>');
+		} else {
+			$('#domain').val('<?=$pconfig['domain'];?>');
+		}
+	}
+
+	$('#track_system_domain').click(function () {
+		setDomainFromSystem();
+	});
+});
+//]]>
+</script>
+
+<?php include("foot.inc");
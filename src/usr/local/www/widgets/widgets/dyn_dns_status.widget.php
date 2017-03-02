458f5aee020859ed4a81bf8d5ad0ffabb3579155
Renato Botelho <renato@netgate.com>
2017-03-02 10:13:37 -0300

    Use | to separate dyndns IPv4 fields on cache file as done by rfc2136 items and for all IPv6 items

--- src/usr/local/www/widgets/widgets/dyn_dns_status.widget.php
+++ src/usr/local/www/widgets/widgets/dyn_dns_status.widget.php
@@ -117,7 +117,7 @@ if ($_REQUEST['getdyndnsstatus']) {
 		$filename_v6 = "{$g['conf_path']}/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($hostname) . "{$dyndns['id']}_v6.cache";
 		if (file_exists($filename)) {
 			$ipaddr = dyndnsCheckIP($dyndns['interface']);
-			$cached_ip_s = explode(":", file_get_contents($filename));
+			$cached_ip_s = explode("|", file_get_contents($filename));
 			$cached_ip = $cached_ip_s[0];
 
 			if ($ipaddr != $cached_ip) {

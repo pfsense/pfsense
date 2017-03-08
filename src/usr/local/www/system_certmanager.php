ddfa8daa985ce4781bd9dccec012d37cc59e2675
jim-p <jimp@pfsense.org>
2017-03-08 09:57:19 -0500

    Fix display of openssl errors when working with CA or certificate entries. Found this while looking into ticket #7370

--- src/usr/local/www/system_certmanager.php
+++ src/usr/local/www/system_certmanager.php
@@ -436,9 +436,11 @@ if ($_POST) {
 					}
 
 					if (!cert_create($cert, $pconfig['caref'], $pconfig['keylen'], $pconfig['lifetime'], $dn, $pconfig['type'], $pconfig['digest_alg'])) {
+						$input_errors = array();
 						while ($ssl_err = openssl_error_string()) {
-							$input_errors = array();
-							array_push($input_errors, "openssl library returns: " . $ssl_err);
+							if (strpos($ssl_err, 'NCONF_get_string:no value') === false) {
+								array_push($input_errors, "openssl library returns: " . $ssl_err);
+							}
 						}
 					}
 				}
@@ -463,9 +465,11 @@ if ($_POST) {
 					}
 
 					if (!csr_generate($cert, $pconfig['csr_keylen'], $dn, $pconfig['csr_digest_alg'])) {
+						$input_errors = array();
 						while ($ssl_err = openssl_error_string()) {
-							$input_errors = array();
-							array_push($input_errors, "openssl library returns: " . $ssl_err);
+							if (strpos($ssl_err, 'NCONF_get_string:no value') === false) {
+								array_push($input_errors, "openssl library returns: " . $ssl_err);
+							}
 						}
 					}
 				}

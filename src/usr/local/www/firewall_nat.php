4302b1d0fba9bfe48d3f35a99f176a932c9540ca
doktornotor <notordoktor@gmail.com>
2017-03-13 16:30:40 +0100

    Fix broken includes

--- src/usr/local/www/firewall_nat.php
+++ src/usr/local/www/firewall_nat.php
@@ -31,10 +31,11 @@
 ##|-PRIV
 
 require_once("guiconfig.inc");
-require_once("functions.inc");
+require_once("config.inc");
 require_once("filter.inc");
-require_once("shaper.inc");
 require_once("itemid.inc");
+require_once("pfsense-utils.inc");
+require_once("util.inc");
 
 if (!is_array($config['nat']['rule'])) {
 	$config['nat']['rule'] = array();

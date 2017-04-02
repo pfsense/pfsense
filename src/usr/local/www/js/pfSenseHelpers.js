dbd0501b78348c5840be26c15815d95eb6d087f7
doktornotor <notordoktor@gmail.com>
2017-04-02 09:23:45 +0200

    Fix an apparent typo

--- src/usr/local/www/js/pfSenseHelpers.js
+++ src/usr/local/www/js/pfSenseHelpers.js
@@ -664,7 +664,7 @@ $('[id*=restartservice-], [id*=stopservice-], [id*=startservice-]').click(functi
 		name = args[0];
 		mode_zone = args[2];
 		id = args[3];
-	} else if (args[0] == "cpativeportal") {
+	} else if (args[0] == "captiveportal") {
 		action = args[1];
 		name = args[0];
 		mode_zone = args[2];

5d518166ed74769c259c28894c5234e9b83de1ba
Lorenz Schori <lo@znerol.ch>
2017-05-15 18:10:13 +0200

    Check whether tmp/config files exist before using/removing them

--- src/usr/local/sbin/openvpn.learn-address.sh
+++ src/usr/local/sbin/openvpn.learn-address.sh
@@ -16,38 +16,42 @@ if [ -n "$IP" -a "$(basename $IP)" = "$IP" ]; then
 	case "$OP" in
 
 		add|update)
-			# Remove all configs which mention the FQDN
-			/usr/bin/grep -l -null "^local-data: \"${CN}.${DOMAIN} A " $DIR/openvpn.client.*.conf | /usr/bin/xargs -0 /bin/rm
-			/bin/rm -f "$CONF"
-
 			TMPCONF=$(mktemp "$CONF.XXXXXX")
 			TMPSRV=$(mktemp "$CONF.XXXXXX")
 
-			# Add new local-data entry.
-			(
-				echo "local-data-ptr: \"${IP} ${CN}.${DOMAIN}\"" &&
-				echo "local-data: \"${CN}.${DOMAIN} A ${IP}\"" &&
-				echo "local-data: \"${CN} A ${IP}\""
-			) > "$TMPCONF"
+			if [ -f "$TMPCONF" -a -f "$TMPSRV" ]; then
+				# Remove all configs which mention the FQDN
+				/usr/bin/grep -l -null "^local-data: \"${CN}.${DOMAIN} A " $DIR/openvpn.client.*.conf | /usr/bin/xargs -0 /bin/rm
+				/bin/test -f "$CONF" && /bin/rm "$CONF"
 
-			# Check syntax, install configuration and restart unbound.
-			(
-				echo "server:" &&
-				echo "chroot: ${DIR}" &&
-				echo "directory: ${DIR}" &&
-				echo "include: ${TMPCONF}"
-			) > "$TMPSRV"
+				# Add new local-data entry.
+				(
+					echo "local-data-ptr: \"${IP} ${CN}.${DOMAIN}\"" &&
+					echo "local-data: \"${CN}.${DOMAIN} A ${IP}\"" &&
+					echo "local-data: \"${CN} A ${IP}\""
+				) > "$TMPCONF"
 
-			/bin/chmod 644 "$TMPCONF" "$TMPSRV"
-			/usr/local/sbin/unbound-checkconf "$TMPSRV" && /bin/mv "$TMPCONF" "$CONF"
-			/bin/rm -f "$TMPCONF" "$TMPSRV"
+				# Check syntax, install configuration and restart unbound.
+				(
+					echo "server:" &&
+					echo "chroot: ${DIR}" &&
+					echo "directory: ${DIR}" &&
+					echo "include: ${TMPCONF}"
+				) > "$TMPSRV"
 
-			/bin/pkill -HUP -F "$PIDFILE"
+				/bin/chmod 644 "$TMPCONF" "$TMPSRV"
+				/usr/local/sbin/unbound-checkconf "$TMPSRV" && /bin/mv "$TMPCONF" "$CONF"
+
+				/bin/pkill -HUP -F "$PIDFILE"
+			fi
+
+			/bin/test -f "$TMPCONF" && /bin/rm "$TMPCONF"
+			/bin/test -f "$TMPSRV" && /bin/rm "$TMPSRV"
 		;;
 
 		delete)
 			# CN is not set on delete.
-			/bin/rm -f "$CONF" && /bin/pkill -HUP -F "$PIDFILE"
+			/bin/test -f "$CONF" && /bin/rm "$CONF" && /bin/pkill -HUP -F "$PIDFILE"
 		;;
 
 	esac

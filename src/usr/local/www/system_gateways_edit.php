6cadf8b16033122336fe2e0d287437575ab8bfb4
Phil Davis <phil@jankaritech.com>
2017-06-25 08:05:49 +0545

    Issue #7659 foreach warning on adding gateway RELENG_2_3
    Signed-off-by: Phil Davis <phil@jankaritech.com>

--- src/usr/local/www/system_gateways_edit.php
+++ src/usr/local/www/system_gateways_edit.php
@@ -500,14 +500,16 @@ if ($_POST) {
 		if ($_POST['defaultgw'] == "yes" || $_POST['defaultgw'] == "on") {
 			$i = 0;
 			/* remove the default gateway bits for all gateways with the same address family */
-			foreach ($a_gateway_item as $gw) {
-				if ($gateway['ipprotocol'] == $gw['ipprotocol']) {
-					unset($config['gateways']['gateway_item'][$i]['defaultgw']);
-					if ($gw['interface'] != $_POST['interface'] && $gw['defaultgw']) {
-						$reloadif = $gw['interface'];
+			if (is_array($a_gateway_item)) {
+				foreach ($a_gateway_item as $gw) {
+					if ($gateway['ipprotocol'] == $gw['ipprotocol']) {
+						unset($config['gateways']['gateway_item'][$i]['defaultgw']);
+						if ($gw['interface'] != $_POST['interface'] && $gw['defaultgw']) {
+							$reloadif = $gw['interface'];
+						}
 					}
+					$i++;
 				}
-				$i++;
 			}
 			$gateway['defaultgw'] = true;
 		}

297316c784068b807fbe5a5f1e4e766bb1f263ad
Renato Botelho <renato@netgate.com>
2017-09-28 11:58:00 -0300

    Send XG-1537 to prodtrack

--- tools/installer/factory.sh
+++ tools/installer/factory.sh
@@ -187,7 +187,7 @@ cur_model=$(get_cur_model)
 
 if [ -n "${is_adi}" ]; then
 	case "${cur_model}" in
-		SG-2220|SG-2440|XG-2758)
+		SG-2220|SG-2440|XG-2758|XG-1537)
 			selected_model="${cur_model}"
 			;;
 		SG-4860|SG-8860)

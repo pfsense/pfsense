82bd941b132947356f8009d5ac3eef1c8820c11f
PiBa-NL <pba_2k3@yahoo.com>
2017-10-04 00:49:53 +0200

    VirtualIP, FIX, VIPs should have a <uniqueid> property inside the config.xml, however if they are added as 'proxy-arp' or 'other' and then modified to ipalias or carp, the uniqueid stays empty.
    (cherry picked from commit 807160e9db2e7fe2296d7312d602874290186505)

--- src/usr/local/www/firewall_virtual_ip_edit.php
+++ src/usr/local/www/firewall_virtual_ip_edit.php
@@ -256,7 +256,6 @@ if ($_POST) {
 		/* CARP specific fields */
 		if ($_POST['mode'] === "carp") {
 			$vipent['vhid'] = $_POST['vhid'];
-			$vipent['uniqid'] = $_POST['uniqid'];
 			$vipent['advskew'] = $_POST['advskew'];
 			$vipent['advbase'] = $_POST['advbase'];
 
@@ -267,9 +266,14 @@ if ($_POST) {
 			}
 		}
 
-		/* IPalias specific fields */
-		if ($_POST['mode'] === "ipalias") {
-			$vipent['uniqid'] = $_POST['uniqid'];
+		/* IPalias and CARP should have a uniqid */
+		if ($_POST['mode'] === "carp" || $_POST['mode'] === "ipalias") {
+			if (empty($_POST['uniqid'])) {
+				// if we changed a 'parp' or 'other' alias to 'carp'/'ipalias' it needs a uniqid
+				$vipent['uniqid'] = uniqid();
+			} else {
+				$vipent['uniqid'] = $_POST['uniqid'];
+			}
 		}
 
 		/* Common fields */

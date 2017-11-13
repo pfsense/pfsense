973395954b41926e9315355387ccec8d234c59c3
jim-p <jimp@pfsense.org>
2017-11-13 11:04:38 -0500

    Fix saving the CRL choice for OpenVPN clients. Fixes #8088
    (cherry picked from commit 5bd94e5f2e3a4b694e3cfa91d16dab76078c118b)
    (cherry picked from commit 2055285920b2714b7115911e0a0ea723256e5b2f)

--- src/usr/local/www/vpn_openvpn_client.php
+++ src/usr/local/www/vpn_openvpn_client.php
@@ -166,6 +166,7 @@ if ($_GET['act'] == "edit") {
 		if ($pconfig['mode'] != "p2p_shared_key") {
 			$pconfig['caref'] = $a_client[$id]['caref'];
 			$pconfig['certref'] = $a_client[$id]['certref'];
+			$pconfig['crlref'] = $a_client[$id]['crlref'];
 			if ($a_client[$id]['tls']) {
 				$pconfig['tlsauth_enable'] = "yes";
 				$pconfig['tls'] = base64_decode($a_client[$id]['tls']);
@@ -398,6 +399,7 @@ if ($_POST) {
 		if ($tls_mode) {
 			$client['caref'] = $pconfig['caref'];
 			$client['certref'] = $pconfig['certref'];
+			$client['crlref'] = $pconfig['crlref'];
 			if ($pconfig['tlsauth_enable']) {
 				if ($pconfig['autotls_enable']) {
 					$pconfig['tls'] = openvpn_create_key();

8dfe6ef426b064645a849cd8f017f5063cc49752
Renato Botelho <renato@netgate.com>
2017-12-11 13:35:17 -0200

    Use 2.3 repo during build process

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -1227,7 +1227,7 @@ clone_to_staging_area() {
 	# Make sure correct repo is available on tmp dir
 	mkdir -p ${STAGE_CHROOT_DIR}/tmp/pkg-repos
 	setup_pkg_repo \
-		$(eval echo \$PKG_REPO_DEFAULT_${TARGET_ARCH}) \
+		$(eval echo \$PKG_REPO_BUILD_${TARGET_ARCH}) \
 		${STAGE_CHROOT_DIR}/tmp/pkg-repos/repo.conf \
 		${TARGET} \
 		${TARGET_ARCH} \
@@ -1623,7 +1623,7 @@ builder_setup() {
 
 		local _arch=$(uname -m)
 		setup_pkg_repo \
-			$(eval echo \$PKG_REPO_DEFAULT_${TARGET_ARCH}) \
+			$(eval echo \$PKG_REPO_BUILD_${TARGET_ARCH}) \
 			${PKG_REPO_PATH} \
 			${_arch} \
 			${_arch} \

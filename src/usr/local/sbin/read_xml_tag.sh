b8f91b7c6bd16602d49f50c47f4ea28649404c97
Luiz Souza <luiz@netgate.com>
2018-01-05 07:44:37 -0600

    Update the Copyright notice for pfSense.

--- src/usr/local/sbin/read_xml_tag.sh
+++ src/usr/local/sbin/read_xml_tag.sh
@@ -3,7 +3,7 @@
 # read_xml_tag.sh
 #
 # part of pfSense (https://www.pfsense.org)
-# Copyright (c) 2015-2016 Rubicon Communications, LLC (Netgate)
+# Copyright (c) 2015-2018 Rubicon Communications, LLC (Netgate)
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");

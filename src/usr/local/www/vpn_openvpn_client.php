a737700afe9f667e35dd7e09d63e2b3f04061bce
Steve Beaver <sbeaver@netgate.com>
2018-05-09 09:00:16 -0400

    PHP7 - Resolve illegal string offset warning

--- src/usr/local/www/vpn_openvpn_client.php
+++ src/usr/local/www/vpn_openvpn_client.php
@@ -34,6 +34,10 @@ require_once("pkg-utils.inc");
 
 global $openvpn_topologies, $openvpn_tls_modes;
 
+if (!is_array($config['openvpn'])) {
+	$config['openvpn'] = array();
+}
+
 if (!is_array($config['openvpn']['openvpn-client'])) {
 	$config['openvpn']['openvpn-client'] = array();
 }

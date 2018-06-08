8d22f4b19126cff52e6283a8c8de8849ad614992
Renato Botelho <renato@netgate.com>
2018-06-08 16:08:20 -0400

    Do not build hybrid images to serial/ADI

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -869,12 +869,9 @@ create_memstick_image() {
 
 	create_distribution_tarball
 
-	FSLABEL=$(echo ${PRODUCT_NAME} | tr '[:lower:]' '[:upper:]')
-
-	sh ${FREEBSD_SRC_DIR}/release/${TARGET}/mkisoimages.sh -b \
-		${FSLABEL} \
-		${_image_path} \
-		${INSTALLER_CHROOT_DIR}
+	sh ${FREEBSD_SRC_DIR}/release/${TARGET}/make-memstick.sh \
+		${INSTALLER_CHROOT_DIR} \
+		${MEMSTICKPATH}
 
 	if [ ! -f "${_image_path}" ]; then
 		echo "ERROR! memstick image was not built"
@@ -920,12 +917,9 @@ create_memstick_serial_image() {
 
 	create_distribution_tarball
 
-	FSLABEL=$(echo ${PRODUCT_NAME} | tr '[:lower:]' '[:upper:]')
-
-	sh ${FREEBSD_SRC_DIR}/release/${TARGET}/mkisoimages.sh -b \
-		${FSLABEL} \
-		${MEMSTICKSERIALPATH} \
-		${INSTALLER_CHROOT_DIR}
+	sh ${FREEBSD_SRC_DIR}/release/${TARGET}/make-memstick.sh \
+		${INSTALLER_CHROOT_DIR} \
+		${MEMSTICKSERIALPATH}
 
 	if [ ! -f "${MEMSTICKSERIALPATH}" ]; then
 		echo "ERROR! memstick serial image was not built"

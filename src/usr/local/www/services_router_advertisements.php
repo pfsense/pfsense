8701bad838720c00fbe4bb6a23f939b25555a36a
Steve  Beaver <sbeaver@netgate.com>
2018-07-23 08:01:41 -0400

    Fixed #8679

--- src/usr/local/www/services_router_advertisements.php
+++ src/usr/local/www/services_router_advertisements.php
@@ -205,6 +205,10 @@ if ($_POST['save']) {
 	}
 
 	if (!$input_errors) {
+		if (!is_array($config['dhcpdv6'])) {
+			$config['dhcpdv6'] = array();
+		}
+
 		if (!is_array($config['dhcpdv6'][$if])) {
 			$config['dhcpdv6'][$if] = array();
 		}

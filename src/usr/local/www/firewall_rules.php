aa21541ab08cd9b6562441953a73be388f199caa
luckman212 <luke.hamburg@gmail.com>
2018-09-18 13:46:30 -0400

    small fix to hide the "Drag to Change Order" text if roworderdragging is disabled

--- src/usr/local/www/firewall_rules.php
+++ src/usr/local/www/firewall_rules.php
@@ -347,7 +347,7 @@ $columns_in_table = 13;
 <form method="post">
 	<input name="if" id="if" type="hidden" value="<?=$if?>" />
 	<div class="panel panel-default">
-		<div class="panel-heading"><h2 class="panel-title"><?=gettext("Rules (Drag to Change Order)")?></h2></div>
+		<div class="panel-heading"><h2 class="panel-title"><?=gettext("Rules" . (!isset($config['system']['webgui']['roworderdragging']) ? print(" (Drag to Change Order)") : ''))?></h2></div>
 		<div id="mainarea" class="table-responsive panel-body">
 			<table id="ruletable" class="table table-hover table-striped table-condensed" style="overflow-x: 'visible'">
 				<thead>

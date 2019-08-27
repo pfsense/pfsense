acd7e5601ac6bc8b079bd6ea7f8b637a5ec89b5f
jim-p <jimp@netgate.com>
2019-08-27 13:19:30 -0400

    User & Group Manager: Improve Deny Config Write Handling. Fixes #9259
    * Denies all changes if a user has the Deny Config Write privilege.
    Previously it only denied the config write but some OS operations were
    performed.
    * Sets an input error so the user is notified that their attempt failed.
    * Hides the add and delete buttons so read only users don't see the
    option to perform those actions (but are still denied if they submit the
    form through other means)

--- src/usr/local/www/system_groupmanager.php
+++ src/usr/local/www/system_groupmanager.php
@@ -62,7 +62,21 @@ function admin_groups_sort() {
 	usort($a_group, "cpusercmp");
 }
 
-if ($_POST['act'] == "delgroup") {
+/*
+ * Check user privileges to test if the user is allowed to make changes.
+ * Otherwise users can end up in an inconsistent state where some changes are
+ * performed and others denied. See https://redmine.pfsense.org/issues/9259
+ */
+phpsession_begin();
+$guiuser = getUserEntry($_SESSION['Username']);
+$read_only = (is_array($guiuser) && userHasPrivilege($guiuser, "user-config-readonly"));
+phpsession_end();
+
+if (!empty($_POST) && $read_only) {
+	$input_errors = array(gettext("Insufficient privileges to make the requested change (read only)."));
+}
+
+if (($_POST['act'] == "delgroup") && !$read_only) {
 
 	if (!isset($id) || !isset($_REQUEST['groupname']) ||
 	    !isset($a_group[$id]) ||
@@ -86,7 +100,7 @@ if ($_POST['act'] == "delgroup") {
 	syslog($logging_level, "{$logging_prefix}: {$savemsg}");
 }
 
-if ($_POST['act'] == "delpriv") {
+if (($_POST['act'] == "delpriv") && !$read_only) {
 
 	if (!isset($id) || !isset($a_group[$id])) {
 		pfSenseHeader("system_groupmanager.php");
@@ -126,7 +140,7 @@ if ($act == "edit") {
 	}
 }
 
-if (isset($_POST['dellall_x'])) {
+if (isset($_POST['dellall_x']) && !$read_only) {
 
 	$del_groups = $_POST['delete_check'];
 	$deleted_groups = array();
@@ -155,7 +169,7 @@ if (isset($_POST['dellall_x'])) {
 	}
 }
 
-if (isset($_POST['save'])) {
+if (isset($_POST['save']) && !$read_only) {
 	unset($input_errors);
 	$pconfig = $_POST;
 
@@ -265,7 +279,7 @@ if (isset($_POST['save'])) {
 }
 
 function build_priv_table() {
-	global $a_group, $id;
+	global $a_group, $id, $read_only;
 
 	$privhtml = '<div class="table-responsive">';
 	$privhtml .=	'<table class="table table-striped table-hover table-condensed">';
@@ -289,7 +303,9 @@ function build_priv_table() {
 			$user_has_root_priv = true;
 		}
 		$privhtml .=			'</td>';
-		$privhtml .=			'<td><a class="fa fa-trash" title="' . gettext('Delete Privilege') . '"	href="system_groupmanager.php?act=delpriv&amp;groupid=' . $id . '&amp;privid=' . $i . '" usepost></a></td>';
+		if (!$read_only) {
+			$privhtml .=			'<td><a class="fa fa-trash" title="' . gettext('Delete Privilege') . '"	href="system_groupmanager.php?act=delpriv&amp;groupid=' . $id . '&amp;privid=' . $i . '" usepost></a></td>';
+		}
 		$privhtml .=		'</tr>';
 
 	}
@@ -310,7 +326,9 @@ function build_priv_table() {
 	$privhtml .= '</div>';
 
 	$privhtml .= '<nav class="action-buttons">';
-	$privhtml .=	'<a href="system_groupmanager_addprivs.php?groupid=' . $id . '" class="btn btn-success"><i class="fa fa-plus icon-embed-btn"></i>' . gettext("Add") . '</a>';
+	if (!$read_only) {
+		$privhtml .=	'<a href="system_groupmanager_addprivs.php?groupid=' . $id . '" class="btn btn-success"><i class="fa fa-plus icon-embed-btn"></i>' . gettext("Add") . '</a>';
+	}
 	$privhtml .= '</nav>';
 
 	return($privhtml);
@@ -383,7 +401,7 @@ if (!($act == "new" || $act == "edit")) {
 						</td>
 						<td>
 							<a class="fa fa-pencil" title="<?=gettext("Edit group"); ?>" href="?act=edit&amp;groupid=<?=$i?>"></a>
-							<?php if ($group['scope'] != "system"): ?>
+							<?php if (($group['scope'] != "system") && !$read_only): ?>
 								<a class="fa fa-trash"	title="<?=gettext("Delete group")?>" href="?act=delgroup&amp;groupid=<?=$i?>&amp;groupname=<?=$group['name']?>" usepost></a>
 							<?php endif;?>
 						</td>
@@ -398,10 +416,12 @@ if (!($act == "new" || $act == "edit")) {
 </div>
 
 <nav class="action-buttons">
+	<?php if (!$read_only): ?>
 	<a href="?act=new" class="btn btn-success btn-sm">
 		<i class="fa fa-plus icon-embed-btn"></i>
 		<?=gettext("Add")?>
 	</a>
+	<?php endif; ?>
 </nav>
 <?php
 	include('foot.inc');

0ffbf1e74e5a92b90e8cae562b8cf039c086ef9c
Renato Botelho do Couto <renato@netgate.com>
2019-09-03 10:18:52 -0300

    Fix PHP warning
    (cherry picked from commit b94eb4b90540dfb294376d6578aa9e9cbec63be9)

--- src/usr/local/www/diag_dump_states.php
+++ src/usr/local/www/diag_dump_states.php
@@ -226,7 +226,7 @@ print $form;
 		$states = count($res);
 	}
 
-	for ($i = 0; $i < $states; $i++) {
+	for ($i = 0; $i < $states; $i++):
 		$info = $res[$i]['src'];
 		$srcip = get_ip($res[$i]['src']);
 		$dstip = get_ip($res[$i]['dst']);
@@ -258,8 +258,8 @@ print $form;
 								title="<?=sprintf(gettext('Remove all state entries from %1$s to %2$s'), $srcip, $killdstip);?>"></a>
 						</td>
 					</tr>
-<?
-	}
+<?php
+	endfor;
 ?>
 				</tbody>
 			</table>

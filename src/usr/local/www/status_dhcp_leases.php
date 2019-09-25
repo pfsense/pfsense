9297ad6504618c5ffcee9f8fe02535cb33f570c9
jim-p <jimp@netgate.com>
2019-09-25 15:23:59 -0400

    Add search/filter to DHCP/DHCPv6 leases, ARP, and NDP. Implements #9791

--- src/usr/local/www/status_dhcp_leases.php
+++ src/usr/local/www/status_dhcp_leases.php
@@ -141,6 +141,49 @@ if (count($leases['failover']) > 0):
 /* only print pool status when we have one */
 endif;
 ?>
+
+<div class="panel panel-default" id="search-panel">
+	<div class="panel-heading">
+		<h2 class="panel-title">
+			<?=gettext('Search')?>
+			<span class="widget-heading-icon pull-right">
+				<a data-toggle="collapse" href="#search-panel_panel-body">
+					<i class="fa fa-plus-circle"></i>
+				</a>
+			</span>
+		</h2>
+	</div>
+	<div id="search-panel_panel-body" class="panel-body collapse in">
+		<div class="form-group">
+			<label class="col-sm-2 control-label">
+				<?=gettext("Search term")?>
+			</label>
+			<div class="col-sm-5"><input class="form-control" name="searchstr" id="searchstr" type="text"/></div>
+			<div class="col-sm-2">
+				<select id="where" class="form-control">
+					<option value="1"><?=gettext("IP Address")?></option>
+					<option value="2"><?=gettext("MAC Address")?></option>
+					<option value="3"><?=gettext("Client ID")?></option>
+					<option value="4"><?=gettext("Hostname")?></option>
+					<option value="5"><?=gettext("Description")?></option>
+					<option value="6"><?=gettext("Start")?></option>
+					<option value="7"><?=gettext("End")?></option>
+					<option value="8"><?=gettext("Online")?></option>
+					<option value="9"><?=gettext("Lease Type")?></option>
+					<option value="10" selected><?=gettext("All")?></option>
+				</select>
+			</div>
+			<div class="col-sm-3">
+				<a id="btnsearch" title="<?=gettext("Search")?>" class="btn btn-primary btn-sm"><i class="fa fa-search icon-embed-btn"></i><?=gettext("Search")?></a>
+				<a id="btnclear" title="<?=gettext("Clear")?>" class="btn btn-info btn-sm"><i class="fa fa-undo icon-embed-btn"></i><?=gettext("Clear")?></a>
+			</div>
+			<div class="col-sm-10 col-sm-offset-2">
+				<span class="help-block"><?=gettext('Enter a search string or *nix regular expression to filter entries.')?></span>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="panel panel-default">
 	<div class="panel-heading"><h2 class="panel-title"><?=gettext('Leases')?></h2></div>
 	<div class="panel-body table-responsive">
@@ -160,7 +203,7 @@ endif;
 					<th data-sortable="false"><?=gettext("Actions")?></th>
 				</tr>
 			</thead>
-			<tbody>
+			<tbody id="leaselist">
 <?php
 $dhcp_leases_subnet_counter = array(); //array to sum up # of leases / subnet
 $iflist = get_configured_interface_with_descr(); //get interface descr for # of leases
@@ -312,6 +355,75 @@ else:
 	<a class="btn btn-info" href="status_dhcp_leases.php?all=0"><i class="fa fa-minus-circle icon-embed-btn"></i><?=gettext("Show active and static leases only")?></a>
 <?php else: ?>
 	<a class="btn btn-info" href="status_dhcp_leases.php?all=1"><i class="fa fa-plus-circle icon-embed-btn"></i><?=gettext("Show all configured leases")?></a>
-<?php endif;
+<?php endif; ?>
 
-include("foot.inc");
+<script type="text/javascript">
+//<![CDATA[
+events.push(function() {
+	// Make these controls plain buttons
+	$("#btnsearch").prop('type', 'button');
+	$("#btnclear").prop('type', 'button');
+
+	// Search for a term in the entry name and/or dn
+	$("#btnsearch").click(function() {
+		var searchstr = $('#searchstr').val().toLowerCase();
+		var table = $("#leaselist");
+		var where = $('#where').val();
+
+		table.find('tr').each(function (i) {
+			var $tds = $(this).find('td'),
+				ipaddr   = $tds.eq(1).text().trim().toLowerCase();
+				macaddr  = $tds.eq(2).text().trim().toLowerCase();
+				clientid = $tds.eq(3).text().trim().toLowerCase(),
+				hostname = $tds.eq(4).text().trim().toLowerCase();
+				descr    = $tds.eq(5).text().trim().toLowerCase();
+				start    = $tds.eq(6).text().trim().toLowerCase();
+				end      = $tds.eq(7).text().trim().toLowerCase();
+				online   = $tds.eq(8).text().trim().toLowerCase();
+				lease    = $tds.eq(9).text().trim().toLowerCase();
+
+			regexp = new RegExp(searchstr);
+			if (searchstr.length > 0) {
+				if (!(regexp.test(ipaddr)   && ((where == 1) || (where == 10))) &&
+				    !(regexp.test(macaddr)  && ((where == 2) || (where == 10))) &&
+				    !(regexp.test(clientid) && ((where == 3) || (where == 10))) &&
+				    !(regexp.test(hostname) && ((where == 4) || (where == 10))) &&
+				    !(regexp.test(descr)    && ((where == 5) || (where == 10))) &&
+				    !(regexp.test(start)    && ((where == 6) || (where == 10))) &&
+				    !(regexp.test(end)      && ((where == 7) || (where == 10))) &&
+				    !(regexp.test(online)   && ((where == 8) || (where == 10))) &&
+				    !(regexp.test(lease)    && ((where == 9) || (where == 10)))
+				    ) {
+					$(this).hide();
+				} else {
+					$(this).show();
+				}
+			} else {
+				$(this).show();	// A blank search string shows all
+			}
+		});
+	});
+
+	// Clear the search term and unhide all rows (that were hidden during a previous search)
+	$("#btnclear").click(function() {
+		var table = $("#leaselist");
+
+		$('#searchstr').val("");
+
+		table.find('tr').each(function (i) {
+			$(this).show();
+		});
+	});
+
+	// Hitting the enter key will do the same as clicking the search button
+	$("#searchstr").on("keyup", function (event) {
+		if (event.keyCode == 13) {
+			$("#btnsearch").get(0).click();
+		}
+	});
+
+});
+//]]>
+</script>
+
+<?php include("foot.inc");

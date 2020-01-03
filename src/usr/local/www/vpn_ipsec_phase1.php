397b5f073cc05602a6746fb208d808e47b5a177f
Viktor Gurov <viktor@netgate.com>
2020-01-03 12:04:34 +0300

    fix double proposal on vpn_ipsec_phase1.php

--- src/usr/local/www/vpn_ipsec_phase1.php
+++ src/usr/local/www/vpn_ipsec_phase1.php
@@ -859,7 +859,6 @@ foreach($pconfig['encryption']['item'] as $key => $p1enc) {
 	$counter += 1;
 }
 $section->addInput(new Form_StaticText('', ''))->setHelp('Note: Blowfish, 3DES, CAST128, MD5, SHA1, and DH groups 1, 2, 22, 23, and 24 provide weak security and should be avoided.');
-$form->add($section);
 
 $btnaddopt = new Form_Button(
 	'algoaddrow',

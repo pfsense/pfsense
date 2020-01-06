e4fe4666b2bc09e8ec0adf4a5b730e7177a5e328
Ronald Schellberg <1888300+rschell@users.noreply.github.com>
2020-01-06 07:39:21 -0700

    Fix typo on last rename find loop
    Incomplete deletion

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -1582,7 +1582,7 @@ poudriere_rename_ports() {
 		fi
 
 		if [ -d ${_pdir}/${_pname}/files ]; then
-			for fd in $(find ${_pdir}/${_pname}/files d -name '*pfSense*'); do
+			for fd in $(find ${_pdir}/${_pname}/files -name '*pfSense*'); do
 				local _fddir=$(dirname ${fd})
 				local _fdname=$(echo $(basename ${fd}) | sed "s,pfSense,${PRODUCT_NAME},")
 

86c560d985b03d421f8b572c33f8e02b2f08ea56
Viktor Gurov <viktor@netgate.com>
2020-02-01 09:44:53 +0300

    Remove bogus warning on limiter/shaper deletion. Issue #9334

--- src/usr/local/www/firewall_shaper.php
+++ src/usr/local/www/firewall_shaper.php
@@ -430,7 +430,7 @@ if (!$dfltmsg && $sform)  {
 			$queue ? 'Delete this queue':'Disable shaper on interface',
 			$url,
 			'fa-trash'
-		))->addClass('btn-danger');
+		))->addClass('btn-danger nowarn');
 
 	}
 

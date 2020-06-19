e2e4c0d5452f36a3e468e43a78f2cc5316e34174
Steve Beaver <sbeaver@netgate.com>
2020-06-19 09:41:09 -0400

    Updated jQuery to 3.5.1 - jQuery-ui does not need to be updated

--- src/usr/local/www/csrf_error.php
+++ src/usr/local/www/csrf_error.php
@@ -107,7 +107,7 @@ $logincssfile = "#770101";
 	});
 	//]]>
 	</script>
-	<script src="/vendor/jquery/jquery-3.4.1.min.js?v=<?=filemtime('/usr/local/www/vendor/jquery/jquery-3.4.1.min.js')?>"></script>
+	<script src="/vendor/jquery/jquery-3.5.1.min.js?v=<?=filemtime('/usr/local/www/vendor/jquery/jquery-3.5.1.min.js')?>"></script>
 	<script src="/vendor/jquery-ui/jquery-ui-1.12.1.min.js?v=<?=filemtime('/usr/local/www/vendor/jquery-ui/jquery-ui-1.12.1.min.js')?>"></script>
 	<script src="/vendor/bootstrap/js/bootstrap.min.js?v=<?=filemtime('/usr/local/www/vendor/bootstrap/js/bootstrap.min.js')?>"></script>
 	<script src="/js/pfSense.js?v=<?=filemtime('/usr/local/www/js/pfSense.js')?>"></script>

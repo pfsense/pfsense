e912f0cf6d3be61cf810a9a752870253ee77e473
Viktor G <viktor@netgate.com>
2020-06-22 16:11:24 +0300

    Improve gateways popup. Implements #885

--- src/usr/local/www/firewall_rules.php
+++ src/usr/local/www/firewall_rules.php
@@ -778,13 +778,17 @@ foreach ($a_filter as $filteri => $filterent):
 							<?php endif; ?>
 						</td>
 						<td>
-							<?php if (isset($config['interfaces'][$filterent['gateway']]['descr'])):?>
-								<?=str_replace('_', '_<wbr>', htmlspecialchars($config['interfaces'][$filterent['gateway']]['descr']))?>
+							<?php if (isset($filterent['gateway'])): ?>
+								<span data-toggle="popover" data-trigger="hover focus" title="<?=gettext('Gateways details')?>" data-content="<?=gateway_info_popup($filterent['gateway'])?>" data-html="true">
 							<?php else: ?>
-							<span data-toggle="popover" data-trigger="hover focus" title="<?=gettext('Gateways details')?>" data-content="<?=gateway_info_popup($filterent['gateway'])?>" data-html="true">
-								<?=htmlspecialchars(pprint_port($filterent['gateway']))?>
-							</span>
+								<span>
 							<?php endif; ?>
+								<?php if (isset($config['interfaces'][$filterent['gateway']]['descr'])): ?>
+									<?=str_replace('_', '_<wbr>', htmlspecialchars($config['interfaces'][$filterent['gateway']]['descr']))?>
+								<?php else: ?>
+									<?=htmlspecialchars(pprint_port($filterent['gateway']))?>
+								<?php endif; ?>
+							</span>
 						</td>
 						<td>
 							<?php

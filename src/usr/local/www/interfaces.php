fd3af9eb111f38144ccf45a55612d5b3372d45da
Viktor G <viktor@netgate.com>
2020-10-02 14:28:58 +0300

    Rebind syslogd on interface change. Issue #9660

--- src/usr/local/www/interfaces.php
+++ src/usr/local/www/interfaces.php
@@ -494,6 +494,11 @@ if ($_POST['apply']) {
 		if (is_subsystem_dirty('staticroutes') && (system_routing_configure() == 0)) {
 			clear_subsystem_dirty('staticroutes');
 		}
+
+		init_config_arr(array('syslog'));
+		if (isset($config['syslog']['enable']) && ($ifapply == $config['syslog']['sourceip'])) {
+			system_syslogd_start();
+		}
 	}
 	@unlink("{$g['tmp_path']}/.interfaces.apply");
 } else if ($_POST['save']) {

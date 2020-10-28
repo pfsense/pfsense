7ce95691a6821ffbc5240a5ac3d338a2b5ea621a
Steve Beaver <sbeaver@netgate.com>
2020-10-28 15:03:09 -0400

    Completed 'row helper' stuff to allow peers to be added and deleted

--- src/usr/local/www/vpn_wg_edit.php
+++ src/usr/local/www/vpn_wg_edit.php
@@ -153,21 +153,21 @@ foreach ($pconfig['peers'] as $peer) {
 		'Description',
 		'text',
 		$peer['descr']
-	))->setHelp(($is_last_peer) ? $peer_help:null);
+	))->setHelp($peer_help);
 
 	$group->add(new Form_Input(
 		'endpoint' . $peer_num,
 		'Endpoint',
 		'text',
 		$peer['endpoint']
-	))->setHelp(($is_last_peer) ? $dnsgw_help:null)->setWidth(3);
+	))->setHelp($dnsgw_help)->setWidth(3);
 
 	$group->add(new Form_Input(
-		'persistentkeepalive',
+		'persistentkeepalive' . $peer_num,
 		'Keepalive',
 		'PDF_pcos_get_number(p, doc, path)',
 		$peer['persistentkeepalive']
-	))->setHelp(($is_last_peer) ? $ka_help:null)->setWidth(1);
+	))->setHelp($ka_help)->setWidth(1);
 
 	$group->add(new Form_Button(
 		'killpeer' . $peer_num,
@@ -184,7 +184,7 @@ foreach ($pconfig['peers'] as $peer) {
 		'Public key',
 		'text',
 		$peer['publickey']
-	))->setHelp(($is_last_peer) ? $dnshost_help:null)->setWidth(4);
+	))->setHelp($dnshost_help)->setWidth(4);
 
 
 	$group2->add(new Form_Input(
@@ -192,7 +192,7 @@ foreach ($pconfig['peers'] as $peer) {
 		'Allowed IPs',
 		'text',
 		$peer['allowedips']
-	))->setHelp(($is_last_peer) ? $aips_help:null);
+	))->setHelp($aips_help);
 
 	$section2->add($group);
 	$section2->add($group2);
@@ -200,7 +200,7 @@ foreach ($pconfig['peers'] as $peer) {
 }
 
 $section2->addInput(new Form_Button(
-	'addrow',
+	'add2rows',
 	'Add peer',
 	null,
 	'fa-plus'
@@ -233,8 +233,99 @@ events.push(function() {
 			$(target).remove();
 			hideDeleteBtn();
 		}
-
 	})
+
+	// These are action buttons, not submit buttons
+	$('#add2rows').prop('type','button');
+
+	// on click . .
+	$('#add2rows').click(function() {
+		add2rows();
+	});
+
+	// Similar to the functions in pfSenseHelpers.js but we add two rows, not just one so the code changes somewhat
+	//Since this is the only page that uses double rows, the code is here rather than in the helper file
+	function add2rows() {
+		// Find the last repeatable group
+		var lastRepeatableGroup = $('.repeatable:last').prev();
+		var lastRepeatableGroup2 = $('.repeatable:last');
+
+		// If the number of repeats exceeds the maximum, do not add another clone
+		if ($('.repeatable').length >= lastRepeatableGroup.attr('max_repeats')) {
+			// Alert user if alert message is specified
+			if (typeof lastRepeatableGroup.attr('max_repeats_alert') !== 'undefined') {
+				alert(lastRepeatableGroup.attr('max_repeats_alert'));
+			}
+
+			return;
+		}
+
+		// Clone it
+		var newGroup = lastRepeatableGroup.clone();
+		var newGroup2 = lastRepeatableGroup2.clone();
+
+		// Increment the suffix number for each input element in the new group(s)
+		bump_ids(newGroup);
+		bump_ids(newGroup2);
+
+		// Increment the peer_group_n class (used for deleting a row)
+		var cn = $(newGroup).prop('className').split(' ')[2];
+		var cn2 = bumpStringInt(cn);
+		$(newGroup.removeClass(cn));
+		$(newGroup).addClass(cn2);
+		$(newGroup2).removeClass(cn);
+		$(newGroup2).addClass(cn2);
+
+		// Insert the updated/cloned row
+		$(lastRepeatableGroup2).after(newGroup2);
+		$(lastRepeatableGroup2).after(newGroup);
+
+		// Delete any help text from the group we have cloned
+		$(lastRepeatableGroup).find('.help-block').each(function() {
+			if ((typeof retainhelp) == "undefined")
+				$(this).remove();
+		});
+
+		checkLastRow();
+
+
+		// Now that we are no longer cloning the event handlers, we need to remove and re-add after a new row
+		// has been added to the table
+		$('[id^=killpeer]').unbind();
+		$('[id^=killpeer]').click(function(event) {
+			event.preventDefault();
+			if (confirm('Are you sure you want to delete this peer?')) {
+				var row = event.target.id.slice(8);
+				var target = '.peer_group_' + row
+				$(target).remove();
+				hideDeleteBtn();
+			}
+		});
+	}
+
+	// Increment the suffix of every element in the row(s)
+	function bump_ids(ng) {
+		$(ng).find('input').each(function() {
+			$(this).prop("id", bumpStringInt(this.id));
+			$(this).prop("name", bumpStringInt(this.name));
+			$(this).val('');
+		});
+
+		// Increment the suffix number for the deleterow button element in the new group
+		$(ng).find('[id^=killpeer]').each(function() {
+			$(this).prop("id", bumpStringInt(this.id));
+			$(this).prop("name", bumpStringInt(this.name));
+		});
+
+		// Increment the label
+		$(ng).find('span:first').each(function() {
+			var label = $(this).html();
+			if (label.startsWith('Peer')) {
+				label = bumpStringInt(label);
+				$(this).parent().find("span").html(label);
+			}
+		});
+	}
 });
 //]]>
 </script>

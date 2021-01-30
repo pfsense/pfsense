f1895d6a0c909553e918a04983faad29fff51846
Viktor G <viktor@netgate.com>
2021-01-30 10:03:31 +0300

    Sanitize DHCP DDNS keys. Issue #11342

--- src/usr/local/www/status.php
+++ src/usr/local/www/status.php
@@ -65,7 +65,7 @@ $filtered_tags = array(
 	'accountkey', 'authorizedkeys', 'auth_pass',
 	'auth_server_shared_secret', 'auth_server_shared_secret2', 'auth_user',
 	'barnyard_dbpwd', 'bcrypt-hash', 'cert_key', 'community', 'crypto_password',
-	'crypto_password2', 'dns_nsupdatensupdate_key', 'encryption_password',
+	'crypto_password2', 'dns_nsupdatensupdate_key', 'ddnsdomainkey', 'encryption_password',
 	'etpro_code', 'etprocode', 'gold_encryption_password', 'gold_password',
 	'influx_pass', 'ipsecpsk', 'ldap_bindpw', 'ldapbindpass', 'ldap_pass',
 	'lighttpd_ls_password', 'maxmind_geoipdb_key', 'maxmind_key', 'md5-hash',
@@ -331,8 +331,8 @@ defCmdT("DNS-Resolver Access Lists", "/bin/cat /var/unbound/access_lists.conf");
 defCmdT("DNS-Resolver Configuration", "/bin/cat /var/unbound/unbound.conf");
 defCmdT("DNS-Resolver Domain Overrides", "/bin/cat /var/unbound/domainoverrides.conf");
 defCmdT("DNS-Resolver Host Overrides", "/bin/cat /var/unbound/host_entries.conf");
-defCmdT("DHCP-IPv4 Configuration", "/bin/cat /var/dhcpd/etc/dhcpd.conf");
-defCmdT("DHCP-IPv6-Configuration", "/bin/cat /var/dhcpd/etc/dhcpdv6.conf");
+defCmdT("DHCP-IPv4 Configuration", '/usr/bin/sed "s/\([[:blank:]]secret \).*/\1<redacted>/" /var/dhcpd/etc/dhcpd.conf');
+defCmdT("DHCP-IPv6-Configuration", '/usr/bin/sed "s/\([[:blank:]]secret \).*/\1<redacted>/" /var/dhcpd/etc/dhcpdv6.conf');
 defCmdT("IPsec-strongSwan Configuration", '/usr/bin/sed "s/\([[:blank:]]secret = \).*/\1<redacted>/" /var/etc/ipsec/strongswan.conf');
 defCmdT("IPsec-Configuration", '/usr/bin/sed "s/\([[:blank:]]secret = \).*/\1<redacted>/" /var/etc/ipsec/swanctl.conf');
 defCmdT("IPsec-Status-Statistics", "/usr/local/sbin/swanctl --stats --pretty");

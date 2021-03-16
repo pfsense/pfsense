c632527d6047d05a5b691dfdbabded021ca96f57
Viktor G <viktor@netgate.com>
2021-03-16 19:46:34 +0300

    Do not try to display too large PHP_errors.log file. Fixes #11685

--- src/usr/local/www/crash_reporter.php
+++ src/usr/local/www/crash_reporter.php
@@ -73,9 +73,13 @@ if ($_POST['Submit'] == "No") {
 } else {
 	$crash_reports = $crash_report_header;
 	if (system_has_php_errors()) {
-		$php_errors = file_get_contents("/tmp/PHP_errors.log");
-		$crash_reports .= "\nPHP Errors:\n";
-		$crash_reports .= $php_errors . "\n\n";
+		if (filesize("/tmp/PHP_errors.log") < FILE_SIZE) {
+			$php_errors = file_get_contents("/tmp/PHP_errors.log");
+			$crash_reports .= "\nPHP Errors:\n";
+			$crash_reports .= $php_errors . "\n\n";
+		} else {
+			$crash_reports .= "\n/tmp/PHP_errors.log file is too large to display.\n";
+		}
 	} else {
 		$crash_reports .= "\nNo PHP errors found.\n";
 	}

df945787c7b7784444381eabeeaf519361cbc2ec
Luiz Otavio O Souza <luiz@netgate.com>
2021-08-26 12:21:16 -0300

    Rename the Netgate devices.
    XG-15xx -> 15xx
    SG-5100 -> Netgate-5100

--- src/usr/local/www/widgets/widgets/thermal_sensors.widget.php
+++ src/usr/local/www/widgets/widgets/thermal_sensors.widget.php
@@ -30,7 +30,7 @@ require_once("system.inc");
 if (isset($_REQUEST["getThermalSensorsData"])) {
 
 	$specplatform = system_identify_specific_platform();
-	if ($specplatform['name'] == 'SG-5100') {
+	if ($specplatform['name'] == '5100') {
 		$_gb = exec("/sbin/sysctl -q dev.cpu | /usr/bin/grep temperature | /usr/bin/sort", $dfout);
 	} else {
 		$_gb = exec("/sbin/sysctl -q hw.acpi.thermal dev.cpu dev.t5nex dev.armada_thermal dev.cordbuc | /usr/bin/grep 'temperature:'", $dfout);

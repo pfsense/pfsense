40d3c9dae950cb4ed2736a158487ece699f599c3
Christian McDonald <cmcdonald@netgate.com>
2021-09-01 12:25:21 -0500

    Adds the TreeGrid plugin for jQuery to the pfSense UI stack.(https://github.com/maxazan/jquery-treegrid)

---
+++ src/usr/local/www/vendor/jquery-treegrid/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> */\n'
+      },
+      build: {
+        src: 'js/jquery.treegrid.js',
+        dest: 'js/jquery.treegrid.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
---
+++ src/usr/local/www/vendor/jquery-treegrid/README.md
@@ -0,0 +1,21 @@
+TreeGrid plugin for jQuery
+==========
+
+See more information at http://maxazan.github.io/jquery-treegrid
+
+## Installation
+
+
+### Using npm
+```
+npm install jquery-treegrid
+```
+
+### Using bower
+```
+bower install jquery-treegrid
+```
+
+### From source
+
+Download [source](https://github.com/maxazan/jquery-treegrid/archive/master.zip) from github.com

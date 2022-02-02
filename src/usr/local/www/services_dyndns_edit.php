c80e6c148af81af4e0126a3ca4d3585fef9fcefc
Viktor G <viktor@netgate.com>
2022-02-02 17:35:28 +0300

    GoDaddy DDNS wildcard support. Fixes #12750

--- src/usr/local/www/services_dyndns_edit.php
+++ src/usr/local/www/services_dyndns_edit.php
@@ -121,14 +121,12 @@ if ($_POST['save'] || $_POST['force']) {
 
 	if (isset($_POST['host']) && in_array("host", $reqdfields)) {
 		$allow_wildcard = false;
-		/* Namecheap can have a @. and *. in hostname */
-		if (($pconfig['type'] == "namecheap") && ($_POST['host'] == '*.' || $_POST['host'] == '*' || $_POST['host'] == '@.' || $_POST['host'] == '@')) {
+		/* Namecheap and GoDaddy can have a @. and *. in hostname */
+		if ((($pconfig['type'] == "godaddy") || ($pconfig['type'] == "godaddy-v6") || ($pconfig['type'] == "namecheap")) && ($_POST['host'] == '*.' || $_POST['host'] == '*' || $_POST['host'] == '@.' || $_POST['host'] == '@')) {
 			$host_to_check = $_POST['domainname'];
 		} elseif (($pconfig['type'] == "cloudflare") || ($pconfig['type'] == "cloudflare-v6")) {
 			$host_to_check = $_POST['host'] == '@' ? $_POST['domainname'] : ( $_POST['host'] . '.' . $_POST['domainname'] );
 			$allow_wildcard = true;
-		} elseif ((($pconfig['type'] == "godaddy") || ($pconfig['type'] == "godaddy-v6")) && ($_POST['host'] == '@.' || $_POST['host'] == '@')) {
-			$host_to_check = $_POST['domainname'];
 		} elseif (($pconfig['type'] == "digitalocean" || $pconfig['type'] == "digitalocean-v6") && ($_POST['host'] == '@.' || $_POST['host'] == '@')) {
 			$host_to_check = $_POST['domainname'];
 		} elseif (($pconfig['type'] == "linode") || ($pconfig['type'] == "linode-v6") || ($pconfig['type'] == "gandi-livedns") || ($pconfig['type'] == "gandi-livedns-v6") || ($pconfig['type'] == "yandex") || ($pconfig['type'] == "yandex-v6")) {

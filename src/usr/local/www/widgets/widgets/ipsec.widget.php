5e3d0f781f29f7c5292a5a41d00d6bd588493d39
Viktor G <viktor@netgate.com>
2022-02-03 09:48:24 +0300

    IPSec widget default tab option. Feature #2456

--- src/usr/local/www/widgets/widgets/ipsec.widget.php
+++ src/usr/local/www/widgets/widgets/ipsec.widget.php
@@ -37,6 +37,26 @@ require_once("ipsec.inc");
 init_config_arr(array('ipsec', 'phase1'));
 init_config_arr(array('ipsec', 'phase2'));
 
+$ipsec_widget_tabs = array(
+	'overview' => gettext('Overview'),
+	'tunnel' => gettext('Tunnels'),
+	'mobile' => gettext('Mobile')
+);
+
+if ($_POST['widgetkey']) {
+	if (!is_array($user_settings['widgets'][$_POST['widgetkey']])) {
+		$user_settings['widgets'][$_POST['widgetkey']] = array();
+	}
+
+	if (isset($_POST['default_tab']) && array_key_exists($_POST['default_tab'], $ipsec_widget_tabs)) {
+		$user_settings['widgets'][$_POST['widgetkey']]['default_tab'] = $_POST['default_tab'];
+		save_widget_settings($_SESSION['Username'], $user_settings['widgets'], gettext("Updated IPsec widget settings via dashboard."));
+	}
+
+	header("Location: /");
+	exit(0);
+}
+
 // Compose the table contents and pass it back to the ajax caller
 if ($_REQUEST && $_REQUEST['ajax']) {
 
@@ -224,10 +244,21 @@ if ($_REQUEST && $_REQUEST['ajax']) {
 $widgetkey_nodash = str_replace("-", "", $widgetkey);
 
 if (ipsec_enabled()) {
+	if (is_array($user_settings['widgets'][$widgetkey]) &&
+	    isset($user_settings['widgets'][$widgetkey]['default_tab'])) {
+		$activetab = $user_settings['widgets'][$widgetkey]['default_tab'];
+	} else {
+		$activetab = 'overview';
+	}
 	$tab_array = array();
-	$tab_array[] = array(gettext("Overview"), true, htmlspecialchars($widgetkey_nodash) . "-Overview");
-	$tab_array[] = array(gettext("Tunnels"), false, htmlspecialchars($widgetkey_nodash) . "-tunnel");
-	$tab_array[] = array(gettext("Mobile"), false, htmlspecialchars($widgetkey_nodash) . "-mobile");
+	foreach ($ipsec_widget_tabs as $tabname => $tabdescr) {
+		if ($tabname == $activetab) {
+			$active = true;
+		} else {
+			$active = false;
+		}
+		$tab_array[] = array($tabdescr, $active, htmlspecialchars($widgetkey_nodash) . '-' . $tabname);
+	}
 
 	display_widget_tabs($tab_array);
 }
@@ -236,7 +267,7 @@ $mobile = ipsec_dump_mobile();
 $widgetperiod = isset($config['widgets']['period']) ? $config['widgets']['period'] * 1000 : 10000;
 
 if (ipsec_enabled()): ?>
-<div id="<?=htmlspecialchars($widgetkey_nodash)?>-Overview" style="display:block;"  class="table-responsive">
+<div id="<?=htmlspecialchars($widgetkey_nodash)?>-overview" style="display:<?=(($activetab == 'overview') ? 'block': 'none')?>;"  class="table-responsive">
 	<table class="table table-striped table-hover">
 		<thead>
 		<tr>
@@ -250,7 +281,7 @@ if (ipsec_enabled()): ?>
 		</tbody>
 	</table>
 </div>
-<div class="table-responsive" id="<?=htmlspecialchars($widgetkey_nodash)?>-tunnel" style="display:none;">
+<div class="table-responsive" id="<?=htmlspecialchars($widgetkey_nodash)?>-tunnel" style="display:<?=(($activetab == 'tunnel') ? 'block': 'none')?>;">
 	<table class="table table-striped table-hover">
 	<thead>
 	<tr>
@@ -266,7 +297,7 @@ if (ipsec_enabled()): ?>
 	</table>
 </div>
 
-	<div id="<?=htmlspecialchars($widgetkey_nodash)?>-mobile" style="display:none;" class="table-responsive">
+	<div id="<?=htmlspecialchars($widgetkey_nodash)?>-mobile" style="display:<?=(($activetab == 'mobile') ? 'block': 'none')?>;" class="table-responsive">
 		<table class="table table-striped table-hover">
 <?php if (is_array($mobile['pool'])): ?>
 		<thead>
@@ -297,10 +328,42 @@ if (ipsec_enabled()): ?>
 <?php endif;
 
 ?>
+<!-- close the body we're wrapped in and add a configuration-panel -->
+</div><div id="<?=$widget_panel_footer_id?>" class="panel-footer collapse">
+<form action="/widgets/widgets/ipsec.widget.php" method="post" class="form-horizontal">
+	<div class="form-group">
+		<label class="col-sm-4 control-label"><?=gettext('Default tab')?></label>
+		<input type="hidden" name="widgetkey" value="<?=htmlspecialchars($widgetkey); ?>">
+		<div class="col-sm-6">
+<?php
+	foreach ($ipsec_widget_tabs as $tabname => $tabdescr) {
+		if ($tabname == $activetab) {
+			$checked_button = 'checked';
+		} else {
+			$checked_button = '';
+		}
+?>
+			<div class="radio">
+				<label><input name="default_tab" type="radio" id="<?=$tabname?>_tab" value="<?=$tabname?>" <?=$checked_button;?> /> <?=$tabdescr?></label>
+			</div>
+<?php
+	};
+?>
+		</div>
+	</div>
+	<br />
+
+	<div class="form-group">
+		<div class="col-sm-offset-3 col-sm-6">
+			<button type="submit" class="btn btn-primary"><i class="fa fa-save icon-embed-btn"></i><?=gettext('Save')?></button>
+			<button id="<?=$widget_showallnone_id?>" type="button" class="btn btn-info"><i class="fa fa-undo icon-embed-btn"></i><?=gettext('All')?></button>
+		</div>
+	</div>
+</form>
 <script type="text/javascript">
 //<![CDATA[
 
-curtab = "Overview";
+curtab = "<?=$activetab?>";
 
 function changeTabDIV(selectedDiv) {
 	var dashpos = selectedDiv.indexOf("-");
@@ -353,7 +416,7 @@ events.push(function(){
 		try{
 			var obj = JSON.parse(s);
 
-			$('tbody', '#<?= htmlspecialchars($widgetkey_nodash) ?>-Overview').html(obj.overview);
+			$('tbody', '#<?= htmlspecialchars($widgetkey_nodash) ?>-overview').html(obj.overview);
 			$('tbody', '#<?= htmlspecialchars($widgetkey_nodash) ?>-tunnel').html(obj.tunnel);
 			$('tbody', '#<?= htmlspecialchars($widgetkey_nodash) ?>-mobile').html(obj.mobile);
 		}catch(e){

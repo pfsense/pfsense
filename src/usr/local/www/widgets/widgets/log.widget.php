44c4a509e84e0d421827e51d5eb8f809fb8bc92f
NobleKangaroo <NobleKangaroo+Github@gmail.com>
2022-03-18 18:45:44 -0400

    Increase max firewall log entries

--- src/usr/local/www/widgets/widgets/log.widget.php
+++ src/usr/local/www/widgets/widgets/log.widget.php
@@ -205,7 +205,7 @@ events.push(function(){
 
 	// Create an object defining the widget refresh AJAX call
 	var logsObject = new Object();
-	logsObject.name = "Gateways";
+	logsObject.name = "Firewall Logs";
 	logsObject.url = "/widgets/widgets/log.widget.php";
 	logsObject.callback = logs_callback;
 	logsObject.parms = postdata;
@@ -236,7 +236,7 @@ $pconfig['nentriesinterval'] = isset($user_settings['widgets'][$widgetkey]['filt
 			<label for="filterlogentries" class="col-sm-4 control-label"><?=gettext('Number of entries')?></label>
 			<div class="col-sm-6">
 				<input type="number" name="filterlogentries" id="filterlogentries" value="<?=$pconfig['nentries']?>" placeholder="5"
-					min="1" max="20" class="form-control" />
+					min="1" max="50" class="form-control" />
 			</div>
 		</div>
 

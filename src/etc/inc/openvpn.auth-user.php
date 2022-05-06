d8a8701961cf58e6232c4f6bff550936d985dc0f
Marcos Mendoza <mmendoza@netgate.com>
2022-05-05 22:12:56 -0500

    Remove topology keyword from openvpn_cc file. Fixes #13133

--- src/etc/inc/openvpn.auth-user.php
+++ src/etc/inc/openvpn.auth-user.php
@@ -148,10 +148,8 @@ if (is_array($attributes['routes'])) {
 
 if (isset($attributes['framed_ip'])) {
 	if (isset($attributes['framed_mask'])) {
-		$content .= "topology subnet\n";
 		$content .= "ifconfig-push {$attributes['framed_ip']} {$attributes['framed_mask']}";
 	} else {
-		$content .= "topology net30\n";
 		$content .= "ifconfig-push {$attributes['framed_ip']} ". long2ip((ip2long($attributes['framed_ip']) - 1));
 	}
 }

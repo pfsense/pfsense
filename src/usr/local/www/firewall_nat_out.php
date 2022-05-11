dfd4d0e9aa949d4308b223c4264649aa4add9446
luckman212 <1992842+luckman212@users.noreply.github.com>
2022-05-11 08:27:53 -0400

    remove colon to stay consistent with the rest of the GUI

--- src/usr/local/www/firewall_nat_out.php
+++ src/usr/local/www/firewall_nat_out.php
@@ -395,7 +395,7 @@ if ($mode == "automatic" || $mode == "hybrid"):
 	$automatic_rules = getAutoRules();
 ?>
 	<div class="panel panel-default">
-		<div class="panel-heading"><h2 class="panel-title"><?=gettext("Automatic Rules:")?></h2></div>
+		<div class="panel-heading"><h2 class="panel-title"><?=gettext("Automatic Rules")?></h2></div>
 		<div class="panel-body table-responsive">
 			<table class="table table-hover table-striped table-condensed">
 				<thead>

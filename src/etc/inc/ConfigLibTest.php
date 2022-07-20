92abdaf0289fcd34b52354044258a59b2cf46b19
Reid Linnemann <rlinnemann@netgate.com>
2022-07-20 15:50:09 -0600

    Add config_get_path and config_set_path for config element access
    config_get_path and config_set_path allow the user to specify a configuration
    element by path rather than by direct access to the global config array. This
    has several advantages, including eliminating errors attempting to access
    subarrays that may not exist in a configuration and abstracting the actual
    implementation of the configuration structure from the codebase so that the code
    is not dependent upon it.
    
    A small set of phpunit tests are supplied, although there is no automated
    mechanism for running them at this time.

---
+++ src/etc/inc/ConfigLibTest.php
@@ -0,0 +1,56 @@
+<?php
+require_once("config.lib.inc");
+use PHPUnit\Framework\TestCase;
+
+class ConfigLibTest extends TestCase {
+	public function test_config_get_path(): void {
+		// Root element access
+		$this->assertEquals("bar", config_get_path("foo", null));
+		// Unfound element returns default, even if non-null
+		$this->assertNull(config_get_path("foobaz", null));
+		$this->assertEquals("test", config_get_path("foobaz", "test"));
+		// Subarray
+		$this->assertIsArray(config_get_path("bar", null));
+		$this->assertEquals("bang", config_get_path("bar/baz", null));
+	}
+
+	public function test_config_set_path(): void {
+		// Root element
+		$this->assertEquals("barbaz", config_set_path("baz", "barbaz"));
+		$this->assertEquals("barbaz", config_get_path("baz", "barbaz"));
+		// Root element already exists
+		$this->assertEquals("barbaz", config_set_path("bar", "barbaz"));
+		$this->assertEquals("barbaz", config_get_path("bar", "barbaz"));
+		// Parent doesn't exist
+		$this->assertNull(config_set_path("barbang/baz", "bang"));
+		$this->assertNull(config_get_path("barbang/baz", null));
+		// Parent doesn't exist, non-null default return
+		$this->assertEquals(-1, config_set_path("barbang/baz", "bang", -1));
+		$this->assertNull(config_get_path("barbang/baz", null));
+		
+		// Subarray
+		$this->assertIsArray(config_set_path("barbang", Array()));
+		$this->assertEquals("bang", config_set_path("barbang/baz", "bang"));
+		$this->assertEquals("bang", config_get_path("barbang/baz", null));
+		// Key exists, replace with array
+		$this->assertNotNull(config_get_path("foo", null));
+		$this->assertNotNull(config_set_path("foo", ["bar" => "barbaz", "baz" => "barbaz"]));
+		$this->assertEquals("barbaz", config_get_path("foo/bar", null));
+		$this->assertEquals("barbaz", config_get_path("foo/baz", null));
+		// Key in subarray exists
+		$this->assertIsArray(config_set_path("bar", Array()));
+		$this->assertEquals("barbaz", config_set_path("bar/baz", "barbaz"));
+		$this->assertEquals("barbaz", config_get_path("bar/baz", null));
+	}
+	public function setUp(): void {
+		global $config;
+		$config = array(
+			"foo" => "bar",
+			"bar" => array(
+				"baz" => "bang",
+				"foobar" => "foobaz"
+			)
+		);
+	}
+}
+?>

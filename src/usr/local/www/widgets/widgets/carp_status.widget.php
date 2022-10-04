40997699d4fda1b3f851f2dcc816c346ebce7d91
Christian McDonald <cmcdonald@netgate.com>
2022-10-04 15:52:11 +0000

    Fix carp_status.widget.php for PHP81. Fixes #13535

--- src/usr/local/www/widgets/widgets/carp_status.widget.php
+++ src/usr/local/www/widgets/widgets/carp_status.widget.php
@@ -41,18 +41,19 @@ $carp_enabled = get_carp_status();
 	</thead>
 	<tbody>
 <?php
-	if (is_array($config['virtualip']['vip'])) {
-		$carpint=0;
-		foreach ($config['virtualip']['vip'] as $carp) {
-			if ($carp['mode'] != "carp") {
-				continue;
-			}
-			$ipaddress = $carp['subnet'];
-			$password = $carp['password'];
-			$netmask = $carp['subnet_bits'];
-			$vhid = $carp['vhid'];
-			$advskew = $carp['advskew'];
-			$status = get_carp_interface_status("_vip{$carp['uniqid']}");
+
+	$carpint=0;
+	foreach (config_get_path('virtualip/vip', []) as $carp) {
+		if ($carp['mode'] != "carp") {
+			continue;
+		}
+		$carpint++;
+		$ipaddress = $carp['subnet'];
+		$password = $carp['password'];
+		$netmask = $carp['subnet_bits'];
+		$vhid = $carp['vhid'];
+		$advskew = $carp['advskew'];
+		$status = get_carp_interface_status("_vip{$carp['uniqid']}");
 ?>
 		<tr>
 			<td>
@@ -60,33 +61,34 @@ $carp_enabled = get_carp_status();
 					<?=htmlspecialchars(convert_friendly_interface_to_friendly_descr($carp['interface']) . "@{$vhid}");?>
 				</a>
 			</td>
-<?php			if ($carp_enabled == false) {
-				$icon = 'times-circle';
-				$status = "DISABLED";
-			} else {
-				if ($status == "MASTER") {
-					$icon = 'play-circle text-success';
-				} else if ($status == "BACKUP") {
-					$icon = 'pause-circle text-warning';
-				} else if ($status == "INIT") {
-					$icon = 'question-circle text-danger';
-				}
+<?php		
+		if ($carp_enabled == false) {
+			$icon = 'times-circle';
+			$status = "DISABLED";
+		} else {
+			if ($status == "MASTER") {
+				$icon = 'play-circle text-success';
+			} else if ($status == "BACKUP") {
+				$icon = 'pause-circle text-warning';
+			} else if ($status == "INIT") {
+				$icon = 'question-circle text-danger';
 			}
-			if ($ipaddress) {
+		}
+		if ($ipaddress) {
 ?>
 				<td><?=htmlspecialchars($ipaddress);?></td>
 				<td><i class="fa fa-<?=$icon?>"></i>&nbsp;<?= htmlspecialchars($status) ?></td>
 <?php
-			} else {
+		} else {
 ?>
 				<td colspan="2">
 <?php
-			}
+		}
 ?>
 		</tr>
 <?php
-		}
-	} else {
+	}
+	if ($carpint === 0) {
 ?>
 		<tr><td colspan="3"><?=gettext('No CARP Interfaces Defined.')?> <?=sprintf(gettext('Click %1$shere%2$s to configure CARP.'), '<a href="status_carp.php">', '</a>')?></td></tr>
 <?php

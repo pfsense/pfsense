0e6c4d622c6046fb76ed1e706ef3788e89be5168
Christopher Cope <ccope@netgate.com>
2022-10-14 13:53:14 -0400

    Rewrite functions for toggle & delete NAT. Fixes #13545

--- src/usr/local/www/firewall_nat.php
+++ src/usr/local/www/firewall_nat.php
@@ -51,21 +51,12 @@ if (array_key_exists('order-store', $_REQUEST) && have_natpfruleint_access($nate
 	reorderNATrules($_POST);
 } else if ($_POST['apply'] && have_natpfruleint_access($natent['interface'])) {
 	$retval = applyNATrules();
-} else if (($_POST['act'] == "del") && have_natpfruleint_access($natent['interface'])) {
-	if ($a_nat[$_POST['id']]) {
+} else if (($_POST['act'] == "del" || isset($_POST['del_x'])) && have_natpfruleint_access($natent['interface'])) {
+	if ($a_nat[$_POST['id']] || (is_array($_POST['rule']) && count($_POST['rule']))) {
 		deleteNATrule($_POST);
 	}
-} else if (isset($_POST['del_x']) && have_natpfruleint_access($natent['interface'])) {
-	/* delete selected rules */
-	if (is_array($_POST['rule']) && count($_POST['rule'])) {
-		deleteMultipleNATrules($_POST);
-	}
-} else if (isset($_POST['toggle_x']) && have_natpfruleint_access($natent['interface'])) {
-	if (is_array($_POST['rule']) && count($_POST['rule'])) {
-		toggleMultipleNATrules($_POST);
-	}
-} elseif (($_POST['act'] == "toggle") && have_natpfruleint_access($natent['interface'])) {
-	if ($a_nat[$_POST['id']]) {
+} elseif (($_POST['act'] == "toggle" || isset($_POST['toggle_x'])) && have_natpfruleint_access($natent['interface'])) {
+	if ($a_nat[$_POST['id']] || (is_array($_POST['rule']) && count($_POST['rule']))) {
 		toggleNATrule($_POST);
 	}
 }

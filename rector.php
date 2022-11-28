f4970dcdac5a86ef373d1d305c415c0f06d6c715
Christian McDonald <cmcdonald@netgate.com>
2022-11-28 12:21:47 -0500

    Update Rector config with pfSense-specific tweaks and notes

--- rector.php
+++ rector.php
@@ -1,19 +1,35 @@
 <?php
 declare(strict_types=1);
 
-use Utils\Rector\Rector\MyFirstRector;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    // skip third-party code
-    $rectorConfig->skip([
-	__DIR__ . '/src/usr/local/pfSense/include/vendor/*',
-    ]);
-
+    // Recursively check these paths...
     $rectorConfig->paths([
         __DIR__ . '/src',
     ]);
 
-    $rectorConfig->rule(MyFirstRector::class);
+    // for files with these extensions...
+    $rectorConfig->fileExtensions([
+        'php',
+        'inc',
+    ]);
 
+    // while skipping third-pary code that isn't our concern.
+    $rectorConfig->skip([
+        __DIR__ . '/src/usr/local/pfSense/include/vendor/*',
+    ]);
+
+    /*
+     * Register Rector rules or rulesets here
+     *
+     * See https://github.com/rectorphp/rector#running-rector
+     *
+     * Place custom Rectors in tools/rector/src/Rector named
+     * SomeDescriptiveNameRector.php namespaced as "Tools\Rector\Rector".
+     * Class should be "final" qualified, extend AbstractRector
+     * and the same name as the filename.
+     *
+     * See https://github.com/rectorphp/rector/blob/main/docs/create_own_rule.md
+     */ 
 };

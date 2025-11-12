78076563f97d042bfb4e993aa2c238dd6038d97f
Marcos Mendoza <mmendoza@netgate.com>
2025-11-12 14:38:35 -0600

    Test full unbound config when validating new settings
    When test_unbound_config() is called, only a partial configuraiton is
    tested. It's possible there may be custom options which depend on the
    full configuration, e.g. from the Advanced Settings page. Merge the
    existing configuraiton with the new settings to ensure a full test.

--- src/usr/local/www/services_unbound.php
+++ src/usr/local/www/services_unbound.php
@@ -167,7 +167,7 @@ if ($_POST['save']) {
 	}
 
 	$test_output = array();
-	if (test_unbound_config($pconfig, $test_output)) {
+	if (test_unbound_config(array_merge(config_get_path('unbound', []), $pconfig), $test_output)) {
 		$input_errors[] = gettext("The generated config file cannot be parsed by unbound. Please correct the following errors:");
 		$input_errors = array_merge($input_errors, $test_output);
 	}

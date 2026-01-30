d1c2fe84a734c33e61b550d4742a2de6729bb236
Bjoern Jakobsen <Bjoern.Jakobsen@lrz.de>
2026-01-30 13:14:52 +0100

    Add option to control ALTQ support for VTNET NICs. Implement #16166
    Signed-off-by: Bjoern Jakobsen <Bjoern.Jakobsen@lrz.de>

--- src/usr/local/www/system_advanced_network.php
+++ src/usr/local/www/system_advanced_network.php
@@ -299,10 +299,10 @@ $section->addInput(new Form_Checkbox(
 
 $section->addInput(new Form_Checkbox(
 	'hnaltqenable',
-	'hn ALTQ support',
-	'Enable the ALTQ support for hn NICs.',
+	'Virtual NIC ALTQ support',
+	'Enable the ALTQ support for vtnet/hn NICs.',
 	$pconfig['hnaltqenable']
-))->setHelp('Checking this option will enable the ALTQ support for hn NICs. '.
+))->setHelp('Checking this option will enable the ALTQ support for vtnet/hn NICs. '.
 	'The ALTQ support disables the multiqueue API and may reduce the system '.
 	'capability to handle traffic. This will take effect after a machine reboot.');
 

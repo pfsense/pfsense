ffe213b2e395d4725930b4588291966f0f91b996
Brad Davis <bdavis@netgate.com>
2026-02-25 20:48:42 +0000

    Clean up assignment and avoid test -o

--- src/usr/local/sbin/cleargmirror.sh
+++ src/usr/local/sbin/cleargmirror.sh
@@ -2,7 +2,8 @@
 
 clear_disk() {
 	local _disk="${1}"
-	local _mirror=$(/sbin/gmirror dump "${_disk}" 2>/dev/null | /usr/bin/sed '/name: /!d; s,^.*: ,,')
+	local _mirror
+	_mirror=$(/sbin/gmirror dump "${_disk}" 2>/dev/null | /usr/bin/sed '/name: /!d; s,^.*: ,,')
 
 	if [ -n "${_mirror}" ]; then
 		/sbin/gmirror destroy -f "${_mirror}" >/dev/null 2>&1
@@ -14,8 +15,8 @@ mirror="${1}"
 disk1="${2}"
 disk2="${3}"
 
-if [ -z "${mirror}" -o -z "${disk1}" -o -z "${disk2}" ]; then
-	echo "You must specify mirror name, and disks that should be cleared"
+if [ -z "${mirror}" ] || [ -z "${disk1}" ] || [ -z "${disk2}" ]; then
+	echo "ERROR: The mirror name and disks that should be cleared are required"
 	exit 1
 fi
 

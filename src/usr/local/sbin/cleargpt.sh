1b452bad6ce6c6d3a966ae6cc04b86ec26acfdd5
Brad Davis <bdavis@netgate.com>
2026-02-25 20:55:15 +0000

    Clean up and properly quote these variables

--- src/usr/local/sbin/cleargpt.sh
+++ src/usr/local/sbin/cleargpt.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 DISK=$1
-if [ "$DISK" = "" ]; then
-	echo "You must specify the disk that should be formatted/cleared."
+if [ -z "$DISK" ]; then
+	echo "ERROR: The disk that should be formatted/cleared must be specified."
 	exit 1
 fi
-for PART in `/sbin/gpart show $DISK | /usr/bin/grep -v '=>' | /usr/bin/awk '{ print $3 }'`; do
-	if [ "$PART" != "" ]; then
-		/sbin/gpart delete -i $PART $DISK >/dev/null
+for PART in `/sbin/gpart show "$DISK" | /usr/bin/grep -v '=>' | /usr/bin/awk '{ print $3 }'`; do
+	if [ -n "$PART" ]; then
+		/sbin/gpart delete -i "$PART" "$DISK" >/dev/null
 	fi
 done
-/sbin/gpart destroy $DISK >/dev/null
+/sbin/gpart destroy "$DISK" >/dev/null
 exit 0

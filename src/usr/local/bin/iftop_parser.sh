c3234b30de6606285bb827a9d820c97b3233cea6
Brad Davis <bdavis@netgate.com>
2026-02-26 22:23:43 +0000

    Replace spaces with tabs

--- src/usr/local/bin/iftop_parser.sh
+++ src/usr/local/bin/iftop_parser.sh
@@ -2,8 +2,8 @@
 
 # Usage
 if [ $# -ne 1 -a $# -ne 2 ]; then
-        echo "Usage : $0 iface"
-        exit
+	echo "Usage : $0 iface"
+	exit
 fi
 
 # files paths
@@ -24,30 +24,30 @@ AWK=/usr/bin/awk
 
 # test if pid file exist
 if [ -f $pid_file ]; then
-        # check how old is the file
-        curTS=`$DATE +%s`
-        pidTS=`$STAT -r $pid_file | $CUT -d " " -f 10`
-        # if more than 10 seconds,
-        # it must be a dead pid file (process killed?)
-        # or a stucked process that we should kill
-        if [ $(( curTS - pidTS )) -gt 10 ]; then
-                oldPID=`$CAT $pid_file`
-                # test if pid still exist
-                run=`$PS -p $oldPID | $GREP -F $0`
-                if [ "$run" != "" ]; then
-                        kill -9 $oldPID
-                fi
-                $RM $pid_file
-                $RM $cache_file 2>> /dev/null
-        else
-                if [ -s $cache_file ]; then
-                        $CAT $cache_file
-                fi
-        fi
+	# check how old is the file
+	curTS=`$DATE +%s`
+	pidTS=`$STAT -r $pid_file | $CUT -d " " -f 10`
+	# if more than 10 seconds,
+	# it must be a dead pid file (process killed?)
+	# or a stucked process that we should kill
+	if [ $(( curTS - pidTS )) -gt 10 ]; then
+		oldPID=`$CAT $pid_file`
+		# test if pid still exist
+		run=`$PS -p $oldPID | $GREP -F $0`
+		if [ "$run" != "" ]; then
+			kill -9 $oldPID
+		fi
+		$RM $pid_file
+		$RM $cache_file 2>> /dev/null
+	else
+		if [ -s $cache_file ]; then
+			$CAT $cache_file
+		fi
+	fi
 else
-        echo -n $$ > $pid_file
-        $IFTOP -nNb -i $1 -s 3 -o 2s -t 2>> /dev/null | $AWK -f $awk_script > ${cache_file}.tmp
-        $CAT ${cache_file}.tmp > $cache_file
-        $CAT $cache_file
-        $RM $pid_file
+	echo -n $$ > $pid_file
+	$IFTOP -nNb -i $1 -s 3 -o 2s -t 2>> /dev/null | $AWK -f $awk_script > ${cache_file}.tmp
+	$CAT ${cache_file}.tmp > $cache_file
+	$CAT $cache_file
+	$RM $pid_file
 fi

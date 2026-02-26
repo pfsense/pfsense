3d5544ebad1346cdeccc4024e519e189dcb48bce
Brad Davis <bdavis@netgate.com>
2026-02-26 22:32:11 +0000

    Clean up and modernize the iftop_parser script

--- src/usr/local/bin/iftop_parser.sh
+++ src/usr/local/bin/iftop_parser.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Usage
-if [ $# -ne 1 -a $# -ne 2 ]; then
-	echo "Usage : $0 iface"
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+	echo "Usage: $0 iface"
 	exit
 fi
 
@@ -23,31 +23,31 @@ IFTOP=/usr/local/sbin/iftop
 AWK=/usr/bin/awk
 
 # test if pid file exist
-if [ -f $pid_file ]; then
+if [ -f "$pid_file" ]; then
 	# check how old is the file
 	curTS=`$DATE +%s`
-	pidTS=`$STAT -r $pid_file | $CUT -d " " -f 10`
+	pidTS=`$STAT -r "$pid_file" | $CUT -d " " -f 10`
 	# if more than 10 seconds,
 	# it must be a dead pid file (process killed?)
 	# or a stucked process that we should kill
 	if [ $(( curTS - pidTS )) -gt 10 ]; then
-		oldPID=`$CAT $pid_file`
+		oldPID=`$CAT "$pid_file"`
 		# test if pid still exist
-		run=`$PS -p $oldPID | $GREP -F $0`
-		if [ "$run" != "" ]; then
-			kill -9 $oldPID
+		run=`$PS -p "$oldPID" | "$GREP -F $0"`
+		if [ -n "$run" ]; then
+			kill -9 "$oldPID"
 		fi
-		$RM $pid_file
-		$RM $cache_file 2>> /dev/null
+		$RM "$pid_file"
+		$RM "$cache_file" 2>> /dev/null
 	else
-		if [ -s $cache_file ]; then
-			$CAT $cache_file
+		if [ -s "$cache_file" ]; then
+			$CAT "$cache_file"
 		fi
 	fi
 else
-	echo -n $$ > $pid_file
-	$IFTOP -nNb -i $1 -s 3 -o 2s -t 2>> /dev/null | $AWK -f $awk_script > ${cache_file}.tmp
-	$CAT ${cache_file}.tmp > $cache_file
-	$CAT $cache_file
-	$RM $pid_file
+	echo -n $$ > "$pid_file"
+	$IFTOP -nNb -i "$1" -s 3 -o 2s -t 2>> /dev/null | $AWK -f $awk_script > "${cache_file}.tmp"
+	$CAT "${cache_file}.tmp" > "$cache_file"
+	$CAT "$cache_file"
+	$RM "$pid_file"
 fi
